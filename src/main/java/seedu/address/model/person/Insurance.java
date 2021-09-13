a87e26119b476fd59b41a7adf5c9c77bb83b1652
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-09-13 22:47:41 +0800

    Add Insurance

---
+++ src/main/java/seedu/address/model/person/Insurance.java
@@ -0,0 +1,16 @@
+package seedu.address.model.person;
+
+/**
+ * Represents an insurance policy that a person can own.
+ */
+public class Insurance {
+    private String name;
+
+    /**
+     * Class constructor
+     * @param name The name of this insurance
+     */
+    public Insurance(String name) {
+        this.name = name;
+    }
+}
