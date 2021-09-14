af12b7d0920e9a7e1df2cb93f3cddd9b10ce61d1
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-09-14 12:01:23 +0800

    Remove @JsonCreator annotation from legacy constructor of JsonAdaptedPerson

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -51,7 +51,6 @@ class JsonAdaptedPerson {
      * Legacy constructor
      */
     @Deprecated
-    @JsonCreator
     public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
                              @JsonProperty("email") String email, @JsonProperty("address") String address,
                              @JsonProperty("tagged") List<JsonAdaptedTag> tagged) {
