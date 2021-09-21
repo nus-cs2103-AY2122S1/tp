efe93bb8a68da821d6b388769c58884edc40cd3c
lwlshawn <37958530+lwlshawn@users.noreply.github.com>
2021-09-21 14:52:02 +0800

    Implement logic components for addtask command
    Two main parts for this commit; AddTaskCommandParser implements parser
    logic for understanding addtask command, and AddTaskCommand implements
    the logical execution that makes changes to the model.
    
    Example syntax for command is:
    "addtask l\the label describing the task d\August 20 2021

---
+++ src/main/java/seedu/address/logic/parser/AddTaskCommandParser.java
@@ -0,0 +1,52 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_LABEL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+
+import java.util.stream.Stream;
+
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.AddTaskCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.task.Task;
+
+public class AddTaskCommandParser implements Parser<AddTaskCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddTaskCommand
+     * and returns an AddTaskCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public AddTaskCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_LABEL, PREFIX_DATE);
+
+        //checks that preamble has only spaces, and none of the prefix values are empty
+        //important for the argmultimap.getvalue().get() calls below
+        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+        }
+
+        String label = argMultimap.getValue(PREFIX_LABEL).get();
+        String date = argMultimap.getValue(PREFIX_DATE).get();
+        Task task = new Task(label, date);
+
+        return new AddTaskCommand(task);
+    }
+
+
+    //Reused from AddCommandParser - consider refactoring in future.
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+}
