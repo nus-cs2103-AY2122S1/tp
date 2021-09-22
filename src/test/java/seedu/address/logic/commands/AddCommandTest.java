bc7366b0f2e24209b35810f4940dc94756102c9d
XXJJXJ <yexinjian1506@gmail.com>
2021-09-22 19:44:24 +0800

    Update AddCommandTest ModelStub

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -153,6 +153,11 @@ public class AddCommandTest {
         public ObservableList<Event> getFilteredEventList() {
             throw new AssertionError("This method should not be called.");
         }
+
+        @Override
+        public void updateFilteredEventList(Predicate<Event> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
     }
 
     /**
