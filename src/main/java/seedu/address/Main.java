b90ad6e6284e8946c96286681f9c4f6b2d21944f
NaokiShum <cxjdwedyk097@gmail.com>
2021-09-22 09:54:06 +0800

    Replace src file with original AB3 src file

--- src/main/java/seedu/address/Main.java
+++ src/main/java/seedu/address/Main.java
@@ -18,16 +18,8 @@ import javafx.application.Application;
  * By having a separate main class (Main) that doesn't extend Application
  * to be the entry point of the application, we avoid this issue.
  */
-
-// pull request
 public class Main {
-    /**
-     * Starting point of this application.
-     *
-     * @param args args.
-     */
     public static void main(String[] args) {
-
         Application.launch(MainApp.class, args);
     }
 }
