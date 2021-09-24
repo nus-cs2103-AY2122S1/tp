0ff6bdd52441c8efefa1eb585b6f922104b6ef8d
LeopardMerkava <andrewbaruch4@gmail.com>
2021-09-25 07:05:13 +0800

    Add Remark

--- src/main/java/seedu/address/ui/PersonListPanel.java
+++ src/main/java/seedu/address/ui/PersonListPanel.java
@@ -20,6 +20,7 @@ public class PersonListPanel extends UiPart<Region> {
     @FXML
     private ListView<Person> personListView;
 
+
     /**
      * Creates a {@code PersonListPanel} with the given {@code ObservableList}.
      */
