08ea9b9f755851cd68136f54cd4606def7ea60bc
Richard Li <richardlidefa@gmail.com>
2021-09-24 19:35:52 +0800

    Change Person constructor to accept appointment

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -24,18 +24,19 @@ public class Person {
     // Data fields
     private final Address address;
     private final Set<Tag> tags = new HashSet<>();
-    private final Appointment appointment = new Appointment(null);
+    private final Appointment appointment;
 
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
+    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags, Appointment appointment) {
         requireAllNonNull(name, phone, email, address, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
         this.tags.addAll(tags);
+        this.appointment = appointment;
     }
 
     public Name getName() {
