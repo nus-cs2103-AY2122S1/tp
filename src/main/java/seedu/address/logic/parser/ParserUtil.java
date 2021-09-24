5d504d00647e43348cf011ce868c2da386745fc3
Richard Li <richardlidefa@gmail.com>
2021-09-24 11:46:45 +0800

    Prepare parser for Appointment commands

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.appointment.Appointment;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
@@ -120,5 +121,12 @@ public class ParserUtil {
             tagSet.add(parseTag(tagName));
         }
         return tagSet;
+    }
+
+    public static Appointment parseAppointment(String meetingDateTime) throws ParseException {
+        if (!Appointment.isValidMeetingTime(meetingDateTime)) {
+            throw new ParseException(Appointment.MESSAGE_CONSTRAINTS);
+        }
+        return new Appointment(meetingDateTime);
     }
 }
