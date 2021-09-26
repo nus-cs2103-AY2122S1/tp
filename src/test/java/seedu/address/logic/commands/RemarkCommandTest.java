c52d5a1ffa64089304754ab14671316f4632d699
erwinqxy <erwin.quek.xian.yi@gmail.com>
2021-09-26 16:42:11 +0800

    Fix style again

--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -1,9 +1,6 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
-
-import org.junit.jupiter.api.Test;
 
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
@@ -14,9 +11,6 @@ import seedu.address.model.UserPrefs;
  */
 public class RemarkCommandTest {
 
-    String MESSAGE_NOT_IMPLEMENTED_YET = "test";
-
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-
 
 }
