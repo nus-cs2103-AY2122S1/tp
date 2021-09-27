578c6010e11abbd76fac4dc79a4ddd437854cc68
jiayi1129 <jiayi6767@gmail.com>
2021-09-27 10:59:55 +0800

    Update PlannerMd to PlannerMD in MainApp.java

--- src/main/java/seedu/plannermd/MainApp.java
+++ src/main/java/seedu/plannermd/MainApp.java
@@ -48,7 +48,7 @@ public class MainApp extends Application {
 
     @Override
     public void init() throws Exception {
-        logger.info("=============================[ Initializing PlannerMd ]===========================");
+        logger.info("=============================[ Initializing PlannerMD ]===========================");
         super.init();
 
         AppParameters appParameters = AppParameters.parse(getParameters());
