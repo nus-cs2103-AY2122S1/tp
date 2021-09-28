734a19f9b697717482b1a234d46d1bd9139dd961
Kan Jitpakdi <55085750+kanjitp@users.noreply.github.com>
2021-09-28 09:47:58 +0800

    Update src/main/java/seedu/address/logic/commands/EditCommand.java
    Co-authored-by: ntwbruce <77188160+ntwbruce@users.noreply.github.com>

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -38,7 +38,7 @@ public class EditCommand extends Command {
     public static final String COMMAND_WORD = "edit";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the participant identified "
-        + "by the index number used in the displayed person list. "
+        + "by the index number used in the displayed Participant list. "
         + "Existing values will be overwritten by the input values.\n"
         + "Parameters: INDEX (must be a positive integer) "
         + "[" + PREFIX_NAME + "NAME] "
