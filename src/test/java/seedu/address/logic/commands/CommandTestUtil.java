2e1103e16d4671b5fdb117299484029d7e1bdd6f
twothicc <chenenliuu@gmail.com>
2021-09-28 23:04:40 +0800

    Update tests in AddCommandParser to include and test for Role

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -57,6 +57,7 @@ public class CommandTestUtil {
     public static final String INVALID_PHONE_DESC = " " + PREFIX_PHONE + "911a"; // 'a' not allowed in phones
     public static final String INVALID_EMAIL_DESC = " " + PREFIX_EMAIL + "bob!yahoo"; // missing '@' symbol
     public static final String INVALID_ADDRESS_DESC = " " + PREFIX_ADDRESS; // empty string not allowed for addresses
+    public static final String INVALID_ROLE_DESC = " " + PREFIX_ROLE + "Softw@re Eng1n33r"; // '@' not allowed in roles
     public static final String INVALID_TAG_DESC = " " + PREFIX_TAG + "hubby*"; // '*' not allowed in tags
 
     public static final String PREAMBLE_WHITESPACE = "\t  \r  \n";
