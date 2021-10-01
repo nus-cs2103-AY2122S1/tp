b291f0caae7ec7f99000c1cf5f9d81b0f6db0fdb
Chesterwongz <psyforce99@gmail.com>
2021-10-01 14:11:11 +0800

    Fix checkstyle

--- src/main/java/seedu/address/model/lesson/Lesson.java
+++ src/main/java/seedu/address/model/lesson/Lesson.java
@@ -7,9 +7,6 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-import seedu.address.model.person.Person;
-
-
 /**
  * Represents a Lesson in the address book.
  * Guarantees: details are present and not null, field values are validated, immutable.
