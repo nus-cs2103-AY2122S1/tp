c74a24deb7ebc8b76c35ac98b6026581cca3845d
LAPTOP-SJ8328VV\Ethan <atomiccore9@rocketmail.com>
2021-10-01 14:44:40 +0800

    rename package to seedu.fast, rename AddressBookParser to FASTParser

---
+++ src/test/java/seedu/fast/logic/commands/ClearCommandTest.java
@@ -0,0 +1,32 @@
+package seedu.fast.logic.commands;
+
+import static seedu.fast.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.fast.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.fast.model.AddressBook;
+import seedu.fast.model.Model;
+import seedu.fast.model.ModelManager;
+import seedu.fast.model.UserPrefs;
+
+public class ClearCommandTest {
+
+    @Test
+    public void execute_emptyAddressBook_success() {
+        Model model = new ModelManager();
+        Model expectedModel = new ModelManager();
+
+        assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_nonEmptyAddressBook_success() {
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel.setAddressBook(new AddressBook());
+
+        assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+}
---
+++ src/test/java/seedu/fast/logic/commands/RemarkCommandTest.java
@@ -0,0 +1,38 @@
+package seedu.fast.logic.commands;
+
+import static seedu.fast.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.fast.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.fast.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.fast.model.AddressBook;
+import seedu.fast.model.Model;
+import seedu.fast.model.ModelManager;
+import seedu.fast.model.UserPrefs;
+import seedu.fast.model.person.Person;
+import seedu.fast.model.person.Remark;
+import seedu.fast.testutil.PersonBuilder;
+
+
+class RemarkCommandTest {
+
+    private static final String REMARK_STUB = "Some remark";
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    void execute_addRemarkUnfilteredList_success() {
+        Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        Person editedPerson = new PersonBuilder(firstPerson).withRemark(REMARK_STUB).build();
+
+        RemarkCommand remarkCommand = new RemarkCommand(INDEX_FIRST_PERSON, new Remark(editedPerson.getRemark().value));
+
+        String expectedMessage = String.format(RemarkCommand.MESSAGE_ADD_REMARK_SUCCESS, editedPerson);
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setPerson(firstPerson, editedPerson);
+
+        assertCommandSuccess(remarkCommand, model, expectedMessage, expectedModel);
+    }
+}
