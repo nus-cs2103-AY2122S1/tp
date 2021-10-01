1aba63738f5ab3d3e4c6953bfc3e0f9400d236a3
bryanwee023 <bryanwee023@gmail.com>
2021-10-01 20:38:49 +0800

    Add Item model

---
+++ src/main/java/seedu/address/newmodel/Model.java
@@ -0,0 +1,88 @@
+package seedu.address.newmodel;
+
+import java.nio.file.Path;
+import java.util.function.Predicate;
+
+import javafx.collections.ObservableList;
+import seedu.address.commons.core.GuiSettings;
+import seedu.address.newmodel.ReadOnlyInventory;
+import seedu.address.newmodel.item.Item;
+
+/**
+ * The API of the Model component.
+ */
+public interface Model {
+    /** {@code Predicate} that always evaluate to true */
+    Predicate<Item> PREDICATE_SHOW_ALL_ITEMS = unused -> true;
+
+    /**
+     * Replaces user prefs data with the data in {@code userPrefs}.
+     */
+    void setUserPrefs(ReadOnlyUserPrefs userPrefs);
+
+    /**
+     * Returns the user prefs.
+     */
+    ReadOnlyUserPrefs getUserPrefs();
+
+    /**
+     * Returns the user prefs' GUI settings.
+     */
+    GuiSettings getGuiSettings();
+
+    /**
+     * Sets the user prefs' GUI settings.
+     */
+    void setGuiSettings(GuiSettings guiSettings);
+
+    /**
+     * Returns the user prefs' address book file path.
+     */
+    Path getInventoryFilePath();
+
+    /**
+     * Sets the user prefs' address book file path.
+     */
+    void setInventoryFilePath(Path addressBookFilePath);
+
+    /**
+     * Replaces inventory data with the data in {@code inventory}.
+     */
+    void setInventory(ReadOnlyInventory inventory);
+
+    /** Returns the Inventory */
+    ReadOnlyInventory getInventory();
+
+    /**
+     * Returns true if a item with the same identity as {@code item} exists in the inventory.
+     */
+    boolean hasItem(Item item);
+
+    /**
+     * Deletes the given item.
+     * The item must exist in the inventory.
+     */
+    void deleteItem(Item target);
+
+    /**
+     * Adds the given item.
+     * If {@code item} must not already exist in the address book, increment its count accordingly.
+     */
+    void addItem(Item item);
+
+    /**
+     * Replaces the given item {@code target} with {@code editedItem}.
+     * {@code target} must exist in the inventory.
+     * The item identity of {@code editedItem} must not be the same as another existing item in the inventory.
+     */
+    void setItem(Item target, Item editedItem);
+
+    /** Returns an unmodifiable view of the filtered item list */
+    ObservableList<Item> getFilteredItemList();
+
+    /**
+     * Updates the filter of the filtered item list to filter by the given {@code predicate}.
+     * @throws NullPointerException if {@code predicate} is null.
+     */
+    void updateFilteredItemList(Predicate<Item> predicate);
+}
---
+++ src/main/java/seedu/address/newmodel/ModelManager.java
@@ -0,0 +1,152 @@
+package seedu.address.newmodel;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.nio.file.Path;
+import java.util.function.Predicate;
+import java.util.logging.Logger;
+
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import seedu.address.commons.core.GuiSettings;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.newmodel.Inventory;
+import seedu.address.newmodel.item.Item;
+
+/**
+ * Represents the in-memory model of the address book data.
+ */
+public class ModelManager implements Model {
+    private static final Logger logger = LogsCenter.getLogger(ModelManager.class);
+
+    private final Inventory inventory;
+    private final UserPrefs userPrefs;
+    private final FilteredList<Item> filteredItems;
+
+    /**
+     * Initializes a ModelManager with the given addressBook and userPrefs.
+     */
+    public ModelManager(ReadOnlyInventory inventory, ReadOnlyUserPrefs userPrefs) {
+        super();
+        requireAllNonNull(inventory, userPrefs);
+
+        logger.fine("Initializing with address book: " + inventory + " and user prefs " + userPrefs);
+
+        this.inventory = new Inventory(inventory);
+        this.userPrefs = new UserPrefs(userPrefs);
+        filteredItems = new FilteredList<>(this.inventory.getItemList());
+    }
+
+    public ModelManager() {
+        this(new Inventory(), new UserPrefs());
+    }
+
+    //=========== UserPrefs ==================================================================================
+
+    @Override
+    public void setUserPrefs(ReadOnlyUserPrefs userPrefs) {
+        requireNonNull(userPrefs);
+        this.userPrefs.resetData(userPrefs);
+    }
+
+    @Override
+    public ReadOnlyUserPrefs getUserPrefs() {
+        return userPrefs;
+    }
+
+    @Override
+    public GuiSettings getGuiSettings() {
+        return userPrefs.getGuiSettings();
+    }
+
+    @Override
+    public void setGuiSettings(GuiSettings guiSettings) {
+        requireNonNull(guiSettings);
+        userPrefs.setGuiSettings(guiSettings);
+    }
+
+    @Override
+    public Path getInventoryFilePath() {
+        return userPrefs.getInventoryFilePath();
+    }
+
+    @Override
+    public void setInventoryFilePath(Path inventoryFilePath) {
+        requireNonNull(inventoryFilePath);
+        userPrefs.setInventoryFilePath(inventoryFilePath);
+    }
+
+    //=========== AddressBook ================================================================================
+
+    @Override
+    public void setInventory(ReadOnlyInventory inventory) {
+        this.inventory.resetData(inventory);
+    }
+
+    @Override
+    public ReadOnlyInventory getInventory() {
+        return inventory;
+    }
+
+    @Override
+    public boolean hasItem(Item item) {
+        requireNonNull(item);
+        return inventory.hasItem(item);
+    }
+
+    @Override
+    public void deleteItem(Item target) {
+        inventory.removeItem(target);
+    }
+
+    @Override
+    public void addItem(Item item) {
+        inventory.addItem(item);
+        updateFilteredItemList(PREDICATE_SHOW_ALL_ITEMS);
+    }
+
+    @Override
+    public void setItem(Item target, Item editedItem) {
+        requireAllNonNull(target, editedItem);
+
+        inventory.setItem(target, editedItem);
+    }
+
+    //=========== Filtered Item List Accessors =============================================================
+
+    /**
+     * Returns an unmodifiable view of the list of {@code Item} backed by the internal list of
+     * {@code versionedInventory}
+     */
+    @Override
+    public ObservableList<Item> getFilteredItemList() {
+        return filteredItems;
+    }
+
+    @Override
+    public void updateFilteredItemList(Predicate<Item> predicate) {
+        requireNonNull(predicate);
+        filteredItems.setPredicate(predicate);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // short circuit if same object
+        if (obj == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(obj instanceof ModelManager)) {
+            return false;
+        }
+
+        // state check
+        ModelManager other = (ModelManager) obj;
+        return inventory.equals(other.inventory)
+                && userPrefs.equals(other.userPrefs)
+                && filteredItems.equals(other.filteredItems);
+    }
+
+}
