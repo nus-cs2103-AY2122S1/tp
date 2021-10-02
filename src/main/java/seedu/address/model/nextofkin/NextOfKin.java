65c03bc75ffd62e67ec91e2242de00d933e47c01
ntwbruce <e0544240@u.nus.edu>
2021-10-03 01:18:45 +0800

    Update NextOfKin
    Undo creation of new method getTagString and modification of toString:
    unsure if NextOfKin#toString is used elsewhere that requires it to
    follow this format.

--- src/main/java/seedu/address/model/nextofkin/NextOfKin.java
+++ src/main/java/seedu/address/model/nextofkin/NextOfKin.java
@@ -48,13 +48,6 @@ public class NextOfKin {
     }
 
     /**
-     * Returns a String representation of the next of kin's tags.
-     */
-    public String getTagString() {
-        return tag.tagName;
-    }
-
-    /**
      * Returns true if both persons have the same identity and data fields.
      * This defines a stronger notion of equality between two persons.
      */
@@ -83,7 +76,7 @@ public class NextOfKin {
     @Override
     public String toString() {
 
-        return getName() + "; Phone: " + getPhone() + "; Relationship: " + getTagString();
+        return getName() + "; Phone: " + getPhone() + "; Relationship: " + getTag();
     }
 
 
