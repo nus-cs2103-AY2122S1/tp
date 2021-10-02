52689d754369d26fa3bd76cd01f18a1ebe6e5e24
XJ Ye <75206536+XXJJXJ@users.noreply.github.com>
2021-10-02 15:51:05 +0800

    Update src/main/java/seedu/address/logic/parser/FilterEventCommandParser.java
    Co-authored-by: lowkangn <77261657+lowkangn@users.noreply.github.com>

--- src/main/java/seedu/address/logic/parser/FilterEventCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FilterEventCommandParser.java
@@ -18,7 +18,7 @@ public class FilterEventCommandParser implements Parser<FilterEventCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the FilterEventCommand
      * and returns a FilterCommand object for execution.
-     * @throws ParseException if the user input does not conform the expected format
+     * @throws ParseException if the user input does not conform to the expected format
      */
     @Override
     public FilterEventCommand parse(String userInput) throws ParseException {
