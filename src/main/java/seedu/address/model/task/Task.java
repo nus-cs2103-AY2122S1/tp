34784e9cf6f819074b5d0a21a2ff34c232c31091
hanif-kamal <hanif.kamal47@gmail.com>
2021-10-02 15:47:48 +0800

    [#91] Implement Task in Model - [#91]

---
+++ src/main/java/seedu/address/model/task/Task.java
@@ -0,0 +1,51 @@
+package seedu.address.model.task;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Objects;
+
+/**
+ * Represents a Task in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+public class Task {
+    private final TaskDescription taskDescription;
+
+    public Task(TaskDescription taskDescription) {
+        requireAllNonNull(taskDescription);
+        this.taskDescription = taskDescription;
+    }
+
+    public TaskDescription getTaskDescription() {
+        return taskDescription;
+    }
+
+    /**
+     * Returns true if both tasks have the same task description.
+     */
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Task)) {
+            return false;
+        }
+
+        Task otherTask = (Task) other;
+        return otherTask.getTaskDescription().equals(getTaskDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(taskDescription);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getTaskDescription())
+                .append("; Task Description: ");
+        return builder.toString();
+    }
+}
