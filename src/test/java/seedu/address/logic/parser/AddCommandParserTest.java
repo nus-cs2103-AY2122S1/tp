8f4b9e01a3550de67a4def745be509b5a2b1700c
kaushikkrdy <kaushikkr@hotmail.com>
2021-10-02 15:28:43 +0800

    Add EmploymentType to LogicManagerTest and EmploymentType message constraints to invalid value test in AddCommandParserTest

--- src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
@@ -38,6 +38,7 @@ import org.junit.jupiter.api.Test;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
+import seedu.address.model.person.EmploymentType;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
@@ -145,7 +146,7 @@ public class AddCommandParserTest {
 
         // invalid employment type
         assertParseFailure(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
-                + INVALID_EMPLOYMENT_TYPE_DESC + INVALID_TAG_DESC + VALID_TAG_FRIEND, Tag.MESSAGE_CONSTRAINTS);
+                + INVALID_EMPLOYMENT_TYPE_DESC + INVALID_TAG_DESC + VALID_TAG_FRIEND, EmploymentType.MESSAGE_CONSTRAINTS);
 
         // two invalid values, only first invalid value reported
         assertParseFailure(parser, INVALID_NAME_DESC + PHONE_DESC_BOB + EMAIL_DESC_BOB + INVALID_ADDRESS_DESC
