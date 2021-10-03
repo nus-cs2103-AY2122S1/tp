1c5503cebc2ed05b77b7902e6027897d479eb36e
wyrchris <wongyunruichris@gmail.com>
2021-10-03 20:53:25 +0800

    Add Search Feature
    Search Command: Generic and more functional find Command
    
    A more functional find Command for the user to search with loose matching and
    also by client's attribute as while.
    
    Let's add SearchCommand along with the resepective parser and a predicate used for
    updating the model.
    
    User guide and Test case has been updated for this feature.

---
+++ src/main/java/seedu/address/model/person/PersonContainsKeywordsPredicate.java
@@ -0,0 +1,59 @@
+package seedu.address.model.person;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import seedu.address.logic.parser.ArgumentMultimap;
+
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+/**
+ * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
+ */
+public class PersonContainsKeywordsPredicate implements Predicate<Person> {
+    private static final String WILDCARD_KEYWORD = "*";
+    private final ArgumentMultimap keywords;
+
+    public PersonContainsKeywordsPredicate(ArgumentMultimap keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        String[] generalKeywords = keywords.getPreamble().split(" ");
+        boolean checkGeneral = generalKeywords[0].equals(WILDCARD_KEYWORD) ||
+                Arrays.stream(generalKeywords).anyMatch(x -> {
+                    boolean checkAttribute = Stream.of(person.getName().fullName, person.getPhone().value,
+                            person.getEmail().value, person.getAddress().value).anyMatch(y -> y.contains(x));
+                    boolean checkAttributeTag = person.getTags().stream().anyMatch(y -> y.tagName.contains(x));
+
+                    return checkAttribute || checkAttributeTag;
+                });
+
+        boolean checkName = keywords.getValue(PREFIX_NAME)
+                .map(x -> person.getName().fullName.contains(x)).orElse(true);
+        boolean checkPhone = keywords.getValue(PREFIX_PHONE)
+                .map(x -> person.getPhone().value.contains(x)).orElse(true);
+        boolean checkEmail = keywords.getValue(PREFIX_EMAIL)
+                .map(x -> person.getEmail().value.contains(x)).orElse(true);
+        boolean checkAddress = keywords.getValue(PREFIX_ADDRESS)
+                .map(x -> person.getAddress().value.contains(x)).orElse(true);
+        boolean checkTags = keywords.getValue(PREFIX_TAG)
+                .map(x -> person.getTags().stream().anyMatch(y -> y.tagName.contains(x))).orElse(true);
+
+        return checkGeneral && checkName && checkPhone && checkEmail && checkAddress && checkTags;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof PersonContainsKeywordsPredicate // instanceof handles nulls
+                && keywords.equals(((PersonContainsKeywordsPredicate) other).keywords)); // state check
+    }
+
+}
---
+++ src/test/java/seedu/address/logic/parser/SearchCommandParserTest.java
@@ -0,0 +1,41 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.SearchCommand;
+import seedu.address.model.person.PersonContainsKeywordsPredicate;
+
+import org.junit.jupiter.api.Test;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+public class SearchCommandParserTest {
+
+    private SearchCommandParser parser = new SearchCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                SearchCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnsSearchCommand() {
+        // no leading and trailing whitespaces
+        SearchCommand expectedSearchCommand =
+                new SearchCommand(new PersonContainsKeywordsPredicate(
+                        ArgumentTokenizer.tokenize("Alice Bob e/example.com a/Blk 30",
+                                PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG)
+                ));
+        assertParseSuccess(parser, "Alice Bob e/example.com a/Blk 30", expectedSearchCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n Alice \n \t Bob  \t e/example.com a/Blk 30", expectedSearchCommand);
+    }
+}
