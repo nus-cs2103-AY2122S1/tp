e77a245af30016a972251915495eb367292b5bdf
VimuthM <vimuthmendis@gmail.com>
2021-10-04 22:15:12 +0800

    Refactoring:
     * Data saving and logging refactored to safeforhall
     * Application name changed
     * User guide link updated
     * Other minor refactoring
     * Note that the model to represent the resident data remains as

--- src/main/java/safeforhall/model/UserPrefs.java
+++ src/main/java/safeforhall/model/UserPrefs.java
@@ -14,7 +14,7 @@ import safeforhall.commons.core.GuiSettings;
 public class UserPrefs implements ReadOnlyUserPrefs {
 
     private GuiSettings guiSettings = new GuiSettings();
-    private Path addressBookFilePath = Paths.get("data" , "addressbook.json");
+    private Path addressBookFilePath = Paths.get("data" , "safeforhall.json");
 
     /**
      * Creates a {@code UserPrefs} with default values.
