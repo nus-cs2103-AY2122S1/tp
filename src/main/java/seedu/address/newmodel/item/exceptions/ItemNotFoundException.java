e2ada5a77dfb27d68dfc283e5a6d4c933cbcd0cb
bryanwee023 <bryanwee023@gmail.com>
2021-10-04 15:45:33 +0800

    Remove temporary newmodel package

--- src/main/java/seedu/address/newmodel/item/exceptions/ItemNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package seedu.address.newmodel.item.exceptions;
-
-/**
- * Signals that the operation is unable to find the specified item.
- */
-public class ItemNotFoundException extends RuntimeException {}
