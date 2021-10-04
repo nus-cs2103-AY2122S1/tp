c30f20102e5ec71ecbdd5987ce2e8dd00eef209f
lethiciars <santosolethicia@gmail.com>
2021-10-05 04:17:20 +0800

    Add Filter command
    Add dummy Filter command without implementation. filter c/CATEGORY_CODE
    returns "Filter contacts in CATEGORY_CODE category." in the Command
    Result

---
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -0,0 +1,44 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Filters contacts in the address book by category.
+ */
+public class FilterCommand extends Command{
+
+    public static final String COMMAND_WORD = "filter";
+
+    private final String category;
+
+    public FilterCommand(String category) {
+        requireNonNull(category);
+        this.category = category;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException(
+                String.format("Filter command not implemented yet. Filter contacts in %s category.", category));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof FilterCommand)) {
+            return false;
+        }
+
+        // state check
+        FilterCommand e = (FilterCommand) other;
+        return category.equals(e.category);
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/FilterCommandParser.java
@@ -0,0 +1,35 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.FilterCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CATEGORY;
+
+/**
+ * Parses input arguments and creates a new FilterCommand object
+ */
+public class FilterCommandParser implements Parser<FilterCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the FilterCommand
+     * and returns a FilterCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public FilterCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args,
+                PREFIX_CATEGORY);
+
+        String category = argMultimap.getValue(PREFIX_CATEGORY).orElse("");
+
+        if (category.isEmpty()) {
+            throw new ParseException(
+                    String.format("Empty filter category."));
+        }
+
+        return new FilterCommand(category);
+    }
+
+}
