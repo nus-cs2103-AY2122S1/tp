f934d6e53702d51109d6db0df67acba361907e6d
Preshita01 <pandipreshita@gmail.com>
2021-10-05 21:30:28 +0800

    Add StudentName.java

---
+++ src/main/java/seedu/address/model/person/StudentName.java
@@ -0,0 +1,16 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+public class StudentName extends Name {
+    /**
+     * Constructs a {@code Name}.
+     *
+     * @param name A valid name.
+     */
+    public StudentName(String name) {
+        super(name);
+        checkArgument(isValidName(name), MESSAGE_CONSTRAINTS);
+    }
+}
