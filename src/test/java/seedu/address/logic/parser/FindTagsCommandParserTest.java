81436e4b2c203a07acb4e2ebb220973d76b8d144
Chan Yu Cheng <e0560244@u.nus.edu>
2021-10-05 17:06:25 +0800

    Modify tests to fulfill checkstyle requirements

--- src/test/java/seedu/address/logic/parser/FindTagsCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindTagsCommandParserTest.java
@@ -1,18 +1,17 @@
 package seedu.address.logic.parser;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.logic.commands.FindTagsCommand;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
-import seedu.address.model.person.PersonTagsContainsTagsPredicate;
-import seedu.address.model.tag.Tag;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.FindTagsCommand;
+import seedu.address.model.person.PersonTagsContainsTagsPredicate;
+import seedu.address.model.tag.Tag;
 
 public class FindTagsCommandParserTest {
 
@@ -20,7 +19,8 @@ public class FindTagsCommandParserTest {
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindTagsCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "     ",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindTagsCommand.MESSAGE_USAGE));
     }
 
     @Test
