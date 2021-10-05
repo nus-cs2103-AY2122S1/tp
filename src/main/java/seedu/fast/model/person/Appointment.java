ce36ff0814e69137904b1595afb3bc8c2953c3f5
Py0000 <>
2021-10-05 20:49:16 +0800

    Edit Appointment class

--- src/main/java/seedu/fast/model/person/Appointment.java
+++ src/main/java/seedu/fast/model/person/Appointment.java
@@ -1,31 +1,32 @@
 package seedu.fast.model.person;
 
-import seedu.fast.model.AppointmentDate;
-
 import java.util.Objects;
 
+/**
+ * Represents an appointment with the Person in the address book.
+ * Guarantees: immutable; is always valid.
+ */
 public class Appointment {
+    public static final String NO_APPOINTMENT = "No Appointment Scheduled Yet";
 
-    private final AppointmentDate date;
-    private final AppointmentTime time;
-    private final AppointmentVenue venue;
+    private final String date;
 
-    public Appointment(AppointmentDate date, AppointmentTime time, AppointmentVenue venue) {
+    /**
+     * Constructs an {@code Appointment}.
+     *
+     * @param date A valid date.
+     */
+    public Appointment(String date) {
         this.date = date;
-        this.time = time;
-        this.venue = venue;
     }
 
-    public AppointmentDate getDate() {
-        return date;
-    }
-
-    public AppointmentTime getTime() {
-        return time;
-    }
-
-    public AppointmentVenue getVenue() {
-        return venue;
+    /**
+     * Returns a string in the format 'dd MMM yyyy' of the appointment.
+     *
+     * @return A string representing the date of the appointment.
+     */
+    public String getDate() {
+        return this.date;
     }
 
     @Override
@@ -39,27 +40,21 @@ public class Appointment {
         }
 
         Appointment otherAppt = (Appointment) other;
-        return otherAppt.getDate().equals(getDate())
-                && otherAppt.getTime().equals(getTime())
-                && otherAppt.getVenue().equals(getVenue());
+        return otherAppt.getDate().equals(getDate());
     }
 
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(date, time, venue);
+        return Objects.hash(date);
     }
 
     @Override
     public String toString() {
         final StringBuilder builder = new StringBuilder();
-        builder.append("Appointment on ")
-                .append(getDate())
-                .append(", ")
-                .append(getTime())
-                .append(", at ")
-                .append(getVenue())
-                .append("!");
+
+        builder.append("Appointment: ")
+                .append(this.getDate());
 
         return builder.toString();
     }
