5d07e4f9ee4ab340bc0eac00f145c2dd88b5111e
dt-td <77134471+dt-td@users.noreply.github.com>
2021-10-05 22:36:09 +0800

    Cleaned up code

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -4,7 +4,6 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import seedu.address.model.Model;
-import seedu.address.ui.UiManager;
 
 /**
  * Lists all persons in the address book to the user.
@@ -15,12 +14,10 @@ public class ListCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Listed all persons";
 
-
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-        UiManager.hideViewWindow();
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
