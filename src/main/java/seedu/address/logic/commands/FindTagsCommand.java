21a9fb6694333c5aabb63a6c9506d0a4bb3dc256
Chan Yu Cheng <e0560244@u.nus.edu>
2021-10-05 16:46:50 +0800

    Change import statement order to fulfill checkstyle.

--- src/main/java/seedu/address/logic/commands/FindTagsCommand.java
+++ src/main/java/seedu/address/logic/commands/FindTagsCommand.java
@@ -1,10 +1,11 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.person.PersonTagsContainsTagsPredicate;
 
-import static java.util.Objects.requireNonNull;
 
 /**
  * Finds and lists all persons in address book whose tags contains all of the argument tags.
