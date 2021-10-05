8fd8984d802000f47cf91528c105a0b068ebb28d
Tanishq4331 <62829987+Tanishq4331@users.noreply.github.com>
2021-10-05 12:02:22 +0800

    Fix javadoc in Model

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -78,18 +78,6 @@ public interface Model {
      */
     void setPerson(Person target, Person editedPerson);
 
-    /**
-     * Returns true if a task with the same identity as {@code task} exists in the task list.
-     */
-    boolean hasTask(Task task);
-
-    /**
-     * Replaces the given task {@code target} with {@code editedTask}.
-     * {@code target} must exist in the task list.
-     * The task identity of {@code editedTask} must not be the same as another existing task in the address book.
-     */
-    void setTask(Task target, Task editedTask);
-
     /** Returns an unmodifiable view of the filtered person list */
     ObservableList<Person> getFilteredPersonList();
 
@@ -104,6 +92,18 @@ public interface Model {
      * Adds the given task.
      */
     void addTask(Task task);
+
+    /**
+     * Returns true if a task with the same identity as {@code task} exists in the task list.
+     */
+    boolean hasTask(Task task);
+
+    /**
+     * Replaces the given task {@code target} with {@code editedTask}.
+     * {@code target} must exist in the task list.
+     * The task identity of {@code editedTask} must not be the same as another existing task in the task list.
+     */
+    void setTask(Task target, Task editedTask);
 
     /** Returns and unmodifiable view of the filtered task list */
     //added prior to the ui functionality actually being implemented.
