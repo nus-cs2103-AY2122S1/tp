83bf6f06a42c8a56dd8a913fcc2d78dd03b1784d
LeopardMerkava <andrewbaruch4@gmail.com>
2021-10-05 17:01:26 +0800

    Fix Checkstyle

--- src/test/java/seedu/address/logic/commands/AddTaskCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddTaskCommandTest.java
@@ -15,7 +15,6 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.commons.util.StringUtil;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
