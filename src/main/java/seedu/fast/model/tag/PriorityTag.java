7c3064c53b72adafdbb29e1c6aa2e8e3abf6eab1
trash-bin99 <77224811+trash-bin99@users.noreply.github.com>
2021-10-06 01:27:09 +0800

    Create new classes PriorityTag and PriorityTagUtils
    They are initialised with minimal fields and constructors at first.

---
+++ src/main/java/seedu/fast/model/tag/PriorityTag.java
@@ -0,0 +1,15 @@
+package seedu.fast.model.tag;
+
+
+public class PriorityTag extends Tag {
+
+    /**
+     * Constructs a {@code Tag}.
+     *
+     * @param tagName A valid tag name.
+     */
+    public PriorityTag(String tagName) {
+        super(tagName);
+    }
+    
+}
