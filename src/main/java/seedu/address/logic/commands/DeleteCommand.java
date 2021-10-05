0e1d2476e0f696f2631bfb0ebd7d3a9e08e029a0
ryantianj <ryantianjun@gmail.com>
2021-10-05 22:21:25 +0800

    Update styling

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -13,8 +13,7 @@ public abstract class DeleteCommand extends Command {
             + "Example: " + COMMAND_WORD + " 1";
 
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
-
-
+    
     public DeleteCommand() {
 
     }
