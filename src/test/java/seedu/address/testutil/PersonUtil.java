2b9e7f81cc1a708709c8f4394742438a87b9155b
Preshita01 <pandipreshita@gmail.com>
2021-10-05 15:22:06 +0800

    Update PersonUtil.java to deal with the fields for a student in TutorAid

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -1,10 +1,9 @@
 package seedu.address.testutil;
 
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_STUDENT_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_STUDENT_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PARENT_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PARENT_PHONE;
 
 import java.util.Set;
 
@@ -30,23 +29,24 @@ public class PersonUtil {
      */
     public static String getPersonDetails(Person person) {
         StringBuilder sb = new StringBuilder();
-        sb.append(PREFIX_NAME + person.getName().fullName + " ");
-        sb.append(PREFIX_PHONE + person.getPhone().value + " ");
-        sb.append(PREFIX_EMAIL + person.getEmail().value + " ");
-        sb.append(PREFIX_ADDRESS + person.getAddress().value + " ");
-        person.getTags().stream().forEach(
-            s -> sb.append(PREFIX_TAG + s.tagName + " ")
-        );
+        sb.append(PREFIX_STUDENT_NAME + person.getStudentName().fullName + " ");
+        sb.append(PREFIX_STUDENT_PHONE + person.getStudentPhone().value + " ");
+        sb.append(PREFIX_PARENT_NAME + person.getParentName().fullName + " ");
+        sb.append(PREFIX_PARENT_PHONE + person.getParentPhone().value + " ");
+
         return sb.toString();
     }
 
     /**
      * Returns the part of command string for the given {@code EditPersonDescriptor}'s details.
      */
+    /**
     public static String getEditPersonDescriptorDetails(EditPersonDescriptor descriptor) {
         StringBuilder sb = new StringBuilder();
-        descriptor.getName().ifPresent(name -> sb.append(PREFIX_NAME).append(name.fullName).append(" "));
-        descriptor.getPhone().ifPresent(phone -> sb.append(PREFIX_PHONE).append(phone.value).append(" "));
+        descriptor.getName().ifPresent(name -> sb.append(PREFIX_STUDENT_NAME).append(studentName.fullName).append(" "));
+        descriptor.getPhone().ifPresent(phone -> sb.append(PREFIX_STUDENT_PHONE).append(phone.value).append(" "));
+        descriptor.getName().ifPresent(name -> sb.append(PREFIX_PARENTT_NAME).append(name.fullName).append(" "));
+        descriptor.getPhone().ifPresent(phone -> sb.append(PREFIX_PARENT_PHONE).append(phone.value).append(" "));
         descriptor.getEmail().ifPresent(email -> sb.append(PREFIX_EMAIL).append(email.value).append(" "));
         descriptor.getAddress().ifPresent(address -> sb.append(PREFIX_ADDRESS).append(address.value).append(" "));
         if (descriptor.getTags().isPresent()) {
@@ -59,4 +59,5 @@ public class PersonUtil {
         }
         return sb.toString();
     }
+     **/
 }
