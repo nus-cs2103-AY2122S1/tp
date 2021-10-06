4655f73111a6e2eba33f1ef11a4db9c9eb1d79f1
HongshuW <hongshu.wang.uni@gmail.com>
2021-10-06 18:06:47 +0800

    Add AddClientCommandParser.java

---
+++ src/main/java/seedu/address/logic/parser/AddClientCommandParser.java
@@ -0,0 +1,11 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.AddClientCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class AddClientCommandParser implements Parser<AddClientCommand> {
+    @Override
+    public AddClientCommand parse(String userInput) throws ParseException {
+        return null;
+    }
+}
