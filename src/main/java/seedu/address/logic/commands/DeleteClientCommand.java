670d80719019e16c6053ea077b7969684f9ee672
Lin Xingchen <linxingchen0428cathy@gmail.com>
2021-10-06 16:46:10 +0800

    Remove traces of Person in new classes

--- src/main/java/seedu/address/logic/commands/DeleteClientCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteClientCommand.java
@@ -5,14 +5,13 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.client.Client;
-import seedu.address.model.person.Person;
 
 import java.util.List;
 
 import static java.util.Objects.requireNonNull;
 
 /**
- * Deletes a person identified using it's displayed index from the address book.
+ * Deletes a client identified using it's displayed index from the address book.
  */
 public class DeleteClientCommand extends Command{
 
