b9f20bd82d5a5c091c1b341de5643df8f18eeb22
Chi Xu <chixucontinue@gmail.com>
2021-10-06 16:51:34 +0800

    Refactoring code: replace add with padd

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.PAddCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
@@ -36,8 +36,8 @@ public class AddressBookParserTest {
     @Test
     public void parseCommand_add() throws Exception {
         Person person = new PersonBuilder().build();
-        AddCommand command = (AddCommand) parser.parseCommand(PersonUtil.getAddCommand(person));
-        assertEquals(new AddCommand(person), command);
+        PAddCommand command = (PAddCommand) parser.parseCommand(PersonUtil.getAddCommand(person));
+        assertEquals(new PAddCommand(person), command);
     }
 
     @Test
