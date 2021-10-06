b4677261af69f193371fb6d7bc7b8e4f8e2f25b7
JunWei3112 <e0543453@u.nus.edu>
2021-10-06 14:55:28 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/logic/commands/PaidCommand.java
+++ src/main/java/seedu/address/logic/commands/PaidCommand.java
@@ -1,16 +1,16 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import java.util.List;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.PaymentStatus;
 import seedu.address.model.person.Person;
-
-import java.util.List;
-
-import static java.util.Objects.requireNonNull;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 /**
  * Sets the payment status of the student identified using the displayed index in TutorAid, to paid.
