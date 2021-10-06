db979de81e27fbfda20d72de9582a09d0c94d0c3
HongshuW <hongshu.wang.uni@gmail.com>
2021-10-06 20:55:35 +0800

    AddressBookParser class: add AddProductCommand case Implement AddProductCommand and AddProductCommandParser classes. Program now supports adding products without displaying or saving.

--- src/main/java/seedu/address/logic/parser/AddProductCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddProductCommandParser.java
@@ -1,14 +1,36 @@
 package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.stream.Stream;
 
 import seedu.address.logic.commands.AddProductCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.commons.Name;
 
 public class AddProductCommandParser implements Parser<AddProductCommand> {
     @Override
     public AddProductCommand parse(String args) throws ParseException {
         requireNonNull(args);
-        return null;
+        Prefix startOfCommand = new Prefix("");
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, startOfCommand);
+
+        if (!arePrefixesPresent(argMultimap, startOfCommand)) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddProductCommand.MESSAGE_USAGE));
+        }
+
+        Name name = ParserUtil.parseName(argMultimap.getValue(startOfCommand).get());
+
+        AddProductCommand.AddProductDescriptor descriptor = new AddProductCommand.AddProductDescriptor(name);
+        return new AddProductCommand(descriptor);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 }
