f05025012e78ea9ae833479da47a8fb885b0e713
JunWei3112 <e0543453@u.nus.edu>
2021-10-06 14:48:17 +0800

    Implement functionality of the 'paid' command

---
+++ src/main/java/seedu/address/logic/parser/PaidCommandParser.java
@@ -0,0 +1,29 @@
+package seedu.address.logic.parser;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.PaidCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+/**
+ * Parses input arguments and creates a new PaidCommand object
+ */
+public class PaidCommandParser {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of PaidCommand
+     * and returns a PaidCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public PaidCommand parse(String args) throws ParseException {
+        try {
+            Index index = ParserUtil.parseIndex(args);
+            return new PaidCommand(index);
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, PaidCommand.MESSAGE_USAGE), pe);
+        }
+    }
+}
