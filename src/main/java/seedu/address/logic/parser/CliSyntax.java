1d973f81bcb8bad37aba406453df27a77019b85b
TohWeiJie01 <tohweijie01@gmail.com>
2021-10-07 03:31:15 +0800

    Add Prefix support for Level of Education

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -12,5 +12,5 @@ public class CliSyntax {
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_ROLE = new Prefix("r/");
-
+    public static final Prefix PREFIX_LEVEL_OF_EDUCATION = new Prefix("l/");
 }
