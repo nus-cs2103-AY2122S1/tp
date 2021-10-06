5616bac8e25d9ee853a37a879fe90aa5489844c9
TohWeiJie01 <tohweijie01@gmail.com>
2021-10-07 05:58:07 +0800

    Update EditPersonDescriptorBuilder to support level of education

--- src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
+++ src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
@@ -7,6 +7,7 @@ import java.util.stream.Stream;
 import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
+import seedu.address.model.person.LevelOfEducation;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
@@ -38,6 +39,7 @@ public class EditPersonDescriptorBuilder {
         descriptor.setEmail(person.getEmail());
         descriptor.setAddress(person.getAddress());
         descriptor.setRole(person.getRole());
+        descriptor.setLevelOfEducation(person.getLevelOfEducation());
         descriptor.setTags(person.getTags());
     }
 
@@ -78,6 +80,14 @@ public class EditPersonDescriptorBuilder {
      */
     public EditPersonDescriptorBuilder withRole(String role) {
         descriptor.setRole(new Role(role));
+        return this;
+    }
+
+    /**
+     * Sets the {@code Level of Education} of the {@code EditPersonDescriptor} that we are building.
+     */
+    public EditPersonDescriptorBuilder withLevelOfEducation(String levelOfEducation) {
+        descriptor.setLevelOfEducation(new LevelOfEducation(levelOfEducation));
         return this;
     }
 
