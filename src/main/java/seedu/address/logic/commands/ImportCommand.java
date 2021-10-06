512fcf5c9e0ba646f8efb244f818a872a6b86ddf
KishendranVendarKon <kishen.vendarkon@gmail.com>
2021-10-06 10:09:31 +0800

    Improve code quality

--- src/main/java/seedu/address/logic/commands/ImportCommand.java
+++ src/main/java/seedu/address/logic/commands/ImportCommand.java
@@ -1,24 +1,31 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.ParserUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
-import seedu.address.model.person.*;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
-
-import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Set;
-
-import static java.util.Objects.requireNonNull;
 
 public class ImportCommand extends Command {
 
-    public static final int NUMBER_OF_FIELDS = 4;
+    public static final int NUMBER_OF_FIELDS = 5;
     public static final String COMMAND_WORD = "import";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD;
@@ -26,17 +33,24 @@ public class ImportCommand extends Command {
     public static final String MESSAGE_SUCCESS = "Contacts added successfully";
 
     private final File csvFile;
+    private final ArrayList<Integer> wronglyFormattedEntries = new ArrayList<>();
+    private final ArrayList<Name> nameList = new ArrayList<>();
+    private final ArrayList<Phone> phoneList = new ArrayList<>();
+    private final ArrayList<Email> emailList = new ArrayList<>();
+    private final ArrayList<Address> addressList = new ArrayList<>();
+    private final ArrayList<Set<Tag>> tagList = new ArrayList<>();
+
 
     /**
-     * Creates an AddCommand to add the specified {@code Person}
+     * Creates an ImportCommand to batch import contacts from a csv file
      */
     public ImportCommand() {
-        int response;
         JFileChooser chooser = new JFileChooser("");
         FileNameExtensionFilter filter = new FileNameExtensionFilter("CSV", "csv");
         chooser.setFileFilter(filter);
         chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-        response = chooser.showOpenDialog(null);
+
+        int response = chooser.showOpenDialog(null);
 
         if (response == JFileChooser.APPROVE_OPTION) {
             csvFile = chooser.getSelectedFile();
@@ -53,52 +67,10 @@ public class ImportCommand extends Command {
             throw new CommandException("File was not selected");
         }
 
-        int rowCounter = 0;
-        ArrayList<Integer> wronglyFormattedEntries = new ArrayList<>();
-        ArrayList<Name> nameList = new ArrayList<>();
-        ArrayList<Phone> phoneList = new ArrayList<>();
-        ArrayList<Email> emailList = new ArrayList<>();
-        ArrayList<Address> addressList = new ArrayList<>();
-        ArrayList<Set<Tag>> tagList = new ArrayList<>();
-
-        try{
-            BufferedReader br = new BufferedReader(new FileReader(csvFile));
-            String line;
-            br.readLine();
-            rowCounter += 1;
-
-            while ((line = br.readLine()) != null) {
-                String[] values = line.split(",");
-                rowCounter += 1;
-
-                try {
-                    Name name = ParserUtil.parseName(values[0]);
-                    nameList.add(name);
-                    Phone phone = ParserUtil.parsePhone(values[1]);
-                    phoneList.add(phone);
-                    Email email = ParserUtil.parseEmail(values[2]);
-                    emailList.add(email);
-                    Address address = ParserUtil.parseAddress(values[3]);
-                    addressList.add(address);
-                    Set<Tag> tags = ParserUtil.parseTags(new ArrayList<String>());
-
-                    if (values.length == NUMBER_OF_FIELDS + 1) {
-                        String[] strTags = values[4].split(" ");
-                        System.out.println("tags: " + Arrays.toString(strTags));
-                        tags = ParserUtil.parseTags(Arrays.asList(strTags));
-                    }
-
-                    tagList.add(tags);
-
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                    wronglyFormattedEntries.add(rowCounter);
-                }
-
-            }
-
+        try {
+            csvParse();
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new CommandException("File could not be read");
         }
 
         if (wronglyFormattedEntries.size() > 0) {
@@ -106,7 +78,57 @@ public class ImportCommand extends Command {
             throw new CommandException(errorString);
         }
 
-        for (int i = 0; i < rowCounter - 1; i++) {
+        addAllEntries(model);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+
+    private void csvParse() throws IOException {
+        int rowCounter = 0;
+
+        BufferedReader br = new BufferedReader(new FileReader(csvFile));
+        String line;
+
+        //Skip header row
+        br.readLine();
+        rowCounter += 1;
+
+        while ((line = br.readLine()) != null) {
+            String[] values = line.split(",");
+            rowCounter += 1;
+
+            try {
+                if (values.length > NUMBER_OF_FIELDS) {
+                    throw new ParseException("Too many columns");
+                }
+
+                Name name = ParserUtil.parseName(values[0]);
+                nameList.add(name);
+                Phone phone = ParserUtil.parsePhone(values[1]);
+                phoneList.add(phone);
+                Email email = ParserUtil.parseEmail(values[2]);
+                emailList.add(email);
+                Address address = ParserUtil.parseAddress(values[3]);
+                addressList.add(address);
+                Set<Tag> tags = ParserUtil.parseTags(new ArrayList<String>());
+
+                //Row has tags
+                if (values.length == NUMBER_OF_FIELDS) {
+                    String[] strTags = values[4].split(" ");
+                    System.out.println("tags: " + Arrays.toString(strTags));
+                    tags = ParserUtil.parseTags(Arrays.asList(strTags));
+                }
+
+                tagList.add(tags);
+
+            } catch (ParseException e) {
+                wronglyFormattedEntries.add(rowCounter);
+            }
+
+        }
+    }
+
+    private void addAllEntries (Model model) {
+        for (int i = 0; i < nameList.size(); i++) {
             Name name = nameList.get(i);
             Phone phone = phoneList.get(i);
             Email email = emailList.get(i);
@@ -114,6 +136,7 @@ public class ImportCommand extends Command {
             Set<Tag> tags = tagList.get(i);
 
             Person person = new Person(name, phone, email, address, tags);
+
             if (model.hasPerson(person)) {
                 continue;
             }
@@ -121,14 +144,13 @@ public class ImportCommand extends Command {
             model.addPerson(person);
 
         }
-
-        return new CommandResult(MESSAGE_SUCCESS);
     }
+
 
     @Override
     public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof seedu.address.logic.commands.ImportCommand // instanceof handles nulls
+        return other == this
+                || (other instanceof seedu.address.logic.commands.ImportCommand
                 && csvFile.equals(((ImportCommand) other).csvFile));
     }
 }
