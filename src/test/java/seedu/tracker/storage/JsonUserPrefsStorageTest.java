ba6ad97dd7fc4bf68c8393537053ba21ac80de12
El0hime <dillontkw@gmail.com>
2021-10-07 23:51:49 +0800

    Update JsonUserPrefsStorageTest.java

--- src/test/java/seedu/tracker/storage/JsonUserPrefsStorageTest.java
+++ src/test/java/seedu/tracker/storage/JsonUserPrefsStorageTest.java
@@ -73,7 +73,7 @@ public class JsonUserPrefsStorageTest {
     private UserPrefs getTypicalUserPrefs() {
         UserPrefs userPrefs = new UserPrefs();
         userPrefs.setGuiSettings(new GuiSettings(1000, 500, 300, 100));
-        userPrefs.setModTrackerFilePath(Paths.get("data\\modtracker.json"));
+        userPrefs.setModTrackerFilePath(Paths.get("data\\moduletracker.json"));
         return userPrefs;
     }
 
