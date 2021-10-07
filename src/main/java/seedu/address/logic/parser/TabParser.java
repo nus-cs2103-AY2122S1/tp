5f286dfce4549ae8b1f5c4b49a7073b82280e05d
WangGLJoseph <joseph.cnsg@gmail.com>
2021-10-08 01:02:20 +0800

    refactor AddressBookParser to TabParser

--- src/main/java/seedu/address/logic/parser/TabParser.java
+++ src/main/java/seedu/address/logic/parser/TabParser.java
@@ -6,21 +6,13 @@ import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.ClearCommand;
-import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.EditCommand;
-import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FindCommand;
-import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.*;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
  * Parses user input.
  */
-public class AddressBookParser {
+public class TabParser {
 
     /**
      * Used for initial separation of command word and args.
@@ -67,6 +59,9 @@ public class AddressBookParser {
 
         case HelpCommand.COMMAND_WORD:
             return new HelpCommand();
+
+        case TaskAddCommand.COMMAND_WORD:
+            return new TaskAddCommand();
 
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
