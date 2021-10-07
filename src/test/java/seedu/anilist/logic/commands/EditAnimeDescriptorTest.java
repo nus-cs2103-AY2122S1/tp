ac60b18d1be23a2983457b246000d9e78a25a533
LuoDan <69646558+luodan01@users.noreply.github.com>
2021-10-07 18:59:51 +0800

    Fix test case
    Swapped SHOUNEN tag with SUPERHERO in the last test case of EditAnimeDescriptorTest

--- src/test/java/seedu/anilist/logic/commands/EditAnimeDescriptorTest.java
+++ src/test/java/seedu/anilist/logic/commands/EditAnimeDescriptorTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.anilist.logic.commands.CommandTestUtil.DESC_AKIRA;
 import static seedu.anilist.logic.commands.CommandTestUtil.DESC_BNHA;
 import static seedu.anilist.logic.commands.CommandTestUtil.VALID_NAME_BNHA;
-import static seedu.anilist.logic.commands.CommandTestUtil.VALID_TAG_SHOUNEN;
+import static seedu.anilist.logic.commands.CommandTestUtil.VALID_TAG_SUPERHERO;
 
 import org.junit.jupiter.api.Test;
 
@@ -37,7 +37,7 @@ public class EditAnimeDescriptorTest {
         assertFalse(DESC_AKIRA.equals(editedAkira));
 
         // different tags -> returns false
-        editedAkira = new EditAnimeDescriptorBuilder(DESC_AKIRA).withTags(VALID_TAG_SHOUNEN).build();
+        editedAkira = new EditAnimeDescriptorBuilder(DESC_AKIRA).withTags(VALID_TAG_SUPERHERO).build();
         assertFalse(DESC_AKIRA.equals(editedAkira));
     }
 }
