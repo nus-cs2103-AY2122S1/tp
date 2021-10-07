af7f7a6266b1b77909ad6071b264d6f0af2f501a
Hong Yao <ganhongyao@u.nus.edu>
2021-10-07 10:54:04 +0800

    Update EditStudentDescriptor

--- src/test/java/seedu/address/testutil/EditStudentDescriptorBuilder.java
+++ src/test/java/seedu/address/testutil/EditStudentDescriptorBuilder.java
@@ -1,16 +1,18 @@
 package seedu.address.testutil;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.EditCommand.EditStudentDescriptor;
-import seedu.address.model.student.Address;
-import seedu.address.model.student.Email;
+import seedu.address.model.student.Assessment;
+import seedu.address.model.student.Group;
+import seedu.address.model.student.ID;
 import seedu.address.model.student.Name;
+import seedu.address.model.student.Score;
 import seedu.address.model.student.Student;
-import seedu.address.model.student.Phone;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -24,19 +26,19 @@ public class EditStudentDescriptorBuilder {
         descriptor = new EditStudentDescriptor();
     }
 
-    public EditStudentDescriptorBuilder(EditCommand.EditStudentDescriptor descriptor) {
-        this.descriptor = new EditCommand.EditStudentDescriptor(descriptor);
+    public EditStudentDescriptorBuilder(EditStudentDescriptor descriptor) {
+        this.descriptor = new EditStudentDescriptor(descriptor);
     }
 
     /**
      * Returns an {@code EditStudentDescriptor} with fields containing {@code student}'s details
      */
     public EditStudentDescriptorBuilder(Student student) {
-        descriptor = new EditCommand.EditStudentDescriptor();
+        descriptor = new EditStudentDescriptor();
         descriptor.setName(student.getName());
-        descriptor.setPhone(student.getPhone());
-        descriptor.setEmail(student.getEmail());
-        descriptor.setAddress(student.getAddress());
+        descriptor.setId(student.getId());
+        descriptor.setGroups(student.getGroups());
+        descriptor.setScores(student.getScores());
         descriptor.setTags(student.getTags());
     }
 
@@ -49,26 +51,28 @@ public class EditStudentDescriptorBuilder {
     }
 
     /**
-     * Sets the {@code Phone} of the {@code EditStudentDescriptor} that we are building.
+     * Sets the {@code id} of the {@code EditStudentDescriptor} that we are building.
      */
-    public EditStudentDescriptorBuilder withPhone(String phone) {
-        descriptor.setPhone(new Phone(phone));
+    public EditStudentDescriptorBuilder withId(String id) {
+        descriptor.setId(new ID(id));
         return this;
     }
 
     /**
-     * Sets the {@code Email} of the {@code EditStudentDescriptor} that we are building.
+     * Parses the {@code groups} into a {@code List<Group>} and set it to the {@code EditStudentDescriptor}
+     * that we are building.
      */
-    public EditStudentDescriptorBuilder withEmail(String email) {
-        descriptor.setEmail(new Email(email));
+    public EditStudentDescriptorBuilder withGroups(String... groups) {
+        List<Group> groupList = Stream.of(groups).map(Group::new).collect(Collectors.toList());
+        descriptor.setGroups(groupList);
         return this;
     }
 
     /**
-     * Sets the {@code Address} of the {@code EditStudentDescriptor} that we are building.
+     * Sets the {@code scores} of the {@code EditStudentDescriptor} that we are building.
      */
-    public EditStudentDescriptorBuilder withAddress(String address) {
-        descriptor.setAddress(new Address(address));
+    public EditStudentDescriptorBuilder withScores(Map<Assessment, Score> scores) {
+        descriptor.setScores(scores);
         return this;
     }
 
@@ -82,7 +86,7 @@ public class EditStudentDescriptorBuilder {
         return this;
     }
 
-    public EditCommand.EditStudentDescriptor build() {
+    public EditStudentDescriptor build() {
         return descriptor;
     }
 }
