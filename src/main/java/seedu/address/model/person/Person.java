a9967ddace168dd0a5fdcae694a5f018aae3468d
JoelChanZhiYang <joelchanzhiyang@gmail.com>
2021-10-07 21:11:19 +0800

    Add claims to storage

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -29,7 +29,7 @@ public class Person {
     /**
      * Every field must be present and not null.
      */
-    private Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags, Set<Claim> claims) {
+    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags, Set<Claim> claims) {
         requireAllNonNull(name, phone, email, address, tags);
         this.name = name;
         this.phone = phone;
