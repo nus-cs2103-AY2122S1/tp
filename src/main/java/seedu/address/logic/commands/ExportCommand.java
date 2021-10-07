7c71a4b51530509fc0b12e367f262c323124a695
Adam Oh Zhi Hong <adamohzhihong@gmail.com>
2021-10-07 12:01:22 +0800

    Edit MESSAGE_USAGE for ExportCommand

--- src/main/java/seedu/address/logic/commands/ExportCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportCommand.java
@@ -18,7 +18,7 @@ public class ExportCommand extends Command {
     public static final String COMMAND_WORD = "export";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + "Exports the current list of contacts to a specified filename.\n"
+            + ": Exports the current list of contacts to a specified filename.\n"
             + "Parameters: FILENAME\n"
             + "Example: " + COMMAND_WORD + " friends";
 
