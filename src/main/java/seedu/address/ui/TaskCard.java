657138359977ec110ff8f0e0b484bfb303e50142
GnohChengYi <chengyignoh@gmail.com>
2021-10-07 12:40:58 +0800

    Create task card fxml and class

---
+++ src/main/java/seedu/address/ui/TaskCard.java
@@ -0,0 +1,53 @@
+package seedu.address.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Region;
+import seedu.address.model.task.Task;
+
+/**
+ * A UI component that displays information of a {@code Task}.
+ */
+public class TaskCard extends UiPart<Region> {
+
+    private static final String FXML = "TaskCard.fxml";
+
+    public final Task task;
+
+    @FXML
+    private Label id;
+    @FXML
+    private Label label;
+    @FXML
+    private CheckBox isDone;
+
+    /**
+     * Creates a {@code TaskCard} with the given {@code Task} and index to display.
+     */
+    public TaskCard(Task task, int displayedIndex) {
+        super(FXML);
+        this.task = task;
+        id.setText(displayedIndex + ". ");
+        label.setText(task.getLabel().toString());
+        isDone.setSelected(task.getIsDone());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof TaskCard)) {
+            return false;
+        }
+
+        // state check
+        TaskCard card = (TaskCard) other;
+        return id.getText().equals(card.id.getText())
+                && task.equals(card.task);
+    }
+}
