2df5758277d6985ff81d208b72f575acf90f7725
Timothyoung97 <timothyoung97@gmail.com>
2021-10-07 17:13:38 +0800

    Add Class AddLessonCommand
    - Class AddLessonCommand follows the format of Class AddCommand

---
+++ src/main/java/seedu/address/logic/commands/AddLessonCommand.java
@@ -0,0 +1,55 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_COST;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DAY;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_GRADE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SUBJECT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TIME;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.lesson.Lesson;
+
+public class AddLessonCommand extends Command {
+
+    public static final String COMMAND_WORD = "add -l";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a lesson to TuitiONE. "
+            + "Parameters: "
+            + PREFIX_SUBJECT + "SUBJECT "
+            + PREFIX_GRADE + "GRADE "
+            + PREFIX_DAY + "DAY "
+            + PREFIX_TIME + "START_TIME"
+            + PREFIX_COST + "COST\n"
+            + "Example: " + "add -l s/Science g/P5 d/WED t/1200";
+
+    public static final String MESSAGE_SUCCESS = "New lesson created: $1$s";
+    public static final String MESSAGE_DUPLICATE_LESSON = "This lesson already exists in TuitiONE";
+
+    private final Lesson toAdd;
+
+    public AddLessonCommand(Lesson lesson) {
+        requireNonNull(lesson);
+        toAdd = lesson;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        if (model.hasLesson(toAdd)) {
+            throw new CommandException(MESSAGE_DUPLICATE_LESSON);
+        }
+
+        model.addLesson(toAdd);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this
+                || (other instanceof AddLessonCommand
+        && toAdd.equals(((AddLessonCommand) other).toAdd));
+    }
+}
