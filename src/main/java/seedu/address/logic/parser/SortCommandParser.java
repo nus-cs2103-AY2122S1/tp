97780a72caf4c3a52a282351aa7744eb5468775f
czhi-bin <iopwslw@gmail.com>
2021-10-07 17:06:20 +0800

    Fix issues in javadoc

--- src/main/java/seedu/address/logic/parser/SortCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SortCommandParser.java
@@ -9,7 +9,7 @@ import seedu.address.logic.commands.SortCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
- * Parses input arguments and creates a new SortCommand object
+ * Parses input arguments and creates a new {@code SortCommand} object
  */
 public class SortCommandParser implements Parser<SortCommand> {
 
@@ -34,8 +34,8 @@ public class SortCommandParser implements Parser<SortCommand> {
     }
 
     /**
-     * Parses the given {@code String} of arguments in the context of the SortCommand
-     * and returns a SortCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the {@code SortCommand}
+     * and returns a {@code SortCommand} object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     @Override
