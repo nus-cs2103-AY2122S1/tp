361869292fec4836b8963f827471fa20ad0e640f
TohWeiJie01 <tohweijie01@gmail.com>
2021-10-08 00:28:31 +0800

    Remove Address field from TypicalPersons

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -1,6 +1,4 @@
 package seedu.address.testutil;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_EMPLOYMENT_TYPE_AMY;
@@ -33,9 +31,8 @@ import seedu.address.model.person.Person;
 public class TypicalPersons {
 
     public static final Person ALICE = new PersonBuilder().withName("Alice Pauline")
-            .withAddress("123, Jurong West Ave 6, #08-111")
-            .withEmail("alice@example.com")
             .withPhone("94351253")
+            .withEmail("alice@example.com")
             .withRole("Preschool Teacher")
             .withEmploymentType("Full time")
             .withExpectedSalary("2500")
@@ -44,9 +41,8 @@ public class TypicalPersons {
             .withTags("friends").build();
 
     public static final Person BENSON = new PersonBuilder().withName("Benson Meier")
-            .withAddress("311, Clementi Ave 2, #02-25")
-            .withEmail("johnd@example.com")
             .withPhone("98765432")
+            .withEmail("johnd@example.com")
             .withRole("Doctor")
             .withEmploymentType("Part time")
             .withExpectedSalary("2700")
@@ -57,7 +53,6 @@ public class TypicalPersons {
     public static final Person CARL = new PersonBuilder().withName("Carl Kurz")
             .withPhone("95352563")
             .withEmail("heinz@example.com")
-            .withAddress("wall street")
             .withRole("Electrical Engineer")
             .withEmploymentType("Temporary")
             .withExpectedSalary("5500")
@@ -67,7 +62,6 @@ public class TypicalPersons {
     public static final Person DANIEL = new PersonBuilder().withName("Daniel Meier")
             .withPhone("87652533")
             .withEmail("cornelia@example.com")
-            .withAddress("10th street")
             .withRole("Sales Assistant")
             .withEmploymentType("Internship")
             .withExpectedSalary("3500")
@@ -78,7 +72,6 @@ public class TypicalPersons {
     public static final Person ELLE = new PersonBuilder().withName("Elle Meyer")
             .withPhone("9482224")
             .withEmail("werner@example.com")
-            .withAddress("michegan ave")
             .withRole("Toilet Cleaner")
             .withEmploymentType("Full time")
             .withExpectedSalary("5700")
@@ -88,7 +81,6 @@ public class TypicalPersons {
     public static final Person FIONA = new PersonBuilder().withName("Fiona Kunz")
             .withPhone("9482427")
             .withEmail("lydia@example.com")
-            .withAddress("little tokyo")
             .withRole("NTUC Cashier")
             .withEmploymentType("Part time")
             .withExpectedSalary("1900")
@@ -98,7 +90,6 @@ public class TypicalPersons {
     public static final Person GEORGE = new PersonBuilder().withName("George Best")
             .withPhone("9482442")
             .withEmail("anna@example.com")
-            .withAddress("4th street")
             .withRole("Waiter")
             .withEmploymentType("Internship")
             .withExpectedSalary("900")
@@ -109,7 +100,6 @@ public class TypicalPersons {
     public static final Person HOON = new PersonBuilder().withName("Hoon Meier")
             .withPhone("8482424")
             .withEmail("stefan@example.com")
-            .withAddress("little india")
             .withRole("Software Engineer")
             .withEmploymentType("Part time")
             .withExpectedSalary("3300")
@@ -119,7 +109,6 @@ public class TypicalPersons {
     public static final Person IDA = new PersonBuilder().withName("Ida Mueller")
             .withPhone("8482131")
             .withEmail("hans@example.com")
-            .withAddress("chicago ave")
             .withRole("Software Tester")
             .withEmploymentType("Full time")
             .withExpectedSalary("7100")
@@ -130,7 +119,6 @@ public class TypicalPersons {
     public static final Person AMY = new PersonBuilder().withName(VALID_NAME_AMY)
             .withPhone(VALID_PHONE_AMY)
             .withEmail(VALID_EMAIL_AMY)
-            .withAddress(VALID_ADDRESS_AMY)
             .withRole(VALID_ROLE_AMY)
             .withEmploymentType(VALID_EMPLOYMENT_TYPE_AMY)
             .withExpectedSalary(VALID_EXPECTED_SALARY_AMY)
@@ -142,7 +130,6 @@ public class TypicalPersons {
     public static final Person BOB = new PersonBuilder().withName(VALID_NAME_BOB)
             .withPhone(VALID_PHONE_BOB)
             .withEmail(VALID_EMAIL_BOB)
-            .withAddress(VALID_ADDRESS_BOB)
             .withRole(VALID_ROLE_BOB)
             .withEmploymentType(VALID_EMPLOYMENT_TYPE_BOB)
             .withExpectedSalary(VALID_EXPECTED_SALARY_BOB)
