45d1b9b0570463f33825b8856922879b143bf592
Bernardus Krishna <krishnabernardus@gmail.com>
2021-10-07 15:37:06 +0800

    Fix checkstyle

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -17,10 +17,10 @@ public class SampleDataUtil {
     public static Item[] getSampleItems() {
         return new Item[] {
             new Item(new Name("Oatmeal Cookie"), "#140121", 3,
-                getTagSet("baked")),
+                    getTagSet("baked")),
             new Item(new Name("Banana Muffin"), "#201928", 5,
                     getTagSet("baked")),
-            new Item(new Name("Pecan Pie"), "#178522",7,
+            new Item(new Name("Pecan Pie"), "#178522", 7,
                     getTagSet("baked")),
             new Item(new Name("Oreo Cheesecake"), "#109128", 1,
                     getTagSet("desert")),
