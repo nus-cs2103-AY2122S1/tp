8cc1b0c399c56bc45af7d3a6a2d93c563e3b54bd
juliussneezer04 <vishnu.sundaresan@gmail.com>
2021-10-07 11:57:36 +0800

    Refactoring AddressBook to Classmate

--- src/main/java/seedu/address/storage/JsonClassmateStorage.java
+++ src/main/java/seedu/address/storage/JsonClassmateStorage.java
@@ -17,13 +17,13 @@ import seedu.address.model.ReadOnlyAddressBook;
 /**
  * A class to access AddressBook data stored as a json file on the hard disk.
  */
-public class JsonAddressBookStorage implements AddressBookStorage {
+public class JsonClassmateStorage implements ClassmateStorage {
 
-    private static final Logger logger = LogsCenter.getLogger(JsonAddressBookStorage.class);
+    private static final Logger logger = LogsCenter.getLogger(JsonClassmateStorage.class);
 
     private Path filePath;
 
-    public JsonAddressBookStorage(Path filePath) {
+    public JsonClassmateStorage(Path filePath) {
         this.filePath = filePath;
     }
 
