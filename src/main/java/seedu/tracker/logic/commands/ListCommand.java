9568b61376b9e22c383b221dfd86a5ec60a58243
LuoZhijie-tom <e0492460@u.nus.edu>
2021-10-08 16:11:18 +0800

    Change address to module

--- src/main/java/seedu/tracker/logic/commands/ListCommand.java
+++ src/main/java/seedu/tracker/logic/commands/ListCommand.java
@@ -6,7 +6,7 @@ import static seedu.tracker.model.Model.PREDICATE_SHOW_ALL_MODULES;
 import seedu.tracker.model.Model;
 
 /**
- * Lists all persons in the address book to the user.
+ * Lists all modules in the module tracker to  the user.
  */
 public class ListCommand extends Command {
 
