af5a156ef02775bdc3ea471c2eda7aabe6aafb3e
TohWeiJie01 <tohweijie01@gmail.com>
2021-10-09 01:15:43 +0800

    Remove Address usage for LogicManagerTest

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -3,7 +3,6 @@ package seedu.address.logic;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
-import static seedu.address.logic.commands.CommandTestUtil.ADDRESS_DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.EMAIL_DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.EMPLOYMENT_TYPE_DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.EXPECTED_SALARY_DESC_AMY;
@@ -84,8 +83,8 @@ public class LogicManagerTest {
         logic = new LogicManager(model, storage);
 
         // Execute add command
-        String addCommand = AddCommand.COMMAND_WORD + NAME_DESC_AMY + PHONE_DESC_AMY + EMAIL_DESC_AMY
-                + ADDRESS_DESC_AMY + ROLE_DESC_AMY + EMPLOYMENT_TYPE_DESC_AMY
+        String addCommand = AddCommand.COMMAND_WORD + NAME_DESC_AMY + PHONE_DESC_AMY
+                + EMAIL_DESC_AMY + ROLE_DESC_AMY + EMPLOYMENT_TYPE_DESC_AMY
                 + EXPECTED_SALARY_DESC_AMY + LEVEL_OF_EDUCATION_DESC_AMY + EXPERIENCE_DESC_AMY;
 
         Person expectedPerson = new PersonBuilder(AMY).withTags().build();
