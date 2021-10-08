01d3283d9abaf5dd9f7d481aebe017c2028ba135
somekoreanboi <77215389+somekoreanboi@users.noreply.github.com>
2021-10-08 10:53:37 +0800

    Fix build errors

--- src/main/java/seedu/address/logic/commands/BackCommand.java
+++ src/main/java/seedu/address/logic/commands/BackCommand.java
@@ -1,15 +1,15 @@
 package seedu.address.logic.commands;
 
-import seedu.address.model.Model;
-
 import static java.util.Objects.requireNonNull;
+
+import seedu.address.model.Model;
 
 public class BackCommand extends Command {
 
     public static final String COMMAND_WORD = "/back";
 
-    public static final String MESSAGE_SUCCESS = "You exited the current directory! " +
-            "Please enter either 'students' or 'tasks' to enter a directory.";
+    public static final String MESSAGE_SUCCESS = "You exited the current directory! "
+            + "Please enter either 'students' or 'tasks' to enter a directory.";
 
     @Override
     public CommandResult execute(Model model) {
--- src/main/java/seedu/address/storage/JsonAdaptedStudent.java
+++ src/main/java/seedu/address/storage/JsonAdaptedStudent.java
@@ -10,7 +10,11 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.model.student.*;
+import seedu.address.model.student.Attendance;
+import seedu.address.model.student.Email;
+import seedu.address.model.student.Name;
+import seedu.address.model.student.Participation;
+import seedu.address.model.student.Student;
 import seedu.address.model.tag.Tag;
 
 /**
