1bb405c5f943381ab2897ac2a1e5a18d9725c649
benedictchuajj <benedictchuajj@gmail.com>
2021-10-08 13:56:15 +0800

    Fix code style based on checkstyle and run-checks

--- src/main/java/seedu/address/model/person/LastMet.java
+++ src/main/java/seedu/address/model/person/LastMet.java
@@ -1,19 +1,18 @@
 package seedu.address.model.person;
 
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.util.AppUtil.checkArgument;
-
 public class LastMet {
+    public static final String MESSAGE_CONSTRAINTS = "LastMet should be in the form of Day-Month-Year, "
+        + "where Day, month and year should be numerical values.";
+    public static final String ALTERNATIVE_VALIDATION_REGEX = "^\\d{4}\\-(0?[1-9]|1[012])\\-(0?[1-9]|[12][0-9]|3[01])$";
+
     public final LocalDate value;
     public final String dateInString;
 
-    public static final String MESSAGE_CONSTRAINTS = "LastMet should be in the form of Day-Month-Year, "
-            + "where Day, month and year should be numerical values.";
-    public static final String VALIDATION_REGEX = "^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[13-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$";
-    public static final String ALTERNATIVE_VALIDATION_REGEX = "^\\d{4}\\-(0?[1-9]|1[012])\\-(0?[1-9]|[12][0-9]|3[01])$";
     /**
      * Constructs an {@code Email}.
      *
@@ -26,7 +25,6 @@ public class LastMet {
         this.value = LocalDate.parse(lastMetDate, formatter);
     }
 
-
     /**
      * Returns if a given string is a valid LastMet.
      */
@@ -38,7 +36,6 @@ public class LastMet {
     public String toString() {
         return this.dateInString;
     }
-
 
     @Override
     public boolean equals(Object other) {
