1c79763b88c947dd935a72e23d6d31fb1622c92b
timothywongej <e0559903@u.nus.edu>
2021-10-08 21:14:18 +0800

    Add new prefixes to CliSyntax

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -9,7 +9,11 @@ public class CliSyntax {
     public static final Prefix PREFIX_NAME = new Prefix("n/");
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
-    public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
+    public static final Prefix PREFIX_ROLE = new Prefix("r/");
+    public static final Prefix PREFIX_FACULTY = new Prefix("f/");
+    public static final Prefix PREFIX_MAJOR = new Prefix("m/");
+    public static final Prefix PREFIX_CCA = new Prefix("cca/");
+    public static final Prefix PREFIX_MODULE = new Prefix("mod/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
 
 }
