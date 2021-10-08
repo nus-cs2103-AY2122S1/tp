b51d45a70a4062aa435ebeedcd68619cd559eb27
luayida99 <77190041+luayida99@users.noreply.github.com>
2021-10-08 16:44:22 +0800

    Add tests

--- src/test/java/seedu/address/logic/parser/VisitCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/VisitCommandParserTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 
 import org.junit.jupiter.api.Test;
@@ -19,6 +20,6 @@ public class VisitCommandParserTest {
         assertParseFailure(parser, VisitCommand.COMMAND_WORD, expectedMessage);
 
         // no index
-        assertParseFailure(parser, VisitCommand.COMMAND_WORD + " " + testVisit, expectedMessage);
+        assertParseFailure(parser, VisitCommand.COMMAND_WORD + " " + PREFIX_DATE + testVisit, expectedMessage);
     }
 }
