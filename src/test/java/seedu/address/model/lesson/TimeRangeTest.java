4690bfb1ab3804812d95aa43d6853c1f06ff5aa0
eeliana <62977276+eeliana@users.noreply.github.com>
2021-10-08 20:51:52 +0800

    Add tests for TimeRange

--- src/test/java/seedu/address/model/lesson/TimeRangeTest.java
+++ src/test/java/seedu/address/model/lesson/TimeRangeTest.java
@@ -1,5 +1,6 @@
 package seedu.address.model.lesson;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
@@ -24,15 +25,19 @@ public class TimeRangeTest {
         // null date
         assertThrows(NullPointerException.class, () -> TimeRange.isValidTimeRange(null));
 
-        // invalid time range
+        // invalid time ranges
         assertFalse(TimeRange.isValidTimeRange("1200-1000")); // end time earlier than start time
         assertFalse(TimeRange.isValidTimeRange("1099-1200")); // not a time value
         assertFalse(TimeRange.isValidTimeRange("0000-1200")); // before 8am
         assertFalse(TimeRange.isValidTimeRange("2000-2359")); // after 10pm
+        assertFalse(TimeRange.isValidTimeRange("2000:2359")); // invalid format
+        assertFalse(TimeRange.isValidTimeRange("")); // empty string
 
-        // valid time
+        // valid time ranges
         assertTrue(TimeRange.isValidTimeRange("1300-1400"));
+        assertTrue(TimeRange.isValidTimeRange("0800-1400"));
     }
+
 
     @Test
     public void isClashing_timeRange() {
@@ -53,5 +58,24 @@ public class TimeRangeTest {
         assertTrue(timeRange.isClashing(startBeforeEndAfter));
         assertTrue(timeRange.isClashing(startDuringEndDuring));
         assertTrue(timeRange.isClashing(duplicateTimeRange));
+    }
+
+    @Test
+    public void compareTo() {
+        TimeRange timeRange = new TimeRange("0900-1200");
+        TimeRange laterNonClashingTimeRange = new TimeRange("1300-1500");
+        TimeRange startBeforeEndDuring = new TimeRange("0900-1000");
+        TimeRange startDuringEndAfter = new TimeRange("1030-1300");
+
+        // clashing time ranges
+        assertEquals(0, timeRange.compareTo(startBeforeEndDuring));
+        assertEquals(0, timeRange.compareTo(startDuringEndAfter));
+
+        // later time range
+        assertEquals(-1, timeRange.compareTo(laterNonClashingTimeRange));
+
+        // earlier time range
+        assertEquals(1, laterNonClashingTimeRange.compareTo(timeRange));
+
     }
 }
