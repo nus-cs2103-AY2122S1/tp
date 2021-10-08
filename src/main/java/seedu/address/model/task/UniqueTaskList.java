39b5ac4c52b87998430aef11e4281172dd2dea23
eltonyeh <e0550632@u.nus.edu>
2021-10-08 16:54:19 +0800

    Add the `UniqueId` class to handle unique ids of tasks and students
    ... and add the id field to tasks

--- src/main/java/seedu/address/model/task/UniqueTaskList.java
+++ src/main/java/seedu/address/model/task/UniqueTaskList.java
@@ -14,9 +14,9 @@ import seedu.address.model.task.exceptions.TaskNotFoundException;
 /**
  * A list of tasks that enforces uniqueness between its elements and does not allow nulls.
  * A task is considered unique by comparing using {@code Task#isSameTask(Task)}. As such, adding and updating of
- * tasks uses Task#isSameTask(Task) for equality so as to ensure that the task being added or updated is
- * unique in terms of identity in the UniquePersonList. However, the removal of a task uses Task#equals(Object) so
- * as to ensure that the task with exactly the same fields will be removed.
+ * tasks uses Task#isSameTask(Task) for equality to ensure that the task being added or updated is
+ * unique in terms of identity in the UniquePersonList. However, the removal of a task uses Task#equals(Object) to
+ * ensure that the task with exactly the same fields will be removed.
  *
  * Supports a minimal set of list operations.
  *
