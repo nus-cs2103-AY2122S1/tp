90fe5564cab423dedd1199251013d6b553b24254
wangpeialex <alexwangpeipei@gmail.com>
2021-10-08 19:51:52 +0800

    Implement Order and OrderManager class

---
+++ src/main/java/seedu/address/model/Order.java
@@ -0,0 +1,24 @@
+package seedu.address.model;
+
+import javafx.collections.ObservableList;
+import seedu.address.model.item.Item;
+
+/**
+ * API of the Order component
+ */
+public interface Order {
+    /**
+     * Add an {@code Item} to the order.
+     */
+    void addItem(Item newItem);
+
+    /**
+     * Remove the specified {@code Item} from order.
+     */
+    void removeItem(Item toBeRemoved);
+
+    /**
+     * Get a list of items in the order.
+     */
+    ObservableList getOrderItems();
+}
