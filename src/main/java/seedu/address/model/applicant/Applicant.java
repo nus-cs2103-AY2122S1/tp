b21ce7d30a5f779ef6b40c32e9409bf1f944c1c5
Darren Heng <darren2pro@gmail.com>
2021-10-08 15:52:11 +0800

    Complete the implementation for applicant class (note: the dependency classes are not added yet)

--- src/main/java/seedu/address/model/applicant/Applicant.java
+++ src/main/java/seedu/address/model/applicant/Applicant.java
@@ -1,3 +1,108 @@
 package seedu.address.model.applicant;
 
-public class Applicant {}
+import java.util.Objects;
+
+/**
+ * Represents an Applicant in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+public class Applicant {
+
+    // Identity fields
+    private final Name name;
+    private final Phone phone;
+    private final Email email;
+
+    // Data fields
+    private final Address address;
+    private final Position position;
+
+    /**
+     * Every field must be present and not null
+     */
+    public Applicant(Name name, Phone phone, Email email, Address address, Position position) {
+        requireAllNonNull(name, phone, email, address, position);
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.position = position;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public Phone getPhone() {
+        return phone;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public Position getPosition() {
+        return address;
+    }
+
+    /**
+     * Returns true if both applicants have the same name.
+     * This defines a weaker notion of equality between two applicants.
+     */
+    public boolean isSameApplicant(Applicant otherApplicant) {
+        if (otherApplicant == this) {
+            return true;
+        }
+
+        return otherApplicant != null
+                && otherApplicant.getName().equals(getName());
+    }
+
+    /**
+     * Returns true if both applicants have the same identity and data fields.
+     * This defines a stronger notion of equality between two applicants.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Applicant)) {
+            return false;
+        }
+
+        Applicant otherApplicant = (Applicant) other;
+        return otherApplicant.getName().equals(getName())
+                && otherApplicant.getPhone().equals(getPhone())
+                && otherApplicant.getEmail().equals(getEmail())
+                && otherApplicant.getAddress().equals(getAddress())
+                && otherApplicant.getPosition().equals(getPosition());
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(name, phone, email, address, position);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getName())
+                .append("; Phone: ")
+                .append(getPhone())
+                .append("; Email: ")
+                .append(getEmail())
+                .append("; Address: ")
+                .append(getAddress())
+                .append("; Position: ")
+                .append(getPosition());
+
+        return builder.toString();
+    }
+}
