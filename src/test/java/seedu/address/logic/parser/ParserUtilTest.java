c50a3b0edd7405f469ed0d8855ff7c07cd598b61
GraceWang2322 <>
2021-10-08 15:48:13 +0800

    Add Test Cases

--- src/test/java/seedu/address/logic/parser/ParserUtilTest.java
+++ src/test/java/seedu/address/logic/parser/ParserUtilTest.java
@@ -20,10 +20,19 @@ import seedu.address.model.tag.Tag;
 public class ParserUtilTest {
     private static final String INVALID_NAME = "Pudding^";
     private static final String INVALID_TAG = "#nice";
+    private static final String INVALID_COUNT_1 = "sweet";
+    private static final String INVALID_COUNT_2 = "-1";
+    private static final String INVALID_Id = "abc";
+    private static final String INVALID_Id_2 = "-1";
+    private static final String INVALID_Id_3 = "123";
 
     private static final String VALID_NAME = "Pudding";
     private static final String VALID_TAG_1 = "nice";
     private static final String VALID_TAG_2 = "sweet";
+    private static final String VALID_COUNT_1 = "2";
+    private static final String VALID_COUNT_2 = "12";
+    private static final String VALID_Id_1 = "223131";
+    private static final String VALID_Id_2 = "002489";
 
     private static final String WHITESPACE = " \t\r\n";
 
@@ -114,5 +123,68 @@ public class ParserUtilTest {
         Set<Tag> expectedTagSet = new HashSet<Tag>(Arrays.asList(new Tag(VALID_TAG_1), new Tag(VALID_TAG_2)));
 
         assertEquals(expectedTagSet, actualTagSet);
+    }
+
+    @Test
+    public void parseCount_null_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseCount(null));
+    }
+
+    @Test
+    public void parseCount_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseCount(INVALID_COUNT_1));
+    }
+
+    @Test
+    public void parseCount_negativeNumber_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseCount(INVALID_COUNT_2));
+    }
+
+    @Test
+    public void parseCount_validValue_returnsCount() throws Exception {
+        Integer expectedCount = Integer.parseInt(VALID_COUNT_1);
+        Integer actualCount = ParserUtil.parseCount(VALID_COUNT_1);
+        assertEquals(expectedCount, actualCount);
+    }
+
+    @Test
+    public void parseCount_validValue2_returnsCount() throws Exception {
+        Integer expectedCount = Integer.parseInt(VALID_COUNT_2);
+        Integer actualCount = ParserUtil.parseCount(VALID_COUNT_2);
+        assertEquals(expectedCount, actualCount);
+    }
+
+    @Test
+    public void parseId_null_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseId(null));
+    }
+
+    @Test
+    public void parseId_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseId(INVALID_Id));
+    }
+
+    @Test
+    public void parseId_negativeValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseId(INVALID_Id_2));
+    }
+
+    @Test
+    public void parseId_notSixDigits_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseId(INVALID_Id_3));
+    }
+
+    @Test
+    public void parseId_validId_returnsId() throws Exception {
+        String expectedId = VALID_Id_1;
+        String actualId = ParserUtil.parseId(VALID_Id_1);
+        assertEquals(expectedId, actualId);
+    }
+
+    @Test
+    public void parseId_validId2_returnsId() throws Exception {
+        String expectedId = VALID_Id_2;
+        String actualId = ParserUtil.parseId(VALID_Id_2);
+        assertEquals(expectedId, actualId);
     }
 }
