04dd8544042fae24946f857d68bb7aeafc02eea3
Haozeee <teyhaoze@gmail.com>
2021-10-09 23:02:55 +0800

    Update comments

--- src/test/java/seedu/plannermd/model/person/PersonTest.java
+++ src/test/java/seedu/plannermd/model/person/PersonTest.java
@@ -49,11 +49,11 @@ public class PersonTest {
         editedAlice = new PersonBuilder(ALICE).withEmail(VALID_EMAIL_BOB).build();
         assertFalse(ALICE.isSamePerson(editedAlice));
 
-        // name differs in case, all other attributes same -> returns false
+        // name differs in case, all other attributes same -> returns true
         Person editedBob = new PersonBuilder(BOB).withName(VALID_NAME_BOB.toLowerCase()).build();
         assertTrue(BOB.isSamePerson(editedBob));
 
-        // email differs in case, all other attributes same -> returns false
+        // email differs in case, all other attributes same -> returns true
         editedBob = new PersonBuilder(BOB).withEmail(VALID_EMAIL_BOB.toUpperCase()).build();
         assertTrue(BOB.isSamePerson(editedBob));
 
