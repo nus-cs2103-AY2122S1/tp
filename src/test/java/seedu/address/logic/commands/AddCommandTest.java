7f3f716ab59ac0be72c2a5dffeeddab10196d604
Timothyoung97 <timothyoung97@gmail.com>
2021-10-09 23:38:05 +0800

    Modify AddCommandTest's ModelStub

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -150,6 +150,16 @@ public class AddCommandTest {
         }
 
         @Override
+        public ObservableList<Lesson> getFilteredLessonList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void updateFilteredLessonList(Predicate<Lesson> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public boolean hasLesson(Lesson lesson) {
             throw new AssertionError("This method should not be called.");
         }
@@ -161,11 +171,6 @@ public class AddCommandTest {
 
         @Override
         public void addLesson(Lesson lesson) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public ObservableList<Lesson> getLessonList() {
             throw new AssertionError("This method should not be called.");
         }
 
