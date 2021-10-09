0497f2cf5017c4ffa93fa3e4a86af3355915472f
DESKTOP-L6MVVEE\Sanguine <e0543860@u.nus.edu>
2021-10-09 18:40:39 +0800

    Fix checkstyle issues

--- src/main/java/seedu/address/ui/TaskListPanel.java
+++ src/main/java/seedu/address/ui/TaskListPanel.java
@@ -1,5 +1,6 @@
 package seedu.address.ui;
 
+import java.util.logging.Logger;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
@@ -8,7 +9,6 @@ import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.task.Task;
 
-import java.util.logging.Logger;
 
 /**
  * Panel containing the list of persons.
