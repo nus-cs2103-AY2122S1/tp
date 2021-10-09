f1fc74e3c5c7a83e2a07d190a00e21b08f9a914e
charltonator <charltonlucian@gmail.com>
2021-10-09 16:19:05 +0800

    Add FindIdCommand and FindIdCommand tests

---
+++ src/test/java/seedu/address/logic/commands/FindIdCommandTest.java
@@ -0,0 +1,81 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.commons.core.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.StudentIdContainsKeywordsPredicate;
+
+/**
+ * Contains integration tests (interaction with the Model) for {@code FindCommand}.
+ */
+public class FindIdCommandTest {
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void equals() {
+        StudentIdContainsKeywordsPredicate firstPredicate =
+                new StudentIdContainsKeywordsPredicate(Collections.singletonList("first"));
+        StudentIdContainsKeywordsPredicate secondPredicate =
+                new StudentIdContainsKeywordsPredicate(Collections.singletonList("second"));
+
+        FindIdCommand findFirstCommand = new FindIdCommand(firstPredicate);
+        FindIdCommand findSecondCommand = new FindIdCommand(secondPredicate);
+
+        // same object -> returns true
+        assertTrue(findFirstCommand.equals(findFirstCommand));
+
+        // same values -> returns true
+        FindIdCommand findFirstCommandCopy = new FindIdCommand(firstPredicate);
+        assertTrue(findFirstCommand.equals(findFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(findFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(findFirstCommand.equals(null));
+
+        // different person -> returns false
+        assertFalse(findFirstCommand.equals(findSecondCommand));
+    }
+
+    @Test
+    public void execute_zeroKeywords_noPersonFound() {
+        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 0);
+        StudentIdContainsKeywordsPredicate predicate = preparePredicate(" ");
+        FindIdCommand command = new FindIdCommand(predicate);
+        expectedModel.updateFilteredPersonList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Collections.emptyList(), model.getFilteredPersonList());
+    }
+
+    @Test
+    public void execute_multipleKeywords_multiplePersonsFound() {
+        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 1);
+        StudentIdContainsKeywordsPredicate predicate = preparePredicate("A1234567A");
+        FindIdCommand command = new FindIdCommand(predicate);
+        expectedModel.updateFilteredPersonList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Arrays.asList(ALICE), model.getFilteredPersonList());
+    }
+
+    /**
+     * Parses {@code userInput} into a {@code StudentIdContainsKeywordsPredicate}.
+     */
+    private StudentIdContainsKeywordsPredicate preparePredicate(String userInput) {
+        return new StudentIdContainsKeywordsPredicate(Arrays.asList(userInput.split("\\s+")));
+    }
+}
