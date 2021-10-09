26ff91b00af48a478d535fe62121cf6d453e2422
Xiu Wen <xiuwenseow@yahoo.com.sg>
2021-10-09 12:51:55 +0800

    Update equals method for model manager and addressbook

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -142,7 +142,8 @@ public class AddressBook implements ReadOnlyAddressBook {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof AddressBook // instanceof handles nulls
-                && persons.equals(((AddressBook) other).persons));
+                && persons.equals(((AddressBook) other).persons)
+                && facilities.equals(((AddressBook) other).facilities));
     }
 
     @Override
