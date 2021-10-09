0e68d52e8ede3e95f958cb93aee5d89226e23e98
kflim <redbulldown@gmail.com>
2021-10-10 01:35:04 +0800

    Fix import order

--- src/test/java/seedu/address/logic/commands/SortCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -1,9 +1,9 @@
 package seedu.address.logic.commands;
 
-import org.junit.jupiter.api.Test;
-
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
 
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
