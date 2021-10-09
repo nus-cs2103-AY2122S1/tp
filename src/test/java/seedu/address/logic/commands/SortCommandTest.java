2e06147922f0c76fbdb20e2f89334c65dea340f1
kflim <redbulldown@gmail.com>
2021-10-10 01:32:30 +0800

    Fix checkstyle errors

--- src/test/java/seedu/address/logic/commands/SortCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -1,18 +1,20 @@
 package seedu.address.logic.commands;
 
 import org.junit.jupiter.api.Test;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 public class SortCommandTest {
     private final Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
-    public void execute_sort_personList_success() {
+    public void execute_sortPersonList_success() {
         SortCommand sortCommand = new SortCommand(true);
         String expectedMessage = "Sorted all persons in reverse order";
         Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
--- src/test/java/seedu/address/ui/HelpWindowTest.java
+++ src/test/java/seedu/address/ui/HelpWindowTest.java
@@ -1,13 +1,13 @@
 package seedu.address.ui;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 public class HelpWindowTest {
 
     @Test
-    public void execute_outOfThread_initialization_failure() {
+    public void execute_outOfThreadInitialization_throwsError() {
         boolean isThrown = false;
         try {
             HelpWindow helpWindow = HelpWindow.getWindow();
