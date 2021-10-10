d78a9bf23d04f63fba6df654090c3c83e15d2aa7
noobmaster19 <dionneo123@gmail.com>
2021-10-11 00:29:27 +0800

    fixed-styling

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -160,7 +160,7 @@ public class EditCommand extends Command {
          * Returns true if at least one field is edited.
          */
         public boolean isAnyFieldEdited() {
-            return CollectionUtil.isAnyNonNull(name, phone, email, address,role,faculty,major, tags);
+            return CollectionUtil.isAnyNonNull(name, phone, email, address, role, faculty, major, tags);
         }
 
         public void setName(Name name) {
