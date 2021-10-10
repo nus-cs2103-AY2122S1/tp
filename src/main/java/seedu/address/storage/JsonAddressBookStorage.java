ff5299f2a70da4aa49952391b1c1de60f5959aeb
Tan Jia Xian <e0543981@u.nus.edu>
2021-10-10 22:50:39 +0800

    Update saveAddressBook to encrypt json

--- src/main/java/seedu/address/storage/JsonAddressBookStorage.java
+++ src/main/java/seedu/address/storage/JsonAddressBookStorage.java
@@ -2,10 +2,22 @@ package seedu.address.storage;
 
 import static java.util.Objects.requireNonNull;
 
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.nio.file.Path;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Optional;
 import java.util.logging.Logger;
+
+import javax.crypto.Cipher;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SealedObject;
+import javax.crypto.spec.SecretKeySpec;
 
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.exceptions.DataConversionException;
@@ -74,6 +86,25 @@ public class JsonAddressBookStorage implements AddressBookStorage {
         requireNonNull(filePath);
 
         FileUtil.createIfMissing(filePath);
+
+        Cipher cipher;
+        try {
+            cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            byte[] keyBytes = new byte[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+            String algorithm = "AES";
+            SecretKeySpec key = new SecretKeySpec(keyBytes, algorithm);
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            SealedObject sealedObject =
+                    new SealedObject(JsonUtil.toJsonString(new JsonSerializableAddressBook(addressBook)), cipher);
+            CipherOutputStream cipherOutputStream =
+                    new CipherOutputStream(new BufferedOutputStream(new FileOutputStream("data/file")), cipher);
+            ObjectOutputStream outputStream = new ObjectOutputStream(cipherOutputStream);
+            outputStream.writeObject(sealedObject);
+            outputStream.close();
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException e) {
+            System.out.println(e);
+        }
+
         JsonUtil.saveJsonFile(new JsonSerializableAddressBook(addressBook), filePath);
     }
 
