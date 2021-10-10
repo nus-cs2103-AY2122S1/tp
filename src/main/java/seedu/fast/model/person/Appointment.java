d0d345b22c5f2dee66bcda34a2fa847f03fa1f6f
Py0000 <>
2021-10-10 18:41:16 +0800

    Add venue field

--- src/main/java/seedu/fast/model/person/Appointment.java
+++ src/main/java/seedu/fast/model/person/Appointment.java
@@ -11,19 +11,24 @@ import java.util.Objects;
 public class Appointment {
     public static final String NO_APPOINTMENT = "No Appointment Scheduled Yet";
     public static final String NO_TIME = "";
+    public static final String NO_VENUE = "";
 
     private final String date;
     private final String time;
+    private final String venue;
 
     /**
      * Constructs an {@code Appointment}.
      *
      * @param date A valid date.
+     * @param time A valid time.
+     * @param venue A venue.
      */
-    public Appointment(String date, String time) {
+    public Appointment(String date, String time, String venue) {
         requireNonNull(date);
         this.date = date;
         this.time = time;
+        this.venue = venue;
     }
 
     /**
@@ -41,7 +46,28 @@ public class Appointment {
      * @return A string representing the time of the appointment.
      */
     public String getTime() {
+        if (this.time.equals(NO_TIME)) {
+            return "";
+        }
+
+        if (!this.time.contains("hrs")) {
+            return this.time + "hrs";
+        }
+
         return this.time;
+    }
+
+    /**
+     * Returns a string representing the venue of the appointment.
+     *
+     * @return A string representing the venue of the appointment.
+     */
+    public String getVenue() {
+        if (this.venue.equals(NO_VENUE)) {
+            return "";
+        }
+
+        return this.venue;
     }
 
     @Override
@@ -56,13 +82,14 @@ public class Appointment {
 
         Appointment otherAppt = (Appointment) other;
         return otherAppt.getDate().equals(getDate())
-                && otherAppt.getTime().equals(getTime());
+                && otherAppt.getTime().equals(getTime())
+                && otherAppt.getVenue().equals(getVenue());
     }
 
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(date, time);
+        return Objects.hash(date, time, venue);
     }
 
     @Override
@@ -70,14 +97,11 @@ public class Appointment {
         final StringBuilder builder = new StringBuilder();
 
         builder.append("Appointment: ")
-                .append(this.getDate());
-
-        // displays the string of the appointment time if it is not empty
-        if (!this.getTime().equals("")) {
-                builder.append(" ")
-                        .append(this.getTime())
-                        .append("hrs");
-        }
+                .append(this.getDate())
+                .append(" ")
+                .append(this.getTime())
+                .append(" ")
+                .append(this.getVenue());
 
         return builder.toString();
     }
