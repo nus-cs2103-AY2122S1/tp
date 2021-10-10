3972843f3cf8c596b624507b5979ccea54911838
erwinqxy <erwin.quek.xian.yi@gmail.com>
2021-10-10 17:16:06 +0800

    Update LogicManager.java

--- src/main/java/seedu/programmer/logic/LogicManager.java
+++ src/main/java/seedu/programmer/logic/LogicManager.java
@@ -26,7 +26,7 @@ public class LogicManager implements Logic {
 
     private final Model model;
     private final Storage storage;
-    private final ProgrammerErrorParser ProgrammerErrorParser;
+    private final ProgrammerErrorParser programmerErrorParser;
 
     /**
      * Constructs a {@code LogicManager} with the given {@code Model} and {@code Storage}.
@@ -34,7 +34,7 @@ public class LogicManager implements Logic {
     public LogicManager(Model model, Storage storage) {
         this.model = model;
         this.storage = storage;
-        ProgrammerErrorParser = new ProgrammerErrorParser();
+        programmerErrorParser = new ProgrammerErrorParser();
     }
 
     @Override
@@ -42,7 +42,7 @@ public class LogicManager implements Logic {
         logger.info("----------------[USER COMMAND][" + commandText + "]");
 
         CommandResult commandResult;
-        Command command = ProgrammerErrorParser.parseCommand(commandText);
+        Command command = programmerErrorParser.parseCommand(commandText);
         commandResult = command.execute(model);
 
         try {
