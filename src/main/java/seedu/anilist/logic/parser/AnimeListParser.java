5c7aa67d32199bfbd9b9c2bca891febfd2db7811
auhc99 <auhc99@gmail.com>
2021-10-10 21:32:07 +0800

    Remove EditCommand COMMAND_WORD
    Removed EditCommand COMMAND_WORD from parser

--- src/main/java/seedu/anilist/logic/parser/AnimeListParser.java
+++ src/main/java/seedu/anilist/logic/parser/AnimeListParser.java
@@ -10,7 +10,6 @@ import seedu.anilist.logic.commands.AddCommand;
 import seedu.anilist.logic.commands.ClearCommand;
 import seedu.anilist.logic.commands.Command;
 import seedu.anilist.logic.commands.DeleteCommand;
-import seedu.anilist.logic.commands.EditCommand;
 import seedu.anilist.logic.commands.ExitCommand;
 import seedu.anilist.logic.commands.FindCommand;
 import seedu.anilist.logic.commands.HelpCommand;
@@ -46,9 +45,6 @@ public class AnimeListParser {
 
         case AddCommand.COMMAND_WORD:
             return new AddCommandParser().parse(arguments);
-
-        case EditCommand.COMMAND_WORD:
-            return new EditCommandParser().parse(arguments);
 
         case DeleteCommand.COMMAND_WORD:
             return new DeleteCommandParser().parse(arguments);
