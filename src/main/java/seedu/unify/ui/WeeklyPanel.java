27a478866450e4a30335bcf2f228b0652ea74c6d
KelvinSoo <e0544272@u.nus.edu.sg>
2021-10-10 20:14:52 +0800

    Fix checkstyle error

--- src/main/java/seedu/unify/ui/WeeklyPanel.java
+++ src/main/java/seedu/unify/ui/WeeklyPanel.java
@@ -37,8 +37,8 @@ public class WeeklyPanel extends UiPart<Region> {
             VBox placeHolderVBox = new VBox();
             placeHolderLabel.setText(indexToDay(i));
             placeHolderVBox.getChildren().add(placeHolderLabel);
-            placeHolderVBox.setStyle("-fx-border-style: solid inside;" +
-                    "-fx-border-width: 1;");
+            placeHolderVBox.setStyle("-fx-border-style: solid inside;"
+                    + "-fx-border-width: 1;");
             dailyHBox.widthProperty().addListener(e -> placeHolderVBox.setPrefWidth(dailyHBox.getWidth() / 7));
             dailyHBox.getChildren().add(0, placeHolderVBox);
         }
