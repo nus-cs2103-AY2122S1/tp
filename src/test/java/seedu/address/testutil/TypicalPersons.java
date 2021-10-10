9bedf85bc0fca0b44b374e4151e3cca5f59f90d7
xianlin <xianlinchng@gmail.com>
2021-10-10 13:23:52 +0800

    Improve test data for add and edit commands

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -30,32 +30,32 @@ public class TypicalPersons {
     public static final Person BENSON = new PersonBuilder().withName("Benson Meier")
             .withAddress("311, Clementi Ave 2, #02-25")
             .withEmail("johnd@example.com").withPhone("98765432")
-            .withTags("owesMoney", "friends").withNote("Has asthma").build();
+            .withTags("owesMoney", "friends").withNote("Has cancer").build();
     public static final Person CARL = new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
-            .withEmail("heinz@example.com").withAddress("wall street").withNote("Has asthma").build();
+            .withEmail("heinz@example.com").withAddress("wall street").withNote("Has dementia").build();
     public static final Person DANIEL = new PersonBuilder().withName("Daniel Meier").withPhone("87652533")
             .withEmail("cornelia@example.com").withAddress("10th street")
-            .withTags("friends").withNote("Has asthma").build();
+            .withTags("friends").withNote("Has diabetes").build();
     public static final Person ELLE = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
-            .withEmail("werner@example.com").withAddress("michegan ave").withNote("Has asthma").build();
+            .withEmail("werner@example.com").withAddress("michegan ave").withNote("Has wrist pain").build();
     public static final Person FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
-            .withEmail("lydia@example.com").withAddress("little tokyo").withNote("Has asthma").build();
+            .withEmail("lydia@example.com").withAddress("little tokyo").withNote("Has migraines").build();
     public static final Person GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
-            .withEmail("anna@example.com").withAddress("4th street").withNote("Has asthma").build();
+            .withEmail("anna@example.com").withAddress("4th street").withNote("Has covid").build();
 
     // Manually added
     public static final Person HOON = new PersonBuilder().withName("Hoon Meier").withPhone("8482424")
-            .withEmail("stefan@example.com").withAddress("little india").withNote("Has asthma").build();
+            .withEmail("stefan@example.com").withAddress("little india").withNote("Has heart disease").build();
     public static final Person IDA = new PersonBuilder().withName("Ida Mueller").withPhone("8482131")
-            .withEmail("hans@example.com").withAddress("chicago ave").withNote("Has asthma").build();
+            .withEmail("hans@example.com").withAddress("chicago ave").withNote("Has had a hip replacement").build();
 
     // Manually added - Person's details found in {@code CommandTestUtil}
     public static final Person AMY = new PersonBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY)
             .withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY)
-            .withTags(VALID_TAG_FRIEND).withNote("Has asthma").build();
+            .withTags(VALID_TAG_FRIEND).withNote("").build();
     public static final Person BOB = new PersonBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
             .withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND)
-            .withNote("Has asthma").build();
+            .withNote("").build();
 
     public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
 
