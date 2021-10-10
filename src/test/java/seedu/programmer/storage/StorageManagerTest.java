807914bed1a22e33307e26d7043e1440be563bf0
erwinqxy <erwin.quek.xian.yi@gmail.com>
2021-10-10 19:27:36 +0800

    Update StorageManagerTest.java

--- src/test/java/seedu/programmer/storage/StorageManagerTest.java
+++ src/test/java/seedu/programmer/storage/StorageManagerTest.java
@@ -1,68 +1,68 @@
-//package seedu.programmer.storage;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertNotNull;
-//import static seedu.programmer.testutil.TypicalPersons.getTypicalAddressBook;
-//
-//import java.nio.file.Path;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.io.TempDir;
-//
-//import seedu.programmer.commons.core.GuiSettings;
-//import seedu.programmer.model.AddressBook;
-//import seedu.programmer.model.ReadOnlyAddressBook;
-//import seedu.programmer.model.UserPrefs;
-//
-//public class StorageManagerTest {
-//
-//    @TempDir
-//    public Path testFolder;
-//
-//    private StorageManager storageManager;
-//
-//    @BeforeEach
-//    public void setUp() {
-//        JsonProgrammerErrorStorage addressBookStorage = new JsonProgrammerErrorStorage(getTempFilePath("ab"));
-//        JsonUserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(getTempFilePath("prefs"));
-//        storageManager = new StorageManager(addressBookStorage, userPrefsStorage);
-//    }
-//
-//    private Path getTempFilePath(String fileName) {
-//        return testFolder.resolve(fileName);
-//    }
-//
-//    @Test
-//    public void prefsReadSave() throws Exception {
-//        /*
-//         * Note: This is an integration test that verifies the StorageManager is properly wired to the
-//         * {@link JsonUserPrefsStorage} class.
-//         * More extensive testing of UserPref saving/reading is done in {@link JsonUserPrefsStorageTest} class.
-//         */
-//        UserPrefs original = new UserPrefs();
-//        original.setGuiSettings(new GuiSettings(300, 600, 4, 6));
-//        storageManager.saveUserPrefs(original);
-//        UserPrefs retrieved = storageManager.readUserPrefs().get();
-//        assertEquals(original, retrieved);
-//    }
-//
-//    @Test
-//    public void addressBookReadSave() throws Exception {
-//        /*
-//         * Note: This is an integration test that verifies the StorageManager is properly wired to the
-//         * {@link JsonProgrammerErrorStorage} class.
-//         * More extensive testing of UserPref saving/reading is done in {@link JsonAddressBookStorageTest} class.
-//         */
-//        AddressBook original = getTypicalAddressBook();
-//        storageManager.saveAddressBook(original);
-//        ReadOnlyAddressBook retrieved = storageManager.readAddressBook().get();
-//        assertEquals(original, new AddressBook(retrieved));
-//    }
-//
-//    @Test
-//    public void getAddressBookFilePath() {
-//        assertNotNull(storageManager.getAddressBookFilePath());
-//    }
-//
-//}
+package seedu.programmer.storage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static seedu.programmer.testutil.TypicalPersons.getTypicalAddressBook;
+
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import seedu.programmer.commons.core.GuiSettings;
+import seedu.programmer.model.ProgrammerError;
+import seedu.programmer.model.ReadOnlyProgrammerError;
+import seedu.programmer.model.UserPrefs;
+
+public class StorageManagerTest {
+
+    @TempDir
+    public Path testFolder;
+
+    private StorageManager storageManager;
+
+    @BeforeEach
+    public void setUp() {
+        JsonProgrammerErrorStorage programmerErrorStorage = new JsonProgrammerErrorStorage(getTempFilePath("ab"));
+        JsonUserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(getTempFilePath("prefs"));
+        storageManager = new StorageManager(programmerErrorStorage, userPrefsStorage);
+    }
+
+    private Path getTempFilePath(String fileName) {
+        return testFolder.resolve(fileName);
+    }
+
+    @Test
+    public void prefsReadSave() throws Exception {
+        /*
+         * Note: This is an integration test that verifies the StorageManager is properly wired to the
+         * {@link JsonUserPrefsStorage} class.
+         * More extensive testing of UserPref saving/reading is done in {@link JsonUserPrefsStorageTest} class.
+         */
+        UserPrefs original = new UserPrefs();
+        original.setGuiSettings(new GuiSettings(300, 600, 4, 6));
+        storageManager.saveUserPrefs(original);
+        UserPrefs retrieved = storageManager.readUserPrefs().get();
+        assertEquals(original, retrieved);
+    }
+
+    @Test
+    public void programmerErrorReadSave() throws Exception {
+        /*
+         * Note: This is an integration test that verifies the StorageManager is properly wired to the
+         * {@link JsonProgrammerErrorStorage} class.
+         * More extensive testing of UserPref saving/reading is done in {@link JsonAddressBookStorageTest} class.
+         */
+        ProgrammerError original = getTypicalAddressBook();
+        storageManager.saveProgrammerError(original);
+        ReadOnlyProgrammerError retrieved = storageManager.readProgrammerError().get();
+        assertEquals(original, new ProgrammerError(retrieved));
+    }
+
+    @Test
+    public void getProgrammerErrorFilePath() {
+        assertNotNull(storageManager.getProgrammerErrorFilePath());
+    }
+
+}
