564fbabfdaf3f5b77037eac349556faba9780b1a
jeremy-7007 <71486951+jeremy-7007@users.noreply.github.com>
2021-10-12 02:49:01 +0800

    Fixes import order (hopefully)

--- src/main/java/seedu/address/logic/parser/tasks/AssignTaskToPersonCommandParser.java
+++ src/main/java/seedu/address/logic/parser/tasks/AssignTaskToPersonCommandParser.java
@@ -1,13 +1,13 @@
 package seedu.address.logic.parser.tasks;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.tasks.AssignTaskToPersonCommand;
 import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.ParserUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
-
-import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 public class AssignTaskToPersonCommandParser implements Parser<AssignTaskToPersonCommand> {
 
--- src/main/java/seedu/address/logic/parser/tasks/TaskCommandsParser.java
+++ src/main/java/seedu/address/logic/parser/tasks/TaskCommandsParser.java
@@ -1,15 +1,15 @@
 package seedu.address.logic.parser.tasks;
 
-import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.tasks.AssignTaskToPersonCommand;
-import seedu.address.logic.parser.exceptions.ParseException;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.tasks.AssignTaskToPersonCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
 
 public class TaskCommandsParser {
 
