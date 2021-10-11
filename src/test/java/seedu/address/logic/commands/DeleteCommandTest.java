efecbc0191c5e297476fb28b61c4989512b43bf9
Chua Kai Jun <chuakaijun.ckj@gmail.com>
2021-10-12 03:13:47 +0800

    Add tests for DeleteCommand to cover deleting all and all shown

--- src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
@@ -5,14 +5,19 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.logic.commands.CommandTestUtil.showPersons;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -77,9 +82,92 @@ public class DeleteCommandTest {
     }
 
     @Test
+    public void execute_deleteAllUnfilteredList_success() {
+        DeleteCommand deleteCommand = DeleteCommand.all();
+
+        String expectedMessage = DeleteCommand.MESSAGE_DELETE_ALL_PERSONS_SUCCESS;
+
+        Model expectedModel = new ModelManager(new AddressBook(), new UserPrefs());
+        showNoPerson(expectedModel);
+
+        assertCommandSuccess(deleteCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_deleteAllFilteredList_success() {
+        DeleteCommand deleteCommand = DeleteCommand.all();
+
+        showAllPersons(model);
+        List<Person> persons = new ArrayList<>(model.getFilteredPersonList());
+
+        showPersons(model, persons.get(0), persons.get(1), persons.get(2));
+
+        assertTrue(model.getFilteredPersonList().size() == 3);
+
+        String expectedMessage = DeleteCommand.MESSAGE_DELETE_ALL_PERSONS_SUCCESS;
+
+        ModelManager expectedModel = new ModelManager(new AddressBook(), new UserPrefs());
+
+        assertCommandSuccess(deleteCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_deleteAllShownUnfilteredList_success() {
+        DeleteCommand deleteCommand = DeleteCommand.allShown();
+
+        showAllPersons(model);
+        List<Person> persons = new ArrayList<>(model.getFilteredPersonList());
+
+        StringBuilder resultSb = new StringBuilder(DeleteCommand.MESSAGE_DELETE_ALL_SHOWN_PERSONS_SUCCESS);
+        for (Person person : persons) {
+            resultSb.append("\n");
+            resultSb.append(person);
+        }
+
+        String expectedMessage = String.format(resultSb.toString(), persons.size());
+
+        ModelManager expectedModel = new ModelManager(new AddressBook(), new UserPrefs());
+
+        assertCommandSuccess(deleteCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_deleteAllShownFilteredList_success() {
+        DeleteCommand deleteCommand = DeleteCommand.allShown();
+
+        showAllPersons(model);
+        List<Person> persons = new ArrayList<>(model.getFilteredPersonList());
+
+        Person[] personsToDelete = {persons.get(0), persons.get(1), persons.get(2)} ;
+
+        showPersons(model, personsToDelete);
+
+        System.out.println(model.getFilteredPersonList());
+
+        StringBuilder resultSb = new StringBuilder(DeleteCommand.MESSAGE_DELETE_ALL_SHOWN_PERSONS_SUCCESS);
+        for (Person person : personsToDelete) {
+            resultSb.append("\n");
+            resultSb.append(person);
+        }
+
+        String expectedMessage = String.format(resultSb.toString(), personsToDelete.length);
+
+        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        for (Person person : personsToDelete) {
+            expectedModel.deletePerson(person);
+        }
+        showNoPerson(expectedModel);
+
+        assertCommandSuccess(deleteCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
     public void equals() {
         DeleteCommand deleteFirstCommand = new DeleteCommand(INDEX_FIRST_PERSON);
         DeleteCommand deleteSecondCommand = new DeleteCommand(INDEX_SECOND_PERSON);
+
+        DeleteCommand deleteAllCommand = DeleteCommand.all();
+        DeleteCommand deleteAllShownCommand = DeleteCommand.allShown();
 
         // same object -> returns true
         assertTrue(deleteFirstCommand.equals(deleteFirstCommand));
@@ -87,6 +175,17 @@ public class DeleteCommandTest {
         // same values -> returns true
         DeleteCommand deleteFirstCommandCopy = new DeleteCommand(INDEX_FIRST_PERSON);
         assertTrue(deleteFirstCommand.equals(deleteFirstCommandCopy));
+
+        // same quantity -> returns true
+        DeleteCommand deleteAllCommandCopy = DeleteCommand.all();
+        assertTrue(deleteAllCommand.equals(deleteAllCommandCopy));
+        DeleteCommand deleteAllShownCommandCopy = DeleteCommand.allShown();
+        assertTrue(deleteAllShownCommand.equals(deleteAllShownCommandCopy));
+
+        // different quantity -> returns false
+        assertFalse(deleteFirstCommand.equals(deleteAllCommand));
+        assertFalse(deleteAllCommand.equals(deleteAllShownCommand));
+        assertFalse(deleteAllShownCommand.equals(deleteAllCommand));
 
         // different types -> returns false
         assertFalse(deleteFirstCommand.equals(1));
@@ -105,5 +204,15 @@ public class DeleteCommandTest {
         model.updateFilteredPersonList(p -> false);
 
         assertTrue(model.getFilteredPersonList().isEmpty());
+    }
+
+
+    /**
+     * Updates {@code model}'s filtered list to show everyone.
+     */
+    private void showAllPersons(Model model) {
+        model.updateFilteredPersonList(p -> true);
+
+        assertTrue(model.getFilteredPersonList().size() == model.getAddressBook().getPersonList().size());
     }
 }
