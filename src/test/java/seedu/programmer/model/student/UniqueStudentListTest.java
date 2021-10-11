d50465299b780f562cd3dfe315bc316ea5d325dc
Allard <AllardQuek@users.noreply.github.com>
2021-10-11 23:41:21 +0800

    Fix tests after refactoring Person to Student

---
+++ src/test/java/seedu/programmer/model/student/UniqueStudentListTest.java
@@ -0,0 +1,167 @@
+package seedu.programmer.model.student;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.programmer.testutil.Assert.assertThrows;
+import static seedu.programmer.logic.commands.CommandTestUtil.VALID_STUDENTID_BOB;
+import static seedu.programmer.testutil.TypicalStudents.ALICE;
+import static seedu.programmer.testutil.TypicalStudents.BOB;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.programmer.model.student.exceptions.DuplicateStudentException;
+import seedu.programmer.model.student.exceptions.StudentNotFoundException;
+import seedu.programmer.testutil.StudentBuilder;
+
+public class UniqueStudentListTest {
+
+    private final UniqueStudentList uniqueStudentList = new UniqueStudentList();
+
+    @Test
+    public void contains_nullStudent_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueStudentList.contains(null));
+    }
+
+    @Test
+    public void contains_StudentNotInList_returnsFalse() {
+        assertFalse(uniqueStudentList.contains(ALICE));
+    }
+
+    @Test
+    public void contains_StudentInList_returnsTrue() {
+        uniqueStudentList.add(ALICE);
+        assertTrue(uniqueStudentList.contains(ALICE));
+    }
+
+    @Test
+    public void contains_StudentWithSameIdentityFieldsInList_returnsFalse() {
+        uniqueStudentList.add(ALICE);
+        Student editedAlice = new StudentBuilder(ALICE).withStudentId(VALID_STUDENTID_BOB).build();
+        assertFalse(uniqueStudentList.contains(editedAlice)); // Same name, different student id considered different
+    }
+
+    @Test
+    public void add_nullStudent_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueStudentList.add(null));
+    }
+
+    @Test
+    public void add_duplicateStudent_throwsDuplicateStudentException() {
+        uniqueStudentList.add(ALICE);
+        assertThrows(DuplicateStudentException.class, () -> uniqueStudentList.add(ALICE));
+    }
+
+    @Test
+    public void setStudentnullTargetStudent_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueStudentList.setStudent(null, ALICE));
+    }
+
+    @Test
+    public void setStudentnullEditedStudent_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueStudentList.setStudent(ALICE, null));
+    }
+
+    @Test
+    public void setStudenttargetStudentNotInList_throwsStudentNotFoundException() {
+        assertThrows(StudentNotFoundException.class, () -> uniqueStudentList.setStudent(ALICE, ALICE));
+    }
+
+    @Test
+    public void setStudenteditedStudentIsSameStudent_success() {
+        uniqueStudentList.add(ALICE);
+        uniqueStudentList.setStudent(ALICE, ALICE);
+        UniqueStudentList expectedUniqueStudentList = new UniqueStudentList();
+        expectedUniqueStudentList.add(ALICE);
+        assertEquals(expectedUniqueStudentList, uniqueStudentList);
+    }
+
+    @Test
+    public void setStudenteditedStudentHasSameIdentity_success() {
+        uniqueStudentList.add(ALICE);
+        Student editedAlice = new StudentBuilder(ALICE).withStudentId(VALID_STUDENTID_BOB).build();
+        uniqueStudentList.setStudent(ALICE, editedAlice);
+        UniqueStudentList expectedUniqueStudentList = new UniqueStudentList();
+        expectedUniqueStudentList.add(editedAlice);
+        assertEquals(expectedUniqueStudentList, uniqueStudentList);
+    }
+
+    @Test
+    public void setStudenteditedStudentHasDifferentIdentity_success() {
+        uniqueStudentList.add(ALICE);
+        uniqueStudentList.setStudent(ALICE, BOB);
+        UniqueStudentList expectedUniqueStudentList = new UniqueStudentList();
+        expectedUniqueStudentList.add(BOB);
+        assertEquals(expectedUniqueStudentList, uniqueStudentList);
+    }
+
+    @Test
+    public void setStudenteditedStudentHasNonUniqueIdentity_throwsDuplicateStudentException() {
+        uniqueStudentList.add(ALICE);
+        uniqueStudentList.add(BOB);
+        assertThrows(DuplicateStudentException.class, () -> uniqueStudentList.setStudent(ALICE, BOB));
+    }
+
+    @Test
+    public void remove_nullStudent_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueStudentList.remove(null));
+    }
+
+    @Test
+    public void remove_StudentDoesNotExist_throwsStudentNotFoundException() {
+        assertThrows(StudentNotFoundException.class, () -> uniqueStudentList.remove(ALICE));
+    }
+
+    @Test
+    public void remove_existingStudent_removesStudent() {
+        uniqueStudentList.add(ALICE);
+        uniqueStudentList.remove(ALICE);
+        UniqueStudentList expectedUniqueStudentList = new UniqueStudentList();
+        assertEquals(expectedUniqueStudentList, uniqueStudentList);
+    }
+
+    @Test
+    public void setStudent_nullUniqueStudentList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueStudentList.setStudents((UniqueStudentList) null));
+    }
+
+    @Test
+    public void setStudent_uniqueStudentList_replacesOwnListWithProvidedUniqueStudentList() {
+        uniqueStudentList.add(ALICE);
+        UniqueStudentList expectedUniqueStudentList = new UniqueStudentList();
+        expectedUniqueStudentList.add(BOB);
+        uniqueStudentList.setStudents(expectedUniqueStudentList);
+        assertEquals(expectedUniqueStudentList, uniqueStudentList);
+    }
+
+    @Test
+    public void setStudent_nullList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueStudentList.setStudents((List<Student>) null));
+    }
+
+    @Test
+    public void setStudent_list_replacesOwnListWithProvidedList() {
+        uniqueStudentList.add(ALICE);
+        List<Student> StudentList = Collections.singletonList(BOB);
+        uniqueStudentList.setStudents(StudentList);
+        UniqueStudentList expectedUniqueStudentList = new UniqueStudentList();
+        expectedUniqueStudentList.add(BOB);
+        assertEquals(expectedUniqueStudentList, uniqueStudentList);
+    }
+
+    @Test
+    public void setStudent_listWithDuplicateStudents_throwsDuplicateStudentException() {
+        List<Student> listWithDuplicateStudents = Arrays.asList(ALICE, ALICE);
+        assertThrows(DuplicateStudentException.class, () -> uniqueStudentList.setStudents(listWithDuplicateStudents));
+    }
+
+    @Test
+    public void asUnmodifiableObservableList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, ()
+            -> uniqueStudentList.asUnmodifiableObservableList().remove(0));
+    }
+}
