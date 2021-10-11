4e88b34f4d191147154cd55df36c0a964f12fa59
Ryan Chung <timelord0830@gmail.com>
2021-10-12 00:21:10 +0800

    Add tests for Episode

---
+++ src/test/java/seedu/anilist/model/anime/EpisodeTest.java
@@ -0,0 +1,38 @@
+package seedu.anilist.model.anime;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.anilist.testutil.Assert.assertThrows;
+
+public class EpisodeTest {
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Episode(null));
+    }
+
+    @Test
+    public void constructor_invalidEpisode_throwsIllegalArgumentException() {
+        String invalidEpisode = "";
+        assertThrows(IllegalArgumentException.class, () -> new Episode(invalidEpisode));
+    }
+
+    @Test
+    public void isValidEpisode() {
+        // null name
+        assertThrows(NullPointerException.class, () -> Episode.isValidEpisode(null));
+
+        // invalid name
+        assertFalse(Episode.isValidEpisode("")); // empty string
+        assertFalse(Episode.isValidEpisode(" ")); // spaces only
+        assertFalse(Episode.isValidEpisode("^")); // only non-numeric characters
+        assertFalse(Episode.isValidEpisode("^123%")); // contains non-numeric characters
+        assertFalse(Episode.isValidEpisode("-1")); // negative numbers
+        assertFalse(Episode.isValidEpisode("3.0")); // float numbers
+
+        // valid name
+        assertTrue(Episode.isValidEpisode("12345")); // numbers only
+        assertTrue(Episode.isValidEpisode("0000")); // leading zeroes
+    }
+}
