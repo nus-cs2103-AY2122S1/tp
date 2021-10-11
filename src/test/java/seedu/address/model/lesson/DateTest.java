069bc5b052bca94f485cae2d968cb7585d3cc688
eeliana <62977276+eeliana@users.noreply.github.com>
2021-10-11 15:20:05 +0800

    Update tests for Date

--- src/test/java/seedu/address/model/lesson/DateTest.java
+++ src/test/java/seedu/address/model/lesson/DateTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
@@ -20,6 +21,55 @@ public class DateTest {
     public void constructor_invalidDate_throwsIllegalArgumentException() {
         String invalidDate = "";
         assertThrows(IllegalArgumentException.class, () -> new Date(invalidDate));
+    }
+
+
+    @Test
+    public void constructor_invalidDate2_throwsIllegalArgumentException() {
+        String invalidDate = "32 Dec 1999";
+        assertThrows(IllegalArgumentException.class, () -> new Date(invalidDate));
+    }
+
+    @Test
+    public void getDayOfTheWeek() {
+        // FRIDAY
+        assertEquals(new Date("8 Oct 2021").getDayOfWeek(), DayOfWeek.FRIDAY);
+
+        // MONDAY
+        assertEquals(new Date("12 jUL 2021").getDayOfWeek(), DayOfWeek.MONDAY);
+
+        // Different days
+        assertFalse(new Date("16 Feb 2022").getDayOfWeek().equals(DayOfWeek.FRIDAY));
+    }
+
+    @Test
+    public void isOver() {
+        // Date has passed
+        assertTrue(new Date("12 Jan 2000").isOver());
+        assertTrue(new Date("12 Feb 1950").isOver());
+        assertTrue(new Date("12 Jan 1324").isOver());
+
+        // Future Date
+        assertFalse(new Date("13 Mar 2100").isOver());
+        assertFalse(new Date("13 Mar 2156").isOver());
+        assertFalse(new Date("13 Mar 2345").isOver());
+    }
+
+    @Test
+    public void compareTo() {
+        Date date1 = new Date("24 Apr 2002");
+        Date date2 = new Date("13 Jul 2001");
+
+        // Same date
+        Date copyOfDate1 = new Date("24 Apr 2002");
+        assertEquals(0, date1.compareTo(copyOfDate1));
+
+        // different date
+        // other date is later
+        assertEquals(1, date1.compareTo(date2));
+
+        // other date is earlier
+        assertEquals(-1, date2.compareTo(date1));
     }
 
     @Test
