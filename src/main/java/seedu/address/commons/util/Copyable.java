d33910dd0f13c5fd57a9b69b4c7b0b14f7714b2f
Bryan Loh <lamofacade@gmail.com>
2021-10-12 00:19:33 +0800

    Add Copyable to represent duplicatable classes

---
+++ src/main/java/seedu/address/commons/util/Copyable.java
@@ -0,0 +1,12 @@
+package seedu.address.commons.util;
+
+/**
+ * Represents a Class that is able to duplicate its own instances.
+ */
+public interface Copyable<T> {
+    /**
+     * Returns a duplicate copy of the instance of an object.
+     * @return The duplicate copy.
+     */
+    T copy();
+}
