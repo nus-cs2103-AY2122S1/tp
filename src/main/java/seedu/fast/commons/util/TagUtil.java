96096b517a8792e55dbc6b1d5ccc350e9458291d
LauTengHon <e0543465@u.nus.edu>
2021-10-11 12:29:56 +0800

    Fix checkstyle issues

--- src/main/java/seedu/fast/commons/util/TagUtil.java
+++ src/main/java/seedu/fast/commons/util/TagUtil.java
@@ -5,8 +5,8 @@ package seedu.fast.commons.util;
  */
 public class TagUtil {
     public static final String FRIENDS = "FRIENDS";
-    public static final String  HIGH_PRIORITY = "HIGHPRIORITY";
-    public static final String  MEDIUM_PRIORITY = "MEDIUMPRIORITY";
-    public static final String  LOW_PRIORITY = "LOWPRIORITY";
+    public static final String HIGH_PRIORITY = "HIGHPRIORITY";
+    public static final String MEDIUM_PRIORITY = "MEDIUMPRIORITY";
+    public static final String LOW_PRIORITY = "LOWPRIORITY";
 
 }
