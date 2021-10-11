a2b157ec7d7f9b15c0628c5fcc506355abc87396
nbvannhi <nbvannhi@gmail.com>
2021-10-11 23:17:22 +0800

    Add Group tests

--- src/test/java/seedu/address/model/student/GroupTest.java
+++ src/test/java/seedu/address/model/student/GroupTest.java
@@ -4,7 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+
+import seedu.address.testutil.GroupBuilder;
+import seedu.address.testutil.IdBuilder;
 
 public class GroupTest {
 
@@ -15,20 +20,20 @@ public class GroupTest {
 
     @Test
     public void constructor_invalidGroupGroup_throwsIllegalArgumentException() {
-        String invalidGroup = "";
-        assertThrows(IllegalArgumentException.class, () -> new Group(invalidGroup));
+        String invalidValue = "";
+        assertThrows(IllegalArgumentException.class, () -> new Group(invalidValue));
     }
 
     @Test
     public void isValidGroup() {
-        // null Group number
+        // null Group name
         assertThrows(NullPointerException.class, () -> Group.isValidGroup(null));
 
-        // invalid Group numbers
+        // invalid Group name
         assertFalse(Group.isValidGroup("")); // empty string
         assertFalse(Group.isValidGroup(" ")); // spaces only
 
-        // valid Group numbers
+        // valid Group name
         assertTrue(Group.isValidGroup("T03C")); // upper case
         assertTrue(Group.isValidGroup("r05b")); // lower case
         assertTrue(Group.isValidGroup("r03A")); // mix of upper lower case
@@ -37,4 +42,23 @@ public class GroupTest {
         assertTrue(Group.isValidGroup("03")); // numbers only
     }
 
+    @Test
+    public void hasStudent_null_throwsNullPointerException() {
+        Group group = new GroupBuilder().build();
+        assertThrows(NullPointerException.class, () -> group.hasStudent(null));
+    }
+
+    @Test
+    public void hasStudent_notIncluded_throwsNullPointerException() {
+        ID id = new IdBuilder().build();
+        Group group = new GroupBuilder().build();
+        assertFalse(() -> group.hasStudent(id));
+    }
+
+    @Test
+    public void hasStudent_included_throwsNullPointerException() {
+        ID id = new IdBuilder().build();
+        Group group = new GroupBuilder().withStudents(List.of(id)).build();
+        assertTrue(() -> group.hasStudent(id));
+    }
 }
---
+++ src/test/java/seedu/address/testutil/GroupBuilder.java
@@ -0,0 +1,57 @@
+package seedu.address.testutil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import seedu.address.model.student.Group;
+import seedu.address.model.student.ID;
+
+/**
+ * A utility class to help with building Group objects.
+ */
+public class GroupBuilder {
+
+    public static final String DEFAULT_VALUE = "P01";
+
+    private List<ID> students;
+    private String value;
+
+    /**
+     * Creates a {@code GroupBuilder} with the default details.
+     */
+    public GroupBuilder() {
+        students = new ArrayList<>();
+        value = DEFAULT_VALUE;
+    }
+
+    /**
+     * Initializes the GroupBuilder with the data of {@code groupToCopy}.
+     */
+    public GroupBuilder(Group groupToCopy) {
+        students = groupToCopy.getStudents();
+        value = groupToCopy.getValue();
+    }
+
+    /**
+     * Sets the {@code students} of the {@code group} that we are building.
+     */
+    public GroupBuilder withStudents(List<ID> students) {
+        this.students = students;
+        return this;
+    }
+
+    /**
+     * Sets the {@code value} of the {@code group} that we are building.
+     */
+    public GroupBuilder withValue(String value) {
+        this.value = value;
+        return this;
+    }
+
+    public Group build() {
+        Group group = new Group(value);
+        group.getStudents().addAll(students);
+        return group;
+    }
+}
