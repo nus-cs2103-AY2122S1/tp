bd7221fdc47968ee2b5a9618b70e145085af119e
Bryan Loh <lamofacade@gmail.com>
2021-10-12 03:51:36 +0800

    Add tests for History class

--- src/test/java/seedu/address/logic/history/CopyableInt.java
+++ src/test/java/seedu/address/logic/history/CopyableInt.java
@@ -2,17 +2,36 @@ package seedu.address.logic.history;
 
 import seedu.address.commons.util.Copyable;
 
+/**
+ * A utility class that implements the Copyable interface.
+ */
 public class CopyableInt implements Copyable<CopyableInt> {
+    public static final CopyableInt COPYABLE_ZERO = new CopyableInt(0);
+    public static final CopyableInt COPYABLE_ONE = new CopyableInt(1);
+    public static final CopyableInt COPYABLE_TWO = new CopyableInt(2);
+    public static final CopyableInt COPYABLE_THREE = new CopyableInt(3);
+    public static final CopyableInt COPYABLE_FOUR = new CopyableInt(4);
+
     private final int number;
 
     protected CopyableInt(int number) {
         this.number = number;
     }
 
+    /**
+     * Gets the integer that is saved within the instance of CopyableInt.
+     *
+     * @return The saved integer.
+     */
     protected int getNumber() {
         return number;
     }
 
+    /**
+     * Creates a duplicate instance of the CopyableInt.
+     *
+     * @return The duplicate instance.
+     */
     @Override
     public CopyableInt copy() {
         return new CopyableInt(number);
---
+++ src/test/java/seedu/address/logic/history/HistoryTest.java
@@ -0,0 +1,64 @@
+package seedu.address.logic.history;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static seedu.address.logic.history.CopyableInt.COPYABLE_ONE;
+import static seedu.address.logic.history.CopyableInt.COPYABLE_THREE;
+import static seedu.address.logic.history.CopyableInt.COPYABLE_TWO;
+import static seedu.address.logic.history.CopyableInt.COPYABLE_ZERO;
+
+import org.junit.jupiter.api.Test;
+
+public class HistoryTest {
+    private Historyable<CopyableInt> history = new History<>();
+
+    protected HistoryTest() {
+        history.push(COPYABLE_ZERO);
+        history.push(COPYABLE_ONE);
+        history.push(COPYABLE_TWO);
+    }
+
+    @Test
+    void size_success() {
+        assertEquals(3, history.size());
+    }
+
+    @Test
+    void go_validInput_success() {
+        CopyableInt copyableOne = history.go(-1);
+        assertEquals(COPYABLE_ONE.getNumber(), copyableOne.getNumber());
+    }
+
+    @Test
+    void go_tooLargeInput_success() {
+        CopyableInt copyableTwo = history.go(100); // Should return the last pushed item.
+        assertEquals(COPYABLE_TWO.getNumber(), copyableTwo.getNumber());
+    }
+
+    @Test
+    void go_tooSmallInput_success() {
+        CopyableInt copyableZero = history.go(-100); // Should return the first pushed item.
+        assertEquals(COPYABLE_ZERO.getNumber(), copyableZero.getNumber());
+    }
+
+    @Test
+    void push_expected_success() {
+        history.push(COPYABLE_THREE);
+        CopyableInt currentCopyableInt = history.getCurrentState();
+        assertNotEquals(COPYABLE_THREE, currentCopyableInt);
+        assertEquals(COPYABLE_THREE.getNumber(), currentCopyableInt.getNumber());
+    }
+
+    @Test
+    void push_null_throwNullPointerException() {
+        assertThrows(NullPointerException.class, () -> history.push(null));
+    }
+
+    @Test
+    void pop_success() {
+        CopyableInt poppedCopyableInt = history.pop();
+        assertNotEquals(COPYABLE_TWO, poppedCopyableInt);
+        assertEquals(COPYABLE_TWO.getNumber(), poppedCopyableInt.getNumber());
+    }
+}
