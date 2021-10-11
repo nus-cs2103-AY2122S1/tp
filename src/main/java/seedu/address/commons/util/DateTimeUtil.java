f38b0d05b531f631c9595d0f96f2a5871c3d5ace
Ai Ling <hong-ailing@hotmail.com>
2021-10-12 02:49:39 +0800

    Add comments to DateTimeUtil class

--- src/main/java/seedu/address/commons/util/DateTimeUtil.java
+++ src/main/java/seedu/address/commons/util/DateTimeUtil.java
@@ -4,16 +4,23 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.ResolverStyle;
 
+/**
+ * Helper functions for handling datetime
+ */
 public class DateTimeUtil {
-    public static final String YEAR_REGEX = "\\d{4}";
-    public static final String MONTH_REGEX = "(0[1-9]|1[0-2])";
-    public static final String DAY_REGEX = "(0[1-9]|[12][0-9]|3[01])";
-    public static final String HOUR_REGEX = "([01][0-9]|2[0-4])";
-    public static final String MINUTE_REGEX = "([0-5][0-9]|60)";
-    public static final String VALIDATION_REGEX = "^" + YEAR_REGEX + "-" + MONTH_REGEX + "-" + DAY_REGEX
+    private static final String YEAR_REGEX = "\\d{4}";
+    private static final String MONTH_REGEX = "(0[1-9]|1[0-2])";
+    private static final String DAY_REGEX = "(0[1-9]|[12][0-9]|3[01])";
+    private static final String HOUR_REGEX = "([01][0-9]|2[0-4])";
+    private static final String MINUTE_REGEX = "([0-5][0-9]|60)";
+    private static final String VALIDATION_REGEX = "^" + YEAR_REGEX + "-" + MONTH_REGEX + "-" + DAY_REGEX
                                                       + " " + HOUR_REGEX + ":" + MINUTE_REGEX + "$";
 
     private static final String DATETIME_PARSE_FORMAT = "uuuu-MM-dd HH:mm";
+
+    /**
+     * Standardised datetime formatter which rejects invalid date and time.
+     */
     public static final DateTimeFormatter FORMATTER =
         DateTimeFormatter.ofPattern(DATETIME_PARSE_FORMAT).withResolverStyle(ResolverStyle.STRICT);
 
