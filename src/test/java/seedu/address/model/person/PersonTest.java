a025f0eae7d002f001a99daf899255875ea350e8
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-10-11 21:45:40 +0800

    Add test for Person Insurance inequality

--- src/test/java/seedu/address/model/person/PersonTest.java
+++ src/test/java/seedu/address/model/person/PersonTest.java
@@ -7,6 +7,7 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_INSURANCE_HEALTH;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BOB;
@@ -86,6 +87,10 @@ public class PersonTest {
 
         // different tags -> returns false
         editedAlice = new PersonBuilder(ALICE).withTags(VALID_TAG_HUSBAND).build();
+        assertFalse(ALICE.equals(editedAlice));
+
+        // different insurances -> returns false
+        editedAlice = new PersonBuilder(ALICE).withInsurances(VALID_INSURANCE_HEALTH).build();
         assertFalse(ALICE.equals(editedAlice));
     }
 }
