857fe8aed1fc0afe623ace95a4ae575dc74ce6d0
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-10-11 19:15:53 +0800

    Add Insurance to EditPersonDescriptorBuilder

--- src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
+++ src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
@@ -7,6 +7,7 @@ import java.util.stream.Stream;
 import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
+import seedu.address.model.person.Insurance;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
@@ -37,6 +38,7 @@ public class EditPersonDescriptorBuilder {
         descriptor.setEmail(person.getEmail());
         descriptor.setAddress(person.getAddress());
         descriptor.setTags(person.getTags());
+        descriptor.setInsurances(person.getInsurances());
     }
 
     /**
@@ -78,6 +80,17 @@ public class EditPersonDescriptorBuilder {
     public EditPersonDescriptorBuilder withTags(String... tags) {
         Set<Tag> tagSet = Stream.of(tags).map(Tag::new).collect(Collectors.toSet());
         descriptor.setTags(tagSet);
+        return this;
+    }
+
+    /**
+     * Parses the {@code insurances} into a {@code Set<Insurance>} and set it to the
+     * {@code EditPersonDescriptor} that we are building.
+     */
+    public EditPersonDescriptorBuilder withInsurances(String... insurances) {
+        Set<Insurance> insuranceSet = Stream.of(insurances).
+                map(Insurance::of).collect(Collectors.toSet());
+        descriptor.setInsurances(insuranceSet);
         return this;
     }
 
