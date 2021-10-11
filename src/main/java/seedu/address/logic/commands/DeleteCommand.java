3b868bf7f7a3bd803f86b5a681fa0c433913ac53
Chua Kai Jun <chuakaijun.ckj@gmail.com>
2021-10-12 03:18:30 +0800

    Update command message for delete

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -21,9 +21,9 @@ public class DeleteCommand extends Command {
     public static final String COMMAND_WORD = "delete";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Deletes the person identified by the index number used in the displayed person list.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " 1";
+            + ": Deletes the person identified by the index number used, or all persons, in the displayed person list.\n"
+            + "Parameters: INDEX (must be a positive integer) / -a (-f)\n"
+            + "Examples: " + COMMAND_WORD + " 1, " + COMMAND_WORD + " -a -f";
 
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
     public static final String MESSAGE_DELETE_ALL_PERSONS_SUCCESS = "Deleted all persons in address book";
