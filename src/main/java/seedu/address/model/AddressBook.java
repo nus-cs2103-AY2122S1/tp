b35970e2409cb062cabed18e410316c21bbcd2bb
Yukun99 <yukunxu123@gmail.com>
2021-10-11 11:01:41 +0800

    Fix Checkstyle Errors

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -19,13 +19,15 @@ public class AddressBook implements ReadOnlyAddressBook {
     private final UniqueList<Person> persons;
     private final UniqueList<Group> groups;
 
+    // @formatter:off
     /*
      * The 'unusual' code block below is a non-static initialization block, sometimes used to avoid duplication
      * between constructors. See https://docs.oracle.com/javase/tutorial/java/javaOO/initial.html
      *
      * Note that non-static init blocks are not recommended to use. There are other ways to avoid duplication
      *   among constructors.
-     */ {
+     */
+    {
         persons = new UniqueList<>();
         groups = new UniqueList<>();
     }
