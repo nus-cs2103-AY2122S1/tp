8a85864fa885c9e8064e60412657a6c12162a5f2
nbvannhi <nbvannhi@gmail.com>
2021-10-12 22:40:03 +0800

    Refine EditCommandTest

--- src/test/java/seedu/address/logic/commands/EditCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditCommandTest.java
@@ -160,7 +160,7 @@ public class EditCommandTest {
         assertEquals(standardCommand, standardCommand);
 
         // null -> returns false
-        assertNotEquals(null, standardCommand);
+        assertNotEquals(standardCommand, null);
 
         // different types -> returns false
         assertNotEquals(standardCommand, new ClearCommand());
