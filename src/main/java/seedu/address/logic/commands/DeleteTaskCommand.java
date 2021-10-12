953b7e1f300b40adacd27b9e5a452425143c8335
Win Lynn <ngaiwinlynn0323@gmail.com>
2021-10-12 21:21:26 +0800

    Change DeleteTaskCommand success message

--- src/main/java/seedu/address/logic/commands/DeleteTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteTaskCommand.java
@@ -19,7 +19,7 @@ public class DeleteTaskCommand extends Command {
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
-    public static final String MESSAGE_DELETE_TASK_SUCCESS = "Deleted Person: %1$s";
+    public static final String MESSAGE_DELETE_TASK_SUCCESS = "Deleted Task: %1$s";
 
     private final Index targetIndex;
 
