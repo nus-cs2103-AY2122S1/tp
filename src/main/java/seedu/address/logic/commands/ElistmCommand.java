9976b15de2d5daa699118c458128f83fe5982a0e
Leeroy Liu <leeroyliu999@gmail.com>
2021-10-13 04:18:55 +0800

    Fix minor checkstyle

--- src/main/java/seedu/address/logic/commands/ElistmCommand.java
+++ src/main/java/seedu/address/logic/commands/ElistmCommand.java
@@ -1,16 +1,16 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+import java.util.Set;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.data.event.Event;
 import seedu.address.model.data.member.Member;
-
-import java.util.List;
-import java.util.Set;
-
-import static java.util.Objects.requireNonNull;
 
 /**
  * Lists all members in Ailurus who are in the event.
