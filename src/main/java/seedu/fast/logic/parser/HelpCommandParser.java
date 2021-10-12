7f402a9a51600d6bb059f3179208acb84896ebc6
Cyn7hius <ctwx29@gmail.com>
2021-10-12 15:32:45 +0800

    Add comments

--- src/main/java/seedu/fast/logic/parser/HelpCommandParser.java
+++ src/main/java/seedu/fast/logic/parser/HelpCommandParser.java
@@ -9,7 +9,9 @@ public class HelpCommandParser implements Parser<HelpCommand> {
      * and returns a HelpCommand object for execution.
      */
     public HelpCommand parse(String args) {
-        return new HelpCommand(args); //Since none of the inputs are valid, we can return the arg.
+        // since the execution of the help command is delayed, we can just pass the original arg here
+        // and parse it later when the HelpWindow is called
+        return new HelpCommand(args);
     }
 
 
