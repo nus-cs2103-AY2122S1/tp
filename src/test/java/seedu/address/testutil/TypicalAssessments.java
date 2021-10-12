f90d11fa12154c189e83989f62300d680e76e410
nbvannhi <nbvannhi@gmail.com>
2021-10-13 00:13:36 +0800

    Update followed up changes of AssessmentBuilder

--- src/test/java/seedu/address/testutil/TypicalAssessments.java
+++ src/test/java/seedu/address/testutil/TypicalAssessments.java
@@ -14,7 +14,7 @@ public class TypicalAssessments {
     public static final Assessment PATH_1 = new AssessmentBuilder()
             .withValue("P01")
             .withScores(
-                    Map.of(new ID("E0543948"), new Score("100"),
-                            new ID("E0473477"), new Score("100"))
+                    Map.of("E0543948", "100",
+                            "E0473477", "100")
             ).build();
 }
