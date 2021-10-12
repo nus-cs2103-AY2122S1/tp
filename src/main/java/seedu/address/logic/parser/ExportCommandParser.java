2d4158a8706d858d9fa2a1e8099e5feaec7c3a66
rachelcheah <rachelcheah06@gmail.com>
2021-10-12 21:09:52 +0800

    Fixed checkstyle

--- src/main/java/seedu/address/logic/parser/ExportCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ExportCommandParser.java
@@ -5,7 +5,6 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 import java.nio.file.Path;
 
 import seedu.address.logic.commands.ExportCommand;
-import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
