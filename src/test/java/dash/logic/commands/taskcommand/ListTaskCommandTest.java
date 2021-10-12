7f1ca26f5b6bf3412db3fab9e8ad935f041b5c01
Zenlzb <leezenbeen@u.nus.edu>
2021-10-12 23:35:25 +0800

    Add tests for ListTaskCommand

---
+++ src/test/java/dash/logic/commands/taskcommand/ListTaskCommandTest.java
@@ -0,0 +1,38 @@
+package dash.logic.commands.taskcommand;
+
+import static dash.logic.commands.CommandTestUtil.assertCommandSuccess;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import dash.logic.commands.CommandTestUtil;
+import dash.model.AddressBook;
+import dash.model.Model;
+import dash.model.ModelManager;
+import dash.model.UserPrefs;
+import dash.testutil.TypicalIndexes;
+import dash.testutil.TypicalTasks;
+
+class ListTaskCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(new AddressBook(), new UserPrefs(), TypicalTasks.getTypicalTaskList());
+        expectedModel = new ModelManager(new AddressBook(), new UserPrefs(), model.getTaskList());
+    }
+
+    @Test
+    public void execute_listIsNotFiltered_showsSameList() {
+        assertCommandSuccess(new ListTaskCommand(), model, ListTaskCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_listIsFiltered_showsEverything() {
+        CommandTestUtil.showTaskAtIndex(model, TypicalIndexes.INDEX_FIRST_PERSON);
+        assertCommandSuccess(new ListTaskCommand(), model, ListTaskCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+}
