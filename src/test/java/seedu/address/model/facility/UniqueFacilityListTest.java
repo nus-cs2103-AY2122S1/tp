1b53931b03735c8bf282056f6da12b0ea5f6eee9
Shruthi0310 <e0564271@u.nus.edu>
2021-10-12 20:15:03 +0800

    Update tests for UniqueFacilityList

--- src/test/java/seedu/address/model/facility/UniqueFacilityListTest.java
+++ src/test/java/seedu/address/model/facility/UniqueFacilityListTest.java
@@ -7,9 +7,16 @@ import static seedu.address.testutil.TypicalFacilities.KENT_RIDGE_OUTDOOR_TENNIS
 import static seedu.address.testutil.TypicalFacilities.KENT_RIDGE_SPORT_HALL_5_COURT_1;
 import static seedu.address.testutil.TypicalFacilities.TAMPINES_HUB_FIELD_SECTION_B;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import seedu.address.model.facility.exceptions.FacilityNotFoundException;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.PersonBuilder;
 
 public class UniqueFacilityListTest {
     public final UniqueFacilityList uniqueFacilityList = new UniqueFacilityList();
@@ -38,6 +45,30 @@ public class UniqueFacilityListTest {
         uniqueFacilityList.replaceFacility(KENT_RIDGE_OUTDOOR_TENNIS_COURTS_COURT_1, editedFacility);
         assertEquals(expectedList, uniqueFacilityList);
 
+    }
+
+    @Test
+    public void allocateMembersToFacilities_addsMembersToFacilities_success() {
+        uniqueFacilityList.add(KENT_RIDGE_OUTDOOR_TENNIS_COURTS_COURT_1);
+        uniqueFacilityList.add(TAMPINES_HUB_FIELD_SECTION_B);
+
+        ObservableList<Person> members = FXCollections.observableArrayList();
+        Person firstMember = new PersonBuilder().build();
+        firstMember.setDays(Arrays.asList("Mon", "Tue"));
+        Person secondMember = new PersonBuilder().build();
+        secondMember.setDays(Arrays.asList("Mon", "Thu"));
+        members.add(firstMember);
+        members.add(secondMember);
+
+        Facility updatedFacility = KENT_RIDGE_OUTDOOR_TENNIS_COURTS_COURT_1;
+        updatedFacility.addPersonToFacility(firstMember);
+        updatedFacility.addPersonToFacility(secondMember);
+        UniqueFacilityList expected = new UniqueFacilityList();
+        expected.setFacilities(Arrays.asList(updatedFacility, TAMPINES_HUB_FIELD_SECTION_B));
+
+        FilteredList<Person> toAllocate = new FilteredList<Person>(members);
+        uniqueFacilityList.allocateMembersToFacilities(toAllocate);
+        assertEquals(expected, uniqueFacilityList);
     }
 
     @Test
