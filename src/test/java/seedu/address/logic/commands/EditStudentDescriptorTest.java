90a243f892f5923e3ed86dfea3278e1a46c958fb
nbvannhi <nbvannhi@gmail.com>
2021-10-13 03:38:26 +0800

    Fix warning in EditStudentDescriptorTest

--- src/test/java/seedu/address/logic/commands/EditStudentDescriptorTest.java
+++ src/test/java/seedu/address/logic/commands/EditStudentDescriptorTest.java
@@ -1,7 +1,7 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static seedu.address.logic.commands.CommandTestUtil.EDIT_DESCRIPTOR_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.EDIT_DESCRIPTOR_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_GROUP_RECITATION;
@@ -21,39 +21,39 @@ public class EditStudentDescriptorTest {
     public void equals() {
         // same values -> returns true
         EditStudentDescriptor descriptorWithSameValues = new EditStudentDescriptor(EDIT_DESCRIPTOR_AMY);
-        assertTrue(EDIT_DESCRIPTOR_AMY.equals(descriptorWithSameValues));
+        assertEquals(EDIT_DESCRIPTOR_AMY, descriptorWithSameValues);
 
         // same object -> returns true
-        assertTrue(EDIT_DESCRIPTOR_AMY.equals(EDIT_DESCRIPTOR_AMY));
+        assertEquals(EDIT_DESCRIPTOR_AMY, EDIT_DESCRIPTOR_AMY);
 
         // null -> returns false
-        assertFalse(EDIT_DESCRIPTOR_AMY.equals(null));
+        assertNotEquals(EDIT_DESCRIPTOR_AMY, null);
 
         // different types -> returns false
-        assertFalse(EDIT_DESCRIPTOR_AMY.equals(5));
+        assertNotEquals(EDIT_DESCRIPTOR_AMY, 5);
 
         // different values -> returns false
-        assertFalse(EDIT_DESCRIPTOR_AMY.equals(EDIT_DESCRIPTOR_BOB));
+        assertNotEquals(EDIT_DESCRIPTOR_AMY, EDIT_DESCRIPTOR_BOB);
 
         // different name -> returns false
-        EditStudentDescriptor editedAmy = new EditStudentDescriptorBuilder(EDIT_DESCRIPTOR_AMY)
-                .withName(VALID_NAME_BOB).build();
-        assertFalse(EDIT_DESCRIPTOR_AMY.equals(editedAmy));
+        EditStudentDescriptor editedAmy =
+                new EditStudentDescriptorBuilder(EDIT_DESCRIPTOR_AMY).withName(VALID_NAME_BOB).build();
+        assertNotEquals(EDIT_DESCRIPTOR_AMY, editedAmy);
 
         // different ID -> returns false
         editedAmy = new EditStudentDescriptorBuilder(EDIT_DESCRIPTOR_AMY).withId(VALID_ID_BOB).build();
-        assertFalse(EDIT_DESCRIPTOR_AMY.equals(editedAmy));
+        assertNotEquals(EDIT_DESCRIPTOR_AMY, editedAmy);
 
         // different groups -> returns false
         editedAmy = new EditStudentDescriptorBuilder(EDIT_DESCRIPTOR_AMY).withGroups(VALID_GROUP_RECITATION).build();
-        assertFalse(EDIT_DESCRIPTOR_AMY.equals(editedAmy));
+        assertNotEquals(EDIT_DESCRIPTOR_AMY, editedAmy);
 
         // different scores -> returns false
         editedAmy = new EditStudentDescriptorBuilder(EDIT_DESCRIPTOR_AMY).withScores(VALID_SCORES_BOB).build();
-        assertFalse(EDIT_DESCRIPTOR_AMY.equals(editedAmy));
+        assertNotEquals(EDIT_DESCRIPTOR_AMY, editedAmy);
 
         // different tags -> returns false
         editedAmy = new EditStudentDescriptorBuilder(EDIT_DESCRIPTOR_AMY).withTags(VALID_TAG_HUSBAND).build();
-        assertFalse(EDIT_DESCRIPTOR_AMY.equals(editedAmy));
+        assertNotEquals(EDIT_DESCRIPTOR_AMY, editedAmy);
     }
 }
