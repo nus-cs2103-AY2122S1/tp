ef5f80b82c1f8f159c9d02b6d32c6ae14fd10d1f
KelvinSoo <e0544272@u.nus.edu.sg>
2021-10-12 17:40:07 +0800

    Logic: update command messages

--- src/main/java/seedu/unify/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/unify/logic/parser/FindCommandParser.java
@@ -1,12 +1,18 @@
 package seedu.unify.logic.parser;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.unify.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.unify.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.unify.logic.parser.CliSyntax.PREFIX_NAME;
 
 import java.util.Arrays;
+import java.util.function.Predicate;
 
 import seedu.unify.logic.commands.FindCommand;
 import seedu.unify.logic.parser.exceptions.ParseException;
 import seedu.unify.model.task.NameContainsKeywordsPredicate;
+import seedu.unify.model.task.Task;
+import seedu.unify.model.task.TaskContainsDatePredicate;
 
 /**
  * Parses input arguments and creates a new FindCommand object
@@ -19,15 +25,28 @@ public class FindCommandParser implements Parser<FindCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindCommand parse(String args) throws ParseException {
-        String trimmedArgs = args.trim();
+        String trimmedArgs;
+        requireNonNull(args);
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_DATE);
+        Predicate<Task> p = null;
+        if (argMultimap.getValue(PREFIX_DATE).isPresent()) {
+            p = new TaskContainsDatePredicate(ParserUtil.parseDate(argMultimap.getValue(PREFIX_DATE).get()));
+            trimmedArgs = args.split("d/")[0].trim();
+        } else {
+            trimmedArgs = args.trim();
+        }
+
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
         String[] nameKeywords = trimmedArgs.split("\\s+");
-
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        if (p == null) {
+            return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        } else {
+            return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        }
     }
-
 }
