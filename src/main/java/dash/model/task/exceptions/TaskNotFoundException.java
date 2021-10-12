1fb61ea3fa780a803e8cb7866ff5ba753da9afe0
hanif-kamal <hanif.kamal47@gmail.com>
2021-10-12 21:50:16 +0800

    Add tests for TaskDescription

--- src/main/java/dash/model/task/exceptions/TaskNotFoundException.java
+++ src/main/java/dash/model/task/exceptions/TaskNotFoundException.java
@@ -2,7 +2,7 @@ package dash.model.task.exceptions;
 
 
 /**
- * Signals that the operation is unable to find the specified person.
+ * Signals that the operation is unable to find the specified task.
  */
 public class TaskNotFoundException extends RuntimeException {
 }
