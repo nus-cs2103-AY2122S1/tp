967e31fedfa714181fbcab673c6bf2c10159dc55
driip-ddrop <germaine5152seah@gmail.com>
2021-10-12 19:02:36 +0800

    Update DeleteCommand to enable delete by module code

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -1,13 +1,18 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULE_CODE;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.AddressBookParser;
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
+import seedu.address.model.person.ModuleCodesContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
 
 /**
@@ -15,16 +20,20 @@ import seedu.address.model.person.Person;
  */
 public class DeleteCommand extends Command {
 
-    public static final String MESSAGE_USAGE =
-            "delete: Deletes the person identified by the index number used in the displayed person list.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example: delete 1 or delete 1-3";
+    public static final String MESSAGE_USAGE = "delete: "
+            + "Deletes the person identified by the index number used in the displayed person list "
+            + "or by module code.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + PREFIX_MODULE_CODE + "MODULE CODE\n"
+            + "Example: delete 1 , delete 1-3 , delete "
+            + PREFIX_MODULE_CODE + "CS2040S";
 
     public static final String MESSAGE_NUMBER_DELETED_PERSON = "%d Deleted Persons: \n";
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "%1$s \n";
 
     private final Index targetIndex;
     private final Index endIndex;
+    private final Predicate<Person> predicate;
 
     /**
      * Creates a DeleteCommand to delete the person at specified index
@@ -34,6 +43,7 @@ public class DeleteCommand extends Command {
     public DeleteCommand(Index targetIndex) {
         this.targetIndex = targetIndex;
         endIndex = targetIndex;
+        predicate = null;
     }
 
     /**
@@ -45,14 +55,26 @@ public class DeleteCommand extends Command {
     public DeleteCommand(Index targetIndex, Index endIndex) {
         this.targetIndex = targetIndex;
         this.endIndex = endIndex;
+        predicate = null;
+    }
+
+    public DeleteCommand(ModuleCodesContainsKeywordsPredicate predicate) {
+        targetIndex = null;
+        endIndex = null;
+        this.predicate = predicate;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
+        boolean hasPredicate = predicate != null;
+        if (hasPredicate) {
+            model.updateFilteredPersonList(predicate);
+        }
+        int first = hasPredicate ? 0 : targetIndex.getZeroBased();
+        int last = hasPredicate ? model.getFilteredPersonList().size() - 1 : endIndex.getZeroBased();
+        int size = last;
         List<Person> lastShownList = model.getFilteredPersonList();
-        int first = targetIndex.getZeroBased();
-        int last = endIndex.getZeroBased();
 
         if (first >= lastShownList.size()) {
             throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
@@ -68,10 +90,19 @@ public class DeleteCommand extends Command {
             deletedPersons.insert(0, String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete));
             last--;
         }
-        String successMessage = String.format(MESSAGE_NUMBER_DELETED_PERSON, endIndex.getZeroBased() - first + 1)
-                + deletedPersons;
+        String successMessage = String.format(MESSAGE_NUMBER_DELETED_PERSON, size + 1) + deletedPersons;
+        model.updateFilteredPersonList(Model.PREDICATE_SHOW_ALL_PERSONS);
         return new CommandResult(successMessage);
     }
+    /*int last, first;
+        if (predicate != null) {
+            model.updateFilteredPersonList(predicate);
+            first = 0; //first item to delete in the filtered list
+            last = model.getFilteredPersonList().size() - 1;
+        } else {
+            first = targetIndex.getZeroBased();
+            last = endIndex.getZeroBased();
+        }*/
 
     @Override
     public boolean equals(Object other) {
--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -1,10 +1,18 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULE_CODE;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.ModuleCode;
+import seedu.address.model.person.ModuleCodesContainsKeywordsPredicate;
 
 /**
  * Parses input arguments and creates a new DeleteCommand object
@@ -17,6 +25,24 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_MODULE_CODE);
+        List<String> moduleCodes = argMultimap.getAllValues(PREFIX_MODULE_CODE);
+
+        if (!moduleCodes.isEmpty()) {
+            try {
+                Set<ModuleCode> moduleCodeSet = ParserUtil.parseModuleCodes(moduleCodes);
+                List<String> stringListOfModuleCodes = moduleCodeSet.stream()
+                        .map(moduleCode -> moduleCode.toString())
+                        .collect(Collectors.toList());
+
+                return new DeleteCommand(
+                        new ModuleCodesContainsKeywordsPredicate(stringListOfModuleCodes));
+            } catch (ParseException pe) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
+            }
+        }
+
         try {
             if (args.contains("-")) {
                 Index start = ParserUtil.parseIndex(args.substring(1, args.indexOf("-")));
