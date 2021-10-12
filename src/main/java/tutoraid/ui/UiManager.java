795723e229cd12348ff4cef979077291d38c6b7f
wpinrui <77185900+wpinrui@users.noreply.github.com>
2021-10-12 11:20:56 +0800

    Use only 2 instances of student list panel

--- src/main/java/tutoraid/ui/UiManager.java
+++ src/main/java/tutoraid/ui/UiManager.java
@@ -43,7 +43,7 @@ public class UiManager implements Ui {
         try {
             mainWindow = new MainWindow(primaryStage, logic);
             mainWindow.show(); //This should be called before creating other UI parts
-            mainWindow.fillInnerParts(false);
+            mainWindow.fillInnerParts();
 
         } catch (Throwable e) {
             logger.severe(StringUtil.getDetails(e));
