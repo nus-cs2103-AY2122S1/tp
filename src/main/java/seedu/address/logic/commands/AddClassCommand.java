9a20f6fdd76c1ab323f2ada248cafa49d2a8f540
NoraYUuu <norafish79@gmail.com>
2021-10-13 10:58:30 +0800

    Delete counter in addclass usage message

--- src/main/java/seedu/address/logic/commands/AddClassCommand.java
+++ src/main/java/seedu/address/logic/commands/AddClassCommand.java
@@ -22,7 +22,7 @@ public class AddClassCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Add tuition class given name, limit, sessions, timeslot, and student \n"
             + "Parameters: NAME LIMIT TIMESLOT STUDENT\n"
-            + "Example: " + COMMAND_WORD + " n/Physics l/10 c/4 ts/Mon 11:00-14:00 s/Alex Yeoh";
+            + "Example: " + COMMAND_WORD + " n/Physics l/10 ts/Mon 11:00-14:00 s/Alex Yeoh";
 
     private static final String MESSAGE_CLASS_LIMIT_EXCEEDED = "The following students are not "
             + "added due to class limit: ";
