4b9416553f692ec62acd1d6b0039cdb5d9757322
Kan Jitpakdi <ryukan2000@gmail.com>
2021-10-13 18:58:27 +0800

    Update Ui (draft 2) - add resultDisplay text animation and window size formatting

--- src/main/java/seedu/address/ui/ResultDisplay.java
+++ src/main/java/seedu/address/ui/ResultDisplay.java
@@ -2,9 +2,12 @@ package seedu.address.ui;
 
 import static java.util.Objects.requireNonNull;
 
+import javafx.animation.Animation;
+import javafx.animation.Transition;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
+import javafx.util.Duration;
 
 /**
  * A ui for the status bar that is displayed at the header of the application.
@@ -22,7 +25,21 @@ public class ResultDisplay extends UiPart<Region> {
 
     public void setFeedbackToUser(String feedbackToUser) {
         requireNonNull(feedbackToUser);
-        resultDisplay.setText(feedbackToUser);
+
+        Animation animation = new Transition() {
+            {
+                setCycleDuration(Duration.millis(500));
+            }
+
+            @Override
+            protected void interpolate(double frac) {
+                // cast to float so Math round to int
+                int n = Math.round(feedbackToUser.length() * (float) frac);
+                resultDisplay.setText(feedbackToUser.substring(0, n));
+            }
+        };
+
+        animation.play();
     }
 
 }
