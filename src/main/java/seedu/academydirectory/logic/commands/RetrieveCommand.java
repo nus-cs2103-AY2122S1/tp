e328342e2634af1fd7dffa27178029bbfc492c14
AmadeusW <amadeusw6@gmail.com>
2021-10-13 18:41:42 +0800

    Add support for multiple information retrieval

--- src/main/java/seedu/academydirectory/logic/commands/RetrieveCommand.java
+++ src/main/java/seedu/academydirectory/logic/commands/RetrieveCommand.java
@@ -5,14 +5,14 @@ import static seedu.academydirectory.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.academydirectory.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.academydirectory.logic.parser.CliSyntax.PREFIX_TELEGRAM;
 
-import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javafx.collections.ObservableList;
 import seedu.academydirectory.commons.core.Messages;
 import seedu.academydirectory.model.Model;
 import seedu.academydirectory.model.student.Information;
-import seedu.academydirectory.model.student.Student;
+import seedu.academydirectory.model.student.InformationWantedFunction;
 
 /**
  * Finds and lists all persons in academy directory whose name contains any of the argument keywords.
@@ -22,6 +22,7 @@ public class RetrieveCommand extends Command {
 
     public static final String COMMAND_WORD = "retrieve";
 
+<<<<<<< HEAD
     public static final String HELP_MESSAGE = "### Retrieving additional information of students': `retrieve`\n"
             + "\n"
             + "Retrieves additional information of students if present. Currently supported information includes: \n"
@@ -32,32 +33,42 @@ public class RetrieveCommand extends Command {
             + "Format: `retrieve INFORMATION [of STUDENT_NAME]`";
 
     // TODO: Support for individual names
+=======
+>>>>>>> Add support for multiple information retrieval
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Retrieve personal details of students "
             + "and displays them as a list\n"
             + "Parameters: " + PREFIX_EMAIL + " | "
             + PREFIX_TELEGRAM + " | " + PREFIX_PHONE + "\n"
             + "Example: " + COMMAND_WORD + " " + PREFIX_PHONE;
 
-    private final Function<? super Student, ? extends Information> filter;
+    private final Stream<InformationWantedFunction> filterStream;
 
-    public RetrieveCommand(Function<? super Student, ? extends Information> filter) {
-        this.filter = filter;
+    public RetrieveCommand(Stream<InformationWantedFunction> filterStream) {
+        this.filterStream = filterStream;
+    }
+
+    public RetrieveCommand(InformationWantedFunction filter) {
+        this(Stream.of(filter));
+    }
+
+    private String executeFilter(Model model, InformationWantedFunction filter) {
+        ObservableList<Information> view = model.getFilteredStudentListView(filter);
+        return view.size() == 0
+                ? String.format(Messages.MESSAGE_STUDENTS_LISTED_OVERVIEW, 0)
+                : view.stream().map(Object::toString).collect(Collectors.joining("\n"));
     }
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        ObservableList<Information> view = model.getFilteredStudentListView(filter);
-        String result = view.size() == 0
-                ? String.format(Messages.MESSAGE_STUDENTS_LISTED_OVERVIEW, view.size())
-                : view.stream().map(Object::toString).collect(Collectors.joining("\n"));
-        return new CommandResult(result);
+        return new CommandResult(filterStream.parallel().map(x -> executeFilter(model, x))
+                .collect(Collectors.joining("\n")));
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof RetrieveCommand // instanceof handles nulls
-                && filter.equals(((RetrieveCommand) other).filter)); // state check
+                && filterStream.equals(((RetrieveCommand) other).filterStream)); // state check
     }
 }
--- src/main/java/seedu/academydirectory/logic/parser/RetrieveCommandParser.java
+++ src/main/java/seedu/academydirectory/logic/parser/RetrieveCommandParser.java
@@ -3,9 +3,11 @@ package seedu.academydirectory.logic.parser;
 import static java.util.Objects.requireNonNull;
 import static seedu.academydirectory.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.academydirectory.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.academydirectory.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.academydirectory.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.academydirectory.logic.parser.CliSyntax.PREFIX_TELEGRAM;
 
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 import seedu.academydirectory.logic.commands.RetrieveCommand;
@@ -16,6 +18,8 @@ import seedu.academydirectory.model.student.InformationWantedFunction;
  * Parses input arguments and creates a new FindCommand object
  */
 public class RetrieveCommandParser implements Parser<RetrieveCommand> {
+    private static final Supplier<Stream<Prefix>> RELEVANT_PREFIXES_SUPPLIER = () ->
+            Stream.of(PREFIX_PHONE, PREFIX_EMAIL, PREFIX_TELEGRAM);
 
     /**
      * Parses the given {@code String} of arguments in the context of the FindCommand
@@ -24,23 +28,32 @@ public class RetrieveCommandParser implements Parser<RetrieveCommand> {
      */
     public RetrieveCommand parse(String args) throws ParseException {
         requireNonNull(args);
-        String trimmedArgs = args.trim();
-        if (trimmedArgs.isEmpty()) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, RetrieveCommand.MESSAGE_USAGE));
-        }
-        String[] keywords = trimmedArgs.split("\\s+");
-        String info = keywords[0];
-        Prefix infoPrefix = new Prefix(info);
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args,
+                        Stream.concat(Stream.of(PREFIX_NAME), RELEVANT_PREFIXES_SUPPLIER.get())
+                                .toArray(Prefix[]::new));
 
+<<<<<<< HEAD
         Stream<Prefix> prefixes = Stream.of(PREFIX_PHONE, PREFIX_EMAIL, PREFIX_TELEGRAM);
         boolean isAnyPrefixMatch = prefixes.anyMatch(x -> x.equals(infoPrefix));
         if (!isAnyPrefixMatch) {
+=======
+        // Check at least one relevant prefix is provided
+        boolean noPrefix = RELEVANT_PREFIXES_SUPPLIER.get()
+                .map(x -> argMultimap.getAllValues(x).isEmpty())
+                .reduce(true, Boolean::logicalAnd);
+
+        if (noPrefix) {
+>>>>>>> Add support for multiple information retrieval
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, RetrieveCommand.MESSAGE_USAGE));
         }
 
-        return new RetrieveCommand(new InformationWantedFunction(infoPrefix));
+        Stream<InformationWantedFunction> filters = RELEVANT_PREFIXES_SUPPLIER.get()
+                .filter(x -> !argMultimap.getAllValues(x).isEmpty())
+                .map(InformationWantedFunction::new);
+
+        return new RetrieveCommand(filters);
     }
 
 }
