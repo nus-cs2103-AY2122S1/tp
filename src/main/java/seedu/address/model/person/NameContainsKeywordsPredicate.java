9aab924a1a7c3eae7bc2d2e39c93fba4be46f3fb
meixuanjin <jin.meixuan02@gmail.com>
2021-10-13 16:49:42 +0800

    Fix style and add Javadocs

--- src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
@@ -1,6 +1,5 @@
 package seedu.address.model.person;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Predicate;
@@ -15,6 +14,11 @@ public class NameContainsKeywordsPredicate implements Predicate<Person> {
     private final List<String> keywords;
     private final String type;
 
+    /**
+     * Constructor for the class.
+     * @param keywords keywords to be searched.
+     * @param type type to be searched.
+     */
     public NameContainsKeywordsPredicate(List<String> keywords, String type) {
         this.keywords = keywords;
         this.type = type;
@@ -23,29 +27,40 @@ public class NameContainsKeywordsPredicate implements Predicate<Person> {
     @Override
     public boolean test(Person person) {
         switch(type) {
-            case "n/": return testName(person);
-            case "s/": return keywords.stream()
-                    .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getStudentId().value, keyword));
-            case "N/": return keywords.stream()
-                    .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getNusNetworkId().value, keyword));
-            case "g/": return keywords.stream()
-                    .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getGitHubId().value, keyword));
-            case "T/": return keywords.stream()
-                    .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getTutorialId().value, keyword));
-            case "p/": return keywords.stream()
-                    .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getPhone().value, keyword));
-            case "a/": return keywords.stream()
-                    .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getAddress().value, keyword));
-            case "t/": return testTags(person, keywords.get(1));
-            default: return false;
+        case "n/": return testName(person);
+        case "s/": return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getStudentId().value, keyword));
+        case "N/": return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getNusNetworkId().value, keyword));
+        case "g/": return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getGitHubId().value, keyword));
+        case "T/": return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getTutorialId().value, keyword));
+        case "p/": return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getPhone().value, keyword));
+        case "a/": return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getAddress().value, keyword));
+        case "t/": return testTags(person, keywords.get(1));
+        default: return false;
         }
     }
 
+    /**
+     * Checks if a searched tag is part of the person profile.
+     * @param person Person to be checked.
+     * @param tag Tag to be searched.
+     * @return boolean
+     */
     public boolean testTags(Person person, String tag) {
         Set<Tag> tags = person.getTags();
         return tags.contains(new Tag(tag));
     }
 
+    /**
+     * Checks if the name matches or partially matches a name of the person profile.
+     * @param person Person to be checked.
+     * @return boolean
+     */
     public boolean testName(Person person) {
         String name = person.getName().fullName;
         String[] nameList = name.split("\\s");
