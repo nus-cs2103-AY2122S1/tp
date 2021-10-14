ab0190267912a63784093d7c905a64669d6e4edb
AmadeusW <amadeusw6@gmail.com>
2021-10-14 16:05:46 +0800

    Add test for RetrieveCommandParser

--- src/test/java/seedu/academydirectory/logic/parser/RetrieveCommandParserTest.java
+++ src/test/java/seedu/academydirectory/logic/parser/RetrieveCommandParserTest.java
@@ -1,8 +1,10 @@
 package seedu.academydirectory.logic.parser;
 
 import static seedu.academydirectory.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.academydirectory.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.academydirectory.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.academydirectory.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.academydirectory.testutil.TypicalStudents.getTypicalStudents;
 
 import java.util.stream.Stream;
 
@@ -10,6 +12,7 @@ import org.junit.jupiter.api.Test;
 
 import seedu.academydirectory.logic.commands.RetrieveCommand;
 import seedu.academydirectory.model.student.InformationWantedFunction;
+import seedu.academydirectory.model.student.Name;
 
 @SuppressWarnings("checkstyle:Regexp")
 public class RetrieveCommandParserTest {
@@ -42,6 +45,19 @@ public class RetrieveCommandParserTest {
         relevantPrefixes.forEach(prefix -> {
             String userInput = " " + prefix.getPrefix();
             RetrieveCommand expectedCommand = new RetrieveCommand(new InformationWantedFunction(prefix));
+            assertParseSuccess(parser, userInput, expectedCommand);
+        });
+    }
+
+    @Test
+    public void parse_validArgsWithName_returnsRetrieveCommand() {
+        Name name = getTypicalStudents().get(0).getName();
+
+        Stream<Prefix> relevantPrefixes = Stream.of(InformationWantedFunction.SUPPORTED_PREFIX.toArray(Prefix[]::new))
+                .parallel();
+        relevantPrefixes.forEach(prefix -> {
+            String userInput = " " + prefix.getPrefix() + " " + PREFIX_NAME + name.fullName;
+            RetrieveCommand expectedCommand = new RetrieveCommand(new InformationWantedFunction(prefix, name));
             assertParseSuccess(parser, userInput, expectedCommand);
         });
     }
