7bb4c55e67a65112e3a2e25952d3e43420b974b7
Au Chen Xi, Gabriel <70463178+Gabau@users.noreply.github.com>
2021-10-14 21:26:54 +0800

    Add union test cases

--- src/test/java/seedu/address/model/person/PeriodTest.java
+++ src/test/java/seedu/address/model/person/PeriodTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,14 +13,17 @@ public class PeriodTest {
     private static final LocalDate TEST_DATE = LocalDate.of(1999, 5, 14);
     private static final LocalDate PAST_TEST_DATE = LocalDate.of(1999, 6, 14);
     private static final LocalDate BEFORE_TEST_DATE = LocalDate.of(1999, 4, 14);
+    private Period singleTestPeriod = new Period(TEST_DATE);
+    private Period testPeriodAfterSingle = new Period(PAST_TEST_DATE, TEST_DATE);
+    private Period testPeriodBeforeSingle = new Period(TEST_DATE, BEFORE_TEST_DATE);
+    private Period singleTestPeriodAfter = new Period(PAST_TEST_DATE);
+    private Period singleTestPeriodBefore = new Period(BEFORE_TEST_DATE);
+    private Period overLargePeriod = new Period(PAST_TEST_DATE, BEFORE_TEST_DATE);
 
     @Test
     public void test_contains_success() {
         //tests with local date time
-        Period singleTestPeriod = new Period(TEST_DATE);
-        Period testPeriodAfterSingle = new Period(PAST_TEST_DATE, TEST_DATE);
-        Period testPeriodBeforeSingle = new Period(TEST_DATE, BEFORE_TEST_DATE);
-        Period overLargePeriod = new Period(PAST_TEST_DATE, BEFORE_TEST_DATE);
+
         assertTrue(singleTestPeriod.contains(TEST_DATE));
         assertTrue(overLargePeriod.contains(TEST_DATE));
         assertTrue(overLargePeriod.contains(PAST_TEST_DATE));
@@ -44,6 +49,36 @@ public class PeriodTest {
 
     }
 
+
+    @Test
+    public void test_union_collection() {
+        Collection<Period> periods = List.of(singleTestPeriod, testPeriodBeforeSingle, testPeriodAfterSingle);
+        Collection<Period> expectedResult = List.of(overLargePeriod);
+        assertTrue(overLargePeriod.union(periods).containsAll(expectedResult));
+        assertTrue(expectedResult.containsAll(overLargePeriod.union(periods)));
+
+
+        periods = List.of(singleTestPeriod, singleTestPeriodBefore, singleTestPeriodAfter);
+        Collection<Period> testPeriods = testPeriodAfterSingle.union(periods);
+        assertFalse(expectedResult.containsAll(testPeriodAfterSingle.union(periods)));
+        assertFalse(testPeriodAfterSingle.union(periods).containsAll(expectedResult));
+        assertNotEqual(expectedResult, testPeriods);
+
+        expectedResult = List.of(testPeriodAfterSingle, singleTestPeriodBefore);
+        assertEquals(expectedResult, testPeriods);
+
+    }
+
+
+    private void assertNotEqual(Collection<Period> expected, Collection<Period> actual) {
+        assertFalse(expected.containsAll(actual) && actual.containsAll(expected));
+    }
+
+
+    private void assertEquals(Collection<Period> expected, Collection<Period> actual) {
+        assertTrue(expected.containsAll(actual));
+        assertTrue(actual.containsAll(expected));
+    }
 
 
 
