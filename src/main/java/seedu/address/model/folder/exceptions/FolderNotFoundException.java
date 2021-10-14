fb13d8e67934d5b80ca42233acc451d430c806a4
sebbycake <leehur_sebastian@hotmail.com>
2021-10-14 18:56:58 +0800

    Change the definition of Folder::equals to fix rmdir bug

--- src/main/java/seedu/address/model/folder/exceptions/FolderNotFoundException.java
+++ src/main/java/seedu/address/model/folder/exceptions/FolderNotFoundException.java
@@ -4,4 +4,7 @@ package seedu.address.model.folder.exceptions;
  * Signals that the operation is unable to find the specified folder.
  */
 public class FolderNotFoundException extends RuntimeException {
+    public FolderNotFoundException() {
+        super("Folder not found. Please try again.");
+    }
 }
