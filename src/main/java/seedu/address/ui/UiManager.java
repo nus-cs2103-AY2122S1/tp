84df976c8ad3eeefdd4486d80c1a7f112e174c22
LMAOboxhack <linfangyuan99@gmail.com>
2021-10-14 21:41:53 +0800

    Add delete cascading between Position and Applicant

--- src/main/java/seedu/address/ui/UiManager.java
+++ src/main/java/seedu/address/ui/UiManager.java
@@ -43,7 +43,7 @@ public class UiManager implements Ui {
         try {
             mainWindow = new MainWindow(primaryStage, logic);
             mainWindow.show(); //This should be called before creating other UI parts
-            mainWindow.fillInnerParts();
+//            mainWindow.fillInnerParts();
             mainWindow.fillInnerPartsWithApplicants();
             mainWindow.fillInnerPartsWithPosition();
 
