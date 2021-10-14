b81c3aa250efc3985773921921138db44025f0f1
Jason <jason.ng.zq99@gmail.com>
2021-10-14 23:47:38 +0800

    Edit and add test cases for FindCommandParaserTest

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -1,8 +1,7 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_MODULE_CODE_CS2030S;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_MODULE_CODE_CS2040;
+import static seedu.address.logic.commands.CommandTestUtil.*;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
@@ -13,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.model.person.ModuleCodesContainsKeywordsPredicate;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.TagsContainsKeywordsPredicate;
 
 public class FindCommandParserTest {
 
@@ -60,8 +60,45 @@ public class FindCommandParserTest {
     }
 
     @Test
-    public void parse_twoPrefixes_throwsParseException() {
+    public void parse_emptyTag_throwsParseException() {
+        assertParseFailure(parser, "find t/",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validTag_returnsFindCommand() {
+        FindCommand expectedFindCommand =
+                new FindCommand(new TagsContainsKeywordsPredicate(
+                        Arrays.asList(
+                                String.format("[%s]", VALID_TAG_LOCAL),
+                                String.format("[%s]", VALID_TAG_INTERNATIONAL)
+                        )
+                ));
+        String userInput = String.format(" t/%s %s", VALID_TAG_LOCAL, VALID_TAG_INTERNATIONAL);
+        assertParseSuccess(parser, userInput, expectedFindCommand);
+    }
+
+    @Test
+    public void parse_twoPrefixes_NameAndModule_throwsParseException() {
         assertParseFailure(parser, "find n/ben m/cs2100",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_SINGLE_PREFIX_SEARCH));
+    }
+
+    @Test
+    public void parse_twoPrefixes_NameAndTag_throwsParseException() {
+        assertParseFailure(parser, "find n/ben t/UwU",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_SINGLE_PREFIX_SEARCH));
+    }
+
+    @Test
+    public void parse_twoPrefixes_TagAndModule_throwsParseException() {
+        assertParseFailure(parser, "find t/UwU m/cs2100",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_SINGLE_PREFIX_SEARCH));
+    }
+
+    @Test
+    public void parse_threePrefixes_NameAndModule_throwsParseException() {
+        assertParseFailure(parser, "find n/ben m/cs2100 t/UwU",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_SINGLE_PREFIX_SEARCH));
     }
 }
