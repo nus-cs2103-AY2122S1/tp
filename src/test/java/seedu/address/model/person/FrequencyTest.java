df3ffb4a06d74af697b3338a78dcd9b6d520a6ee
Ai Ling <hong-ailing@hotmail.com>
2021-10-14 14:46:55 +0800

    Increase test coverage for frequency model

--- src/test/java/seedu/address/model/person/FrequencyTest.java
+++ src/test/java/seedu/address/model/person/FrequencyTest.java
@@ -1,5 +1,6 @@
 package seedu.address.model.person;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
@@ -7,6 +8,8 @@ import static seedu.address.testutil.Assert.assertThrows;
 import org.junit.jupiter.api.Test;
 
 public class FrequencyTest {
+    private final Visit VISIT = new Visit("2022-01-01 12:00");
+
     @Test
     public void find_null_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> Frequency.find(null));
@@ -32,5 +35,47 @@ public class FrequencyTest {
         assertTrue(Frequency.isValidFrequency("biweekly"));
         assertTrue(Frequency.isValidFrequency("monthly"));
         assertTrue(Frequency.isValidFrequency("quarterly"));
+    }
+
+    @Test
+    public void nextVisit_daily() {
+        Frequency f = Frequency.DAILY;
+        Visit nextVisit = new Visit("2022-01-02 12:00");
+        assertEquals(f.nextVisit(VISIT), nextVisit);
+    }
+
+    @Test
+    public void nextVisit_weekly() {
+        Frequency f = Frequency.WEEKLY;
+        Visit nextVisit = new Visit("2022-01-08 12:00");
+        assertEquals(f.nextVisit(VISIT), nextVisit);
+    }
+
+    @Test
+    public void nextVisit_biweekly() {
+        Frequency f = Frequency.BIWEEKLY;
+        Visit nextVisit = new Visit("2022-01-15 12:00");
+        assertEquals(f.nextVisit(VISIT), nextVisit);
+    }
+
+    @Test
+    public void nextVisit_monthly() {
+        Frequency f = Frequency.MONTHLY;
+        Visit nextVisit = new Visit("2022-02-01 12:00");
+        assertEquals(f.nextVisit(VISIT), nextVisit);
+    }
+
+    @Test
+    public void nextVisit_quarterly() {
+        Frequency f = Frequency.QUARTERLY;
+        Visit nextVisit = new Visit("2022-04-01 12:00");
+        assertEquals(f.nextVisit(VISIT), nextVisit);
+    }
+
+    @Test
+    public void nextVisit_empty() {
+        Frequency f = Frequency.EMPTY;
+        Visit nextVisit = new Visit("");
+        assertEquals(f.nextVisit(VISIT), nextVisit);
     }
 }
