be8694dbf48d423d74221a23ce9f2f5ed37dc53a
IrvingHe000 <heoutong@gmail.com>
2021-10-15 18:00:01 +0800

    Change magic literal to constant

--- src/test/java/seedu/address/model/person/SlotTest.java
+++ src/test/java/seedu/address/model/person/SlotTest.java
@@ -12,12 +12,21 @@ public class SlotTest {
     private static final Slot MORNING = Slot.MORNING;
     private static final Slot AFTERNOON = Slot.AFTERNOON;
 
+    private static final String MORNING_LOWERCASE = "morning";
+    private static final String MORNING_UPPERCASE = "MORNING";
+    private static final String MORNING_START_WITH_UPPERCASE = "Morning";
+    private static final String AFTERNOON_LOWERCASE = "afternoon";
+    private static final String AFTERNOON_UPPERCASE = "AFTERNOON";
+    private static final String AFTERNOON_START_WITH_UPPERCASE = "Afternoon";
+
     @Test
     public void getValueTest() {
-        assertEquals("morning", MORNING.getValue());
-        assertEquals("afternoon", AFTERNOON.getValue());
-        assertNotEquals("Morning", MORNING.getValue());
-        assertNotEquals("Afternoon", AFTERNOON.getValue());
+        assertEquals(MORNING_LOWERCASE, MORNING.getValue());
+        assertEquals(AFTERNOON_LOWERCASE, AFTERNOON.getValue());
+        assertNotEquals(MORNING_UPPERCASE, MORNING.getValue());
+        assertNotEquals(AFTERNOON_UPPERCASE, AFTERNOON.getValue());
+        assertNotEquals(MORNING_START_WITH_UPPERCASE, MORNING.getValue());
+        assertNotEquals(AFTERNOON_START_WITH_UPPERCASE, AFTERNOON.getValue());
     }
 
     @Test
@@ -28,11 +37,11 @@ public class SlotTest {
 
     @Test
     public void translateStringToSlotTest() {
-        assertEquals(MORNING, Slot.translateStringToSlot("morning"));
-        assertEquals(MORNING, Slot.translateStringToSlot("Morning"));
-        assertEquals(MORNING, Slot.translateStringToSlot("MORNING"));
-        assertEquals(MORNING, Slot.translateStringToSlot("    morning   "));
-        assertEquals(AFTERNOON, Slot.translateStringToSlot("afternoon"));
+        assertEquals(MORNING, Slot.translateStringToSlot(MORNING_LOWERCASE));
+        assertEquals(MORNING, Slot.translateStringToSlot(MORNING_START_WITH_UPPERCASE));
+        assertEquals(MORNING, Slot.translateStringToSlot(MORNING_UPPERCASE));
+        assertEquals(MORNING, Slot.translateStringToSlot("   " + MORNING_LOWERCASE + "   "));
+        assertEquals(AFTERNOON, Slot.translateStringToSlot(AFTERNOON_LOWERCASE));
     }
 
     @Test
@@ -43,11 +52,11 @@ public class SlotTest {
 
     @Test
     public void isValidSlotTest() {
-        assertTrue(Slot.isValidSlot("morning"));
-        assertTrue(Slot.isValidSlot("  morning   "));
-        assertTrue(Slot.isValidSlot("afternoon"));
+        assertTrue(Slot.isValidSlot(MORNING_LOWERCASE));
+        assertTrue(Slot.isValidSlot("   " + MORNING_LOWERCASE + "   "));
+        assertTrue(Slot.isValidSlot(AFTERNOON_LOWERCASE));
 
-        assertFalse(Slot.isValidSlot("Morning"));
+        assertFalse(Slot.isValidSlot(MORNING_START_WITH_UPPERCASE));
         assertFalse(Slot.isValidSlot(""));
         assertFalse(Slot.isValidSlot("random text"));
     }
