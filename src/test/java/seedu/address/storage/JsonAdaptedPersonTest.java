c1bf51b70fbffe79d19506f738b75205ec1f88e3
timothywongej <e0559903@u.nus.edu>
2021-10-15 15:39:19 +0800

    Update testutil for test cases

--- src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
@@ -12,25 +12,32 @@ import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
+import seedu.address.model.person.Faculty;
+import seedu.address.model.person.Major;
 import seedu.address.model.person.Name;
-import seedu.address.model.person.Phone;
 
 public class JsonAdaptedPersonTest {
     private static final String INVALID_NAME = "R@chel";
-    private static final String INVALID_PHONE = "+651234";
-    private static final String INVALID_ADDRESS = " ";
     private static final String INVALID_EMAIL = "example.com";
+    private static final String INVALID_FACULTY = "wrong_faculty";
+    private static final String INVALID_MAJOR = "##computer_2science";
     private static final String INVALID_TAG = "#friend";
 
+
     private static final String VALID_NAME = BENSON.getName().toString();
-    private static final String VALID_PHONE = BENSON.getPhone().toString();
     private static final String VALID_EMAIL = BENSON.getEmail().toString();
-    private static final String VALID_ADDRESS = BENSON.getAddress().toString();
-    private static final String VALID_ROLE = BENSON.getRole().toString();
     private static final String VALID_FACULTY = BENSON.getFaculty().toString();
     private static final String VALID_MAJOR = BENSON.getMajor().toString();
+    private static final List<JsonAdaptedSkill> VALID_SKILLS = BENSON.getSkills().stream()
+            .map(JsonAdaptedSkill::new)
+            .collect(Collectors.toList());
+    private static final List<JsonAdaptedLanguage> VALID_LANGUAGES = BENSON.getLanguages().stream()
+            .map(JsonAdaptedLanguage::new)
+            .collect(Collectors.toList());
+    private static final List<JsonAdaptedFramework> VALID_FRAMEWORK = BENSON.getFrameworks().stream()
+            .map(JsonAdaptedFramework::new)
+            .collect(Collectors.toList());
     private static final List<JsonAdaptedTag> VALID_TAGS = BENSON.getTags().stream()
             .map(JsonAdaptedTag::new)
             .collect(Collectors.toList());
@@ -44,75 +51,79 @@ public class JsonAdaptedPersonTest {
     @Test
     public void toModelType_invalidName_throwsIllegalValueException() {
         JsonAdaptedPerson person =
-                new JsonAdaptedPerson(INVALID_NAME, VALID_PHONE, VALID_EMAIL, VALID_ADDRESS,
-                        VALID_ROLE, VALID_FACULTY, VALID_MAJOR, VALID_TAGS);
+                new JsonAdaptedPerson(INVALID_NAME, VALID_EMAIL, VALID_FACULTY, VALID_MAJOR,
+                        VALID_SKILLS, VALID_LANGUAGES, VALID_FRAMEWORK, VALID_TAGS);
         String expectedMessage = Name.MESSAGE_CONSTRAINTS;
         assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
     }
 
     @Test
     public void toModelType_nullName_throwsIllegalValueException() {
-        JsonAdaptedPerson person = new JsonAdaptedPerson(null, VALID_PHONE, VALID_EMAIL, VALID_ADDRESS,
-                VALID_ROLE, VALID_FACULTY, VALID_MAJOR, VALID_TAGS);
+        JsonAdaptedPerson person = new JsonAdaptedPerson(null, VALID_EMAIL, VALID_FACULTY, VALID_MAJOR,
+                VALID_SKILLS, VALID_LANGUAGES, VALID_FRAMEWORK, VALID_TAGS);
         String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName());
         assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
     }
 
     @Test
-    public void toModelType_invalidPhone_throwsIllegalValueException() {
-        JsonAdaptedPerson person =
-                new JsonAdaptedPerson(VALID_NAME, INVALID_PHONE, VALID_EMAIL, VALID_ADDRESS,
-                        VALID_ROLE, VALID_FACULTY, VALID_MAJOR, VALID_TAGS);
-        String expectedMessage = Phone.MESSAGE_CONSTRAINTS;
-        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
-    }
-
-    @Test
-    public void toModelType_nullPhone_throwsIllegalValueException() {
-        JsonAdaptedPerson person = new JsonAdaptedPerson(VALID_NAME, null, VALID_EMAIL, VALID_ADDRESS,
-                VALID_ROLE, VALID_FACULTY, VALID_MAJOR, VALID_TAGS);
-        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Phone.class.getSimpleName());
-        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
-    }
-
-    @Test
     public void toModelType_invalidEmail_throwsIllegalValueException() {
-        JsonAdaptedPerson person = new JsonAdaptedPerson(VALID_NAME, VALID_PHONE, INVALID_EMAIL, VALID_ADDRESS,
-                VALID_ROLE, VALID_FACULTY, VALID_MAJOR, VALID_TAGS);
+        JsonAdaptedPerson person = new JsonAdaptedPerson(VALID_NAME, INVALID_EMAIL, VALID_FACULTY, VALID_MAJOR,
+                VALID_SKILLS, VALID_LANGUAGES, VALID_FRAMEWORK, VALID_TAGS);
         String expectedMessage = Email.MESSAGE_CONSTRAINTS;
         assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
     }
 
     @Test
     public void toModelType_nullEmail_throwsIllegalValueException() {
-        JsonAdaptedPerson person = new JsonAdaptedPerson(VALID_NAME, VALID_PHONE, null, VALID_ADDRESS,
-                VALID_ROLE, VALID_FACULTY, VALID_MAJOR, VALID_TAGS);
+        JsonAdaptedPerson person = new JsonAdaptedPerson(VALID_NAME, null, VALID_FACULTY, VALID_MAJOR,
+                VALID_SKILLS, VALID_LANGUAGES, VALID_FRAMEWORK, VALID_TAGS);
         String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Email.class.getSimpleName());
         assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
     }
 
     @Test
-    public void toModelType_invalidAddress_throwsIllegalValueException() {
-        JsonAdaptedPerson person = new JsonAdaptedPerson(VALID_NAME, VALID_PHONE, VALID_EMAIL, INVALID_ADDRESS,
-                VALID_ROLE, VALID_FACULTY, VALID_MAJOR, VALID_TAGS);
-        String expectedMessage = Address.MESSAGE_CONSTRAINTS;
+    public void toModelType_invalidFaculty_throwsIllegalValueException() {
+        JsonAdaptedPerson person =
+                new JsonAdaptedPerson(VALID_NAME, VALID_EMAIL, INVALID_FACULTY, VALID_MAJOR,
+                        VALID_SKILLS, VALID_LANGUAGES, VALID_FRAMEWORK, VALID_TAGS);
+        String expectedMessage = Faculty.MESSAGE_CONSTRAINTS;
         assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
     }
 
     @Test
-    public void toModelType_nullAddress_throwsIllegalValueException() {
-        JsonAdaptedPerson person = new JsonAdaptedPerson(VALID_NAME, VALID_PHONE, VALID_EMAIL, null,
-                VALID_ROLE, VALID_FACULTY, VALID_MAJOR, VALID_TAGS);
-        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Address.class.getSimpleName());
+    public void toModelType_nullFaculty_throwsIllegalValueException() {
+        JsonAdaptedPerson person =
+                new JsonAdaptedPerson(VALID_NAME, VALID_EMAIL, null, VALID_MAJOR,
+                        VALID_SKILLS, VALID_LANGUAGES, VALID_FRAMEWORK, VALID_TAGS);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Faculty.class.getSimpleName());
         assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
     }
+
+    @Test
+    public void toModelType_invalidMajor_throwsIllegalValueException() {
+        JsonAdaptedPerson person =
+                new JsonAdaptedPerson(VALID_NAME, VALID_EMAIL, VALID_FACULTY, INVALID_MAJOR,
+                        VALID_SKILLS, VALID_LANGUAGES, VALID_FRAMEWORK, VALID_TAGS);
+        String expectedMessage = Major.MESSAGE_CONSTRAINTS;
+        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
+    }
+
+    @Test
+    public void toModelType_nullMajor_throwsIllegalValueException() {
+        JsonAdaptedPerson person =
+                new JsonAdaptedPerson(VALID_NAME, VALID_EMAIL, VALID_FACULTY, null,
+                        VALID_SKILLS, VALID_LANGUAGES, VALID_FRAMEWORK, VALID_TAGS);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Major.class.getSimpleName());
+        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
+    }
+
 
     @Test
     public void toModelType_invalidTags_throwsIllegalValueException() {
         List<JsonAdaptedTag> invalidTags = new ArrayList<>(VALID_TAGS);
         invalidTags.add(new JsonAdaptedTag(INVALID_TAG));
-        JsonAdaptedPerson person = new JsonAdaptedPerson(VALID_NAME, VALID_PHONE, VALID_EMAIL, VALID_ADDRESS,
-                VALID_ROLE, VALID_FACULTY, VALID_MAJOR, invalidTags);
+        JsonAdaptedPerson person = new JsonAdaptedPerson(VALID_NAME, VALID_EMAIL, VALID_FACULTY, VALID_MAJOR,
+                VALID_SKILLS, VALID_LANGUAGES, VALID_FRAMEWORK, invalidTags);
         assertThrows(IllegalValueException.class, person::toModelType);
     }
 
--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -1,12 +1,12 @@
 package seedu.address.testutil;
 
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_FACULTY;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_FRAMEWORK;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_LANGUAGE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MAJOR;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ROLE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SKILL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Set;
@@ -14,6 +14,9 @@ import java.util.Set;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
 import seedu.address.model.person.Person;
+import seedu.address.model.skill.Framework;
+import seedu.address.model.skill.Language;
+import seedu.address.model.skill.Skill;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -34,15 +37,26 @@ public class PersonUtil {
     public static String getPersonDetails(Person person) {
         StringBuilder sb = new StringBuilder();
         sb.append(PREFIX_NAME + person.getName().fullName + " ");
-        sb.append(PREFIX_PHONE + person.getPhone().value + " ");
         sb.append(PREFIX_EMAIL + person.getEmail().value + " ");
-        sb.append(PREFIX_ADDRESS + person.getAddress().value + " ");
-        sb.append(PREFIX_ROLE + person.getRole().value + " ");
         sb.append(PREFIX_FACULTY + person.getFaculty().value + " ");
         sb.append(PREFIX_MAJOR + person.getMajor().value + " ");
+
+        person.getSkills().stream().forEach(
+                s -> sb.append(PREFIX_SKILL + s.skillName + " ")
+        );
+
+        person.getLanguages().stream().forEach(
+                s -> sb.append(PREFIX_LANGUAGE + s.languageName + " ")
+        );
+
+        person.getFrameworks().stream().forEach(
+                s -> sb.append(PREFIX_FRAMEWORK + s.frameworkName + " ")
+        );
+
         person.getTags().stream().forEach(
             s -> sb.append(PREFIX_TAG + s.tagName + " ")
         );
+
         return sb.toString();
     }
 
@@ -52,13 +66,41 @@ public class PersonUtil {
     public static String getEditPersonDescriptorDetails(EditPersonDescriptor descriptor) {
         StringBuilder sb = new StringBuilder();
         descriptor.getName().ifPresent(name -> sb.append(PREFIX_NAME).append(name.fullName).append(" "));
-        descriptor.getPhone().ifPresent(phone -> sb.append(PREFIX_PHONE).append(phone.value).append(" "));
         descriptor.getEmail().ifPresent(email -> sb.append(PREFIX_EMAIL).append(email.value).append(" "));
-        descriptor.getAddress().ifPresent(address -> sb.append(PREFIX_ADDRESS).append(address.value).append(" "));
+        descriptor.getFaculty().ifPresent(faculty -> sb.append(PREFIX_FACULTY).append(faculty.value).append(" "));
+        descriptor.getMajor().ifPresent(major -> sb.append(PREFIX_MAJOR).append(major.value).append(" "));
+
+        if (descriptor.getSkills().isPresent()) {
+            Set<Skill> skills = descriptor.getSkills().get();
+            if (skills.isEmpty()) {
+                sb.append(PREFIX_SKILL).append(" "); // TODO: check why this is here
+            } else {
+                skills.forEach(s -> sb.append(PREFIX_SKILL).append(s.skillName).append(" "));
+            }
+        }
+
+        if (descriptor.getLanguages().isPresent()) {
+            Set<Language> languages = descriptor.getLanguages().get();
+            if (languages.isEmpty()) {
+                sb.append(PREFIX_LANGUAGE).append(" "); // TODO: check why this is here
+            } else {
+                languages.forEach(s -> sb.append(PREFIX_LANGUAGE).append(s.languageName).append(" "));
+            }
+        }
+
+        if (descriptor.getFrameworks().isPresent()) {
+            Set<Framework> frameworks = descriptor.getFrameworks().get();
+            if (frameworks.isEmpty()) {
+                sb.append(PREFIX_FRAMEWORK).append(" "); // TODO: check why this is here
+            } else {
+                frameworks.forEach(s -> sb.append(PREFIX_FRAMEWORK).append(s.frameworkName).append(" "));
+            }
+        }
+
         if (descriptor.getTags().isPresent()) {
             Set<Tag> tags = descriptor.getTags().get();
             if (tags.isEmpty()) {
-                sb.append(PREFIX_TAG);
+                sb.append(PREFIX_TAG); // TODO: check why this is here
             } else {
                 tags.forEach(s -> sb.append(PREFIX_TAG).append(s.tagName).append(" "));
             }
