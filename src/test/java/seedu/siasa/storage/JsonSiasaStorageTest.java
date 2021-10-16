f9fab7bd2dfb2acac82723782a60a879f6cb881a
meerian <myronneozx@gmail.com>
2021-10-17 01:20:26 +0800

    Fix checkstyle errors

--- src/test/java/seedu/siasa/storage/JsonSiasaStorageTest.java
+++ src/test/java/seedu/siasa/storage/JsonSiasaStorageTest.java
@@ -1,7 +1,8 @@
 package seedu.siasa.storage;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import static seedu.siasa.testutil.Assert.assertThrows;
 import static seedu.siasa.testutil.TypicalPersons.HOON;
 import static seedu.siasa.testutil.TypicalPersons.IDA;
--- src/test/java/seedu/siasa/storage/StorageManagerTest.java
+++ src/test/java/seedu/siasa/storage/StorageManagerTest.java
@@ -1,6 +1,9 @@
 package seedu.siasa.storage;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import static seedu.siasa.testutil.TypicalSiasa.getTypicalSiasa;
 
 import java.nio.file.Path;
