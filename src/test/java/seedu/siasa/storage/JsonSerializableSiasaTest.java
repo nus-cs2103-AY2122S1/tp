8c06c55de674fef25f65a327be805a9d2803386d
meerian <myronneozx@gmail.com>
2021-10-17 01:44:27 +0800

    Fix some checks

--- src/test/java/seedu/siasa/storage/JsonSerializableSiasaTest.java
+++ src/test/java/seedu/siasa/storage/JsonSerializableSiasaTest.java
@@ -18,6 +18,7 @@ public class JsonSerializableSiasaTest {
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonSerializableSiasaTest");
     private static final Path TYPICAL_PERSONS_FILE = TEST_DATA_FOLDER.resolve("typicalPersonsSiasa.json");
     private static final Path INVALID_PERSON_FILE = TEST_DATA_FOLDER.resolve("invalidPersonSiasa.json");
+    private static final Path INVALID_POLICY_FILE = TEST_DATA_FOLDER.resolve("invalidPolicySiasa.json");
     private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicatePersonSiasa.json");
     private static final Path DUPLICATE_POLICY_FILE = TEST_DATA_FOLDER.resolve("duplicatePolicySiasa.json");
 
@@ -35,6 +36,14 @@ public class JsonSerializableSiasaTest {
     public void toModelType_invalidPersonFile_throwsIllegalValueException() throws Exception {
         JsonSerializableSiasa dataFromFile = JsonUtil.readJsonFile(INVALID_PERSON_FILE,
                 JsonSerializableSiasa.class).get();
+        assertThrows(IllegalValueException.class, dataFromFile::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidPolicyFile_throwsIllegalValueException() throws Exception {
+        JsonSerializableSiasa dataFromFile = JsonUtil.readJsonFile(INVALID_POLICY_FILE,
+                JsonSerializableSiasa.class).get();
+        assertThrows(IllegalValueException.class, dataFromFile::toModelType);
     }
 
     @Test
