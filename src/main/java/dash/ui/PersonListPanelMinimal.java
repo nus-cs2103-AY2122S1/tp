13af11c492de18197e593135c8e4a8f79ecbbef4
e0543860 <e0543860@u.nus.edu>
2021-10-16 17:11:47 +0800

    Add a side contacts bar to the task tab

---
+++ src/main/java/dash/ui/PersonListPanelMinimal.java
@@ -0,0 +1,49 @@
+package dash.ui;
+
+import dash.commons.core.LogsCenter;
+import dash.model.person.Person;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Region;
+
+import java.util.logging.Logger;
+
+/**
+ * Panel containing the list of persons.
+ */
+public class PersonListPanelMinimal extends UiPart<Region> {
+    private static final String FXML = "PersonListPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(PersonListPanelMinimal.class);
+
+    @FXML
+    private ListView<Person> personListView;
+
+    /**
+     * Creates a {@code PersonListPanel} with the given {@code ObservableList}.
+     */
+    public PersonListPanelMinimal(ObservableList<Person> personList) {
+        super(FXML);
+        personListView.setItems(personList);
+        personListView.setCellFactory(listView -> new PersonListViewCell());
+    }
+
+    /**
+     * Custom {@code ListCell} that displays the graphics of a {@code Person} using a {@code PersonCard}.
+     */
+    class PersonListViewCell extends ListCell<Person> {
+        @Override
+        protected void updateItem(Person person, boolean empty) {
+            super.updateItem(person, empty);
+
+            if (empty || person == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new PersonCardMinimal(person, getIndex() + 1).getRoot());
+            }
+        }
+    }
+
+}
