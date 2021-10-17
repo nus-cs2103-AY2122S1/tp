892cd2ebd7befff03c2348cb0ae204dd9417031f
Yukun99 <yukunxu123@gmail.com>
2021-10-17 23:15:46 +0800

    Rename Constants to CAPITAL

--- src/test/java/seedu/notor/testutil/TypicalTries.java
+++ src/test/java/seedu/notor/testutil/TypicalTries.java
@@ -7,14 +7,14 @@ import java.util.List;
 import seedu.notor.commons.core.trie.Trie;
 
 public class TypicalTries {
-    public static final Trie zeroItem = new Trie();
-    public static final Trie oneItem = Trie.createTrie("aaaaa");
-    public static final Trie twoItem = Trie.createTrie("aaaaa", "bbbbb");
-    public static final Trie fourItem = Trie.createTrie("aaaaa", "aaabb", "aabbb", "bbbbb");
+    public static final Trie ZERO_ITEM = new Trie();
+    public static final Trie ONE_ITEM = Trie.createTrie("aaaaa");
+    public static final Trie TWO_ITEM = Trie.createTrie("aaaaa", "bbbbb");
+    public static final Trie FOUR_ITEM = Trie.createTrie("aaaaa", "aaabb", "aabbb", "bbbbb");
 
     private TypicalTries() {}
 
     public static List<Trie> getTypicalTries() {
-        return new ArrayList<>(Arrays.asList(zeroItem, oneItem, twoItem, fourItem));
+        return new ArrayList<>(Arrays.asList(ZERO_ITEM, ONE_ITEM, TWO_ITEM, FOUR_ITEM));
     }
 }
