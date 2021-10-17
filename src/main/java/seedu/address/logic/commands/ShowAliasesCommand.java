167467e42c1fc055b929c2225e01190a1e225cee
Xiu Wen <xiuwenseow@yahoo.com.sg>
2021-10-17 20:42:37 +0800

    Remove whitespace from success message (aliases)

--- src/main/java/seedu/address/logic/commands/ShowAliasesCommand.java
+++ src/main/java/seedu/address/logic/commands/ShowAliasesCommand.java
@@ -11,7 +11,7 @@ import seedu.address.model.Model;
 public class ShowAliasesCommand extends Command {
 
     public static final String COMMAND_WORD = "aliases";
-    public static final String MESSAGE_SUCCESS = "List of Aliases:\n %1$s";
+    public static final String MESSAGE_SUCCESS = "List of Aliases:\n%1$s";
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
