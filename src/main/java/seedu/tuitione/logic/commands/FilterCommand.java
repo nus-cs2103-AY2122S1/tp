1ef25ab4b65d0a0d1b85a15adc6d341f04c7913c
eezj35 <justineezj0@gmail.com>
2021-10-17 15:59:03 +0800

    Create FilterCommand class
    Filter students only for now

---
+++ src/main/java/seedu/tuitione/logic/commands/FilterCommand.java
@@ -0,0 +1,43 @@
+package seedu.tuitione.logic.commands;
+
+import seedu.tuitione.commons.core.Messages;
+import seedu.tuitione.model.Model;
+import seedu.tuitione.model.student.NameContainsKeywordsPredicate;
+import seedu.tuitione.model.student.isSpecifiedGrade;
+
+import static java.util.Objects.requireNonNull;
+
+public class FilterCommand extends Command{
+
+    public static final String COMMAND_WORD = "filter";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Filters out students and lessons if they are"
+            + " of the specified grade.\n"
+            + "Parameters: GRADE \n"
+            + "Example: " + COMMAND_WORD + " S2";
+
+    private final isSpecifiedGrade predicate;
+
+    public FilterCommand(isSpecifiedGrade predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredStudentList(predicate);
+        String output = model.getFilteredStudentList().size() == 0 || model.getFilteredStudentList().size() == 1
+                ? String.format(Messages.MESSAGE_SINGULAR_STUDENT_LISTED_OVERVIEW,
+                model.getFilteredStudentList().size())
+                : String.format(Messages.MESSAGE_PLURAL_STUDENT_LISTED_OVERVIEW,
+                model.getFilteredStudentList().size());
+        return new CommandResult(output);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof FilterCommand // instanceof handles nulls
+                && predicate.equals(((FilterCommand) other).predicate)); // state check
+    }
+}
