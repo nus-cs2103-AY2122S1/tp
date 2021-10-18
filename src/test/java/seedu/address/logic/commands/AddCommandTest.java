ac669ebe7eb97de5ea08ee1710acb85b55a2b17b
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2021-10-18 17:47:27 +0800

    Fix ModelStub in AddCommandTest

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -119,6 +119,11 @@ public class AddCommandTest {
         }
 
         @Override
+        public int importAddressBook(ReadOnlyAddressBook addressBook) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public ReadOnlyAddressBook getAddressBook() {
             throw new AssertionError("This method should not be called.");
         }
