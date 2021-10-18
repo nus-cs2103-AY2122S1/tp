875affd66d6cd0ee84ab0f016a6fdcb8e5e2c63a
trash-bin99 <77224811+trash-bin99@users.noreply.github.com>
2021-10-18 20:12:30 +0800

    Remove unused method

--- src/main/java/seedu/fast/logic/commands/TagCommandUtils.java
+++ src/main/java/seedu/fast/logic/commands/TagCommandUtils.java
@@ -18,23 +18,6 @@ public class TagCommandUtils {
     };
 
     /**
-     * Checks if a given Set of Tags contain a Tag that is equal to a given Tag.
-     * Conditions for Tags to be equal: They are the same object, or they have the same Tag name.
-     *
-     * @param tags The Set of Tags to be checked.
-     * @param newTag The tag to be compared with.
-     * @return A boolean indicating the result.
-     */
-    public static boolean checkIfContains(Set<Tag> tags, Tag newTag) {
-        for (Tag tag : tags) {
-            if (tag.equals(newTag)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
      * Checks if a given Set of Tags contains multiple Priority tags.
      *
      * @param tags The Set of Tags to be checked.
