74517f2850d3e61b18e87e259a342006d0ac3efc
twothicc <chenenliuu@gmail.com>
2021-10-18 15:20:49 +0800

    Reduce method length and modify ShowCommand to find unique search terms in all applicants

--- src/main/java/seedu/address/logic/commands/ShowCommand.java
+++ src/main/java/seedu/address/logic/commands/ShowCommand.java
@@ -8,12 +8,15 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ROLE;
 
-import java.util.List;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javafx.collections.ObservableList;
 import seedu.address.logic.parser.Prefix;
 import seedu.address.model.Model;
+import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.person.Person;
 
 
 /**
@@ -47,78 +50,76 @@ public class ShowCommand extends Command {
 
     private String getUniqueCategoryInputs(Model model) {
 
+        // obtains an unmodifiable list of all applicants
+        ReadOnlyAddressBook addressBook = model.getAddressBook();
+        ObservableList<Person> ol = addressBook.getPersonList();
+
         String prefixString = prefix.getPrefix();
 
+        // temporary variables to hold unique search terms and part of UI message to user
+        Set<String> uniqueInputs = new HashSet<>();
+        String userText = "";
+
         if (prefixString.equals(PREFIX_NAME.getPrefix())) {
-
-            List<String> names = model.getFilteredPersonList().stream()
-                    .map(x -> x.getName().toString()).collect(Collectors.toList());
-
-            StringBuilder sb = new StringBuilder("Here are all the names present:\n");
-            for (String name: names) {
-                sb.append(name);
-                sb.append("\n");
-            }
-
-            return sb.toString();
+            userText = "names";
+            uniqueInputs = getUniqueNameInputs(ol);
         }
 
         if (prefixString.equals(PREFIX_PHONE.getPrefix())) {
-
-            List<String> phoneNumbers = model.getFilteredPersonList().stream()
-                    .map(x -> x.getPhone().toString()).collect(Collectors.toList());
-
-            StringBuilder sb = new StringBuilder("Here are all the phone numbers present:\n");
-            for (String phoneNumber: phoneNumbers) {
-                sb.append(phoneNumber);
-                sb.append("\n");
-            }
-
-            return sb.toString();
+            userText = "contact numbers";
+            uniqueInputs = getUniquePhoneInputs(ol);
         }
 
         if (prefixString.equals(PREFIX_EMAIL.getPrefix())) {
-
-            List<String> emails = model.getFilteredPersonList().stream()
-                    .map(x -> x.getEmail().toString()).collect(Collectors.toList());
-
-            StringBuilder sb = new StringBuilder("Here are all the emails present:\n");
-            for (String email: emails) {
-                sb.append(email);
-                sb.append("\n");
-            }
-
-            return sb.toString();
+            userText = "emails";
+            uniqueInputs = getUniqueEmailInputs(ol);
         }
 
         if (prefixString.equals(PREFIX_ROLE.getPrefix())) {
-
-            Set<String> roles = model.getFilteredPersonList().stream()
-                    .map(x -> x.getRole().toString()).collect(Collectors.toSet());
-
-            StringBuilder sb = new StringBuilder("Here are all the roles present:\n");
-            for (String role: roles) {
-                sb.append(role);
-                sb.append("\n");
-            }
-
-            return sb.toString();
+            userText = "roles";
+            uniqueInputs = getUniqueRoleInputs(ol);
         }
 
         if (prefixString.equals(PREFIX_EMPLOYMENT_TYPE.getPrefix())) {
-
-            Set<String> employmentTypes = model.getFilteredPersonList().stream()
-                    .map(x -> x.getEmploymentType().toString()).collect(Collectors.toSet());
-
-            StringBuilder sb = new StringBuilder("Here are all the employment types present:\n");
-            for (String employmentType: employmentTypes) {
-                sb.append(employmentType);
-                sb.append("\n");
-            }
-
-            return sb.toString();
+            userText = "employment types";
+            uniqueInputs = getUniqueEmploymentTypeInputs(ol);
         }
 
-        return "";
+        if (!uniqueInputs.isEmpty()) {
+            StringBuilder sb = new StringBuilder("Here are all the " + userText + " present:\n");
+            for (String uniqueInput: uniqueInputs) {
+                sb.append(uniqueInput);
+                sb.append("\n");
+            }
+            return sb.toString();
+        } else {
+            return "No search terms exist for " + userText;
+        }
     }
+
+    private Set<String> getUniqueEmploymentTypeInputs(ObservableList<Person> ol) {
+        return ol.stream()
+                .map(x -> x.getEmploymentType().toString()).collect(Collectors.toSet());
+    }
+
+    private Set<String> getUniqueRoleInputs(ObservableList<Person> ol) {
+        return ol.stream()
+                .map(x -> x.getRole().toString()).collect(Collectors.toSet());
+    }
+
+    private Set<String> getUniqueEmailInputs(ObservableList<Person> ol) {
+        return ol.stream()
+                .map(x -> x.getEmail().toString()).collect(Collectors.toSet());
+    }
+
+    private Set<String> getUniquePhoneInputs(ObservableList<Person> ol) {
+        return ol.stream()
+                .map(x -> x.getPhone().toString()).collect(Collectors.toSet());
+    }
+
+    private Set<String> getUniqueNameInputs(ObservableList<Person> ol) {
+        return ol.stream()
+                .map(x -> x.getName().toString()).collect(Collectors.toSet());
+    }
+
 }
