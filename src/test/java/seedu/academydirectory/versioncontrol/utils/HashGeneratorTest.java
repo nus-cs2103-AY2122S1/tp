83edcec5947059fb3cd162c7a9c0a083ca6ade47
Amadeus-Winarto <amadeusw6@gmail.com>
2021-10-18 18:20:17 +0800

    reduce test

--- src/test/java/seedu/academydirectory/versioncontrol/utils/HashGeneratorTest.java
+++ src/test/java/seedu/academydirectory/versioncontrol/utils/HashGeneratorTest.java
@@ -1,37 +1,33 @@
 package seedu.academydirectory.versioncontrol.utils;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 
 import org.junit.jupiter.api.Test;
 
 public class HashGeneratorTest {
-    private final HashMethod[] supportedMethods = {HashMethod.MD5, HashMethod.SHA1, HashMethod.SHA256};
+    private static final HashMethod[] supportedMethods = {HashMethod.SHA1, HashMethod.SHA256};
+    private static final HashMap<HashMethod, String> expectedHash = new HashMap<>();
+    private static final String filename = "CommitParserTest";
 
     @Test
     public void hashGenerator_correctHash() {
-        String filename = "CommitParserTest";
+        expectedHash.put(HashMethod.MD5, "00f8e0d3e2f06918e4b3f2ef2709e2df");
+        expectedHash.put(HashMethod.SHA1, "1ba3aa88d488e76d30086af249a8d17072e04185");
+        expectedHash.put(HashMethod.SHA256, "0c54914698ffaee27e52d66038ced1b69c2cea854c16c906c6764196514217d5");
+
         Path filepath = Paths.get("src", "test", "data", "VersionControlTest", "ParserTest", filename);
         assertTrue(() -> filepath.toFile().exists());
 
-        String[] actualHash = new String[3];
-        String[] expectedHash = {
-            "00f8e0d3e2f06918e4b3f2ef2709e2df",
-            "1ba3aa88d488e76d30086af249a8d17072e04185",
-            "0c54914698ffaee27e52d66038ced1b69c2cea854c16c906c6764196514217d5"
-        };
-
-        int idx = 0;
         for (HashMethod hashMethod: supportedMethods) {
             HashGenerator generator = new HashGenerator(hashMethod);
-            actualHash[idx] = assertDoesNotThrow(() -> generator.generateHashFromFile(filepath));
-            idx += 1;
+            String actualHash = assertDoesNotThrow(() -> generator.generateHashFromFile(filepath));
+            assertEquals(expectedHash.get(hashMethod), actualHash);
         }
-
-        assertArrayEquals(actualHash, expectedHash);
     }
 }
