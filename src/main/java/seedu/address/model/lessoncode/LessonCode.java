cc551b27caefb0e8305b9240e415ef21b7d998f2
Jason <jason.ng.zq99@gmail.com>
2021-10-19 02:16:25 +0800

    Edit MESSAGE_CONSTRAINTS and javadocs

--- src/main/java/seedu/address/model/lessoncode/LessonCode.java
+++ src/main/java/seedu/address/model/lessoncode/LessonCode.java
@@ -9,7 +9,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class LessonCode {
 
-    public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric";
+    public static final String MESSAGE_CONSTRAINTS = "Lesson codes should be alphanumeric";
     public static final String VALIDATION_REGEX = "\\p{Alnum}+";
 
     public final String lessonCode;
@@ -17,7 +17,7 @@ public class LessonCode {
     /**
      * Constructs a {@code LessonCode}.
      *
-     * @param lessonCode A valid tag name.
+     * @param lessonCode A valid lesson code.
      */
     public LessonCode(String lessonCode) {
         requireNonNull(lessonCode);
