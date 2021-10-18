011d7051028ef8a01a26f78726829e37cfa8f945
Xiu Wen <xiuwenseow@yahoo.com.sg>
2021-10-18 22:44:50 +0800

    Remove commands from Command class

--- src/main/java/seedu/address/logic/commands/Command.java
+++ src/main/java/seedu/address/logic/commands/Command.java
@@ -1,7 +1,5 @@
 package seedu.address.logic.commands;
 
-import java.util.List;
-
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
@@ -9,28 +7,6 @@ import seedu.address.model.Model;
  * Represents a command with hidden internal logic and the ability to be executed.
  */
 public abstract class Command {
-
-    /** All possible commands. */
-    private static List<String> commands = List.of(AddMemberCommand.COMMAND_WORD, EditMemberCommand.COMMAND_WORD,
-            DeleteMemberCommand.COMMAND_WORD, DeleteFacilityCommand.COMMAND_WORD,
-            EditFacilityCommand.COMMAND_WORD, ClearMembersCommand.COMMAND_WORD,
-            ClearFacilitiesCommand.COMMAND_WORD, FindMemberCommand.COMMAND_WORD,
-            ListMemberCommand.COMMAND_WORD, ListFacilityCommand.COMMAND_WORD, ExitCommand.COMMAND_WORD,
-            HelpCommand.COMMAND_WORD, FindFacilityCommand.COMMAND_WORD, AddFacilityCommand.COMMAND_WORD,
-            SetMemberAvailabilityCommand.COMMAND_WORD, SplitCommand.COMMAND_WORD,
-            AddAliasCommand.COMMAND_WORD, DeleteAliasCommand.COMMAND_WORD, ShowAliasesCommand.COMMAND_WORD);
-
-    /**
-     * Returns true if given string is a command word.
-     *
-     * @param command string to check.
-     * @return true if is command, false if otherwise.
-     */
-    public static boolean isCommand(String command) {
-        return commands.contains(command);
-    }
-
-
     /**
      * Executes the command and returns the result message.
      *
