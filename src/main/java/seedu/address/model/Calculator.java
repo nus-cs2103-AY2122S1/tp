b81e1caca0b039eefd462193c301fc83d507b078
BenjaminTan99 <benjamin.tan.chin.han@gmail.com>
2021-10-19 13:24:40 +0800

    Add Calculator class

---
+++ src/main/java/seedu/address/model/Calculator.java
@@ -0,0 +1,13 @@
+package seedu.address.model;
+
+/**
+ * This singleton class deals with calculating the rejection rate of a specified position only.
+ */
+public class Calculator {
+    public int calculateRejRate(int totalApplied, int totalRejected) {
+        if (totalApplied == 0) {
+            return -1;
+        }
+        return totalRejected / totalApplied;
+    }
+}
