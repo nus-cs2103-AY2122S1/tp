22382864aaadc853b37151219be0f5323eaae8bd
MGRL2201 <liminggaorickie@outlook.com>
2021-10-19 19:12:32 +0800

    Revert "Update unenroll command tests"
    This reverts commit 091e04d43d771d8a731ca4f89c3d0395e9610637.

--- src/test/java/seedu/tuitione/logic/commands/UnenrollCommandTest.java
+++ src/test/java/seedu/tuitione/logic/commands/UnenrollCommandTest.java
@@ -4,11 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static seedu.tuitione.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.tuitione.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.tuitione.testutil.TypicalIndexes.INDEX_FIRST_LESSON;
 import static seedu.tuitione.testutil.TypicalIndexes.INDEX_FIRST_STUDENT;
-import static seedu.tuitione.testutil.TypicalIndexes.INDEX_SECOND_LESSON;
 import static seedu.tuitione.testutil.TypicalIndexes.INDEX_SECOND_STUDENT;
-import static seedu.tuitione.testutil.TypicalIndexes.INDEX_THIRD_LESSON;
 import static seedu.tuitione.testutil.TypicalIndexes.INDEX_THIRD_STUDENT;
 import static seedu.tuitione.testutil.TypicalTuition.getTypicalTuitione;
 
@@ -21,6 +18,7 @@ import seedu.tuitione.model.Model;
 import seedu.tuitione.model.ModelManager;
 import seedu.tuitione.model.UserPrefs;
 import seedu.tuitione.model.lesson.Lesson;
+import seedu.tuitione.model.lesson.LessonCode;
 import seedu.tuitione.model.student.Student;
 
 public class UnenrollCommandTest {
@@ -34,12 +32,13 @@ public class UnenrollCommandTest {
 
     @Test
     public void execute_validUnenrollment_success() {
-        UnenrollCommand unenrollCommand = new UnenrollCommand(INDEX_SECOND_STUDENT, INDEX_SECOND_LESSON);
+        LessonCode code = new LessonCode("Mathematics-S2-Tue-0930");
+        UnenrollCommand unenrollCommand = new UnenrollCommand(INDEX_SECOND_STUDENT, code.value);
 
         Model expectedModel = new ModelManager(getTypicalTuitione(), new UserPrefs());
         Student studentToUnenroll = expectedModel.getFilteredStudentList().get(INDEX_SECOND_STUDENT.getZeroBased());
 
-        Lesson testLesson = expectedModel.getFilteredLessonList().get(INDEX_SECOND_LESSON.getZeroBased());
+        Lesson testLesson = expectedModel.searchLessons(code).get();
         testLesson.removeStudent(studentToUnenroll);
         String expectedMessage = String.format(UnenrollCommand.MESSAGE_UNENROLL_STUDENT_SUCCESS,
                 studentToUnenroll.getName(),
@@ -50,52 +49,45 @@ public class UnenrollCommandTest {
 
     @Test
     public void execute_invalidUnenrollment_failure() {
-        Lesson testLesson = model.getFilteredLessonList().get(INDEX_THIRD_LESSON.getZeroBased());
+        LessonCode code = new LessonCode("Physics-S2-Tue-0930");
         Student alice = model.getFilteredStudentList().get(INDEX_FIRST_STUDENT.getZeroBased()); //ALICE
         Student benson = model.getFilteredStudentList().get(INDEX_SECOND_STUDENT.getZeroBased()); //BENSON
         Student carl = model.getFilteredStudentList().get(INDEX_THIRD_STUDENT.getZeroBased()); //CARL
 
         String expectedMessageAlice = String.format(UnenrollCommand.MESSAGE_STUDENT_NOT_IN_LESSON,
                 alice.getName(),
-                testLesson.getLessonCode());
+                code);
         String expectedMessageBenson = String.format(UnenrollCommand.MESSAGE_STUDENT_NOT_IN_LESSON,
                 benson.getName(),
-                testLesson.getLessonCode());
+                code);
         String expectedMessageCarl = String.format(UnenrollCommand.MESSAGE_STUDENT_NOT_IN_LESSON,
                 carl.getName(),
-                testLesson.getLessonCode());
+                code);
 
-        assertCommandFailure(new UnenrollCommand(INDEX_FIRST_STUDENT, INDEX_THIRD_LESSON), model, expectedMessageAlice);
-        assertCommandFailure(new UnenrollCommand(INDEX_SECOND_STUDENT, INDEX_THIRD_LESSON), model, expectedMessageBenson);
-        assertCommandFailure(new UnenrollCommand(INDEX_THIRD_STUDENT, INDEX_THIRD_LESSON), model, expectedMessageCarl);
+        assertCommandFailure(new UnenrollCommand(INDEX_FIRST_STUDENT, code.value), model, expectedMessageAlice);
+        assertCommandFailure(new UnenrollCommand(INDEX_SECOND_STUDENT, code.value), model, expectedMessageBenson);
+        assertCommandFailure(new UnenrollCommand(INDEX_THIRD_STUDENT, code.value), model, expectedMessageCarl);
     }
 
     @Test
-    public void execute_invalidUnenrollmentWrongStudentIndex_failure() {
+    public void execute_invalidUnenrollmentWrongIndex_failure() {
+        LessonCode code = new LessonCode("Mathematics-S2-Tue-0930");
         Index outOfBoundIndex = Index.fromOneBased(model.getFilteredStudentList().size() + 1);
-        UnenrollCommand unenrollCommand = new UnenrollCommand(outOfBoundIndex, INDEX_FIRST_LESSON);
+        UnenrollCommand unenrollCommand = new UnenrollCommand(outOfBoundIndex, code.value);
 
         assertCommandFailure(unenrollCommand, model, Messages.MESSAGE_INVALID_STUDENT_DISPLAYED_INDEX);
     }
 
     @Test
-    public void execute_invalidUnenrollmentWrongLessonIndex_failure() {
-        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredLessonList().size() + 1);
-        UnenrollCommand unenrollCommand = new UnenrollCommand(INDEX_SECOND_STUDENT, outOfBoundIndex);
-
-        assertCommandFailure(unenrollCommand, model, Messages.MESSAGE_INVALID_LESSON_DISPLAYED_INDEX);
-    }
-
-    @Test
     public void equals() {
-        UnenrollCommand unenrollFirstCommand = new UnenrollCommand(INDEX_FIRST_STUDENT, INDEX_FIRST_LESSON);
-        UnenrollCommand unenrollSecondCommand = new UnenrollCommand(INDEX_SECOND_STUDENT, INDEX_SECOND_LESSON);
+        UnenrollCommand unenrollFirstCommand = new UnenrollCommand(INDEX_FIRST_STUDENT, "l/Science-P5-Wed-1230");
+        UnenrollCommand unenrollSecondCommand = new UnenrollCommand(INDEX_SECOND_STUDENT, "l/Science-P5-Wed-1230");
 
         // same object -> returns true
         assertEquals(unenrollFirstCommand, unenrollFirstCommand);
 
         // same values -> returns true
-        UnenrollCommand enrollFirstCommandCopy = new UnenrollCommand(INDEX_FIRST_STUDENT, INDEX_FIRST_LESSON);
+        UnenrollCommand enrollFirstCommandCopy = new UnenrollCommand(INDEX_FIRST_STUDENT, "l/Science-P5-Wed-1230");
         assertEquals(unenrollFirstCommand, enrollFirstCommandCopy);
 
         // different types -> returns false
