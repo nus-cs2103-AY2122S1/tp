a6d667b94db21ba257e733115b5c257fa47319f6
shermannws <shermannws@gmail.com>
2021-10-20 13:55:45 +0800

    Add Javadoc comments

--- src/main/java/seedu/programmer/ui/CommandHistory.java
+++ src/main/java/seedu/programmer/ui/CommandHistory.java
@@ -9,12 +9,23 @@ public class CommandHistory {
     private static int size = 0;
     private static int counter = -1;
 
+    /**
+     * Adds the {@code command} to the {@code commandHistory}.
+     * {@code counter} resets itself to point to the most recently added command to {@code commandHistory}.
+     * @param command The string to be added to the history of commands.
+     */
     public static void add(String command) {
         commandHistory.add(command);
         size++;
         counter = size - 1;
     }
-    
+
+    /**
+     * Returns the next most recent entered command according to the {@code counter} pointer.
+     * Returns the {@code DEFAULT_COMMAND} if the {@code commandHistory} is empty.
+     * Returns the least recent command if the {@code counter} is already pointer at the oldest command.
+     * @return The string of the next most recent entered command.
+     */
     public static String getPrevCommand() {
         if (counter == -1) {
             return DEFAULT_COMMAND;
@@ -25,6 +36,12 @@ public class CommandHistory {
         return commandHistory.get(counter--);
     }
 
+    /**
+     * Returns the next least recent entered command according to the {@code counter} pointer.
+     * Returns the {@code DEFAULT_COMMAND} if the {@code commandHistory} is empty.
+     * Returns the most recent command if the {@code counter} is already pointer at the latest command.
+     * @return The string of the next least recent entered command.
+     */
     public static String getNextCommand() {
         if (counter == -1) {
             return DEFAULT_COMMAND;
@@ -35,5 +52,4 @@ public class CommandHistory {
         }
         return commandHistory.get(counter++);
     }
-    
 }
