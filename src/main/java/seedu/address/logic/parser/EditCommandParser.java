04f56e9a1ac487ec456d29397c6fd7062530cf9a
LeopardMerkava <andrewbaruch4@gmail.com>
2021-10-21 07:14:59 +0800

    Fix bug

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -37,7 +37,7 @@ public class EditCommandParser implements Parser<EditCommand> {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG,
-                        PREFIX_TASK);
+                        PREFIX_TASK, PREFIX_DESCRIPTION);
 
         Index index;
 
