19cdaa6f43b031f4032cdfb1cd299a06dda0375a
KT27Learn <keithtan20@gmail.com>
2021-10-21 02:47:44 +0800

    Add list of upcoming meetings for scheduler

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -25,6 +25,7 @@ import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.person.ClientId;
+import seedu.address.model.person.NextMeeting;
 import seedu.address.model.person.Person;
 import seedu.address.model.tag.Tag;
 import seedu.address.testutil.PersonBuilder;
@@ -161,6 +162,11 @@ public class AddCommandTest {
 
         @Override
         public ObservableList<Person> getFilteredPersonList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<NextMeeting> getFilteredNextMeetingList() {
             throw new AssertionError("This method should not be called.");
         }
 
