68514131216842d7159de7776a928003440951e4
Chen Hsiao Ting <hsiaoting0919@gmail.com>
2021-10-20 17:03:09 +0800

    Add DeleteMultipleCommand

--- src/main/java/seedu/address/logic/commands/DeleteMultipleCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteMultipleCommand.java
@@ -7,7 +7,9 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NATIONALITY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TUTORIAL_GROUP;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import java.util.List;
 import java.util.function.Predicate;
 
 import seedu.address.commons.core.Messages;
@@ -15,15 +17,15 @@ import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 
 /**
- * Finds and lists all persons in address book whose name contains any of the argument keywords.
+ * Deletes all persons in address book whose name contains any of the argument keywords.
  * Keyword matching is case insensitive.
  */
-public class FindCommand extends Command {
+public class DeleteMultipleCommand extends Command {
 
-    public static final String COMMAND_WORD = "find";
+    public static final String COMMAND_WORD = "deletem";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose details contain any of "
-            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Deletes all persons whose details contain any of "
+            + "the specified keywords (case-insensitive) from the list.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "[" + PREFIX_NAME + "NAME] " + "[" + PREFIX_NAME + "MORE_NAMES] "
             + "[" + PREFIX_PHONE + "PHONE] " + "[" + PREFIX_PHONE + "MORE_PHONES] "
@@ -33,9 +35,11 @@ public class FindCommand extends Command {
             + "[" + PREFIX_TAG + "TAG] " + "[" + PREFIX_TAG + "MORE_TAGS]...\n "
             + "Example: " + COMMAND_WORD + " n/alice p/91234567 tg/19";
 
+    private int startIndex;
+    private int endIndex;
     private Predicate<Person> predicate;
 
-    public FindCommand(Predicate<Person> predicate) {
+    public DeleteMultipleCommand(Predicate<Person> predicate) {
         this.predicate = predicate;
     }
 
@@ -43,14 +47,27 @@ public class FindCommand extends Command {
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredPersonList(predicate);
-        return new CommandResult(
-                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        this.startIndex = 0;
+        this.endIndex = lastShownList.size();
+
+        StringBuilder result = new StringBuilder(
+                );
+
+        for (int i = startIndex; i < endIndex; i++) {
+            Person personToDelete = lastShownList.get(startIndex);
+            model.deletePerson(personToDelete);
+        }
+
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        return new CommandResult(String.format(Messages.MESSAGE_MULTIPLE_PERSONS_DELETED_OVERVIEW, endIndex));
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
-                || (other instanceof FindCommand // instanceof handles nulls
-                && predicate.equals(((FindCommand) other).predicate)); // state check
+                || (other instanceof DeleteMultipleCommand // instanceof handles nulls
+                && predicate.equals(((DeleteMultipleCommand) other).predicate)); // state check
     }
 }
