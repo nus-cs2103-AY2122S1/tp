882bd5953515a09fbf7bef00ff7399cd5da72234
LMAOboxhack <linfangyuan99@gmail.com>
2021-10-20 16:01:00 +0800

    Rename AddressBookParser to MrTechRecruiterParser

--- src/test/java/seedu/address/logic/parser/MrTechRecruiterParserTest.java
+++ src/test/java/seedu/address/logic/parser/MrTechRecruiterParserTest.java
@@ -29,9 +29,9 @@ import seedu.address.testutil.EditPersonDescriptorBuilder;
 import seedu.address.testutil.PersonBuilder;
 import seedu.address.testutil.PersonUtil;
 
-public class AddressBookParserTest {
+public class MrTechRecruiterParserTest {
 
-    private final AddressBookParser parser = new AddressBookParser();
+    private final MrTechRecruiterParser parser = new MrTechRecruiterParser();
 
     @Test
     public void parseCommand_add() throws Exception {
