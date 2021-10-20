31e9247edc339e52920af0bc47b424bbd3e3ea2c
TohWeiJie01 <tohweijie01@gmail.com>
2021-10-20 22:59:36 +0800

    Edit Done class

--- src/main/java/seedu/address/model/done/Done.java
+++ src/main/java/seedu/address/model/done/Done.java
@@ -1,49 +1,56 @@
 package seedu.address.model.done;
 
+import java.util.Stack;
+
 /**
  * Represents an Applicant's status in RecruitIn; if an Applicant is considered Done, it means that
  * the recruiter has finished helping an Applicant out and will no longer need to contact the Applicant.
  */
 public class Done {
 
-    public static final String DONE_MESSAGE = "This applicant is Done";
-    public static final String UNDONE_MESSAGE = "This applicant is not yet Done";
+    public static final String STATUS_DONE = "Done";
+    public static final String STATUS_UNDONE = "Not Done";
 
-    private Boolean isDone;
+    private String doneStatus;
+    private boolean isDone;
 
     /**
-     * Constructor for Done; every Applicant will initially be marked as not Done.
+     * Constructor for Done; every Applicant will initially be marked as Not Done.
      */
     public Done() {
+        this.doneStatus = STATUS_UNDONE;
         this.isDone = false;
     }
 
-    public Boolean applicantDoneStatus() {
-        return isDone;
+    public String getDoneStatus() {
+        return doneStatus;
     }
 
-    public void setOppositeDoneStatus() {
-        isDone = !isDone;
+    public void setAsDone() {
+        this.doneStatus = STATUS_DONE;
+        this.isDone = true;
+    }
+
+    public void setAsUndone() {
+        this.doneStatus = STATUS_UNDONE;
+        this.isDone = false;
     }
 
     @Override
     public String toString() {
-        if (isDone) {
-            return DONE_MESSAGE;
-        } else {
-            return UNDONE_MESSAGE;
-        }
+        return doneStatus;
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Done // instanceof handles nulls
-                && (isDone.equals(((Done) other).isDone))); // state check
+                && (doneStatus.equals(((Done) other).doneStatus))
+                && (isDone && ((Done) other).isDone)); // state check
     }
 
     @Override
     public int hashCode() {
-        return isDone.hashCode();
+        return doneStatus.hashCode();
     }
 }
