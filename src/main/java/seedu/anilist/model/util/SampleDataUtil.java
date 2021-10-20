b9898a76d7615ad8647a33118a0e08a99377d938
LuoDan <69646558+luodan01@users.noreply.github.com>
2021-10-20 10:58:41 +0800

    Add animes to sample data
    - Add two finished animes and two towatch animes to sample data to help with testing
    - Replaced Higehiro

--- src/main/java/seedu/anilist/model/util/SampleDataUtil.java
+++ src/main/java/seedu/anilist/model/util/SampleDataUtil.java
@@ -7,7 +7,9 @@ import java.util.stream.Collectors;
 import seedu.anilist.model.AnimeList;
 import seedu.anilist.model.ReadOnlyAnimeList;
 import seedu.anilist.model.anime.Anime;
+import seedu.anilist.model.anime.Episode;
 import seedu.anilist.model.anime.Name;
+import seedu.anilist.model.anime.Status;
 import seedu.anilist.model.genre.Genre;
 
 /**
@@ -15,16 +17,24 @@ import seedu.anilist.model.genre.Genre;
  */
 public class SampleDataUtil {
     public static Anime[] getSampleAnime() {
-        return new Anime[] {
+        return new Anime[]{
             new Anime(new Name("Asobi Asobase"), getGenreSet("comedy")),
             new Anime(new Name("BLEACH"), getGenreSet("adventure", "supernatural")),
             new Anime(new Name("Charlotte"), getGenreSet("drama", "supernatural")),
             new Anime(new Name("Darling in the Franxx"), getGenreSet("action", "mecha", "science fiction")),
             new Anime(new Name("Haikyuu!!: To the Top"), getGenreSet("sports")),
-            new Anime(new Name("Higehiro: After Being Rejected, I Shaved and Took in a High School Runaway"),
+            new Anime(new Name("Steins;Gate 0"), getGenreSet("science fiction")),
+            new Anime(new Name("Watashi ga Motenai no wa Dou Kangaetemo Omaera ga Warui!"),
                     getGenreSet("genre1", "genre2", "genre3", "genre4", "genre5", "genre6", "genre7", "genre8",
                             "genre9", "genre10", "genre11", "genre12", "genre13", "genre14", "genre15")),
-            new Anime(new Name("Steins;Gate 0"), getGenreSet("science fiction"))
+            new Anime(new Name("3-gatsu no Lion 2nd Season"), new Episode("22"), new Status("finished"),
+                    getGenreSet("drama", "slice of life")),
+            new Anime(new Name("Fullmetal Alchemist: Brotherhood"), new Episode("64"), new Status("finished"),
+                    getGenreSet("action", "adventure", "comedy", "drama", "fantasy")),
+            new Anime(new Name("Blue Period"), new Episode("0"), new Status("towatch"),
+                    getGenreSet( "drama", "slice of life")),
+            new Anime(new Name("Komi-san wa, Comyushou desu."), new Episode("0"), new Status("towatch"),
+                    getGenreSet( "comedy", "slice of life"))
         };
     }
 
