9ada658005ac863bb922a10c791028a4f5874242
Zhou-Jiahao-1998 <54730603+Zhou-Jiahao-1998@users.noreply.github.com>
2021-10-21 23:03:44 +0800

    Command result should not be empty

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -35,6 +35,7 @@ public class FindCommand extends Command {
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredPersonList(predicate);
+        assert(predicate.toString() != "");
         return new CommandResult("Showing results for: " + '\n' + predicate + '\n'
                 + String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
     }
