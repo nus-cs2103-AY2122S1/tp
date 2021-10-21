90569fb7f446ace80d1655de2706f40884e792c0
halfofning <77186833+wanninglim@users.noreply.github.com>
2021-10-21 18:03:53 +0800

    Clean up code

--- src/test/java/seedu/address/logic/commands/FindStudentCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindStudentCommandTest.java
@@ -64,6 +64,7 @@ public class FindStudentCommandTest {
         assertEquals(Collections.emptyList(), model.getFilteredStudentList());
     }
 
+    @Test
     public void execute_multipleKeywords_multipleStudentsFound() {
         String expectedMessage = String.format(MESSAGE_STUDENTS_LISTED_OVERVIEW, 3);
         NameContainsKeywordsPredicate predicate = preparePredicate("Kurz Elle Kunz");
