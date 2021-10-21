3715a66a5c280cce46b07cf02adf7fcb8dbb406c
HongshuW <hongshu.wang.uni@gmail.com>
2021-10-21 16:19:49 +0800

    JsonAdaptedOrder class: change to map

--- src/main/java/seedu/address/storage/JsonAdaptedOrder.java
+++ src/main/java/seedu/address/storage/JsonAdaptedOrder.java
@@ -1,50 +1,42 @@
 package seedu.address.storage;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.order.Order;
 import seedu.address.model.product.Quantity;
 
 public class JsonAdaptedOrder {
-    private final ArrayList<Object> orderProperties = new ArrayList<>();
+    private final LocalDate time;
+    private final int productId;
+    private final String quantity;
 
     /**
      * Constructs a {@code JsonAdaptedOrder} with the given {@code time}, {@code productId} and {@code quantity}.
      */
     @JsonCreator
-    public JsonAdaptedOrder(LocalDate time, int productId, Quantity quantity) {
-        orderProperties.add(productId);
-        orderProperties.add(quantity.value);
-        orderProperties.add(time);
+    private JsonAdaptedOrder(@JsonProperty("time") LocalDate time, @JsonProperty("productId") int productId,
+                             @JsonProperty("quantity") String quantity) {
+        this.time = time;
+        this.productId = productId;
+        this.quantity = quantity;
     }
 
     /**
      * Converts a given {@code Order} into this class for Jackson use.
      */
     public JsonAdaptedOrder(Order source) {
-        orderProperties.add(source.productId);
-        orderProperties.add(source.quantity.value);
-        orderProperties.add(source.time);
-    }
-
-    @JsonValue
-    public ArrayList<Object> getOrderProperties() {
-        return orderProperties;
+        this(source.time, source.productId, source.quantity.value);
     }
 
     /**
      * Converts this Jackson-friendly adapted order object into the model's {@code Order} object.
      */
     public Order toModelType() throws IllegalValueException {
-        int productId = (Integer) orderProperties.get(0);
-        String quantityVal = (String) orderProperties.get(1);
-        LocalDate time = (LocalDate) orderProperties.get(2);
-        Quantity quantity = new Quantity(quantityVal);
-        return new Order(time, productId, quantity);
+        Quantity q = new Quantity(quantity);
+        return new Order(time, productId, q);
     }
 }
