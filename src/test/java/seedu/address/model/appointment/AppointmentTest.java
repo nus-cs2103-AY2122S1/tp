a06b311372239748669bfd7f1ce2b07305da348e
Richard Li <richardlidefa@gmail.com>
2021-10-22 01:02:05 +0800

    Resolve checkstyle warninings

--- src/test/java/seedu/address/model/appointment/AppointmentTest.java
+++ src/test/java/seedu/address/model/appointment/AppointmentTest.java
@@ -4,10 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
-import org.junit.jupiter.api.Test;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+
+import org.junit.jupiter.api.Test;
 
 public class AppointmentTest {
     @Test
