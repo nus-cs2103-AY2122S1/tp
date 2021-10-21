991910b32838ce6dfd89fd20265dd428cfb31003
erwinqxy <erwin.quek.xian.yi@gmail.com>
2021-10-21 16:40:05 +0800

    Fix style

--- src/main/java/seedu/programmer/model/student/UniqueStudentList.java
+++ src/main/java/seedu/programmer/model/student/UniqueStudentList.java
@@ -3,13 +3,11 @@ package seedu.programmer.model.student;
 import static java.util.Objects.requireNonNull;
 import static seedu.programmer.commons.util.CollectionUtil.requireAllNonNull;
 
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import seedu.programmer.commons.util.CollectionUtil;
 import seedu.programmer.model.student.exceptions.DuplicateStudentException;
 import seedu.programmer.model.student.exceptions.StudentNotFoundException;
 
