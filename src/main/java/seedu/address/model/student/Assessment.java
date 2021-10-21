a54187c786818af96a5f3c19d7466d0c8806d0f2
junwei26 <limjunwei26@gmail.com>
2021-10-21 19:11:55 +0800

    Add stubs for Assessment and Note class to implement Ui

---
+++ src/main/java/seedu/address/model/student/Assessment.java
@@ -0,0 +1,26 @@
+package seedu.address.model.student;
+
+/**
+ * Stub for implementing Ui
+ */
+public class Assessment {
+    private String name;
+    private Double totalScore;
+    private Double actualScore;
+
+    public String getName() {
+        return "erin";
+    }
+
+    public Double getTotalScore() {
+        return 100.0;
+    }
+
+    public Double getActualScore() {
+        return 100.0;
+    }
+
+    public Double getPercentage() {
+        return 0.92;
+    }
+}
---
+++ src/main/java/seedu/address/model/student/Note.java
@@ -0,0 +1,23 @@
+package seedu.address.model.student;
+
+/**
+ * Stub for implementing Ui
+ */
+public class Note {
+
+    public String getNote() {
+        return "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae mattis nunc," +
+                " eu dictum ex. Proin auctor, lectus sit amet semper posuere, lorem nulla maximus lacus, " +
+                "vitae finibus urna enim ac augue. Sed sapien lectus, faucibus sed ipsum non, laoreet ultrices " +
+                "felis. Nullam ornare nisi eget lectus interdum aliquam. Curabitur vulputate, lacus nec feugiat " +
+                "placerat, ligula magna tempor augue, ac maximus nunc justo a tortor. Etiam id elit eros. " +
+                "Etiam vel mauris in dolor viverra dictum. Donec vel purus eu lorem tempor molestie. " +
+                "Integer pharetra feugiat neque, at sagittis nulla commodo id. Nulla justo erat, commodo eget " +
+                "mattis ac, sollicitudin eget erat. Curabitur accumsan ipsum eu mi vulputate, tincidunt pharetra " +
+                "odio euismod. Praesent condimentum tristique quam, id viverra massa. Etiam at tristique sapien. " +
+                "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. " +
+                "Nulla facilisi.";
+    }
+
+
+}
