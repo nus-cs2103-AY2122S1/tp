a757a6f8bd2a7bb72576ec1dc13dd3a4a3ba3287
Taufiq Abdul Rahman <tau-bar@users.noreply.github.com>
2021-10-22 02:59:28 +0800

    Add tests for newly added predicates

---
+++ src/test/java/seedu/address/model/friend/FriendGameFriendLinksContainsGamePredicateTest.java
@@ -0,0 +1,70 @@
+package seedu.address.model.friend;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.model.util.SampleDataUtil.getSampleFriends;
+import static seedu.address.model.util.SampleDataUtil.getSampleGames;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.game.Game;
+
+
+
+public class FriendGameFriendLinksContainsGamePredicateTest {
+    @Test
+    void getGame() {
+        Game game = getSampleGames()[0];
+        FriendGameFriendLinksContainsGamePredicate predicate =
+                new FriendGameFriendLinksContainsGamePredicate(game);
+        assertTrue(predicate.getGame().equals(game));
+    }
+
+    @Test
+    void equals() {
+        Game[] gameArr = getSampleGames();
+        FriendGameFriendLinksContainsGamePredicate firstPredicate =
+                new FriendGameFriendLinksContainsGamePredicate(gameArr[0]);
+        FriendGameFriendLinksContainsGamePredicate secondPredicate =
+                new FriendGameFriendLinksContainsGamePredicate(gameArr[1]);
+
+        // same object -> returns true
+        assertTrue(firstPredicate.equals(firstPredicate));
+
+        // same values -> returns true
+        FriendGameFriendLinksContainsGamePredicate firstPredicateCopy =
+                new FriendGameFriendLinksContainsGamePredicate(gameArr[0]);
+        assertTrue(firstPredicate.equals(firstPredicateCopy));
+
+        // different types -> returns false
+        assertFalse(firstPredicate.equals(1));
+
+        // null -> returns false
+        assertFalse(firstPredicate.equals(null));
+
+        // different person -> returns false
+        assertFalse(firstPredicate.equals(secondPredicate));
+    }
+
+    @Test
+    void test_GFLContainGame_returnsTrue() {
+        Game game = getSampleGames()[1];
+        Friend friend = getSampleFriends()[0];
+
+        // friend's gfl contains game -> true
+        FriendGameFriendLinksContainsGamePredicate predicate =
+                new FriendGameFriendLinksContainsGamePredicate(game);
+        assertTrue(predicate.test(friend));
+    }
+
+    @Test
+    void test_GFLDoesNotContainGame_returnsFalse() {
+        Game game = getSampleGames()[2];
+        Friend friend = getSampleFriends()[0];
+
+        // friend's gfl does not contain game -> false
+        FriendGameFriendLinksContainsGamePredicate predicate =
+                new FriendGameFriendLinksContainsGamePredicate(game);
+        assertFalse(predicate.test(friend));
+    }
+}
---
+++ src/test/java/seedu/address/model/game/GameFriendLinksContainsGameIdPredicateTest.java
@@ -0,0 +1,71 @@
+package seedu.address.model.game;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.model.friend.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static seedu.address.model.util.SampleDataUtil.getSampleFriends;
+import static seedu.address.model.util.SampleDataUtil.getSampleGames;
+
+class GameFriendLinksContainsGameIdPredicateTest {
+
+
+    @Test
+    void getFriend() {
+        Friend friend = getSampleFriends()[0];
+        GameFriendLinksContainsGameIdPredicate predicate =
+                new GameFriendLinksContainsGameIdPredicate(friend);
+        assertTrue(predicate.getFriend().equals(friend));
+    }
+
+    @Test
+    void equals() {
+        Friend friend = getSampleFriends()[0];
+        Friend otherFriend = getSampleFriends()[1];
+        GameFriendLinksContainsGameIdPredicate firstPredicate =
+                new GameFriendLinksContainsGameIdPredicate(friend);
+        GameFriendLinksContainsGameIdPredicate secondPredicate =
+                new GameFriendLinksContainsGameIdPredicate(otherFriend);
+
+        // same object -> returns true
+        assertTrue(firstPredicate.equals(firstPredicate));
+
+        // same values -> returns true
+        GameFriendLinksContainsGameIdPredicate firstPredicateCopy =
+                new GameFriendLinksContainsGameIdPredicate(friend);
+        assertTrue(firstPredicate.equals(firstPredicateCopy));
+
+        // different types -> returns false
+        assertFalse(firstPredicate.equals(1));
+
+        // null -> returns false
+        assertFalse(firstPredicate.equals(null));
+
+        // different person -> returns false
+        assertFalse(firstPredicate.equals(secondPredicate));
+
+    }
+
+    @Test
+    void test_GFLContainsGameId_returnsTrue() {
+        Game game = getSampleGames()[1];
+        Friend friend = getSampleFriends()[0];
+
+        // gfl contains game -> true
+        GameFriendLinksContainsGameIdPredicate predicate =
+                new GameFriendLinksContainsGameIdPredicate(friend);
+        assertTrue(predicate.test(game));
+    }
+
+    @Test
+    void test_GFLContainsGameId_returnsFalse() {
+        Game game = getSampleGames()[2];
+        Friend friend = getSampleFriends()[0];
+
+        // gfl contains game -> true
+        GameFriendLinksContainsGameIdPredicate predicate =
+                new GameFriendLinksContainsGameIdPredicate(friend);
+        assertFalse(predicate.test(game));
+    }
+}
