d9fec5626effc1db3f822b516f320a0e4ab60c8d
Chan Yu Cheng <e0560244@u.nus.edu>
2021-10-21 23:10:49 +0800

    Improve code quality

--- src/test/java/seedu/address/model/person/PinTest.java
+++ src/test/java/seedu/address/model/person/PinTest.java
@@ -1,10 +1,13 @@
 package seedu.address.model.person;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.testutil.Assert;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
 
 public class PinTest {
 
