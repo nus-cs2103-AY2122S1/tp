1679c53d0fa867c6e6ecfce002a201aae147858e
KT27Learn <66994197+KT27Learn@users.noreply.github.com>
2021-10-21 22:36:49 +0800

    Revert "Add list of upcoming meetings for scheduler"

--- src/main/java/seedu/address/ui/SideBar.java
+++ src/main/java/seedu/address/ui/SideBar.java
@@ -4,7 +4,6 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
-import seedu.address.model.person.NextMeeting;
 import seedu.address.model.person.Person;
 
 /**
@@ -16,25 +15,17 @@ public class SideBar extends UiPart<Region> {
 
     private PersonViewPanel personViewPanel;
 
-    private MeetingsListPanel meetingsListPanel;
-
     @FXML
     private StackPane personViewPanelPlaceholder;
-
-    @FXML
-    private StackPane nextMeetingListPanelPlaceholder;
 
     /**
      * Creates a {@code SideBar} with panels initiated.
      */
-    public SideBar(ObservableList<Person> personToView, ObservableList<NextMeeting> nextMeetings) {
+    public SideBar(ObservableList<Person> personToView) {
         super(FXML);
 
         personViewPanel = new PersonViewPanel(personToView);
         personViewPanelPlaceholder.getChildren().add(personViewPanel.getRoot());
-
-        meetingsListPanel = new MeetingsListPanel(nextMeetings);
-        nextMeetingListPanelPlaceholder.getChildren().add(meetingsListPanel.getRoot());
     }
 
 }
--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -25,7 +25,6 @@ import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.person.ClientId;
-import seedu.address.model.person.NextMeeting;
 import seedu.address.model.person.Person;
 import seedu.address.model.tag.Tag;
 import seedu.address.testutil.PersonBuilder;
@@ -162,11 +161,6 @@ public class AddCommandTest {
 
         @Override
         public ObservableList<Person> getFilteredPersonList() {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public ObservableList<NextMeeting> getFilteredNextMeetingList() {
             throw new AssertionError("This method should not be called.");
         }
 
