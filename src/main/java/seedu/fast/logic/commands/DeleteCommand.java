2e327ae4ce9c4cf59bb8ca2c7de771d49a9500f8
Py0000 <>
2021-10-21 12:04:37 +0800

    Enhance multiple delete

--- src/main/java/seedu/fast/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/fast/logic/commands/DeleteCommand.java
@@ -3,7 +3,9 @@ package seedu.fast.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import seedu.fast.commons.core.Messages;
 import seedu.fast.commons.core.index.Index;
@@ -17,17 +19,23 @@ import seedu.fast.model.person.Person;
 public class DeleteCommand extends Command {
 
     public static final String COMMAND_WORD = "del";
+    public static final int MULTIPLE_DELETE_LIMIT = 10;
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Deletes the person identified by the index number used in the displayed person list.\n\n"
+            + ": Deletes the person identified by the index number(s) used in the displayed person list.\n\n"
             + "Parameters: \nINDEX (must be a positive integer)\n\n"
-            + "Example: \n" + COMMAND_WORD + " 1";
+            + "Example: \n" + COMMAND_WORD + " 1"
+            + "Example: \n" + COMMAND_WORD + " 5 10 15";
 
     public static final String MESSAGE_SINGLE_DELETE_SUCCESS = "Deleted Person: %1$s";
     public static final String MESSAGE_MULTIPLE_DELETE_SUCCESS = "Successfully deleted %1$s contacts from "
             + "your contact list.";
-    public static final String MESSAGE_MULTIPLE_DELETE_FAILED = "The number of contacts you want to delete "
+    public static final String MESSAGE_MULTIPLE_DELETE_FAILED_DUPLICATES = "Cannot delete the same contact twice! "
+            + "(Cannot have duplicated index!)";
+    public static final String MESSAGE_MULTIPLE_DELETE_FAILED_WITHIN_LIMIT = "The number of contacts you want to delete "
             + "cannot be more than the number of contacts you currently have!";
+    public static final String MESSAGE_MULTIPLE_DELETE_FAILED_EXCEED_LIMIT = "You cannot delete more than " +
+            + MULTIPLE_DELETE_LIMIT + " contacts at one time!";
     public static final String MESSAGE_MULTIPLE_DELETE_INVALID_INDEX_DETECTED = "%1$s contact(s) has been deleted"
             + " before the invalid index at 'index %2$s' is detected.";
 
@@ -47,11 +55,15 @@ public class DeleteCommand extends Command {
         requireNonNull(model);
         List<Person> lastShownList = model.getFilteredPersonList();
 
+        if (indexArray.length > MULTIPLE_DELETE_LIMIT) {
+            throw new CommandException(MESSAGE_MULTIPLE_DELETE_FAILED_EXCEED_LIMIT);
+        }
+
         if (indexArray.length > lastShownList.size()) {
             if (isSingleDelete()) {
                 throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
             }
-            throw new CommandException(MESSAGE_MULTIPLE_DELETE_FAILED);
+            throw new CommandException(MESSAGE_MULTIPLE_DELETE_FAILED_WITHIN_LIMIT);
         }
 
         Index targetIndex;
@@ -65,7 +77,13 @@ public class DeleteCommand extends Command {
             model.deletePerson(personToDelete);
 
             return new CommandResult(String.format(MESSAGE_SINGLE_DELETE_SUCCESS, personToDelete));
-        } else if (isMultipleDelete()) {
+        }
+
+        if (isMultipleDelete()) {
+            if (hasDuplicates(indexArray)) {
+                throw new CommandException(MESSAGE_MULTIPLE_DELETE_FAILED_DUPLICATES);
+            }
+
             for (int i = 0; i < indexArray.length; i++) {
                 targetIndex = Index.indexModifier(indexArray[i], i);
                 if (targetIndex.getZeroBased() >= lastShownList.size()) {
@@ -78,9 +96,10 @@ public class DeleteCommand extends Command {
             }
 
             return new CommandResult(String.format(MESSAGE_MULTIPLE_DELETE_SUCCESS, indexArray.length));
-        } else {
-            throw new CommandException(Messages.MESSAGE_UNKNOWN_COMMAND);
         }
+        
+        // should never reach here
+        throw new CommandException(Messages.MESSAGE_UNKNOWN_COMMAND);
     }
 
     private boolean isSingleDelete() {
@@ -89,6 +108,16 @@ public class DeleteCommand extends Command {
 
     private boolean isMultipleDelete() {
         return indexArray.length > 1;
+    }
+
+    private boolean hasDuplicates(Index[] array) {
+        Set<Integer> set = new HashSet<>();
+        for (Index index : array) {
+            if (!set.add(index.getZeroBased())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
