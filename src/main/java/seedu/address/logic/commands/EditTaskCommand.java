befb350129081b1202498fa0336ab16fbac67a5e
WangGLJoseph <joseph.cnsg@gmail.com>
2021-10-21 17:17:32 +0800

    Create objects for attributes of Task

---
+++ src/main/java/seedu/address/logic/commands/EditTaskCommand.java
@@ -0,0 +1,210 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULE_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK_DEADLINE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK_NAME;
+
+import java.util.List;
+import java.util.Optional;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.util.CollectionUtil;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.module.Module;
+import seedu.address.model.module.ModuleName;
+import seedu.address.model.module.student.Student;
+import seedu.address.model.task.Task;
+import seedu.address.model.task.UniqueTaskList;
+
+/**
+ * Edits a task's information.
+ */
+public class EditTaskCommand extends EditCommand {
+
+    public static final String COMMAND_WORD = "edit task";
+
+    public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits a task's information. Must provide at least "
+            + "one field (name/deadline) to be edited. "
+            + "Parameters: "
+            + PREFIX_MODULE_NAME + "MODULE NAME "
+            + PREFIX_TASK_NAME + "TASK NAME "
+            + PREFIX_TASK_DEADLINE + "TASK DEADLINE "
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_MODULE_NAME + "CS2103 "
+            + PREFIX_TASK_NAME + "Assignment 1 "
+            + PREFIX_TASK_DEADLINE + "21 Oct 2021 "
+            + "(edits all fields)";
+
+    private EditTaskDescriptor editTaskDescriptor;
+    private ModuleName moduleName;
+
+    /**
+     * Creates an EditTaskCommand to edit the specified {@code Task}
+     *
+     * @param moduleName The name of the module for which the task is assigned.
+     * @param editTaskDescriptor The edited task descriptor.
+     */
+    public EditTaskCommand(ModuleName moduleName, EditTaskDescriptor editTaskDescriptor) {
+        requireNonNull(editTaskDescriptor);
+        requireNonNull(moduleName);
+        this.editTaskDescriptor = editTaskDescriptor;
+        this.moduleName = moduleName;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Module> lastShownList = model.getFilteredModuleList();
+        for (Module module : lastShownList) {
+            if (module.getName().equals(moduleName)) {
+                // right module reached
+                List<Student> studentList = module.getFilteredStudentList();
+                for (Student student : studentList) {
+                    editTaskInformation(student);
+                }
+                // return editTaskInformation(module); what to return here?
+            }
+        }
+        throw new CommandException(String.format(Messages.MESSAGE_MODULE_NAME_NOT_FOUND, moduleName.moduleName));
+    }
+
+    // Edit Task -> Edit task in Module
+    // Go through student list of module, for each student, change the task's description but not the completion(?)
+
+    /**
+     * Edits a task's information. The task will be from the specified student.
+     *
+     * @param student The student whose task will be edited.
+     * @return Statement indicating that the edit is successful.
+     * @throws CommandException Exception thrown when task is not found.
+     */
+    public CommandResult editTaskInformation(Student student) throws CommandException {
+        UniqueTaskList studentTaskList = student.getTaskList();
+        for (Task task : studentTaskList) {
+            if (task.getName().equals(editTaskDescriptor.getName())) {
+                Task editedTask = createEditedTask(task, editTaskDescriptor);
+                // replaces the old task with the editedTask
+                task = editedTask;
+                return new CommandResult(String.format(Messages.MESSAGE_EDIT_STUDENT_SUCCESS, task.getName()));
+            }
+        }
+        student.setTaskList(studentTaskList);
+        throw new CommandException(String.format(Messages.MESSAGE_STUDENT_NOT_FOUND, editTaskDescriptor.name));
+    }
+
+    /**
+     * Creates and returns a {@code Person} with the details of {@code personToEdit}
+     * edited with {@code editPersonDescriptor}.
+     *
+     * @param taskToEdit The task to be edited.
+     * @param editTaskDescriptor The edited task descriptions.
+     * @return The edited task.
+     */
+    private static Task createEditedTask(Task taskToEdit, EditTaskDescriptor editTaskDescriptor) {
+        assert taskToEdit != null;
+
+        ModuleName moduleName = taskToEdit.getModuleName();
+        String updatedName = editTaskDescriptor.getName().orElse(taskToEdit.getName());
+        String updatedDeadline = editTaskDescriptor.getDeadline().orElse(taskToEdit.getDeadline());
+
+
+        Task editedTask = new Task(moduleName, updatedName, updatedDeadline);
+        return editedTask;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof EditTaskCommand)) {
+            return false;
+        }
+
+        // state check
+        EditTaskCommand e = (EditTaskCommand) other;
+        return moduleName.equals(e.moduleName)
+                && editTaskDescriptor.equals(e.editTaskDescriptor);
+    }
+
+    /**
+     * Stores the details to edit the task with. Each non-empty field value will replace the
+     * corresponding field value of the task.
+     */
+    public static class EditTaskDescriptor {
+        private ModuleName moduleName;
+        private String name = "default_edited_task_name";
+        private String deadline;
+        private boolean isComplete;
+
+        public EditTaskDescriptor() {}
+
+        /**
+         * Copy constructor.
+         *
+         * @param  toCopy The edit task descriptor to be copied.
+         */
+        public EditTaskDescriptor(EditTaskDescriptor toCopy) {
+            setName(toCopy.name);
+            setDeadline(toCopy.deadline);
+        }
+
+        /**
+         * Returns true if at least one field is edited.
+         */
+        public boolean isAnyFieldEdited() {
+            return CollectionUtil.isAnyNonNull(name, deadline);
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Optional<String> getName() {
+            return Optional.ofNullable(name);
+        }
+
+        public void setDeadline(String deadline) {
+            this.deadline = deadline;
+        }
+
+        public Optional<String> getDeadline() {
+            return Optional.ofNullable(deadline);
+        }
+
+        public void setIsComplete(boolean isComplete) {
+            this.isComplete = isComplete;
+        }
+
+        public Optional<Boolean> getIsComplete() {
+            return Optional.ofNullable(isComplete);
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            // short circuit if same object
+            if (other == this) {
+                return true;
+            }
+
+            // instanceof handles nulls
+            if (!(other instanceof EditTaskDescriptor)) {
+                return false;
+            }
+
+            // state check
+            EditTaskDescriptor e = (EditTaskDescriptor) other;
+
+            return getName().equals(e.getName())
+                    && getDeadline().equals(e.getDeadline())
+                    && getIsComplete().equals(e.getIsComplete());
+        }
+    }
+}
---
+++ src/main/java/seedu/address/model/task/TaskDeadline.java
@@ -0,0 +1,53 @@
+package seedu.address.model.module.student;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Task's deadline, must only contain alphanumeric characters.
+ * Guarantees: immutable; is valid as declared in {@link #isValidTeleHandle(String)}
+ */
+public class TaskDeadline {
+
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "TaskDeadline must only contain alphanumeric characters";
+    public static final String VALIDATION_REGEX = "[a-zA-Z0-9]";
+    public final String value;
+
+    /**
+     * Constructs a {@code taskDeadline}.
+     *
+     * @param taskDeadline A valid taskDeadline.
+     */
+    public TaskDeadline(String taskDeadline) {
+        requireNonNull(taskDeadline);
+        checkArgument(isValidTeleHandle(taskDeadline), MESSAGE_CONSTRAINTS);
+        value = taskDeadline;
+    }
+
+    /**
+     * Returns true if a given string is a valid phone number.
+     */
+    public static boolean isValidTeleHandle(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof TeleHandle // instanceof handles nulls
+                && value.equals(((TeleHandle) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
