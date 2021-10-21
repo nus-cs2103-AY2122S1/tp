1a960310fb1447ac88c6fcdd94096874e8f097ed
zhixuanlee <overlypositiveplayer@gmail.com>
2021-10-21 13:06:28 +0800

    Add testcases to FindCommandParserTest and FindOrCommandParserTest

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -23,7 +23,72 @@ public class FindCommandParserTest {
     }
 
     @Test
-    public void parse_validArgs_returnsFindCommand() {
+    public void parse_oneName_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        List<Name> nameList = List.of(new Name("Alice"));
+        List<Tag> tagList = List.of();
+        FindCommand expectedFindCommand =
+                new FindCommand(new FindPredicate(nameList, tagList));
+        assertParseSuccess(parser, " n/Alice", expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n n/Alice \n \t  \t", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_multipleNames_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        List<Name> nameList = List.of(new Name("Alice"), new Name("Bob"));
+        List<Tag> tagList = List.of();
+        FindCommand expectedFindCommand =
+                new FindCommand(new FindPredicate(nameList, tagList));
+        assertParseSuccess(parser, " n/Alice n/Bob", expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n n/Alice \n \t n/Bob  \t", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_oneTag_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        List<Name> nameList = List.of();
+        List<Tag> tagList = List.of(new Tag("friends"));
+        FindCommand expectedFindCommand =
+                new FindCommand(new FindPredicate(nameList, tagList));
+        assertParseSuccess(parser, "  t/friends", expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n \n \t  \t t/friends", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_multipleTags_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        List<Name> nameList = List.of();
+        List<Tag> tagList = List.of(new Tag("friends"), new Tag("colleagues"));
+        FindCommand expectedFindCommand =
+                new FindCommand(new FindPredicate(nameList, tagList));
+        assertParseSuccess(parser, " t/friends t/colleagues", expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n \n \t  t/friends \t t/colleagues", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_oneNamesAndTag_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        List<Name> nameList = List.of(new Name("Alice"));
+        List<Tag> tagList = List.of(new Tag("friends"));
+        FindCommand expectedFindCommand =
+                new FindCommand(new FindPredicate(nameList, tagList));
+        assertParseSuccess(parser, " n/Alice t/friends", expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n n/Alice \n \t  \t t/friends", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_multipleNamesAndTags_returnsFindCommand() {
         // no leading and trailing whitespaces
         List<Name> nameList = List.of(new Name("Alice"), new Name("Bob"));
         List<Tag> tagList = List.of(new Tag("friends"), new Tag("colleagues"));
--- src/test/java/seedu/address/logic/parser/FindOrCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindOrCommandParserTest.java
@@ -25,7 +25,72 @@ public class FindOrCommandParserTest {
     }
 
     @Test
-    public void parse_validArgs_returnsFindCommand() {
+    public void parse_oneName_returnsFindOrCommand() {
+        // no leading and trailing whitespaces
+        List<Name> nameList = List.of(new Name("Alice"));
+        List<Tag> tagList = List.of();
+        FindOrCommand expectedFindOrCommand =
+                new FindOrCommand(new FindOrPredicate(nameList, tagList));
+        assertParseSuccess(parser, " n/Alice", expectedFindOrCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n n/Alice \n \t  \t", expectedFindOrCommand);
+    }
+
+    @Test
+    public void parse_multipleNames_returnsFindOrCommand() {
+        // no leading and trailing whitespaces
+        List<Name> nameList = List.of(new Name("Alice"), new Name("Bob"));
+        List<Tag> tagList = List.of();
+        FindOrCommand expectedFindOrCommand =
+                new FindOrCommand(new FindOrPredicate(nameList, tagList));
+        assertParseSuccess(parser, " n/Alice n/Bob", expectedFindOrCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n n/Alice \n \t n/Bob  \t", expectedFindOrCommand);
+    }
+
+    @Test
+    public void parse_oneTag_returnsFindOrCommand() {
+        // no leading and trailing whitespaces
+        List<Name> nameList = List.of();
+        List<Tag> tagList = List.of(new Tag("friends"));
+        FindOrCommand expectedFindOrCommand =
+                new FindOrCommand(new FindOrPredicate(nameList, tagList));
+        assertParseSuccess(parser, " t/friends", expectedFindOrCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n \n \t  \t t/friends", expectedFindOrCommand);
+    }
+
+    @Test
+    public void parse_multipleTags_returnsFindOrCommand() {
+        // no leading and trailing whitespaces
+        List<Name> nameList = List.of();
+        List<Tag> tagList = List.of(new Tag("friends"), new Tag("colleagues"));
+        FindOrCommand expectedFindOrCommand =
+                new FindOrCommand(new FindOrPredicate(nameList, tagList));
+        assertParseSuccess(parser, " t/friends t/colleagues", expectedFindOrCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n t/friends \n \t  \t t/colleagues", expectedFindOrCommand);
+    }
+
+    @Test
+    public void parse_oneNameAndTag_returnsFindOrCommand() {
+        // no leading and trailing whitespaces
+        List<Name> nameList = List.of(new Name("Alice"));
+        List<Tag> tagList = List.of(new Tag("friends"));
+        FindOrCommand expectedFindOrCommand =
+                new FindOrCommand(new FindOrPredicate(nameList, tagList));
+        assertParseSuccess(parser, " n/Alice t/friends", expectedFindOrCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n n/Alice \n \t  \t t/friends", expectedFindOrCommand);
+    }
+
+    @Test
+    public void parse_multipleNamesAndTags_returnsFindOrCommand() {
         // no leading and trailing whitespaces
         List<Name> nameList = List.of(new Name("Alice"), new Name("Bob"));
         List<Tag> tagList = List.of(new Tag("friends"), new Tag("colleagues"));
