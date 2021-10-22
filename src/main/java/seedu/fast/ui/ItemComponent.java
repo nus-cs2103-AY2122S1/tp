2cece24fe19cfeffca698edec7abf65e7a824b2f
LauTengHon <e0543465@u.nus.edu>
2021-10-22 11:38:53 +0800

    Abstract out icons for ui

--- src/main/java/seedu/fast/ui/ItemComponent.java
+++ src/main/java/seedu/fast/ui/ItemComponent.java
@@ -14,22 +14,22 @@ import javafx.scene.layout.HBox;
  * This control represents a dialog box consisting of an ImageView to represent the speaker's face and a label
  * containing text from the speaker.
  */
-public class Phone extends HBox {
+public class ItemComponent extends HBox {
     @FXML
-    private Label phone;
+    private Label component;
     @FXML
     private ImageView displayPicture;
 
-    public Phone(String text, Image img) {
+    public ItemComponent(String text, Image img) {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/phone.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/PersonComponent.fxml"));
             fxmlLoader.setController(this);
             fxmlLoader.setRoot(this);
             fxmlLoader.load();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        phone.setText(text);
+        component.setText(text);
         displayPicture.setImage(img);
     }
 }
