451a2660e1f7b6c962785efd794190ca7fb1f240
nataniayp <75572811+nataniayp@users.noreply.github.com>
2021-10-22 13:18:05 +0800

    Change design of PersonCard, add listener (not functional), modify design of rating label in SelectedPersonCard, edit javafx version

--- src/main/java/seedu/address/ui/PersonListPanel.java
+++ src/main/java/seedu/address/ui/PersonListPanel.java
@@ -2,6 +2,7 @@ package seedu.address.ui;
 
 import java.util.logging.Logger;
 
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.event.EventHandler;
@@ -10,7 +11,6 @@ import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.Region;
-import javafx.scene.layout.StackPane;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.person.Person;
 
@@ -18,7 +18,7 @@ import seedu.address.model.person.Person;
  * Panel containing the list of persons.
  */
 public class PersonListPanel extends UiPart<Region> {
-    private static final String FXML = "PersonListPanelUpdated.fxml";
+    private static final String FXML = "PersonListPanel.fxml";
     private final Logger logger = LogsCenter.getLogger(PersonListPanel.class);
 
     private SelectedPersonCard selected = new SelectedPersonCard();
@@ -35,8 +35,8 @@ public class PersonListPanel extends UiPart<Region> {
     public PersonListPanel(ObservableList<Person> personList) {
         super(FXML);
         setSelectedPersonPanel();
-//        selectedPersonPanelPlaceholder.getChildren().add(selected.getRoot());
         selectedPersonPanelPlaceholder.setContent(selected.getRoot());
+        updateDetailsIfChanged(personList);
         personListView.setItems(personList);
         personListView.setCellFactory(listView -> new PersonListViewCell());
         personListView.setOnMouseClicked(new EventHandler<Event>() {
@@ -44,6 +44,7 @@ public class PersonListPanel extends UiPart<Region> {
             public void handle(Event event) {
                 Person selectedPerson = personListView.getSelectionModel().getSelectedItem();
                 logger.info("selected " + selectedPerson);
+                selectedPersonPanelPlaceholder.setVvalue(0.0);
                 selected.updatePerson(selectedPerson);
                 selected.setPersonDetails();
             }
@@ -52,6 +53,31 @@ public class PersonListPanel extends UiPart<Region> {
 
     public void setSelectedPersonPanel() {
         selected.setPersonDetails();
+    }
+
+    private void updateDetailsIfChanged(ObservableList<Person> personList) {
+        personList.addListener(new ListChangeListener<Person>() {
+            @Override
+            public void onChanged(Change<? extends Person> change) {
+                while (change.next()) {
+                    if (change.wasAdded()) {
+                        int indexToChange = change.getFrom();
+                        Person updatedPerson = change.getList().get(indexToChange);
+                        personListView.scrollTo(indexToChange);
+                        personListView.getSelectionModel().select(indexToChange);
+                        personListView.getFocusModel().focus(indexToChange);
+                        selected.updatePerson(updatedPerson);
+                    } else if (change.wasRemoved()) {
+                        if (personList.size() > 0) {
+                            personListView.scrollTo(0);
+                            personListView.getSelectionModel().select(0);
+                            personListView.getFocusModel().focus(0);
+                        }
+                        setSelectedPersonPanel();
+                    }
+                }
+            }
+        });
     }
 
     /**
