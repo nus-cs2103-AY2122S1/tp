a0a7c7d95de0dda0f2a91c69ad0c7dd03bd514cf
cookiedan42 <cookiedan42@gmail.com>
2021-10-22 19:30:42 +0800

    Add documentation for MailingListCommand

--- src/main/java/seedu/address/logic/commands/MailingListCommand.java
+++ src/main/java/seedu/address/logic/commands/MailingListCommand.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.Prefix;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
@@ -10,26 +11,52 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_BIRTHDAY;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 /**
- * Command to export the current view as a csv mailing list
- * parser --> command --> return string object --> UI should handle output
+ * Exports the current view as a mailing list in the csv file format.
  */
 public class MailingListCommand extends Command {
 
-    public static final String COMMAND_WORD = "export";
-    public static final String MESSAGE_SUCCESS = "Exported current view";
+    public static final String COMMAND_WORD = "contact";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Exports the current view as a mailing list. "
+            + "Parameters: "
+            + "[" + PREFIX_PHONE + "] "
+            + "[" + PREFIX_EMAIL + "] "
+            + "[" + PREFIX_ADDRESS + "] "
+            + "[" + PREFIX_TAG + "]"
+            + "[" + PREFIX_BIRTHDAY + "] \n"
+            + "Default export is Phone numbers and Email addresses.\n"
+            + "Example: " + COMMAND_WORD
+            + PREFIX_PHONE + " "
+            + PREFIX_EMAIL;
+
+    public static final String MESSAGE_EMPTY_PERSON_LIST = "Trying to export an empty mailing list does not make sense";
+
+    public static final String MESSAGE_SUCCESS = "Exporting current view as a CSV file";
 
     private final Set<Prefix> prefixToWrite;
 
+    /**
+     * @param prefixToWrite Set of Prefixes to write into the csv file
+     */
     public MailingListCommand(Set<Prefix> prefixToWrite) {
         this.prefixToWrite = Collections.unmodifiableSet(prefixToWrite);
     }
 
     @Override
-    public CommandResult execute(Model model) {
+    public CommandResult execute(Model model) throws CommandException{
         requireNonNull(model);
         List<Person> personList = model.getFilteredPersonList().stream().collect(Collectors.toList());
+
+        if (personList.isEmpty()) {
+            throw new CommandException(MESSAGE_EMPTY_PERSON_LIST);
+        }
+
         return new CommandResultExport(MESSAGE_SUCCESS,personList,prefixToWrite);
     }
 
