db30d9b9f07c0101a58a8497c5ecdb06348c8391
Ryan Chung <timelord0830@gmail.com>
2021-10-23 07:44:42 +0800

    Add tests for GenresContainedPredicate

---
+++ src/test/java/seedu/anilist/model/anime/GenresContainedPredicateTest.java
@@ -0,0 +1,91 @@
+package seedu.anilist.model.anime;
+
+import org.junit.jupiter.api.Test;
+import seedu.anilist.logic.parser.exceptions.ParseException;
+import seedu.anilist.testutil.AnimeBuilder;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GenresContainedPredicateTest {
+
+    @Test
+    public void equals() throws ParseException {
+        List<String> firstGenreKeywordList = Collections.singletonList("first");
+        List<String> secondGenreKeywordList = Arrays.asList("first", "second");
+        GenresContainedPredicate firstPredicate = new GenresContainedPredicate(firstGenreKeywordList);
+        GenresContainedPredicate secondPredicate  = new GenresContainedPredicate(secondGenreKeywordList);
+
+        // same object -> returns true
+        assertTrue(firstPredicate.equals(firstPredicate));
+
+        // same values -> returns true
+        GenresContainedPredicate secondPredicateCopy = new GenresContainedPredicate(secondGenreKeywordList);
+        assertTrue(secondPredicateCopy.equals(secondPredicateCopy));
+
+        // different types -> returns false
+        assertFalse(firstPredicate.equals(1));
+
+        // null -> returns false
+        assertFalse(firstPredicate.equals(null));
+
+        // different keyword -> returns false
+        assertFalse(firstPredicate.equals(secondPredicate));
+    }
+
+    @Test
+    public void constructor_InvalidGenreKeyword_throwsParseException() {
+        // Empty String
+        assertThrows(ParseException.class,
+            () -> new GenresContainedPredicate(Collections.singletonList("")));
+
+        // Non-ASCII
+        assertThrows(ParseException.class,
+            () -> new GenresContainedPredicate(Collections.singletonList("非ascii字符")));
+    }
+
+    @Test
+    public void test_genresSetContainsGenre_returnsTrue() throws ParseException {
+        // One keyword
+        GenresContainedPredicate predicate =
+            new GenresContainedPredicate(Collections.singletonList("comedy"));
+        assertTrue(predicate.test(new AnimeBuilder().withGenres("horror", "comedy").build()));
+
+
+        // Multiple keywords
+        predicate = new GenresContainedPredicate(Arrays.asList("comedy", "horror"));
+        assertTrue(predicate.test(new AnimeBuilder().withGenres("horror", "comedy").build()));
+
+        // Only one matching keyword
+        predicate = new GenresContainedPredicate(Arrays.asList("comedy", "shonen"));
+        assertTrue(predicate.test(new AnimeBuilder().withGenres("horror", "comedy").build()));
+
+        // Mixed-case keywords
+        predicate = new GenresContainedPredicate(Arrays.asList("CoMeDy"));
+        assertTrue(predicate.test(new AnimeBuilder().withGenres("horror", "comedy").build()));
+    }
+
+    @Test
+    public void test_genresSetDoesNotContainGenre_returnsFalse() throws ParseException {
+        // Zero keywords
+        // Note: This is how it should work if you pass an empty list into genresContainedPredicates's
+        // constructor. However, a predicate is not created if it does not have a g/ arg.
+        GenresContainedPredicate predicate = new GenresContainedPredicate(Collections.emptyList());
+        assertFalse(predicate.test(new AnimeBuilder().withGenres("horror", "comedy").build()));
+
+        // Non-matching keyword
+        predicate = new GenresContainedPredicate(Arrays.asList("shonen"));
+        assertFalse(predicate.test(new AnimeBuilder().withGenres("horror", "comedy").build()));
+
+        // Keywords match names, but does not match genre
+        predicate = new GenresContainedPredicate(Arrays.asList("attack on titan", "adventure"));
+        assertFalse(predicate.test(new AnimeBuilder().withName("attack on titan")
+            .withGenres("horror", "comedy").build()));
+    }
+
+}
