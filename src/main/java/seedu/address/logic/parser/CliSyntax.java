6e58e9a9c05b3b722eaaaad9608e1d9554f80a15
angkoonhwee <akoonhwee@gmail.com>
2021-10-22 12:05:18 +0800

    sdfsd

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -28,6 +28,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_SUBJECT = new Prefix("subject/");
     public static final Prefix PREFIX_HOMEWORK = new Prefix("hw/");
     public static final Prefix PREFIX_RATES = new Prefix("rates/");
+    public static final Prefix PREFIX_OUTSTANDING_FEES = new Prefix("f/");
 
     /* Prefix definitions for command options */
     public static final Prefix PREFIX_FIND_CONDITION = new Prefix("cond/");
