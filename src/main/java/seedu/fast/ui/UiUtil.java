dcd2e4b7bdc780b1b1bfca0486d04db8c25a80ab
LauTengHon <e0543465@u.nus.edu>
2021-10-22 11:57:52 +0800

    Fix bug in rendering icons
    Icons were placed in PersonCard, causing a new image object to be
    created everytime there was an update to the PersonCard.
    
    Abstracted out the image objects as a static object.

---
+++ src/main/java/seedu/fast/ui/UiUtil.java
@@ -0,0 +1,10 @@
+package seedu.fast.ui;
+
+import javafx.scene.image.Image;
+
+public class UiUtil {
+    public final static Image phoneImage = new Image("/images/phone.png");
+    public final static Image addressImage = new Image("/images/address.png");
+    public final static Image emailImage = new Image("/images/email.png");
+    public final static Image tagImage = new Image("/images/tag.png");
+}
