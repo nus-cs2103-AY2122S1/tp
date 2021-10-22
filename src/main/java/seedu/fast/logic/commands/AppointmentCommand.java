ddf0b847f3612d5516320f3ea5aad154e9c0f412
Py0000 <>
2021-10-22 10:29:40 +0800

    Edit Command Word

--- src/main/java/seedu/fast/logic/commands/AppointmentCommand.java
+++ src/main/java/seedu/fast/logic/commands/AppointmentCommand.java
@@ -20,7 +20,7 @@ import seedu.fast.model.person.Person;
  */
 public class AppointmentCommand extends Command {
 
-    public static final String COMMAND_WORD = "appt";
+    public static final String COMMAND_WORD = "aa";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Add appointment with the person identified"
             + " by the index number used in the last person listing.\n\n"
