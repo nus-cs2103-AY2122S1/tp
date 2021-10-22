489b1330b535c2c8b8fac07214f28c524cf5c793
LauTengHon <e0543465@u.nus.edu>
2021-10-22 12:00:02 +0800

    Add Tag icons
    There is a need to create a seperate fxml for tags since tags are not
    just string text, they are set<Tag>

---
+++ src/main/java/seedu/fast/ui/TagComponent.java
@@ -0,0 +1,71 @@
+package seedu.fast.ui;
+
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.Set;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import seedu.fast.commons.util.Colors;
+import seedu.fast.commons.util.TagUtil;
+import seedu.fast.model.tag.Tag;
+
+/**
+ * An example of a custom control using FXML.
+ * This control represents a dialog box consisting of an ImageView to represent the speaker's face and a label
+ * containing text from the speaker.
+ */
+public class TagComponent extends HBox {
+    @FXML
+    private FlowPane component;
+    @FXML
+    private ImageView displayPicture;
+
+    public TagComponent(Set<Tag> tagSet, Image img) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/TagComponent.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        tagSet.stream()
+                .sorted(Comparator.comparing(tag -> tag.tagName))
+                .forEach(tag -> component.getChildren().add(colorSelector(tag.tagName)));
+        displayPicture.setImage(img);
+    }
+
+
+    public Label colorSelector(String tagName) {
+        Label temp = new Label(tagName);
+
+        switch (tagName.toUpperCase()) { //Todo: add more color tags & abstract out case names
+
+        case TagUtil.FRIENDS:
+            temp.setStyle(Colors.BLUE);
+            break;
+
+        case TagUtil.HIGH_PRIORITY:
+            temp.setStyle(Colors.RED);
+            break;
+
+        case TagUtil.MEDIUM_PRIORITY:
+            temp.setStyle(Colors.ORANGE);
+            break;
+
+        case TagUtil.LOW_PRIORITY:
+            temp.setStyle(Colors.GREEN);
+            break;
+
+        default:
+            temp.setStyle(Colors.GREY);
+        }
+        return temp;
+    }
+}
