55b746bcbe0bfcd1fbc5e760eab4cf88388771e6
Siddharth Srivastava <siddharth.srivastava2508@gmail.com>
2021-10-23 18:07:02 +0800

    Increase coverage of the Config Test

--- src/test/java/seedu/address/commons/core/ConfigTest.java
+++ src/test/java/seedu/address/commons/core/ConfigTest.java
@@ -1,10 +1,15 @@
 package seedu.address.commons.core;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import seedu.address.model.person.Phone;
+
+import java.nio.file.Paths;
+import java.util.logging.Level;
 
 public class ConfigTest {
 
@@ -18,10 +23,31 @@ public class ConfigTest {
 
     @Test
     public void equalsMethod() {
-        Config defaultConfig = new Config();
-        assertNotNull(defaultConfig);
-        assertTrue(defaultConfig.equals(defaultConfig));
+        Config defaultConfig1 = new Config();
+        Config defaultConfig2 = new Config();
+        defaultConfig2.setUserPrefsFilePath(Paths.get("addressbook.json"));
+        assertNotNull(defaultConfig1);
+        assertTrue(defaultConfig1.equals(defaultConfig1));
+        assertFalse(defaultConfig1.equals(new Phone("12345678")));
+        assertFalse(defaultConfig1.equals(defaultConfig2));
     }
 
+    @Test
+    public void getLogLevelTest() {
+        Config defaultConfig = new Config();
+        assertEquals(defaultConfig.getLogLevel(), Level.INFO);
+    }
 
+    @Test
+    public void getUserPrefsFilePathTest() {
+        Config defaultConfig = new Config();
+        assertEquals(defaultConfig.getUserPrefsFilePath(), Paths.get("preferences.json"));
+    }
+
+    @Test
+    public void hashCodeTest() {
+        Config defaultConfig1 = new Config();
+        Config defaultConfig2 = new Config();
+        assertEquals(defaultConfig1.hashCode(), defaultConfig2.hashCode());
+    }
 }
