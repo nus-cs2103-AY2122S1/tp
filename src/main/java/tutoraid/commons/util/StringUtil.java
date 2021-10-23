65849226558dfd8db26e97c16a5c2b85e1aed9a5
Preshita01 <pandipreshita@gmail.com>
2021-10-24 02:47:20 +0800

    Add method to return searches that contain the search words (no longer required to only return searches that exactly match the search words as they are)

--- src/main/java/tutoraid/commons/util/StringUtil.java
+++ src/main/java/tutoraid/commons/util/StringUtil.java
@@ -6,6 +6,7 @@ import static tutoraid.commons.util.AppUtil.checkArgument;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Arrays;
+import java.util.Locale;
 
 /**
  * Helper functions for handling strings.
@@ -36,6 +37,37 @@ public class StringUtil {
 
         return Arrays.stream(wordsInPreppedSentence)
                 .anyMatch(preppedWord::equalsIgnoreCase);
+    }
+
+    /**
+     * Returns true if the {@code sentence} contains the {@code word} as a substring.
+     *   Ignores case, but a full word match is required.
+     *   <br>examples:<pre>
+     *       containsWordIgnoreCase("ABc def", "abc") == true
+     *       containsWordIgnoreCase("ABc def", "DEF") == true
+     *       containsWordIgnoreCase("ABc def", "AB") == true //a partial word match
+     *       </pre>
+     * @param sentence cannot be null
+     * @param word cannot be null, cannot be empty, must be a single word
+     */
+    public static boolean containsSubstringIgnoreCase(String sentence, String word) {
+        requireNonNull(sentence);
+        requireNonNull(word);
+
+        String preppedWord = word.trim().toLowerCase(); //Convert to lower case to ensure case insensitivity
+        checkArgument(!preppedWord.isEmpty(), "Word parameter cannot be empty");
+        checkArgument(preppedWord.split("\\s+").length == 1, "Word parameter should be a single word");
+
+        String preppedSentenceLowerCase = sentence.toLowerCase(); //Convert to lower case to ensure case insensitivity
+        String[] wordsInPreppedSentence = preppedSentenceLowerCase.split("\\s+");
+
+        for (String currWord : wordsInPreppedSentence) {
+            if (currWord.contains(preppedWord)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /**
