c37309e65e8b883227321c775ee2836a1e3bc0d9
greyguy21 <e0559779@u.nus.edu>
2021-10-23 16:57:39 +0800

    change viewstudent command to take in name

--- src/main/java/seedu/address/logic/commands/ViewGroupCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewGroupCommand.java
@@ -39,8 +39,7 @@ public class ViewGroupCommand extends Command {
         requireNonNull(model);
         model.updateFilteredStudentList(predicate);
         String displayedText = getGroupInformation(model) + "\n" + Messages.MESSAGE_STUDENTS_LISTED_OVERVIEW;
-        return new CommandResult(String.format(displayedText, model.getFilteredStudentList().size()),
-            false, false, true);
+        return new CommandResult(String.format(displayedText, model.getFilteredStudentList().size()));
     }
 
     @Override
--- src/main/java/seedu/address/logic/commands/ViewStudentCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewStudentCommand.java
@@ -2,44 +2,33 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.List;
-
-import seedu.address.commons.core.Messages;
-import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.student.IsStudentPredicate;
-import seedu.address.model.student.Student;
+import seedu.address.model.student.ContainsStudentNamePredicate;
+import seedu.address.model.student.Name;
 
 public class ViewStudentCommand extends Command {
 
     public static final String COMMAND_WORD = "viewstudent";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-        + ": Finds and displays details of the student who matches the specified group name (case-sensitive) \n"
-        + "Parameters: INDEX (must be a positive integer)\n"
+        + ": Finds and displays details of the student identified by the specified name (case-sensitive) \n"
+        + "Parameters: NAME (case-sensitive)\n"
         + "Example:" + COMMAND_WORD + " 2";
 
-    private final Index index;
+    private final Name name;
     private final String MESSAGE_VIEW_STUDENT_SUCCESS = "Viewing details of %1$s";
 
-    public ViewStudentCommand(Index index) {
-        this.index = index;
+    public ViewStudentCommand(Name name) {
+        this.name = name;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        List<Student> lastShownList = model.getFilteredStudentList();
-
-        if (index.getZeroBased() >= lastShownList.size()) {
-            throw new CommandException(Messages.MESSAGE_INVALID_STUDENT_DISPLAYED_INDEX);
-        }
-
-        Student studentToView = lastShownList.get(this.index.getZeroBased());
-        IsStudentPredicate predicate = new IsStudentPredicate(studentToView);
+        ContainsStudentNamePredicate predicate = new ContainsStudentNamePredicate(name);
         model.updateFilteredStudentList(predicate);
-        return new CommandResult(String.format(MESSAGE_VIEW_STUDENT_SUCCESS, studentToView.getName()),
+        return new CommandResult(String.format(MESSAGE_VIEW_STUDENT_SUCCESS, name),
             false, false, true);
     }
 }
