b9837e761588ded833778cc71316de69452e570d
Imerbear <e0543864@u.nus.edu>
2021-10-24 03:32:25 +0800

    Changed output for MESSAGE_UNKNOWN_COMMAND

--- src/main/java/seedu/address/logic/commands/CmdCommand.java
+++ src/main/java/seedu/address/logic/commands/CmdCommand.java
@@ -10,6 +10,9 @@ public class CmdCommand extends Command {
 
     public static final String SHOWING_CMD_MESSAGE = "Opened Command Summary.";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows a list of all available commands.\n"
+            + "Example: " + COMMAND_WORD;
+
     @Override
     public CommandResult execute(Model model) {
         return new CommandResult(SHOWING_CMD_MESSAGE, true, false, false);
