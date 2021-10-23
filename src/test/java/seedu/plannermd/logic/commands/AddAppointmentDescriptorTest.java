f969a4ed29b6bd548f5b5ba393eb2e0c591a142c
ChuangZheQuan <zhequan7chuang@gmail.com>
2021-10-23 16:39:10 +0800

    Fix checkstyle

--- src/test/java/seedu/plannermd/logic/commands/AddAppointmentDescriptorTest.java
+++ src/test/java/seedu/plannermd/logic/commands/AddAppointmentDescriptorTest.java
@@ -1,28 +1,18 @@
 package seedu.plannermd.logic.commands;
 
-import org.junit.jupiter.api.Test;
-import seedu.plannermd.logic.commands.apptcommand.AddAppointmentCommand;
-import seedu.plannermd.logic.commands.editcommand.EditDoctorCommand;
-import seedu.plannermd.testutil.appointment.AddAppointmentDescriptorBuilder;
-import seedu.plannermd.testutil.doctor.EditDoctorDescriptorBuilder;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.plannermd.logic.commands.CommandTestUtil.DESC_DR_AMY;
-import static seedu.plannermd.logic.commands.CommandTestUtil.DESC_DR_BOB;
 import static seedu.plannermd.logic.commands.CommandTestUtil.DESC_THIRTY_MIN_APPT;
 import static seedu.plannermd.logic.commands.CommandTestUtil.DESC_TWO_HOUR_APPT;
 import static seedu.plannermd.logic.commands.CommandTestUtil.REMARK_DESC_AMY;
-import static seedu.plannermd.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
 import static seedu.plannermd.logic.commands.CommandTestUtil.VALID_APPT_DATE_TWO_HOUR;
 import static seedu.plannermd.logic.commands.CommandTestUtil.VALID_APPT_DURATION_TWO_HOUR;
 import static seedu.plannermd.logic.commands.CommandTestUtil.VALID_APPT_TIME_TWO_HOUR;
-import static seedu.plannermd.logic.commands.CommandTestUtil.VALID_BIRTH_DATE_AMY;
-import static seedu.plannermd.logic.commands.CommandTestUtil.VALID_BIRTH_DATE_BOB;
-import static seedu.plannermd.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
-import static seedu.plannermd.logic.commands.CommandTestUtil.VALID_NAME_BOB;
-import static seedu.plannermd.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
-import static seedu.plannermd.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.plannermd.logic.commands.apptcommand.AddAppointmentCommand;
+import seedu.plannermd.testutil.appointment.AddAppointmentDescriptorBuilder;
 
 public class AddAppointmentDescriptorTest {
 
