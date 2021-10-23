d9d2f97600d30513f08b0ad924596c1fba3b048d
Hong Yao <ganhongyao@u.nus.edu>
2021-10-23 18:33:47 +0800

    Move createBarChart to ChartUtil to remove Logic's dependency on Ui

---
+++ src/main/java/seedu/address/commons/util/ChartUtil.java
@@ -0,0 +1,32 @@
+package seedu.address.commons.util;
+
+import java.util.Map;
+
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+
+public class ChartUtil {
+    public static BarChart<String, Number> createBarChart(String title, String xLabel, String yLabel,
+                                                          Map<String, Number> data) {
+        final CategoryAxis xAxis = new CategoryAxis();
+        xAxis.setLabel(xLabel);
+
+        final NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel(yLabel);
+
+        final BarChart<String,Number> barChart = new BarChart<String, Number>(xAxis, yAxis);
+        barChart.setTitle(title);
+        barChart.setLegendVisible(false);
+
+        XYChart.Series series = new XYChart.Series();
+        for (Map.Entry<String, Number> entry : data.entrySet()) {
+            series.getData().add(new XYChart.Data<>(entry.getKey(), entry.getValue()));
+        }
+
+        barChart.getData().add(series);
+
+        return barChart;
+    }
+}
--- src/main/java/seedu/address/ui/GraphDisplay.java
+++ src/main/java/seedu/address/ui/GraphDisplay.java
@@ -30,25 +30,4 @@ public class GraphDisplay extends UiPart<Region> {
         placeHolder.getChildren().add(chart);
     }
 
-    public static BarChart<String, Number> createBarChart(String title, String xLabel, String yLabel,
-                                                           Map<String, Number> data) {
-        final CategoryAxis xAxis = new CategoryAxis();
-        xAxis.setLabel(xLabel);
-
-        final NumberAxis yAxis = new NumberAxis();
-        yAxis.setLabel(yLabel);
-
-        final BarChart<String,Number> barChart = new BarChart<String, Number>(xAxis, yAxis);
-        barChart.setTitle(title);
-        barChart.setLegendVisible(false);
-
-        XYChart.Series series = new XYChart.Series();
-        for (Map.Entry<String, Number> entry : data.entrySet()) {
-            series.getData().add(new XYChart.Data<>(entry.getKey(), entry.getValue()));
-        }
-
-        barChart.getData().add(series);
-
-        return barChart;
-    }
 }
