a3d7eadcaf70765b516c089144e79e267087d995
driip-ddrop <germaine5152seah@gmail.com>
2021-10-24 00:29:32 +0800

    Resolve checkstyle

--- src/main/java/seedu/address/logic/commands/modulelesson/DeleteModuleLessonCommand.java
+++ src/main/java/seedu/address/logic/commands/modulelesson/DeleteModuleLessonCommand.java
@@ -5,7 +5,6 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.logic.commands.person.DeletePersonCommand;
 import seedu.address.model.Model;
 import seedu.address.model.modulelesson.ModuleLesson;
 
