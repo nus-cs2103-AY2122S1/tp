37f1ff77cf14df3fc78fd31c0f1c9474d85550d1
El0hime <dillontkw@gmail.com>
2021-10-24 02:37:29 +0800

    Update tag

--- src/main/java/seedu/tracker/model/tag/Tag.java
+++ src/main/java/seedu/tracker/model/tag/Tag.java
@@ -4,13 +4,12 @@ import static java.util.Objects.requireNonNull;
 import static seedu.tracker.commons.util.AppUtil.checkArgument;
 
 /**
- * Represents a Tag in the address book.
+ * Represents a Tag in the module tracker.
  * Guarantees: immutable; name is valid as declared in {@link #isValidTagName(String)}
  */
 public class Tag {
 
-    public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric";
-    public static final String VALIDATION_REGEX = "\\p{Alnum}+";
+    public static final String MESSAGE_CONSTRAINTS = "Tags names should not be empty";
 
     public final String tagName;
 
@@ -29,7 +28,7 @@ public class Tag {
      * Returns true if a given string is a valid tag name.
      */
     public static boolean isValidTagName(String test) {
-        return test.matches(VALIDATION_REGEX);
+        return !test.isEmpty();
     }
 
     @Override
