7d7479a5f6800c65299b3a2da9a12e572d02b685
IrvingHe000 <heoutong@gmail.com>
2021-10-24 16:21:13 +0800

    Update exception class related to setShiftTime

--- src/main/java/seedu/address/commons/exceptions/InvalidTimeException.java
+++
@@ -1,9 +0,0 @@
-package seedu.address.commons.exceptions;
-
-import java.time.DateTimeException;
-
-public class InvalidTimeException extends DateTimeException {
-    public InvalidTimeException() {
-        super("message");
-    }
-}
