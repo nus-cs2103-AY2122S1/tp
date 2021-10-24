9a98735ff852130ad23cc9db365fb366de4d54d0
Jason <jason.ng.zq99@gmail.com>
2021-10-24 15:07:40 +0800

    Edit MESSAGE_USAGE for findc command

--- src/main/java/seedu/address/logic/commands/modulelesson/FindModuleLessonCommand.java
+++ src/main/java/seedu/address/logic/commands/modulelesson/FindModuleLessonCommand.java
@@ -4,6 +4,7 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_LESSON_DAY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_LESSON_TIME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULE_CODE;
+import static seedu.address.logic.parser.CommandWord.*;
 
 import java.util.function.Predicate;
 
@@ -11,6 +12,7 @@ import seedu.address.commons.core.Messages;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.CommandWord;
 import seedu.address.model.Model;
 import seedu.address.model.modulelesson.LessonDayContainsKeywordsPredicate;
 import seedu.address.model.modulelesson.LessonTimeContainsKeywordsPredicate;
@@ -19,15 +21,17 @@ import seedu.address.model.modulelesson.ModuleLesson;
 
 public class FindModuleLessonCommand extends Command {
 
+    private static final String commandWord = getAliasList(FIND_MODULE_LESSON).get(0);
+
     public static final String MESSAGE_USAGE = "find: Finds all lesson whose details contain "
             + "any of the specified keywords (case-insensitive).\n"
             + "You can choose one of three ways to search:\n"
-            + "1) search by module code(s) using the prefix " + PREFIX_MODULE_CODE
-            + "find " + PREFIX_MODULE_CODE + "CS2030S CS2100\n"
-            + "2) search by lesson day(s) using the prefix " + PREFIX_LESSON_DAY
-            + "find " + PREFIX_LESSON_DAY + "2\n"
-            + "3) search by lesson start time(s) using the prefix " + PREFIX_LESSON_TIME
-            + "find " + PREFIX_LESSON_TIME + "14:00\n";
+            + "1) search by module code(s) using the prefix '" + PREFIX_MODULE_CODE
+            + "' e.g. " + commandWord + " " + PREFIX_MODULE_CODE + " CS2030S CS2100\n"
+            + "2) search by lesson day(s) using the prefix '" + PREFIX_LESSON_DAY
+            + "' e.g. " + commandWord + " " + PREFIX_LESSON_DAY + "2\n"
+            + "3) search by lesson start time(s) using the prefix '" + PREFIX_LESSON_TIME
+            + "' e.g. " + commandWord + " " + PREFIX_LESSON_TIME + "14:00\n";
     public static final String MESSAGE_SINGLE_PREFIX_SEARCH = "You can only search with a single prefix.";
 
     private final Predicate<ModuleLesson> predicate;
