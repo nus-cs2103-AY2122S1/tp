550aa7206cfc6d2f8feae3d7e04596305718e6f0
SkyBlaise <zsc990124@gmail.com>
2021-10-24 17:17:56 +0800

    Fix null pointer exception due to switch statement receiving null commandType for clear command

--- src/main/java/seedu/address/logic/commands/CommandType.java
+++ src/main/java/seedu/address/logic/commands/CommandType.java
@@ -1,5 +1,5 @@
 package seedu.address.logic.commands;
 
 public enum CommandType {
-    ADD, DELETE, EDIT, EXIT, FIND, HELP, LIST, STAT, VIEW
+    ADD, DELETE, EDIT, FIND, LIST, VIEW, STAT, CLEAR, HELP, EXIT
 }
