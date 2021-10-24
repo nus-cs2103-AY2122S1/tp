a4f99fbf7563f6f7863bc00f3d755ced23385670
kflim <redbulldown@gmail.com>
2021-10-24 21:31:57 +0800

    Fix import bug

--- src/test/java/seedu/address/logic/parser/ViewTaskListCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ViewTaskListCommandParserTest.java
@@ -8,6 +8,7 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+
 import seedu.address.logic.commands.ViewTaskListCommand;
 
 public class ViewTaskListCommandParserTest {
