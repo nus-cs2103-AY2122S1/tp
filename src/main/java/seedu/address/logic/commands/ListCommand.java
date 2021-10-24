aac299625044426df2c4be99cfe381bd0efb5067
Chen-Yiyang <iycspfa@gmail.com>
2021-10-24 22:43:14 +0800

    Update command usages

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -1,6 +1,10 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_INCOMING_MONTH;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_INCOMING_WEEK;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_LAST_VISIT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_VISIT;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import java.util.function.Predicate;
@@ -12,8 +16,17 @@ import seedu.address.model.person.Person;
  * Lists all persons in the address book to the user.
  */
 public class ListCommand extends Command {
-
     public static final String COMMAND_WORD = "list";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Displays a list of all the persons in the system by default"
+            + " or display a list of all the persons with incoming visits in the next month (next 30 days) using "
+            + " flag " + PREFIX_INCOMING_MONTH
+            + " or in the next week (next 7 days) using flag " + PREFIX_INCOMING_WEEK + "\n"
+            + "Parameters: [" + PREFIX_INCOMING_MONTH + "] or [" + PREFIX_INCOMING_WEEK + "]\n"
+            + "Example: " + COMMAND_WORD
+            + " or " + COMMAND_WORD + " " + PREFIX_INCOMING_MONTH
+            + " or " + COMMAND_WORD + " " + PREFIX_INCOMING_WEEK;
 
     public static final String MESSAGE_SUCCESS_ALL = "Listed all persons";
     public static final String MESSAGE_SUCCESS_INCOMING = "Listed all persons with existing visits in next %1$s.";
@@ -22,7 +35,7 @@ public class ListCommand extends Command {
     public static final Predicate<Person> PREDICATE_HAS_VISIT_THIS_WEEK = (Person::hasVisitThisWeek);
 
     // TODO
-    public static final String MESSAGE_USAGE = "All or incoming";
+
 
     private final boolean isIncoming;
     private final boolean isNextMonth;
@@ -50,10 +63,10 @@ public class ListCommand extends Command {
             model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
             return new CommandResult(MESSAGE_SUCCESS_ALL);
         } else if (isNextMonth) {
-            model.updateFilteredPersonList(Person::hasVisitThisMonth);
+            model.updateFilteredPersonList(PREDICATE_HAS_VISIT_THIS_MONTH);
             return new CommandResult(String.format(MESSAGE_SUCCESS_INCOMING, "month"));
         } else {
-            model.updateFilteredPersonList(Person::hasVisitThisWeek);
+            model.updateFilteredPersonList(PREDICATE_HAS_VISIT_THIS_WEEK);
             return new CommandResult(String.format(MESSAGE_SUCCESS_INCOMING, "week"));
         }
     }
