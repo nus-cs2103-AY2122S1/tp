415e5dcbfe6e5fd53c4a95f3f12c977dbe45a838
rachelcheah <rachelcheah06@gmail.com>
2021-10-24 17:48:01 +0800

    Updated testcase to ignore last line of expected output to include extra line at EOF

--- src/test/java/seedu/address/logic/commands/ExportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExportCommandTest.java
@@ -41,7 +41,7 @@ public class ExportCommandTest {
 
         assertEquals(file1.size(), file2.size());
 
-        for (int i = 0; i < file1.size(); i++) {
+        for (int i = 0; i < file2.size()-1; i++) {
             System.out.println("Comparing line: " + i);
             assertEquals(file1.get(i), file2.get(i));
         }
