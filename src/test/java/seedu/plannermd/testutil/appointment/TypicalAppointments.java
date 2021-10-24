80b94c96e2241f4e27d7fdadb90f64c577303168
ChuangZheQuan <zhequan7chuang@gmail.com>
2021-10-24 08:30:27 +0800

    Change typical appointments

--- src/test/java/seedu/plannermd/testutil/appointment/TypicalAppointments.java
+++ src/test/java/seedu/plannermd/testutil/appointment/TypicalAppointments.java
@@ -1,14 +1,11 @@
 package seedu.plannermd.testutil.appointment;
 
-import static seedu.plannermd.logic.commands.CommandTestUtil.VALID_APPT_DATE_THIRTY_MIN;
-import static seedu.plannermd.logic.commands.CommandTestUtil.VALID_APPT_DURATION_THIRTY_MIN;
-import static seedu.plannermd.logic.commands.CommandTestUtil.VALID_APPT_TIME_THIRTY_MIN;
-import static seedu.plannermd.testutil.doctor.TypicalDoctors.DR_AMY;
-import static seedu.plannermd.testutil.doctor.TypicalDoctors.DR_BENSON;
-import static seedu.plannermd.testutil.doctor.TypicalDoctors.DR_CARL;
+import static seedu.plannermd.testutil.doctor.TypicalDoctors.DR_ELLE;
+import static seedu.plannermd.testutil.doctor.TypicalDoctors.DR_FIONA;
+import static seedu.plannermd.testutil.doctor.TypicalDoctors.DR_GEORGE;
 import static seedu.plannermd.testutil.patient.TypicalPatients.ALICE;
-import static seedu.plannermd.testutil.patient.TypicalPatients.ELLE;
-import static seedu.plannermd.testutil.patient.TypicalPatients.FIONA;
+import static seedu.plannermd.testutil.patient.TypicalPatients.BENSON;
+import static seedu.plannermd.testutil.patient.TypicalPatients.CARL;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,13 +14,18 @@ import java.util.List;
 import seedu.plannermd.model.appointment.Appointment;
 
 public class TypicalAppointments {
-    public static final Appointment TWO_HOUR_APPOINTMENT = new AppointmentBuilder().withPatient(ELLE)
-            .withDoctor(DR_AMY).withDate(VALID_APPT_DATE_THIRTY_MIN).withDate("12/8/2022").build();
-    public static final Appointment FIVE_MIN_APPOINTMENT = new AppointmentBuilder().withPatient(FIONA)
-            .withDoctor(DR_CARL).withDate(VALID_APPT_DATE_THIRTY_MIN).withDate("19/12/2021").build();
-    public static final Appointment THIRTY_MIN_APPOINTMENT = new AppointmentBuilder().withPatient(ALICE)
-            .withDoctor(DR_BENSON).withDate(VALID_APPT_DATE_THIRTY_MIN)
-            .withSession(VALID_APPT_TIME_THIRTY_MIN, VALID_APPT_DURATION_THIRTY_MIN).build();
+    public static final Appointment TWO_HOUR_APPOINTMENT = new AppointmentBuilder()
+            .withPatient(ALICE).withDoctor(DR_GEORGE).withDate("6/6/2022")
+            .withSession("18:00", 120)
+            .withRemark("Therapy Session").build();
+    public static final Appointment FIVE_MIN_APPOINTMENT = new AppointmentBuilder()
+            .withPatient(BENSON).withDoctor(DR_FIONA).withDate("8/8/2022")
+            .withSession("12:00", 5)
+            .withRemark("Blocked Nose").build();
+    public static final Appointment THIRTY_MIN_APPOINTMENT = new AppointmentBuilder()
+            .withPatient(CARL).withDoctor(DR_ELLE).withDate("10/10/2022")
+            .withSession("09:00", 30)
+            .withRemark("High Fever and Cough").build();
 
     private TypicalAppointments() {
     } // prevents instantiation
