885d89483e6d8a8a4c771222ae08e4d7c01bbbaf
GWphua <phuaguanwei99@gmail.com>
2021-10-24 15:51:54 +0800

    Add method to return ObservableList in Logic.

--- src/main/java/seedu/address/logic/Logic.java
+++ src/main/java/seedu/address/logic/Logic.java
@@ -10,6 +10,7 @@ import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.module.event.Event;
 import seedu.address.model.module.member.Member;
+import seedu.address.model.module.task.Task;
 
 /**
  * API of the Logic component
@@ -32,12 +33,19 @@ public interface Logic {
     ReadOnlyAddressBook getAddressBook();
 
     /**
-     * Returns an unmodifiable view of the filtered list of members
+     * Returns an unmodifiable view of the filtered list of members.
      */
     ObservableList<Member> getFilteredMemberList();
 
-    /** Returns an unmodifiable view of the filtered list of events */
+    /**
+     * Returns an unmodifiable view of the filtered list of events.
+     */
     ObservableList<Event> getFilteredEventList();
+
+    /**
+     * Returns an unmodifiable view of the filtered list of tasks.
+     */
+    ObservableList<Task> getFilteredTaskList();
 
     /**
      * Returns the user prefs' address book file path.
