1dea221f18c7311862d971378b9540cbb5ada1eb
HongshuW <hongshu.wang.uni@gmail.com>
2021-10-25 00:47:22 +0800

    OrderTest class: checkStyle

--- src/test/java/seedu/address/model/order/OrderTest.java
+++ src/test/java/seedu/address/model/order/OrderTest.java
@@ -1,6 +1,7 @@
 package seedu.address.model.order;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static seedu.address.testutil.TypicalProducts.AIRPODS;
 import static seedu.address.testutil.TypicalProducts.IPHONE;
 
@@ -11,23 +12,23 @@ import org.junit.jupiter.api.Test;
 import seedu.address.model.product.Quantity;
 
 public class OrderTest {
-    LocalDate now = LocalDate.now();
-    Order oneIphone = new Order(IPHONE.getId(), new Quantity("1"), now);
-    Order oneIphoneCopy = new Order(IPHONE.getId(), new Quantity("1"), now);
-    Order twoAirpods = new Order(AIRPODS.getId(), new Quantity("2"), LocalDate.MAX);
+    private final LocalDate now = LocalDate.now();
+    private final Order oneIphone = new Order(IPHONE.getId(), new Quantity("1"), now);
+    private final Order oneIphoneCopy = new Order(IPHONE.getId(), new Quantity("1"), now);
+    private final Order twoAirpods = new Order(AIRPODS.getId(), new Quantity("2"), LocalDate.MAX);
 
     @Test
     public void equals() {
         // same object -> returns true
-        assertTrue(oneIphone.equals(oneIphone));
+        assertEquals(oneIphone, oneIphone);
 
         // order compares to an object that is not an order
-        assertTrue(!oneIphone.equals(IPHONE));
+        assertNotEquals(oneIphone, IPHONE);
 
         // different objects with the same values
-        assertTrue(oneIphone.equals(oneIphoneCopy));
+        assertEquals(oneIphone, oneIphoneCopy);
 
         // two different orders
-        assertTrue(!oneIphone.equals(twoAirpods));
+        assertNotEquals(oneIphone, twoAirpods);
     }
 }
