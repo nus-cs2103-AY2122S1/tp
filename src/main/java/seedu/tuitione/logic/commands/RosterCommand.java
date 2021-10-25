adb340d3d07b30800f79f06a2925f41a7c3e14c8
Timothyoung97 <timothyoung97@gmail.com>
2021-10-25 13:41:24 +0800

    Add roster command class skeleton

---
+++ src/main/java/seedu/tuitione/logic/commands/RosterCommand.java
@@ -0,0 +1,72 @@
+package seedu.tuitione.logic.commands;
+
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+import seedu.tuitione.commons.core.index.Index;
+import seedu.tuitione.logic.commands.exceptions.CommandException;
+import static seedu.tuitione.logic.parser.CliSyntax.PREFIX_LESSON;
+
+import seedu.tuitione.commons.core.Messages;
+import seedu.tuitione.model.Model;
+import seedu.tuitione.model.lesson.Lesson;
+import seedu.tuitione.model.lesson.LessonCode;
+
+/**
+ * Display a list of students who are enrolled in a specific lesson in tuitone.
+ */
+public class RosterCommand extends Command {
+
+    public static final String COMMAND_WORD = "roster";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": displays the list of students who are"
+            + "enrolled in a specific lesson registered in TuitiONE.\n"
+            + "Parameters: "
+            + "[" + PREFIX_LESSON + "LESSON_INDEX] "
+            + "Example: " + COMMAND_WORD + " " + PREFIX_LESSON + "1";
+
+    public static final String MESSAGE_ROSTER_LESSON_SUCCESS = "Roster of Lesson : %1$s is successfully displayed.\n"
+            + "A total of %d student(s) found.";
+
+    private final Index targetIndex;
+
+    /**
+     * Constructor
+     * @param targetIndex
+     */
+    public RosterCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Lesson> lastShownList = model.getFilteredLessonList();
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_LESSON_DISPLAYED_INDEX);
+        }
+
+        String output = "";
+
+        Lesson lessonToShow = lastShownList.get(targetIndex.getZeroBased());
+        LessonCode lessonCodeToUse = lessonToShow.getLessonCode();
+
+        if (lessonCodeToUse != null) {
+            model.updateFilteredLessonList(new LessonIsOfSpecifiedLessonCode(lessonCodeToUse));
+            model.updateFilteredStudentList(new StudentIsOfSpecifiedLessonCode(lessonCodeToUse));
+            output = String.format(MESSAGE_ROSTER_LESSON_SUCCESS,
+                    lessonCodeToUse.toString(),
+                    model.getFilteredStudentList().size());
+        }
+
+        return new CommandResult(output);   // output should never be empty string
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof RosterCommand // instanceof handles nulls
+                && targetIndex.equals(((RosterCommand) other).targetIndex)); // state check
+    }
+
+}
