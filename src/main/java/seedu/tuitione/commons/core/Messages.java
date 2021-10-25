04dd77fbc7b83e39e05ea58746036b7ae05dcbc1
Timothyoung97 <timothyoung97@gmail.com>
2021-10-26 03:59:34 +0800

    Icon Updates

--- src/main/java/seedu/tuitione/commons/core/Messages.java
+++ src/main/java/seedu/tuitione/commons/core/Messages.java
@@ -6,9 +6,9 @@ package seedu.tuitione.commons.core;
 public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "❓\tUnknown command";
-    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "⚠\tAlert!\nInvalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_STUDENT_DISPLAYED_INDEX = "❌\tIndex Error!\nThe student index cannot be found";
-    public static final String MESSAGE_INVALID_LESSON_DISPLAYED_INDEX = "❌\tIndex Error!\nThe lesson index cannot be found";
+    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "⚠\tAlert:\nInvalid command format! \n%1$s";
+    public static final String MESSAGE_INVALID_STUDENT_DISPLAYED_INDEX = "❌\tIndex Error:\nThe student index cannot be found";
+    public static final String MESSAGE_INVALID_LESSON_DISPLAYED_INDEX = "❌\tIndex Error:\nThe lesson index cannot be found";
     public static final String MESSAGE_PLURAL_STUDENT_LISTED_OVERVIEW = "ℹ\tUpdate:\n%1$d students found!";
     public static final String MESSAGE_SINGULAR_STUDENT_LISTED_OVERVIEW = "ℹ\tUpdate:\n%1$d student found!";
     public static final String MESSAGE_LESSON_FOUND_OVERVIEW = "ℹ\tUpdate:\n%1$d lessons(s) found!";
