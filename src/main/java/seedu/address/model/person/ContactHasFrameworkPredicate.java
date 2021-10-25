043f08ee1f9b82cadee21f9f22149a5e76ae107e
timothywongej <e0559903@u.nus.edu>
2021-10-25 18:25:34 +0800

    Format code for checkstyle test

--- src/main/java/seedu/address/model/person/ContactHasFrameworkPredicate.java
+++ src/main/java/seedu/address/model/person/ContactHasFrameworkPredicate.java
@@ -6,8 +6,13 @@ import java.util.function.Predicate;
 import seedu.address.model.skill.Framework;
 
 public class ContactHasFrameworkPredicate implements Predicate<Person> {
+
     private final List<String> tags;
-    public ContactHasFrameworkPredicate(List<String> tags) { this.tags = tags; }
+
+    public ContactHasFrameworkPredicate(List<String> tags) {
+        this.tags = tags;
+    }
+
     @Override
     public boolean test(Person person) {
         if (!tags.isEmpty()) {
