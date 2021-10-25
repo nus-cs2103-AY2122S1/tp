c5a2af895261a3ac7a24fe90ede54cb298978f4c
angkoonhwee <akoonhwee@gmail.com>
2021-10-26 03:00:59 +0800

    Add PaidCommandParserTest

--- src/main/java/seedu/address/logic/parser/PaidCommandParser.java
+++ src/main/java/seedu/address/logic/parser/PaidCommandParser.java
@@ -8,7 +8,6 @@ import seedu.address.model.lesson.Money;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PAID_AMOUNT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
 
 public class PaidCommandParser implements Parser<PaidCommand> {
 
--- src/test/java/seedu/address/logic/parser/PaidCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/PaidCommandParserTest.java
@@ -7,12 +7,16 @@ import seedu.address.logic.commands.PaidCommand;
 import seedu.address.model.lesson.Money;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_PAYMENT_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.PAYMENT_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PAYMENT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PAID_AMOUNT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_LESSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_LESSON;
 
 class PaidCommandParserTest {
     private static final String MESSAGE_INVALID_FORMAT =
@@ -41,6 +45,19 @@ class PaidCommandParserTest {
     }
 
     @Test
+    public void parse_missingParts_failure() {
+        // no index specified
+        assertParseFailure(parser, PAYMENT_DESC, MESSAGE_INVALID_FORMAT);
+
+        // no field specified
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, PaidCommand.MESSAGE_USAGE);
+        assertParseFailure(parser, "1 1", expectedMessage);
+
+        // no index and no field specified
+        assertParseFailure(parser, "", MESSAGE_INVALID_FORMAT);
+    }
+
+    @Test
     public void parse_amountFieldSpecified_success() {
         Index targetIndex = INDEX_SECOND_PERSON;
         Index lessonTargetIndex = INDEX_FIRST_LESSON;
@@ -50,6 +67,24 @@ class PaidCommandParserTest {
 
         PaidCommand expectedCommand = new PaidCommand(targetIndex, lessonTargetIndex, payment);
 
+        assertParseSuccess(parser, userInput, expectedCommand);
+    }
+
+    @Test
+    public void parse_invalidAmount_failure() {
+        assertParseFailure(parser, "1 1" + INVALID_PAYMENT_DESC, Money.MESSAGE_CONSTRAINTS);
+    }
+
+    @Test
+    public void parse_multipleRepeatedAmount_acceptsLast() {
+        Index targetIndex = INDEX_FIRST_PERSON;
+        Index lessonTargetIndex = INDEX_THIRD_LESSON;
+        String testPayment = " " + PREFIX_PAID_AMOUNT + "10.00 ";
+
+        String userInput = targetIndex.getOneBased() + " " + lessonTargetIndex.getOneBased()
+                + testPayment + PAYMENT_DESC;
+
+        PaidCommand expectedCommand = new PaidCommand(targetIndex, lessonTargetIndex, new Money(VALID_PAYMENT));
         assertParseSuccess(parser, userInput, expectedCommand);
     }
 }
