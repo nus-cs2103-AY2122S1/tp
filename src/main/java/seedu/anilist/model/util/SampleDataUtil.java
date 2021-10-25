185bf2165ea5b08cef4fba16ec8fc079093ae183
LuoDan <69646558+luodan01@users.noreply.github.com>
2021-10-25 15:24:53 +0800

    Revert sample data

--- src/main/java/seedu/anilist/model/util/SampleDataUtil.java
+++ src/main/java/seedu/anilist/model/util/SampleDataUtil.java
@@ -16,66 +16,66 @@ import seedu.anilist.model.genre.Genre;
  * Contains utility methods for populating {@code AnimeList} with sample data.
  */
 public class SampleDataUtil {
-//    public static Anime[] getSampleAnime() {
-//        return new Anime[] {
-//            new Anime(new Name("Asobi Asobase"),
-//                    new Episode("0"),
-//                    new Status("w"),
-//                    getGenreSet("comedy")),
-//            new Anime(new Name("BLEACH"),
-//                    new Episode("0"),
-//                    new Status("w"),
-//                    getGenreSet("adventure", "supernatural")),
-//            new Anime(new Name("Charlotte"),
-//                    new Episode("0"),
-//                    new Status("w"),
-//                    getGenreSet("drama", "supernatural")),
-//            new Anime(new Name("Darling in the Franxx"),
-//                    new Episode("0"),
-//                    new Status("w"),
-//                    getGenreSet("action", "mecha", "science fiction")),
-//            new Anime(new Name("Haikyuu!!: To the Top"),
-//                    new Episode("0"),
-//                    new Status("w"),
-//                    getGenreSet("sports")),
-//            new Anime(new Name("Steins;Gate 0"),
-//                    new Episode("0"),
-//                    new Status("w"),
-//                    getGenreSet("science fiction")),
-//            new Anime(new Name("Watashi ga Motenai no wa Dou Kangaetemo Omaera ga Warui!"),
-//                    new Episode("0"),
-//                    new Status("w"),
-//                    getGenreSet("genre1", "genre2", "genre3", "genre4", "genre5", "genre6", "genre7", "genre8",
-//                            "genre9", "genre10", "genre11", "genre12", "genre13", "genre14", "genre15")),
-//            new Anime(new Name("3-gatsu no Lion 2nd Season"),
-//                    new Episode("22"),
-//                    new Status("f"),
-//                    getGenreSet("drama", "slice of life")),
-//            new Anime(new Name("Fullmetal Alchemist: Brotherhood"),
-//                    new Episode("64"),
-//                    new Status("f"),
-//                    getGenreSet("action", "adventure", "comedy", "drama", "fantasy")),
-//            new Anime(new Name("Blue Period"),
-//                    new Episode("0"),
-//                    new Status("t"),
-//                    getGenreSet( "drama", "slice of life")),
-//            new Anime(new Name("Komi-san wa, Comyushou desu."),
-//                    new Episode("0"),
-//                    new Status("t"),
-//                    getGenreSet("comedy", "slice of life"))
-//        };
-//    }
+    public static Anime[] getSampleAnime() {
+        return new Anime[] {
+            new Anime(new Name("Asobi Asobase"),
+                    new Episode("0"),
+                    new Status("w"),
+                    getGenreSet("comedy")),
+            new Anime(new Name("BLEACH"),
+                    new Episode("0"),
+                    new Status("w"),
+                    getGenreSet("adventure", "supernatural")),
+            new Anime(new Name("Charlotte"),
+                    new Episode("0"),
+                    new Status("w"),
+                    getGenreSet("drama", "supernatural")),
+            new Anime(new Name("Darling in the Franxx"),
+                    new Episode("0"),
+                    new Status("w"),
+                    getGenreSet("action", "mecha", "science fiction")),
+            new Anime(new Name("Haikyuu!!: To the Top"),
+                    new Episode("0"),
+                    new Status("w"),
+                    getGenreSet("sports")),
+            new Anime(new Name("Steins;Gate 0"),
+                    new Episode("0"),
+                    new Status("w"),
+                    getGenreSet("science fiction")),
+            new Anime(new Name("Watashi ga Motenai no wa Dou Kangaetemo Omaera ga Warui!"),
+                    new Episode("0"),
+                    new Status("w"),
+                    getGenreSet("genre1", "genre2", "genre3", "genre4", "genre5", "genre6", "genre7", "genre8",
+                            "genre9", "genre10", "genre11", "genre12", "genre13", "genre14", "genre15")),
+            new Anime(new Name("3-gatsu no Lion 2nd Season"),
+                    new Episode("22"),
+                    new Status("f"),
+                    getGenreSet("drama", "slice of life")),
+            new Anime(new Name("Fullmetal Alchemist: Brotherhood"),
+                    new Episode("64"),
+                    new Status("f"),
+                    getGenreSet("action", "adventure", "comedy", "drama", "fantasy")),
+            new Anime(new Name("Blue Period"),
+                    new Episode("0"),
+                    new Status("t"),
+                    getGenreSet( "drama", "slice of life")),
+            new Anime(new Name("Komi-san wa, Comyushou desu."),
+                    new Episode("0"),
+                    new Status("t"),
+                    getGenreSet("comedy", "slice of life"))
+        };
+    }
 
     //for testing
-    public static Anime[] getSampleAnime() {
-        Anime[] animeArr = new Anime[100];
-        String animeName = "x";
-        for (int i = 0; i < 100; i++) {
-            animeArr[i] = new Anime(new Name(animeName), new Episode("0"), new Status("t"), getGenreSet("comedy", "slice of life"));
-            animeName += "x";
-        }
-        return animeArr;
-    }
+//    public static Anime[] getSampleAnime() {
+//        Anime[] animeArr = new Anime[100];
+//        String animeName = "x";
+//        for (int i = 0; i < 100; i++) {
+//            animeArr[i] = new Anime(new Name(animeName), new Episode("0"), new Status("t"), getGenreSet("comedy", "slice of life"));
+//            animeName += "x";
+//        }
+//        return animeArr;
+//    }
 
     public static ReadOnlyAnimeList getSampleAnimeList() {
         AnimeList sampleAb = new AnimeList();
