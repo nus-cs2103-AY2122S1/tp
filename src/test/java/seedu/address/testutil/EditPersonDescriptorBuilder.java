783033c33a90d6b2559bd8130364f87a3c8962cf
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-10-25 23:55:15 +0800

    Remove more unused imports

--- src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
+++ src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
@@ -15,7 +15,6 @@ import seedu.address.model.person.Note;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
-import seedu.address.model.util.SampleDataUtil;
 
 /**
  * A utility class to help with building EditPersonDescriptor objects.
