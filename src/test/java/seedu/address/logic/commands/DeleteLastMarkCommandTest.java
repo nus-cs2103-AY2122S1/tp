77e1c4a0f65913e442e7beaccecc2c0d8fdbf4de
juliussneezer04 <vishnu.sundaresan@gmail.com>
2021-10-25 18:23:21 +0800

    Adding DeleteLastMarkCommand test cases. Let's add test cases for DeleteLastMarkCommand.

--- src/test/java/seedu/address/logic/commands/DeleteLastMarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteLastMarkCommandTest.java
@@ -7,6 +7,7 @@ import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showStudentAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_STUDENT;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_STUDENT;
+import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_STUDENT;
 import static seedu.address.testutil.TypicalStudents.getTypicalClassmate;
 import static seedu.address.testutil.TypicalStudents.ALICE;
 
@@ -17,26 +18,25 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.student.Student;
-import seedu.address.model.student.StudentMark;
 import seedu.address.testutil.StudentBuilder;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for AddLastMarkCommand.
  */
-public class AddLastMarkCommandTest {
+public class DeleteLastMarkCommandTest {
 
     private Model model = new ModelManager(getTypicalClassmate(), new UserPrefs());
 
     @Test
-    public void execute_markSpecifiedUnfilteredList_success() {
-        AddLastMarkCommand addLastMarkCommand = new AddLastMarkCommand(INDEX_FIRST_STUDENT, StudentMark.GOOD);
-        Student editedStudent = new StudentBuilder(ALICE).withMarks("LOW", "HIGH", "GOOD").build();
+    public void execute_indexSpecifiedUnfilteredList_success() {
+        DeleteLastMarkCommand deleteLastMarkCommand = new DeleteLastMarkCommand(INDEX_FIRST_STUDENT);
+        Student editedStudent = new StudentBuilder(ALICE).withMarks("LOW").build();
 
-        String expectedMessage = String.format(AddLastMarkCommand.MESSAGE_ADD_MARK_STUDENT_SUCCESS, editedStudent);
+        String expectedMessage = String.format(DeleteLastMarkCommand.MESSAGE_DELETE_MARK_STUDENT_SUCCESS, editedStudent);
 
         Model expectedModel = new ModelManager(new Classmate(model.getClassmate()), new UserPrefs());
 
-        assertCommandSuccess(addLastMarkCommand, model, expectedMessage, expectedModel);
+        assertCommandSuccess(deleteLastMarkCommand, model, expectedMessage, expectedModel);
     }
 
     @Test
@@ -44,23 +44,30 @@ public class AddLastMarkCommandTest {
         showStudentAtIndex(model, INDEX_FIRST_STUDENT);
 
         Student studentInFilteredList = model.getFilteredStudentList().get(INDEX_FIRST_STUDENT.getZeroBased());
-        Student editedStudent = new StudentBuilder(studentInFilteredList).withMarks("LOW", "HIGH", "GOOD").build();
-        AddLastMarkCommand addLastMarkCommand = new AddLastMarkCommand(INDEX_FIRST_STUDENT, StudentMark.GOOD);
+        Student editedStudent = new StudentBuilder(studentInFilteredList).withMarks("LOW").build();
+        DeleteLastMarkCommand deleteLastMarkCommand = new DeleteLastMarkCommand(INDEX_FIRST_STUDENT);
 
-        String expectedMessage = String.format(AddLastMarkCommand.MESSAGE_ADD_MARK_STUDENT_SUCCESS, editedStudent);
+        String expectedMessage = String.format(DeleteLastMarkCommand.MESSAGE_DELETE_MARK_STUDENT_SUCCESS, editedStudent);
 
         Model expectedModel = new ModelManager(new Classmate(model.getClassmate()), new UserPrefs());
         expectedModel.setStudent(model.getFilteredStudentList().get(0), editedStudent);
 
-        assertCommandSuccess(addLastMarkCommand, model, expectedMessage, expectedModel);
+        assertCommandSuccess(deleteLastMarkCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_deleteEmptyMarksFilteredList_failure() {
+        DeleteLastMarkCommand deleteLastMarkCommand = new DeleteLastMarkCommand(INDEX_THIRD_STUDENT);
+
+        assertCommandFailure(deleteLastMarkCommand, model, DeleteLastMarkCommand.MESSAGE_NO_MARKS);
     }
 
     @Test
     public void equals() {
-        final AddLastMarkCommand standardCommand = new AddLastMarkCommand(INDEX_FIRST_STUDENT, StudentMark.EXCELLENT);
+        final DeleteLastMarkCommand standardCommand = new DeleteLastMarkCommand(INDEX_FIRST_STUDENT);
 
         // same values -> returns true
-        AddLastMarkCommand commandWithSameValues = new AddLastMarkCommand(INDEX_FIRST_STUDENT, StudentMark.EXCELLENT);
+        DeleteLastMarkCommand commandWithSameValues = new DeleteLastMarkCommand(INDEX_FIRST_STUDENT);
         assertTrue(standardCommand.equals(commandWithSameValues));
 
         // same object -> returns true
@@ -73,10 +80,7 @@ public class AddLastMarkCommandTest {
         assertFalse(standardCommand.equals(new ClearCommand()));
 
         // different index -> returns false
-        assertFalse(standardCommand.equals(new AddLastMarkCommand(INDEX_SECOND_STUDENT, StudentMark.EXCELLENT)));
-
-        // different mark -> returns false
-        assertFalse(standardCommand.equals(new AddLastMarkCommand(INDEX_FIRST_STUDENT, StudentMark.GOOD)));
+        assertFalse(standardCommand.equals(new DeleteLastMarkCommand(INDEX_SECOND_STUDENT)));
     }
 
 }
