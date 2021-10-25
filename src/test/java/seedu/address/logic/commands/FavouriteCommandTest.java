3170579fda0e3012f02f8c26313af202fe37145a
CheongYeeMing <e0526001@u.nus.edu>
2021-10-26 02:06:38 +0800

    Initialise `FavouriteCommandTest`.

---
+++ src/test/java/seedu/address/logic/commands/FavouriteCommandTest.java
@@ -0,0 +1,44 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for EditCommand.
+ */
+public class FavouriteCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs(), null);
+
+    @Test
+    public void equals() {
+        FavouriteCommand favouriteFirstCommand = new FavouriteCommand(INDEX_FIRST_PERSON);
+        FavouriteCommand favouriteSecondCommand = new FavouriteCommand(INDEX_SECOND_PERSON);
+
+        // same object -> returns true
+        assertTrue(favouriteFirstCommand.equals(favouriteFirstCommand));
+
+        // same values -> returns true
+        FavouriteCommand favouriteFirstCommandCopy = new FavouriteCommand(INDEX_FIRST_PERSON);
+        assertTrue(favouriteFirstCommand.equals(favouriteFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(favouriteFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(favouriteFirstCommand.equals(null));
+
+        // different person -> returns false
+        assertFalse(favouriteFirstCommand.equals(favouriteSecondCommand));
+    }
+
+}
