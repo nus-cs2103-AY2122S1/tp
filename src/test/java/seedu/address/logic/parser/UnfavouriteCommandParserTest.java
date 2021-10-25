5b5739b4f4239f250aab336eb7828d92a0326d4a
CheongYeeMing <e0526001@u.nus.edu>
2021-10-26 03:44:45 +0800

    Add `UnfavouriteCommandParserTest`.

--- src/test/java/seedu/address/logic/parser/UnfavouriteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/UnfavouriteCommandParserTest.java
@@ -7,7 +7,7 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.FavouriteCommand;
+import seedu.address.logic.commands.UnfavouriteCommand;
 
 /**
  * As we are only doing white-box testing, our test cases do not cover path variations
@@ -16,18 +16,18 @@ import seedu.address.logic.commands.FavouriteCommand;
  * The path variation for those two cases occur inside the ParserUtil, and
  * therefore should be covered by the ParserUtilTest.
  */
-public class FavouriteCommandParserTest {
+public class UnfavouriteCommandParserTest {
 
-    private FavouriteCommandParser parser = new FavouriteCommandParser();
+    private UnfavouriteCommandParser parser = new UnfavouriteCommandParser();
 
     @Test
-    public void parse_validArgs_returnsFavouriteCommand() {
-        assertParseSuccess(parser, "1", new FavouriteCommand(INDEX_FIRST_PERSON));
+    public void parse_validArgs_returnsUnfavouriteCommand() {
+        assertParseSuccess(parser, "1", new UnfavouriteCommand(INDEX_FIRST_PERSON));
     }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
         assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                FavouriteCommand.MESSAGE_USAGE));
+                UnfavouriteCommand.MESSAGE_USAGE));
     }
 }
