620cdf7be523108ef647b7f6c491a9225e34d48f
michael-lee-sk <e0543977@u.nus.edu>
2021-10-25 20:39:32 +0800

    Changes to some messages

--- src/main/java/seedu/tuitione/commons/core/Messages.java
+++ src/main/java/seedu/tuitione/commons/core/Messages.java
@@ -7,10 +7,8 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_STUDENT_DISPLAYED_INDEX = "The student index provided is invalid";
-    public static final String MESSAGE_INVALID_LESSON_DISPLAYED_INDEX = "The lesson index provided is invalid";
-    public static final String MESSAGE_STUDENTS_LISTED_OVERVIEW = "%1$d student(s) listed!";
-    public static final String MESSAGE_INVALID_LESSON_CODE = "The lesson indicated by the lesson code does not exist!";
+    public static final String MESSAGE_INVALID_STUDENT_DISPLAYED_INDEX = "The student index cannot be found";
+    public static final String MESSAGE_INVALID_LESSON_DISPLAYED_INDEX = "The lesson index cannot be found";
     public static final String MESSAGE_PLURAL_STUDENT_LISTED_OVERVIEW = "%1$d students found!";
     public static final String MESSAGE_SINGULAR_STUDENT_LISTED_OVERVIEW = "%1$d student found!";
     public static final String MESSAGE_LESSON_FOUND_OVERVIEW = "%1$d lessons(s) found!";
