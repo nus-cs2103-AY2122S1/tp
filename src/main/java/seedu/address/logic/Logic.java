091ae38d33fa5484367af43e2f770b4c654c997a
xianlin <xianlinchng@gmail.com>
2021-10-26 13:20:55 +0800

    Add getClaimList as a method in Logic interface

--- src/main/java/seedu/address/logic/Logic.java
+++ src/main/java/seedu/address/logic/Logic.java
@@ -3,11 +3,14 @@ package seedu.address.logic;
 import java.nio.file.Path;
 
 import javafx.collections.ObservableList;
+import javafx.util.Pair;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.claim.Claim;
+import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 
 /**
@@ -32,6 +35,9 @@ public interface Logic {
 
     /** Returns an unmodifiable view of the filtered list of persons */
     ObservableList<Person> getFilteredPersonList();
+
+    /** Returns an unmodifiable view of the claims in the filtered list of persons */
+    ObservableList<Pair<Claim, Name>> getClaimList();
 
     /**
      * Returns the user prefs' address book file path.
