8f7df7bdc96ae3b0dbde7b12dde88e28516c74b3
trash-bin99 <77224811+trash-bin99@users.noreply.github.com>
2021-10-26 15:45:46 +0800

    Add constructor testcase for TagCommand

--- src/test/java/seedu/fast/logic/commands/TagCommandTest.java
+++ src/test/java/seedu/fast/logic/commands/TagCommandTest.java
@@ -2,29 +2,59 @@ package seedu.fast.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.fast.testutil.Assert.assertThrows;
 import static seedu.fast.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.fast.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 
 import java.util.HashSet;
 import java.util.Set;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import seedu.fast.model.tag.Tag;
 
 public class TagCommandTest {
 
+    private int zeroIndex = 0;
+    private Set<Tag> emptySet = new HashSet<>();
+    private Set<Tag> setWithPriorityTag = new HashSet<>();
+    private Set<Tag> setWithInvestmentPlanTag = new HashSet<>();
+    private Set<Tag> setWithNormalTag = new HashSet<>();
+    private Set<Tag> setWithMixedTag = new HashSet<>();
+
+    @BeforeEach
+    public void setUp() {
+        setWithNormalTag.add(Tag.createTag("test"));
+        setWithPriorityTag.add(Tag.createTag("pr/low"));
+        setWithInvestmentPlanTag.add(Tag.createTag("ip/travel"));
+        setWithMixedTag.add(Tag.createTag("testtest"));
+        setWithMixedTag.add(Tag.createTag("pr/med"));
+        setWithMixedTag.add(Tag.createTag("ip/property"));
+    }
+
+    @Test
+    public void constructor_nullArgs_throwsNullPointerException() {
+        //null index
+        assertThrows(NullPointerException.class, () -> new TagCommand(null,
+                setWithMixedTag, setWithMixedTag));
+
+        //null addSet
+        assertThrows(NullPointerException.class, () -> new TagCommand(INDEX_FIRST_PERSON,
+                null, setWithMixedTag));
+
+        //null deleteSet
+        assertThrows(NullPointerException.class, () -> new TagCommand(INDEX_FIRST_PERSON,
+                setWithMixedTag, null));
+
+    }
+
     @Test
     public void equals() {
-        Set<Tag> emptySet = new HashSet<>();
-        Set<Tag> setWithOneTag = new HashSet<>();
-        Tag normalTag = Tag.createTag("test");
-        setWithOneTag.add(normalTag);
-
-        final TagCommand standardCommand = new TagCommand(INDEX_FIRST_PERSON, emptySet, setWithOneTag);
+        final TagCommand standardCommand = new TagCommand(INDEX_FIRST_PERSON, emptySet, setWithMixedTag);
 
         // same values -> returns true
-        TagCommand commandWithSameValues = new TagCommand(INDEX_FIRST_PERSON, emptySet, setWithOneTag);
+        TagCommand commandWithSameValues = new TagCommand(INDEX_FIRST_PERSON, emptySet, setWithMixedTag);
         assertTrue(standardCommand.equals(commandWithSameValues));
 
         // same object -> returns true
@@ -37,10 +67,13 @@ public class TagCommandTest {
         assertFalse(standardCommand.equals(new ClearCommand()));
 
         // different index -> returns false
-        assertFalse(standardCommand.equals(new TagCommand(INDEX_SECOND_PERSON, emptySet, setWithOneTag)));
+        assertFalse(standardCommand.equals(new TagCommand(INDEX_SECOND_PERSON, emptySet, setWithMixedTag)));
 
         // different sets -> returns false
-        assertFalse(standardCommand.equals(new TagCommand(INDEX_FIRST_PERSON, setWithOneTag, emptySet)));
+        assertFalse(standardCommand.equals(new TagCommand(INDEX_FIRST_PERSON, setWithMixedTag, setWithMixedTag)));
+        assertFalse(standardCommand.equals(new TagCommand(INDEX_FIRST_PERSON, emptySet, emptySet)));
+        assertFalse(standardCommand.equals(new TagCommand(INDEX_FIRST_PERSON, setWithMixedTag, emptySet)));
+
     }
 
     //TODO: Add more testcases
