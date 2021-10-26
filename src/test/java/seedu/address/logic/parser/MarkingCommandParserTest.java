44c39af81612a56596e7e49db1c07d69d9e582e6
TohWeiJie01 <tohweijie01@gmail.com>
2021-10-26 19:36:27 +0800

    Add tests for MarkingCommandParser

---
+++ src/test/java/seedu/address/logic/parser/MarkingCommandParserTest.java
@@ -0,0 +1,75 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_PERSON;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.MarkCommand;
+import seedu.address.logic.commands.UnmarkCommand;
+
+public class MarkingCommandParserTest {
+
+    private final String MARK_COMMAND_WORD = "mark";
+    private final String UNMARK_COMMAND_WORD = "unmark";
+
+    private MarkingCommandParser parserForMark = new MarkingCommandParser(MARK_COMMAND_WORD);
+    private MarkingCommandParser parserForUnmark = new MarkingCommandParser(UNMARK_COMMAND_WORD);
+
+    @Test
+    public void parse_validArgs_returnsMarkingCommand() {
+        // One index
+        assertParseSuccess(parserForMark, "1", new MarkCommand(new Index[]{INDEX_FIRST_PERSON}));
+        assertParseSuccess(parserForUnmark, "1", new UnmarkCommand(new Index[]{INDEX_FIRST_PERSON}));
+
+        // Multiple index
+        assertParseSuccess(parserForMark, "1 2",
+                new MarkCommand(new Index[]{INDEX_FIRST_PERSON, INDEX_SECOND_PERSON}));
+        assertParseSuccess(parserForUnmark, "1 2",
+                new UnmarkCommand(new Index[]{INDEX_FIRST_PERSON, INDEX_SECOND_PERSON}));
+
+        // Multiple index with varying white space
+        assertParseSuccess(parserForMark, "3            1     2",
+                new MarkCommand(new Index[]{INDEX_THIRD_PERSON, INDEX_FIRST_PERSON, INDEX_SECOND_PERSON}));
+        assertParseSuccess(parserForUnmark, "3            1     2",
+                new UnmarkCommand(new Index[]{INDEX_THIRD_PERSON, INDEX_FIRST_PERSON, INDEX_SECOND_PERSON}));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        // Not a number
+        assertParseFailure(parserForMark, "a",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, MarkCommand.MESSAGE_USAGE));
+        assertParseFailure(parserForUnmark, "a",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnmarkCommand.MESSAGE_USAGE));
+
+        // Zero index
+        assertParseFailure(parserForMark, "0",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, MarkCommand.MESSAGE_USAGE));
+        assertParseFailure(parserForUnmark, "0",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnmarkCommand.MESSAGE_USAGE));
+
+        // Negative index
+        assertParseFailure(parserForMark, "-1",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, MarkCommand.MESSAGE_USAGE));
+        assertParseFailure(parserForUnmark, "-1",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnmarkCommand.MESSAGE_USAGE));
+
+        // Multiple inputs, one invalid
+        assertParseFailure(parserForMark, "0 1",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, MarkCommand.MESSAGE_USAGE));
+        assertParseFailure(parserForUnmark, "0 1",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnmarkCommand.MESSAGE_USAGE));
+
+        // Multiple inputs, all invalid
+        assertParseFailure(parserForMark, "0 -1",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, MarkCommand.MESSAGE_USAGE));
+        assertParseFailure(parserForUnmark, "0 -1",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnmarkCommand.MESSAGE_USAGE));
+    }
+}
