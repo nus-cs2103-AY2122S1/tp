caadff3c9e2992558b6f5aae73a79c9d134c0e68
TohWeiJie01 <tohweijie01@gmail.com>
2021-10-26 23:17:51 +0800

    Add test cases for UnmarkCommand

---
+++ src/test/java/seedu/address/logic/commands/UnmarkCommandTest.java
@@ -0,0 +1,106 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+
+public class UnmarkCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute_validIndexUnfilteredList_success() {
+        Person personToUnmark = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        UnmarkCommand unmarkCommand = new UnmarkCommand(new Index[]{INDEX_FIRST_PERSON});
+
+        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.unmarkPerson(personToUnmark);
+
+        String expectedMessage = String.format(UnmarkCommand.MESSAGE_UNMARKED_PERSON_SUCCESS, personToUnmark);
+
+        assertCommandSuccess(unmarkCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidIndexUnfilteredList_throwsCommandException() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        UnmarkCommand unmarkCommand = new UnmarkCommand(new Index[]{outOfBoundIndex});
+
+        assertCommandFailure(unmarkCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_validIndexFilteredList_success() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+
+        Person personToUnmark = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        UnmarkCommand unmarkCommand = new UnmarkCommand(new Index[]{INDEX_FIRST_PERSON});
+
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.unmarkPerson(personToUnmark);
+        showPersonAtIndex(expectedModel, INDEX_FIRST_PERSON);
+
+        String expectedMessage = String.format(UnmarkCommand.MESSAGE_UNMARKED_PERSON_SUCCESS, personToUnmark);
+
+        assertCommandSuccess(unmarkCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidIndexFilteredList_throwsCommandException() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+
+        Index outOfBoundIndex = INDEX_SECOND_PERSON;
+        // ensures that outOfBoundIndex is still in bounds of address book list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
+
+        UnmarkCommand unmarkCommand = new UnmarkCommand(new Index[]{outOfBoundIndex});
+
+        assertCommandFailure(unmarkCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void equals() {
+        UnmarkCommand unmarkFirstCommand = new UnmarkCommand(new Index[]{INDEX_FIRST_PERSON});
+        UnmarkCommand unmarkSecondCommand = new UnmarkCommand(new Index[]{INDEX_SECOND_PERSON});
+
+        // same object -> returns true
+        assertTrue(unmarkFirstCommand.equals(unmarkFirstCommand));
+
+        // same values -> returns true
+        UnmarkCommand unmarkFirstCommandCopy = new UnmarkCommand(new Index[]{INDEX_FIRST_PERSON});
+        assertTrue(unmarkFirstCommand.equals(unmarkFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(unmarkFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(unmarkFirstCommand.equals(null));
+
+        // different person -> returns false
+        assertFalse(unmarkFirstCommand.equals(unmarkSecondCommand));
+    }
+
+    /**
+     * Updates {@code model}'s filtered list to show no one.
+     */
+    private void showNoPerson(Model model) {
+        model.updateFilteredPersonList(p -> false);
+
+        assertTrue(model.getFilteredPersonList().isEmpty());
+    }
+
+}
