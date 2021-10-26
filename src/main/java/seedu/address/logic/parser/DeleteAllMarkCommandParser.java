59e6af691a4bff1b3f1faaf93988d1dd1f697a65
juliussneezer04 <vishnu.sundaresan@gmail.com>
2021-10-26 20:23:19 +0800

    Adding DeleteAllMarks Functionality.

--- src/main/java/seedu/address/logic/parser/DeleteAllMarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteAllMarkCommandParser.java
@@ -4,20 +4,20 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.DeleteLastMarkCommand;
+import seedu.address.logic.commands.DeleteAllMarkCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
  * Parses input arguments and creates a new DeleteLastMarkCommand object
  */
-public class DeleteLastMarkCommandParser implements Parser<DeleteLastMarkCommand> {
+public class DeleteAllMarkCommandParser implements Parser<DeleteAllMarkCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the DeleteLastMarkCommand
-     * and returns an DeleteLastMarkCommand object for execution.
+     * and returns an DeleteAllMarkCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-    public DeleteLastMarkCommand parse(String args) throws ParseException {
+    public DeleteAllMarkCommand parse(String args) throws ParseException {
         requireNonNull(args);
         Index index;
         ArgumentMultimap argMultimap =
@@ -27,10 +27,10 @@ public class DeleteLastMarkCommandParser implements Parser<DeleteLastMarkCommand
             index = ParserUtil.parseIndex(argMultimap.getPreamble());
         } catch (ParseException pe) {
             throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteLastMarkCommand.MESSAGE_USAGE), pe);
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteAllMarkCommand.MESSAGE_USAGE), pe);
         }
 
-        return new DeleteLastMarkCommand(index);
+        return new DeleteAllMarkCommand(index);
     }
 
 }
