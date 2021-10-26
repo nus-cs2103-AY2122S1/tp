2016457b92d7169b2afc0cf2fbc41675d243328c
MGRL2201 <liminggaorickie@outlook.com>
2021-10-27 01:30:24 +0800

    Add fourth and fifth index for both student and lesson

--- src/test/java/seedu/tuitione/testutil/TypicalIndexes.java
+++ src/test/java/seedu/tuitione/testutil/TypicalIndexes.java
@@ -10,8 +10,10 @@ public class TypicalIndexes {
     public static final Index INDEX_SECOND_STUDENT = Index.fromOneBased(2);
     public static final Index INDEX_THIRD_STUDENT = Index.fromOneBased(3);
     public static final Index INDEX_FOURTH_STUDENT = Index.fromOneBased(4);
+    public static final Index INDEX_FIFTH_STUDENT = Index.fromOneBased(5);
     public static final Index INDEX_FIRST_LESSON = Index.fromOneBased(1);
     public static final Index INDEX_SECOND_LESSON = Index.fromOneBased(2);
     public static final Index INDEX_THIRD_LESSON = Index.fromOneBased(3);
     public static final Index INDEX_FOURTH_LESSON = Index.fromOneBased(4);
+    public static final Index INDEX_FIFTH_LESSON = Index.fromOneBased(5);
 }
