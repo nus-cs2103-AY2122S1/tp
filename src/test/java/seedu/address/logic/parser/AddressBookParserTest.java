7b816cf11ce88110ab527fbb586dce95cace8eb4
SkyBlaise <zsc990124@gmail.com>
2021-10-26 23:27:35 +0800

    Update test case for edit parts of parseCommand

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -6,25 +6,39 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_CLIENT;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PRODUCT;
 
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
 import org.junit.jupiter.api.Test;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import seedu.address.logic.commands.AddClientCommand;
 import seedu.address.logic.commands.AddProductCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.DeleteClientCommand;
 import seedu.address.logic.commands.DeleteProductCommand;
+import seedu.address.logic.commands.EditClientCommand;
+import seedu.address.logic.commands.EditClientCommand.EditClientDescriptor;
+import seedu.address.logic.commands.EditProductCommand;
+import seedu.address.logic.commands.EditProductCommand.EditProductDescriptor;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListClientCommand;
 import seedu.address.logic.commands.ListProductCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.client.Client;
 import seedu.address.model.product.Product;
 import seedu.address.testutil.ClientBuilder;
 import seedu.address.testutil.ClientUtil;
+import seedu.address.testutil.EditClientDescriptorBuilder;
+import seedu.address.testutil.EditProductDescriptorBuilder;
 import seedu.address.testutil.ProductBuilder;
 import seedu.address.testutil.ProductUtil;
+import seedu.address.testutil.TypicalClients;
+import seedu.address.testutil.TypicalProducts;
 
 public class AddressBookParserTest {
     private final AddressBookParser parser = new AddressBookParser();
@@ -64,34 +78,41 @@ public class AddressBookParserTest {
                 actualOutput.substring(actualOutput.indexOf("Name")));
     }
 
-//    @Test
-//    public void parseCommand_editClient() throws Exception {
-//        Client client = new ClientBuilder().build();
-//        EditClientCommand.EditClientDescriptor descriptor = new EditClientDescriptorBuilder(client).build();
-//        EditClientCommand expectedCommand =
-//                (EditClientCommand) parser.parseCommand(EditClientCommand.COMMAND_WORD + " " +
-//                        INDEX_FIRST_CLIENT.getOneBased() + " " +
-//                                ClientUtil.getEditClientDescriptorDetails(descriptor), model);
-//        EditClientCommand actualCommand = new EditClientCommand(INDEX_FIRST_CLIENT, descriptor);
-//        String expectedOutput = expectedCommand.execute(model).getFeedbackToUser();
-//        String actualOutput = actualCommand.execute(model).getFeedbackToUser();
-//        assertEquals(expectedOutput, actualOutput);
-//    }
+    @Test
+    public void parseCommand_editClient() throws Exception {
+        Client client = TypicalClients.BOB;
+        EditClientDescriptor descriptor = new EditClientDescriptorBuilder(client).build();
 
-//    @Test
-//    public void parseCommand_editProduct() throws Exception {
-//        Product product = new ProductBuilder().build();
-//        EditProductCommand.EditProductDescriptor descriptor =
-//                new EditProductCommand.EditProductDescriptor(product).build();
-//        EditProductCommand expectedCommand =
-//                (EditProductCommand) parser.parseCommand(EditProductCommand.COMMAND_WORD + " " +
-//                                INDEX_FIRST_PRODUCT.getOneBased() + " " +
-//                                ProductUtil.getEditProductDescriptorDetails(descriptor), model);
-//        EditProductCommand actualCommand = new EditProductCommand(INDEX_FIRST_PRODUCT, descriptor);
-//        String expectedOutput = expectedCommand.execute(model).getFeedbackToUser();
-//        String actualOutput = actualCommand.execute(model).getFeedbackToUser();
-//        assertEquals(expectedOutput, actualOutput);
-//    }
+        Model expectedModel = new ModelStub();
+        Model actualModel = new ModelStub();
+
+        EditClientCommand expectedCommand = (EditClientCommand) parser.parseCommand(
+                EditClientCommand.COMMAND_WORD + " " + INDEX_FIRST_CLIENT.getOneBased() + " "
+                        + ClientUtil.getEditClientDescriptorDetails(descriptor), expectedModel);
+        EditClientCommand actualCommand = new EditClientCommand(INDEX_FIRST_CLIENT, descriptor);
+
+        String expectedOutput = expectedCommand.execute(expectedModel).getFeedbackToUser();
+        String actualOutput = actualCommand.execute(actualModel).getFeedbackToUser();
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void parseCommand_editProduct() throws Exception {
+        Product product = TypicalProducts.DAISY;
+        EditProductDescriptor descriptor = new EditProductDescriptorBuilder(product).build();
+
+        Model expectedModel = new ModelStub();
+        Model actualModel = new ModelStub();
+
+        EditProductCommand expectedCommand = (EditProductCommand) parser.parseCommand(
+                EditProductCommand.COMMAND_WORD + " " + INDEX_FIRST_PRODUCT.getOneBased() + " "
+                        + ProductUtil.getEditProductDescriptorDetails(descriptor), expectedModel);
+        EditProductCommand actualCommand = new EditProductCommand(INDEX_FIRST_PRODUCT, descriptor);
+
+        String expectedOutput = expectedCommand.execute(expectedModel).getFeedbackToUser();
+        String actualOutput = actualCommand.execute(actualModel).getFeedbackToUser();
+        assertEquals(expectedOutput, actualOutput);
+    }
 
     @Test
     public void parseCommand_deleteClient() throws Exception {
@@ -119,23 +140,23 @@ public class AddressBookParserTest {
         assertTrue(parser.parseCommand(ListProductCommand.COMMAND_WORD + " 3", model) instanceof ListProductCommand);
     }
 
-//    @Test
-//    public void parseCommand_findClient() throws Exception {
-//        List<String> keywords = Arrays.asList("foo", "bar", "baz");
-//        FindClientCommand command = (FindClientCommand) parser.parseCommand(
-//                FindClientCommand.COMMAND_WORD + " " +
-//                        keywords.stream().collect(Collectors.joining(" ")), model);
-//        assertEquals(new FindClientCommand(new NameContainsKeywordsPredicate(keywords)), command);
-//    }
+    //    @Test
+    //    public void parseCommand_findClient() throws Exception {
+    //        List<String> keywords = Arrays.asList("foo", "bar", "baz");
+    //        FindClientCommand command = (FindClientCommand) parser.parseCommand(
+    //                FindClientCommand.COMMAND_WORD + " " +
+    //                        keywords.stream().collect(Collectors.joining(" ")), model);
+    //        assertEquals(new FindClientCommand(new NameContainsKeywordsPredicate(keywords)), command);
+    //    }
 
-//    @Test
-//    public void parseCommand_findProduct() throws Exception {
-//        List<String> keywords = Arrays.asList("foo", "bar", "baz");
-//        FindProductCommand command = (FindProductCommand) parser.parseCommand(
-//                FindProductCommand.COMMAND_WORD + " " +
-//                        keywords.stream().collect(Collectors.joining(" ")), model);
-//        assertEquals(new FindProductCommand(new NameContainsKeywordsPredicate(keywords)), command);
-//    }
+    //    @Test
+    //    public void parseCommand_findProduct() throws Exception {
+    //        List<String> keywords = Arrays.asList("foo", "bar", "baz");
+    //        FindProductCommand command = (FindProductCommand) parser.parseCommand(
+    //                FindProductCommand.COMMAND_WORD + " " +
+    //                        keywords.stream().collect(Collectors.joining(" ")), model);
+    //        assertEquals(new FindProductCommand(new NameContainsKeywordsPredicate(keywords)), command);
+    //    }
 
     @Test
     public void parseCommand_help() throws Exception {
@@ -166,15 +187,56 @@ public class AddressBookParserTest {
     }
 
     public static class ModelStub extends ModelManager {
-        /**
-         * Assume there are no duplicate clients, return false.
-         *
-         * @param client the client to be checked.
-         * @return False.
-         */
+        private final ArrayList<Client> filteredClients = new ArrayList<>();
+        private final ArrayList<Product> filteredProducts = new ArrayList<>();
+
+        public ModelStub() {
+            filteredClients.add(TypicalClients.ALICE);
+            filteredProducts.add(TypicalProducts.CANNON);
+        }
+
+        // Client ---------------------------------------------------------------------------
+
         @Override
         public boolean hasClient(Client client) {
             return false;
         }
+
+        @Override
+        public ObservableList<Client> getFilteredClientList() {
+            return FXCollections.observableList(filteredClients);
+        }
+
+        @Override
+        public void setClient(Client target, Client editedClient) {
+            if (filteredClients.get(0).equals(target)) {
+                filteredClients.set(0, editedClient);
+            }
+        }
+
+        @Override
+        public void updateFilteredClientList(Predicate<Client> predicate) {}
+
+        // Product ---------------------------------------------------------------------------
+
+        @Override
+        public boolean hasProduct(Product product) {
+            return false;
+        }
+
+        @Override
+        public ObservableList<Product> getFilteredProductList() {
+            return FXCollections.observableList(filteredProducts);
+        }
+
+        @Override
+        public void setProduct(Product target, Product editedProduct) {
+            if (filteredProducts.get(0).equals(target)) {
+                filteredProducts.set(0, editedProduct);
+            }
+        }
+
+        @Override
+        public void updateFilteredProductList(Predicate<Product> predicate) {}
     }
 }
