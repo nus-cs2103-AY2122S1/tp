1db4a7327f288ce50fe2d1bd98d0958f9066ed1a
nbvannhi <nbvannhi@gmail.com>
2021-10-27 03:02:15 +0800

    Fix checkstyle (after adapting changes from v1.3-export-graph)

--- src/main/java/seedu/address/logic/parser/ShowCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ShowCommandParser.java
@@ -2,8 +2,8 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ASSESSMENT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_GROUP;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_FILE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_GROUP;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ID;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 
