3370ef01a679bd8080c24404d9260ebf989b61ad
HalphasX <70026153+HalphasX@users.noreply.github.com>
2021-10-27 14:43:22 +0800

    Checkstyle fixes

--- src/main/java/seedu/notor/ui/NotePane.java
+++ src/main/java/seedu/notor/ui/NotePane.java
@@ -2,9 +2,9 @@ package seedu.notor.ui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 import javafx.scene.layout.Region;
-import seedu.notor.model.common.Note;
+import javafx.scene.layout.VBox;
+//import seedu.notor.model.common.Note;
 
 public class NotePane extends UiPart<Region> {
 
