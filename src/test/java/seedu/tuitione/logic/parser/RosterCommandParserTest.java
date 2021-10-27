07cec7a18a598f1e7b98437aba328a8b4da21d78
MGRL2201 <liminggaorickie@outlook.com>
2021-10-28 03:44:42 +0800

    Create RosterCommandParserTest and add emptyArg test

--- src/test/java/seedu/tuitione/logic/parser/RosterCommandParserTest.java
+++ src/test/java/seedu/tuitione/logic/parser/RosterCommandParserTest.java
@@ -1,2 +1,22 @@
-package seedu.tuitione.logic.parser;public class RosterCommandParserTest {
+package seedu.tuitione.logic.parser;
+
+import static seedu.tuitione.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.tuitione.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.tuitione.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.tuitione.testutil.TypicalIndexes.INDEX_SECOND_LESSON;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.tuitione.logic.commands.FindCommand;
+import seedu.tuitione.logic.commands.RosterCommand;
+
+public class RosterCommandParserTest {
+
+    private RosterCommandParser parser = new RosterCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+    }
+
 }
