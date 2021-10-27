8045fe0e312245239e6102432f13b3cc4402cda9
lowkangn <e0543462@u.nus.edu>
2021-10-27 17:57:43 +0800

    Switch positions of indexes for deleteNok

--- src/main/java/seedu/address/logic/parser/DeleteNextOfKinParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteNextOfKinParser.java
@@ -4,23 +4,23 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_ZERO_BASED_INDEX;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.RemoveNextOfKinCommand;
+import seedu.address.logic.commands.DeleteNextOfKinCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
-public class RemoveNextOfKinParser implements Parser<RemoveNextOfKinCommand> {
+public class DeleteNextOfKinParser implements Parser<DeleteNextOfKinCommand> {
     @Override
-    public RemoveNextOfKinCommand parse(String args) throws ParseException {
+    public DeleteNextOfKinCommand parse(String args) throws ParseException {
         String[] sections = args.split(" ");
 
         if (sections.length != 3) {
             throw new ParseException(String.format(
-                    MESSAGE_INVALID_COMMAND_FORMAT, RemoveNextOfKinCommand.MESSAGE_USAGE));
+                    MESSAGE_INVALID_COMMAND_FORMAT, DeleteNextOfKinCommand.MESSAGE_USAGE));
         }
 
         try {
-            Index participantIndex = ParserUtil.parseIndex(sections[1]);
-            Index nokIndex = ParserUtil.parseIndex(sections[2]);
-            return new RemoveNextOfKinCommand(participantIndex, nokIndex);
+            Index nokIndex = ParserUtil.parseIndex(sections[1]);
+            Index participantIndex = ParserUtil.parseIndex(sections[2]);
+            return new DeleteNextOfKinCommand(nokIndex, participantIndex);
         } catch (ParseException pe) {
             throw new ParseException(MESSAGE_INVALID_ZERO_BASED_INDEX);
         }
