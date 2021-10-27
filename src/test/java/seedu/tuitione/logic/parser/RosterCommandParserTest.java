acb55af7981ddbabf19efb2d64dff39d32582dce
MGRL2201 <liminggaorickie@outlook.com>
2021-10-28 03:45:27 +0800

    Add invalidArgs test to RosterCommandParserTest

--- src/test/java/seedu/tuitione/logic/parser/RosterCommandParserTest.java
+++ src/test/java/seedu/tuitione/logic/parser/RosterCommandParserTest.java
@@ -29,4 +29,12 @@ public class RosterCommandParserTest {
         assertParseSuccess(parser, "\n   2    \t", expectedRosterCommand);
     }
 
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                RosterCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "0", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                RosterCommand.MESSAGE_USAGE));
+    }
+
 }
