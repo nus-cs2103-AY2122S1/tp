3267fbab63ed291ef44f10d084aa59c48964d88c
VimuthM <vimuthmendis@gmail.com>
2021-10-27 21:42:11 +0800

    Fix checkstyle

--- src/main/java/safeforhall/ui/MainWindow.java
+++ src/main/java/safeforhall/ui/MainWindow.java
@@ -2,7 +2,7 @@ package safeforhall.ui;
 
 import java.util.logging.Logger;
 
-import javafx.application.*;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -11,8 +11,14 @@ import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextInputControl;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.*;
-import javafx.scene.layout.*;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import safeforhall.commons.core.GuiSettings;
 import safeforhall.commons.core.LogsCenter;
@@ -130,11 +136,11 @@ public class MainWindow extends UiPart<Stage> {
             }
             e.consume();
         });
-        
+
         exitTab.setGraphic(createTab("    Exit     ", "exit"));
         exitTab.setOnSelectionChanged(e -> {
             if (exitTab.isSelected()) {
-                handleExit(); 
+                handleExit();
             }
         });
 
