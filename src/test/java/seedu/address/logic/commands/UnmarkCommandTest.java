cfb3413f301eb7c42bb3ef4d86a7edbb4eabcf27
TohWeiJie01 <tohweijie01@gmail.com>
2021-10-27 09:58:24 +0800

    Remove unused method

--- src/test/java/seedu/address/logic/commands/UnmarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UnmarkCommandTest.java
@@ -63,13 +63,4 @@ public class UnmarkCommandTest {
         assertFalse(unmarkFirstCommand.equals(unmarkSecondCommand));
     }
 
-    /**
-     * Updates {@code model}'s filtered list to show no one.
-     */
-    private void showNoPerson(Model model) {
-        model.updateFilteredPersonList(p -> false);
-
-        assertTrue(model.getFilteredPersonList().isEmpty());
-    }
-
 }
