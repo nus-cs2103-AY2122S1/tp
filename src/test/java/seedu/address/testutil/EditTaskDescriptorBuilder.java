539943f708b125bd439a100017d2c1f3a7e1bb5d
WangGLJoseph <joseph.cnsg@gmail.com>
2021-10-28 13:57:18 +0800

    Add skeleton test files

--- src/test/java/seedu/address/testutil/EditTaskDescriptorBuilder.java
+++ src/test/java/seedu/address/testutil/EditTaskDescriptorBuilder.java
@@ -1,2 +1,13 @@
-package seedu.address.testutil;public class EditTaskDescriptorBuilder {
+package seedu.address.testutil;
+
+import seedu.address.logic.commands.EditTaskCommand.EditTaskDescriptor;
+import seedu.address.model.task.Task;
+import seedu.address.model.task.TaskDeadline;
+import seedu.address.model.task.TaskId;
+import seedu.address.model.task.TaskName;
+
+/**
+ * A utility class to help with building EditTaskDescriptor objects.
+ */
+public class EditTaskDescriptorBuilder {
 }
