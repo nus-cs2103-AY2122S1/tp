8bedb168b8dec6a878736d89a09416d5debec2b9
eezj35 <justineezj0@gmail.com>
2021-10-28 17:04:43 +0800

    Fix edit command test cases

--- src/test/java/seedu/tuitione/logic/commands/EditCommandTest.java
+++ src/test/java/seedu/tuitione/logic/commands/EditCommandTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.tuitione.logic.commands.CommandTestUtil.DESC_AMY;
 import static seedu.tuitione.logic.commands.CommandTestUtil.DESC_BOB;
 import static seedu.tuitione.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.tuitione.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
+import static seedu.tuitione.logic.commands.CommandTestUtil.VALID_REMARK_FINANCIAL_AID;
 import static seedu.tuitione.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.tuitione.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.tuitione.logic.commands.CommandTestUtil.showStudentAtIndex;
@@ -39,41 +41,47 @@ public class EditCommandTest {
 
     // To fix first 2 tests in v1.4
 
-    //    @Test
-    //    public void execute_allFieldsSpecifiedUnfilteredListStudentNoLessons_success() {
-    //
-    //        Student editedStudent = new StudentBuilder().build();
-    //        EditStudentDescriptor descriptor = new EditStudentDescriptorBuilder(editedStudent).build();
-    //        EditCommand editCommand = new EditCommand(INDEX_FIRST_STUDENT, descriptor);
-    //
-    //        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_STUDENT_SUCCESS, editedStudent);
-    //
-    //        Model expectedModel = new ModelManager(new Tuitione(model.getTuitione()), new UserPrefs());
-    //        expectedModel.setStudent(model.getFilteredStudentList().get(0), editedStudent);
-    //
-    //        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
-    //    }
-    //
-    //    @Test
-    //    public void execute_someFieldsSpecifiedUnfilteredList_success() {
-    //        // Student with no lessons
-    //        Student firstStudent = model.getFilteredStudentList().get(INDEX_FIRST_STUDENT.getZeroBased());
-    //
-    //        StudentBuilder studentInList = new StudentBuilder(firstStudent);
-    //        Student editedStudent = studentInList.withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
-    //                .withRemarks(VALID_REMARK_FINANCIAL_AID).build();
-    //
-    //        EditStudentDescriptor descriptor = new EditStudentDescriptorBuilder().withName(VALID_NAME_BOB)
-    //                .withPhone(VALID_PHONE_BOB).withRemarks(VALID_REMARK_FINANCIAL_AID).build();
-    //        EditCommand editCommand = new EditCommand(INDEX_FIRST_STUDENT, descriptor);
-    //
-    //        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_STUDENT_SUCCESS, editedStudent);
-    //
-    //        Model expectedModel = new ModelManager(new Tuitione(model.getTuitione()), new UserPrefs());
-    //        expectedModel.setStudent(firstStudent, editedStudent);
-    //
-    //        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
-    //    }
+    @Test
+    public void execute_allFieldsSpecifiedUnfilteredListStudentNoLessons_success() {
+
+        Student editedStudent = new StudentBuilder().build();
+        EditStudentDescriptor descriptor = new EditStudentDescriptorBuilder(editedStudent).build();
+        EditCommand editCommand = new EditCommand(INDEX_FIRST_STUDENT, descriptor);
+
+        // friends is the original remark of the original student
+        Student expectedStudent = new StudentBuilder(editedStudent).withRemarks("friends").build();
+        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_STUDENT_SUCCESS, expectedStudent);
+
+        Model expectedModel = new ModelManager(new Tuitione(model.getTuitione()), new UserPrefs());
+        expectedModel.setStudent(model.getFilteredStudentList().get(0), expectedStudent);
+
+        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_someFieldsSpecifiedUnfilteredList_success() {
+        // Student with no lessons
+        Student firstStudent = model.getFilteredStudentList().get(INDEX_FIRST_STUDENT.getZeroBased());
+
+        StudentBuilder studentInList = new StudentBuilder(firstStudent);
+        Student editedStudent = studentInList.withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
+                .withRemarks(VALID_REMARK_FINANCIAL_AID).build();
+
+        EditStudentDescriptor descriptor = new EditStudentDescriptorBuilder().withName(VALID_NAME_BOB)
+                .withPhone(VALID_PHONE_BOB).withRemarks(VALID_REMARK_FINANCIAL_AID).build();
+        EditCommand editCommand = new EditCommand(INDEX_FIRST_STUDENT, descriptor);
+
+        // friends is the original remark of the original student
+        Student expectedStudent = new StudentBuilder(editedStudent)
+                .withRemarks("friends", VALID_REMARK_FINANCIAL_AID).withName(VALID_NAME_BOB)
+                .withPhone(VALID_PHONE_BOB).build();
+        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_STUDENT_SUCCESS, expectedStudent);
+
+        Model expectedModel = new ModelManager(new Tuitione(model.getTuitione()), new UserPrefs());
+        expectedModel.setStudent(firstStudent, expectedStudent);
+
+        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
+    }
 
     @Test
     public void execute_noFieldSpecifiedUnfilteredList_success() {
