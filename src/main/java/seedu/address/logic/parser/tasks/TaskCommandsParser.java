cd9ee35983ee50e1c8a28dcd116bf828abd0a056
jeremy-7007 <71486951+jeremy-7007@users.noreply.github.com>
2021-10-28 22:38:15 +0800

    Add mark task as done command

--- src/main/java/seedu/address/logic/parser/tasks/TaskCommandsParser.java
+++ src/main/java/seedu/address/logic/parser/tasks/TaskCommandsParser.java
@@ -14,6 +14,7 @@ import seedu.address.logic.commands.tasks.AssignTaskToPersonCommand;
 import seedu.address.logic.commands.tasks.EditTaskCommand;
 import seedu.address.logic.commands.tasks.FindTaskCommand;
 import seedu.address.logic.commands.tasks.ListTaskCommand;
+import seedu.address.logic.commands.tasks.MarkTaskDoneCommand;
 import seedu.address.logic.commands.tasks.UnassignTaskFromGroupCommand;
 import seedu.address.logic.commands.tasks.UnassignTaskFromPersonCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -62,6 +63,9 @@ public class TaskCommandsParser {
 
         case UnassignTaskFromGroupCommand.COMMAND_WORD:
             return new UnassignTaskFromGroupCommandParser().parse(arguments);
+
+        case MarkTaskDoneCommand.COMMAND_WORD:
+            return new MarkTaskDoneCommandParser().parse(arguments);
 
         case FindTaskCommand.COMMAND_WORD:
             return new FindTaskCommandParser().parse(arguments);
