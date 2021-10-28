b297ec77e420b5b98fc473c6b7558dcc90a109e7
Cai Kaian <caipngg@gmail.com>
2021-10-29 00:25:55 +0800

    Add previous command feature

--- src/main/java/seedu/edrecord/ui/CommandBox.java
+++ src/main/java/seedu/edrecord/ui/CommandBox.java
@@ -1,8 +1,12 @@
 package seedu.edrecord.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Region;
 import seedu.edrecord.logic.commands.CommandResult;
 import seedu.edrecord.logic.commands.exceptions.CommandException;
@@ -17,6 +21,8 @@ public class CommandBox extends UiPart<Region> {
     private static final String FXML = "CommandBox.fxml";
 
     private final CommandExecutor commandExecutor;
+    private final List<String> commandHistory = new ArrayList<>();
+    private int commandHistoryPtr = 0;
 
     @FXML
     private TextField commandTextField;
@@ -29,6 +35,18 @@ public class CommandBox extends UiPart<Region> {
         this.commandExecutor = commandExecutor;
         // calls #setStyleToDefault() whenever there is a change to the text of the command box.
         commandTextField.textProperty().addListener((unused1, unused2, unused3) -> setStyleToDefault());
+
+        commandTextField.setOnKeyPressed(e -> {
+            KeyCode c = e.getCode();
+            if (commandHistory.size() > 0 && (c.equals(KeyCode.UP) || c.equals(KeyCode.DOWN))) {
+               e.consume();
+               commandHistoryPtr += c.equals(KeyCode.DOWN) ? 1 : -1;
+               commandHistoryPtr = Math.max(0, commandHistoryPtr);
+               commandHistoryPtr = Math.min(commandHistory.size() - 1, commandHistoryPtr);
+               commandTextField.setText(commandHistory.get(commandHistoryPtr));
+               commandTextField.end();
+            }
+        });
     }
 
     /**
@@ -40,6 +58,9 @@ public class CommandBox extends UiPart<Region> {
         if (commandText.equals("")) {
             return;
         }
+
+        commandHistory.add(commandText);
+        commandHistoryPtr = commandHistory.size();
 
         try {
             commandExecutor.execute(commandText);
