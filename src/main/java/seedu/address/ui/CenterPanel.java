f20b5b84a16212dcf8662a412223ff547385a216
Chesterwongz <psyforce99@gmail.com>
2021-10-28 15:59:40 +0800

    Fix view command not selecting student card

--- src/main/java/seedu/address/ui/CenterPanel.java
+++ src/main/java/seedu/address/ui/CenterPanel.java
@@ -129,6 +129,7 @@ public class CenterPanel extends UiPart<Region> {
         personGridPanel.fillListPanels(student, lessons);
         personGridPanel.setListPanels();
         centerPanelPlaceholder.getChildren().setAll(personGridPanel.getRoot());
+        personGridPanel.getPersonListView().getSelectionModel().select(student);
     }
 
     /**
