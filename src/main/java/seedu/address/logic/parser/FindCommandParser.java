42ae5a5bbd01b1a4c93c6104f547093e0af0efb0
GraceWang2322 <>
2021-10-28 23:32:02 +0800

    Allow finding by tag

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -1,8 +1,7 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ID;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.*;
 
 import java.util.List;
 
@@ -10,6 +9,7 @@ import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.item.IdContainsNumberPredicate;
 import seedu.address.model.item.NameContainsKeywordsPredicate;
+import seedu.address.model.item.TagContainsKeywordsPredicate;
 
 /**
  * Parses input arguments and creates a new FindCommand object
@@ -23,26 +23,33 @@ public class FindCommandParser implements Parser<FindCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindCommand parse(String args) throws ParseException {
-        boolean areNames = false;
         boolean areIds = false;
         List<String> id;
         List<String> names;
+        List<String> tags;
         String trimmedArgs = args.trim();
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_ID);
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_ID, PREFIX_TAG);
 
-        // Check that either name or id specified
-        if (argMultimap.getValue(PREFIX_NAME).isEmpty() && argMultimap.getValue(PREFIX_ID).isEmpty()) {
+        // Check that either name or id or tag is specified
+        if (argMultimap.getValue(PREFIX_NAME).isEmpty() && argMultimap.getValue(PREFIX_ID).isEmpty()
+                && argMultimap.getValue(PREFIX_TAG).isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
         // Parse id
         if (!argMultimap.getValue(PREFIX_ID).isEmpty()) {
             areIds = true;
+        }
+
+        // Parse tag
+        if (!argMultimap.getValue(PREFIX_TAG).isEmpty()) {
+            tags = argMultimap.getAllValues(PREFIX_TAG);
+            return new FindCommand((new TagContainsKeywordsPredicate(tags)));
         }
 
         // Initialise the list of names or id
@@ -56,7 +63,5 @@ public class FindCommandParser implements Parser<FindCommand> {
         names = argMultimap.getAllValues(PREFIX_NAME);
         return new FindCommand((new NameContainsKeywordsPredicate(names)));
     }
-
-
 
 }
