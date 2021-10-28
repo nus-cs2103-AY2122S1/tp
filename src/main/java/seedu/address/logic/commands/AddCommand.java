d26863c4728c0467ab2bafbe2885c4b75e99bb42
GraceWang2322 <>
2021-10-28 13:21:22 +0800

    AddCommand shows warning for inexistent id or inexistent name

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -9,6 +9,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.model.Model.DisplayMode.DISPLAY_INVENTORY;
 
 import java.util.List;
+import java.util.Optional;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
@@ -24,7 +25,7 @@ public class AddCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds item(s) to the inventory. "
             + "\nEnter all fields if item added for the first time"
-            + "\nIf replenish an existing item, only name or id, and count are needed"
+            + "\nIf replenish an existing item, only name or/and id, and count are needed"
             + "\nParameters: "
             + "NAME "
             + PREFIX_ID + "ID "
@@ -45,6 +46,8 @@ public class AddCommand extends Command {
     public static final String MESSAGE_SUCCESS_REPLENISH = "Item replenished: %d x %s";
     public static final String MESSAGE_INCOMPLETE_INFO = "Item has not been added before,"
             + " please provide name, id, cost price, and sales price";
+    public static final String MESSAGE_ID_NOT_FOUND = "Name provided exists but id provided is nonexistent";
+    public static final String MESSAGE_NAME_NOT_FOUND = "Id provided exists but name provided is nonexistent";
     public static final String MESSAGE_MULTIPLE_MATCHES = "Multiple candidates found, which one did you mean to add?";
 
     private final ItemDescriptor toAddDescriptor;
@@ -76,6 +79,20 @@ public class AddCommand extends Command {
             Item newItem = toAddDescriptor.buildItem();
             model.addItem(newItem);
             return new CommandResult(String.format(MESSAGE_SUCCESS_NEW, newItem));
+        }
+        //check that id and name given matches
+        if (!toAddDescriptor.getName().equals(Optional.empty())
+                && !toAddDescriptor.getId().equals(Optional.empty())) {
+            toAddDescriptor.setCostPrice(1.0);
+            toAddDescriptor.setSalesPrice(1.0);
+            //check that id exists
+            if (!model.hasId(toAddDescriptor.buildItem())) {
+                throw new CommandException(MESSAGE_ID_NOT_FOUND);
+            }
+            //check that name exists
+            if (!model.hasName(toAddDescriptor.buildItem())) {
+                throw new CommandException(MESSAGE_NAME_NOT_FOUND);
+            }
         }
 
         // Check that only 1 item fit the description
