0ea46b8e23c403aadc8a15a696e7e059c8be04a3
Chan Choon Yong <43673767+choonyongchan@users.noreply.github.com>
2021-10-29 16:08:46 +0800

    Patch emptyString bug for Find and DeleteMultiple
    "find n/ n/Test" will list all persons, because the first search term is "" and emptyString is found in all string.
    
    This bug fix fix this specific problem by preventing users from typing empty string

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -15,6 +15,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_TUTORIAL_GROUP;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -53,46 +54,100 @@ public class FindCommandParser implements Parser<FindCommand> {
         if (argMultimap.getValue(PREFIX_NAME).isPresent()
             && !argMultimap.getValue(PREFIX_NAME).get().trim().isEmpty()) {
             List<String> nameKeywords = argMultimap.getAllValues(PREFIX_NAME);
+            int numOfEmptyValue = nameKeywords.stream()
+                    .filter(item-> item.isEmpty()).collect(Collectors.toList()).size();
+            if (numOfEmptyValue != 0) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+            }
             predicateList.add(new NameContainsKeywordsPredicate(nameKeywords));
         }
         if (argMultimap.getValue(PREFIX_PHONE).isPresent()
             && !argMultimap.getValue(PREFIX_PHONE).get().trim().isEmpty()) {
             List<String> phoneKeywords = argMultimap.getAllValues(PREFIX_PHONE);
+            int numOfEmptyValue = phoneKeywords.stream()
+                    .filter(item-> item.isEmpty()).collect(Collectors.toList()).size();
+            if (numOfEmptyValue != 0) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+            }
             predicateList.add(new PhoneContainsKeywordsPredicate(phoneKeywords));
         }
         if (argMultimap.getValue(PREFIX_EMAIL).isPresent()
             && !argMultimap.getValue(PREFIX_EMAIL).get().trim().isEmpty()) {
             List<String> emailKeywords = argMultimap.getAllValues(PREFIX_EMAIL);
+            int numOfEmptyValue = emailKeywords.stream()
+                    .filter(item-> item.isEmpty()).collect(Collectors.toList()).size();
+            if (numOfEmptyValue != 0) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+            }
             predicateList.add(new EmailContainsKeywordsPredicate(emailKeywords));
         }
         if (argMultimap.getValue(PREFIX_GENDER).isPresent()
                 && !argMultimap.getValue(PREFIX_GENDER).get().trim().isEmpty()) {
             List<String> genderKeywords = argMultimap.getAllValues(PREFIX_GENDER);
+            int numOfEmptyValue = genderKeywords.stream()
+                    .filter(item-> item.isEmpty()).collect(Collectors.toList()).size();
+            if (numOfEmptyValue != 0) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+            }
             predicateList.add(new GenderContainsKeywordsPredicate(genderKeywords));
         }
         if (argMultimap.getValue(PREFIX_NATIONALITY).isPresent()
             && !argMultimap.getValue(PREFIX_NATIONALITY).get().trim().isEmpty()) {
             List<String> nationalityKeywords = argMultimap.getAllValues(PREFIX_NATIONALITY);
+            int numOfEmptyValue = nationalityKeywords.stream()
+                    .filter(item-> item.isEmpty()).collect(Collectors.toList()).size();
+            if (numOfEmptyValue != 0) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+            }
             predicateList.add(new NationalityContainsKeywordsPredicate(nationalityKeywords));
         }
         if (argMultimap.getValue(PREFIX_TUTORIAL_GROUP).isPresent()
             && !argMultimap.getValue(PREFIX_TUTORIAL_GROUP).get().trim().isEmpty()) {
             List<String> tutorialGroupKeywords = argMultimap.getAllValues(PREFIX_TUTORIAL_GROUP);
+            int numOfEmptyValue = tutorialGroupKeywords.stream()
+                    .filter(item-> item.isEmpty()).collect(Collectors.toList()).size();
+            if (numOfEmptyValue != 0) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+            }
             predicateList.add(new TutorialGroupContainsKeywordsPredicate(tutorialGroupKeywords));
         }
         if (argMultimap.getValue(PREFIX_SOCIAL_HANDLE).isPresent()
                 && !argMultimap.getValue(PREFIX_SOCIAL_HANDLE).get().trim().isEmpty()) {
             List<String> socialHandleKeywords = argMultimap.getAllValues(PREFIX_SOCIAL_HANDLE);
+            int numOfEmptyValue = socialHandleKeywords.stream()
+                    .filter(item-> item.isEmpty()).collect(Collectors.toList()).size();
+            if (numOfEmptyValue != 0) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+            }
             predicateList.add(new SocialHandleContainsKeywordsPredicate(socialHandleKeywords));
         }
         if (argMultimap.getValue(PREFIX_REMARK).isPresent()
                 && !argMultimap.getValue(PREFIX_REMARK).get().trim().isEmpty()) {
             List<String> remarkKeywords = argMultimap.getAllValues(PREFIX_REMARK);
+            int numOfEmptyValue = remarkKeywords.stream()
+                    .filter(item-> item.isEmpty()).collect(Collectors.toList()).size();
+            if (numOfEmptyValue != 0) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+            }
             predicateList.add(new RemarkContainsKeywordsPredicate(remarkKeywords));
         }
         if (argMultimap.getValue(PREFIX_TAG).isPresent()
             && !argMultimap.getValue(PREFIX_TAG).get().trim().isEmpty()) {
             List<String> tagKeywords = argMultimap.getAllValues(PREFIX_TAG);
+            int numOfEmptyValue = tagKeywords.stream()
+                    .filter(item-> item.isEmpty()).collect(Collectors.toList()).size();
+            if (numOfEmptyValue != 0) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+            }
             predicateList.add(new TagContainsKeywordsPredicate(tagKeywords));
         }
 
