9d6a2cb3e91138e49bf8a193aa355f8dced38613
James-Kua <kua.james30@gmail.com>
2021-10-29 11:44:30 +0800

    Update SampleDataUtil

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -29,31 +29,31 @@ public class SampleDataUtil {
 
     public static Person[] getSamplePersons() {
         return new Person[] {
-            new Person(new Name("Alex Yeoh"), new Email("alexyeoh@example.com"), getModuleCodeSet("CS1231 T12 L02"),
+            new Person(new Name("Alex Yeoh"), new Email("alexyeoh@u.nus.edu"), getModuleCodeSet("CS1231 T12"),
                     new Phone("87438807"), new TeleHandle("@alexyeoh"), new Remark("overseas")),
-            new Person(new Name("Bernice Yu"), new Email("berniceyu@example.com"), getModuleCodeSet("CS2030S T10"),
+            new Person(new Name("Bernice Yu"), new Email("berniceyu@u.nus.edu"), getModuleCodeSet("CS2030S T10"),
                     new Phone("99272758"), new TeleHandle("@berniceyu"), EMPTY_REMARK),
-            new Person(new Name("Charlotte Oliveiro"), new Email("charlotte@example.com"),
+            new Person(new Name("Charlotte Oliveiro"), new Email("charlotte@u.nus.edu"),
                     getModuleCodeSet("CS1231 T12"), new Phone("93210283"), new TeleHandle("@charlotteO"), EMPTY_REMARK),
-            new Person(new Name("David Li"), new Email("lidavid@example.com"), getModuleCodeSet("CS2030S T10",
+            new Person(new Name("David Li"), new Email("lidavid@u.nus.edu"), getModuleCodeSet("CS2030S T10",
                     "CS2040 T05"), new Phone("91031282"), new TeleHandle("@davidli"), new Remark("late")),
             new Person(new Name("Irfan Ibrahim"), new Email("irfan@example.com"), getModuleCodeSet("CS2040 T05"),
                     new Phone("92492021"), new TeleHandle("@irfan"), EMPTY_REMARK),
-            new Person(new Name("Roy Balakrishnan"), new Email("royb@example.com"), getModuleCodeSet("CS2103T T09"),
+            new Person(new Name("Roy Balakrishnan"), new Email("royb@u.nus.edu"), getModuleCodeSet("CS2103T T09"),
                     new Phone("92624417"), new TeleHandle("@royBala"), new Remark("lab5 done"))
         };
     }
 
     public static ModuleLesson[] getSampleModuleLessons() {
         return new ModuleLesson[] {
-            new ModuleLesson(parseModuleCode("CS2040 T05"), new LessonDay("2"),
+            new ModuleLesson(parseModuleCode("CS1231 T12"), new LessonDay("2"),
                     new LessonTime("10:00"), new LessonTime("11:00"), new Remark("COM1-113")),
-            new ModuleLesson(parseModuleCode("CS2100 T21"), new LessonDay("2"),
+            new ModuleLesson(parseModuleCode("CS2030S T10"), new LessonDay("2"),
                     new LessonTime("15:00"), new LessonTime("16:00"), new Remark("COM2-0223")),
-            new ModuleLesson(parseModuleCode("CS2103 TG09"), new LessonDay("1"),
+            new ModuleLesson(parseModuleCode("CS2103 T09"), new LessonDay("1"),
                     new LessonTime("14:00"), new LessonTime("15:00"), new Remark("I3 Audi")),
-            new ModuleLesson(parseModuleCode("CS2106 TG01"), new LessonDay("5"),
-                    new LessonTime("10:00"), new LessonTime("11:00"), new Remark("COM1-120")),
+            new ModuleLesson(parseModuleCode("CS2040 T05"), new LessonDay("1"),
+                    new LessonTime("14:00"), new LessonTime("15:00"), new Remark("COM1- 120")),
         };
     }
 
