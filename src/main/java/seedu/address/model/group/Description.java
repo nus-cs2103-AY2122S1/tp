f3a9d650c457a2996f8fe218471ddb41fc4c2fe1
skcheongbrian <skcheongbrian@gmail.com>
2021-10-30 14:13:35 +0800

    Fix wrong error message being displayed for invalid descriptions

--- src/main/java/seedu/address/model/group/Description.java
+++ src/main/java/seedu/address/model/group/Description.java
@@ -10,7 +10,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Description {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names should only contain alphanumeric characters and spaces, and it should not be blank";
+            "A group's description should only contain alphanumeric characters and spaces, and it should not be blank";
 
     public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
 
