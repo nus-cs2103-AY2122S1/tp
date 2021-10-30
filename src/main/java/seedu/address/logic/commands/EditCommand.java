2f0fcfa7344371efe45ea52907fa4f89fa2aaa24
nataniayp <75572811+nataniayp@users.noreply.github.com>
2021-10-30 19:27:00 +0800

    Modify EditCommand to show editedPerson on the right panel

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -94,7 +94,7 @@ public class EditCommand extends Command {
         model.setPerson(personToEdit, editedPerson);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
         Summary summary = new Summary(model.getAddressBook());
-        return new CommandResult(String.format(MESSAGE_EDIT_PERSON_SUCCESS, editedPerson), summary);
+        return new CommandResult(String.format(MESSAGE_EDIT_PERSON_SUCCESS, editedPerson), editedPerson);
     }
 
     /**
