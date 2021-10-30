474969e4e788c2df78158573b49fba20484654fd
timiditi <timothy.ng.jw@gmail.com>
2021-10-30 22:49:58 +0800

    Remove unnecessary imports from TimetableInfoPage

--- src/main/java/seedu/address/ui/infopage/TimetableInfoPage.java
+++ src/main/java/seedu/address/ui/infopage/TimetableInfoPage.java
@@ -3,10 +3,8 @@ import java.util.logging.Logger;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
