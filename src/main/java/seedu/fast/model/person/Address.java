721f067d626951bf9583a59c7611f2a87b378e32
LauTengHon <e0543465@u.nus.edu>
2021-10-31 14:06:38 +0800

    Set a limit of 100 characters for address

--- src/main/java/seedu/fast/model/person/Address.java
+++ src/main/java/seedu/fast/model/person/Address.java
@@ -8,8 +8,9 @@ import static seedu.fast.commons.util.AppUtil.checkArgument;
  * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
  */
 public class Address {
-
-    public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, and it should not be blank";
+    public static final int MAX_LENGTH_ADDRESS = 100;
+    public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, and it should not be blank. " +
+            "But it has a limit of 100 characters.";
 
     /*
      * The first character of the address must not be a whitespace,
@@ -34,7 +35,7 @@ public class Address {
      * Returns true if a given string is a valid email.
      */
     public static boolean isValidAddress(String test) {
-        return test.matches(VALIDATION_REGEX);
+        return test.matches(VALIDATION_REGEX) && test.length() <= MAX_LENGTH_ADDRESS;
     }
 
     @Override
