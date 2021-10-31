f721e1636702cc2bc84afd582096897ae5030eff
jodichoo <jodi.choo@gmail.com>
2021-10-31 17:01:00 +0800

    Minor edit for ClearCommand success message

--- src/main/java/seedu/tracker/logic/commands/ClearCommand.java
+++ src/main/java/seedu/tracker/logic/commands/ClearCommand.java
@@ -33,7 +33,7 @@ public class ClearCommand extends Command {
             + "Example: " + COMMAND_WORD
             + PREFIX_ACADEMIC_YEAR + "1 "
             + PREFIX_SEMESTER + "1";;
-    public static final String MESSAGE_SUCCESS = "All modules on %1$s has been untaked!";
+    public static final String MESSAGE_SUCCESS = "All modules in %1$s has been unscheduled!";
     private final ModuleInSpecificSemesterPredicate predicate;
     private final AcademicCalendar academicCalendar;
 
