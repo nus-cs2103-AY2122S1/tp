875887140716cf3ebe21c00afbd2864d07d1f91e
adam-ky <69505439+adam-ky@users.noreply.github.com>
2021-10-31 14:18:56 +0800

    Update HelpCommand
    Instead of copying the url, the button now opens the user guide
    in the user's browser.

---
+++ src/main/java/seedu/address/commons/core/Browser.java
@@ -0,0 +1,58 @@
+package seedu.address.commons.core;
+
+import java.awt.Desktop;
+import java.awt.GraphicsEnvironment;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.logging.Logger;
+
+/**
+ * Represents the user's browser to open url links.
+ */
+public class Browser {
+    private static Logger logger = LogsCenter.getLogger(Browser.class);
+
+    /**
+     * Opens URL inside user's Desktop Browser.
+     * Assume running platform supports desktop. Use {@link #isDisplayAndBrowseCompatible()} to check.
+     * No validity checks of valid url.
+     *
+     * @param url
+     */
+    public static void openUrl(String url) {
+        if (!isDisplayAndBrowseCompatible()) {
+            return;
+        }
+
+        Desktop d = Desktop.getDesktop();
+
+        logger.info("Opening URL in browser: " + url + "...");
+
+        try {
+            d.browse(new URI(url));
+        } catch (URISyntaxException | IOException e) {
+            logger.severe("Unable to open URL in user browser" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Checks if platform running code supports Desktop and Desktop.Action.BROWSE.
+     * Helper function used to verify platform compatibility before calls to {@link #openUrl(String)}
+     */
+    public static boolean isDisplayAndBrowseCompatible() {
+        if (GraphicsEnvironment.isHeadless()) {
+            logger.warning("Platform does not support display.\n GraphicsEnviornment.isHeadless()");
+            return false;
+        }
+
+        Desktop d = Desktop.getDesktop();
+        if (!d.isSupported(Desktop.Action.BROWSE)) {
+            logger.warning("Platform does not support browsing in desktop");
+            return false;
+        }
+
+        return true;
+    }
+}
