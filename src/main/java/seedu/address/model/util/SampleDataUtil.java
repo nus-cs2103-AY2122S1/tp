044485046172109aa7b3f381a15225360fe4ca8c
halfofning <77186833+wanninglim@users.noreply.github.com>
2021-10-31 21:49:15 +0800

    Add sample groups

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -2,13 +2,18 @@ package seedu.address.model.util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.commons.RepoName;
+import seedu.address.model.group.Group;
 import seedu.address.model.group.GroupName;
+import seedu.address.model.group.LinkYear;
+import seedu.address.model.group.Members;
 import seedu.address.model.student.Attendance;
 import seedu.address.model.student.Email;
 import seedu.address.model.student.Name;
@@ -23,25 +28,36 @@ import seedu.address.model.tag.Tag;
  */
 public class SampleDataUtil {
     public static Student[] getSampleStudents() {
-        return new Student[] {
+        return new Student[]{
             new Student(new Name("Alex Yeoh"), new Email("alexyeoh@u.nus.edu"), new StudentNumber("A0123436B"),
                     new UserName("ayeoh"), new RepoName("ip"), getTagSet("year2", "cs"),
-                    getDefaultAttendance(), getDefaultParticipation(), new GroupName()),
+                    getDefaultAttendance(), getDefaultParticipation(), new GroupName("W14-4")),
             new Student(new Name("Bernice Yu"), new Email("berniceyu@u.nus.edu"), new StudentNumber("A0123456A"),
                     new UserName("BYU"), null, getTagSet("year2", "cs"),
-                    getDefaultAttendance(), getDefaultParticipation(), new GroupName()),
+                    getDefaultAttendance(), getDefaultParticipation(), new GroupName("W14-4")),
             new Student(new Name("Charlotte Oliveiro"), new Email("charlotte@u.nus.edu"),
                     new StudentNumber("A0123450B"), null, null, getTagSet("year2", "engineering"),
-                    getDefaultAttendance(), getDefaultParticipation(), new GroupName()),
+                    getDefaultAttendance(), getDefaultParticipation(), new GroupName("W10-2")),
             new Student(new Name("David Li"), new Email("lidavid@u.nus.edu"), new StudentNumber("A0123956B"),
                     null, new RepoName("repo"), getTagSet("year3"),
-                    getDefaultAttendance(), getDefaultParticipation(), new GroupName()),
+                    getDefaultAttendance(), getDefaultParticipation(), new GroupName("W10-2")),
             new Student(new Name("Irfan Ibrahim"), new Email("irfan@u.nus.edu"), new StudentNumber("A0823456B"),
                     new UserName("IrIb"), new RepoName("myRepo"), getTagSet("year3"),
-                    getDefaultAttendance(), getDefaultParticipation(), new GroupName()),
+                    getDefaultAttendance(), getDefaultParticipation(), new GroupName("W07-3")),
             new Student(new Name("Roy Balakrishnan"), new Email("royb@u.nus.edu"), new StudentNumber("A1123456B"),
                     null, null, getTagSet("year1"),
-                    getDefaultAttendance(), getDefaultParticipation(), new GroupName()),
+                    getDefaultAttendance(), getDefaultParticipation(), new GroupName("W07-3")),
+        };
+    }
+
+    public static Group[] getSampleGroups() {
+        return new Group[] {
+            new Group(new GroupName("W14-4"), getMemberList(0, 1), new LinkYear("AY2122"),
+                    new RepoName("tp"), getTagSet("monday")),
+            new Group(new GroupName("W10-2"), getMemberList(2, 3), new LinkYear("AY2122"),
+                    new RepoName("tp"), getTagSet("tuesday")),
+            new Group(new GroupName("W07-3"), getMemberList(4, 5), new LinkYear("AY2122"),
+                    new RepoName("tp"), getTagSet("wednesday")),
         };
     }
 
@@ -50,6 +66,12 @@ public class SampleDataUtil {
         for (Student sampleStudent : getSampleStudents()) {
             sampleAb.addStudent(sampleStudent);
         }
+        for (Group sampleGroup : getSampleGroups()) {
+            sampleAb.addGroup(sampleGroup);
+        }
+        //for (Task sampleTask : getSampleTasks()) {
+        //    sampleAb.addTask(sampleTask);
+        //}
         return sampleAb;
     }
 
@@ -61,6 +83,20 @@ public class SampleDataUtil {
                 .map(Tag::new)
                 .collect(Collectors.toSet());
     }
+
+    /**
+     * Returns a member set containing the list of students given.
+     */
+    public static Members getMemberList(Integer... indexes) {
+        List<Student> students = List.of(getSampleStudents());
+        List<Integer> indexList = List.of(indexes);
+        List<Student> members = IntStream.range(0, students.size())
+                        .filter(i -> indexList.contains(i))
+                        .mapToObj(students::get)
+                        .collect(Collectors.toList());
+        return new Members(new ArrayList<>(members));
+    }
+
 
     /**
      * Returns an attendance list containing the list of integers given.
