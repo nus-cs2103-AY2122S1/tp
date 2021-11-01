58c98c8fddbb66bd87ff62b5a1649786c6d75be1
LuoDan <69646558+luodan01@users.noreply.github.com>
2021-11-01 11:06:40 +0800

    Revert changes

--- src/main/java/seedu/anilist/logic/commands/ConfirmClearCommand.java
+++ src/main/java/seedu/anilist/logic/commands/ConfirmClearCommand.java
@@ -17,8 +17,8 @@ public class ConfirmClearCommand extends Command {
     public CommandResult execute(Model model) {
         requireNonNull(model);
         List<Anime> lastShownList = model.getFilteredAnimeList();
-        for (int i = 0; i < lastShownList.size(); i++) {
-            model.deleteAnime(lastShownList.get(i));
+        while (!model.getFilteredAnimeList().isEmpty()) {
+            model.deleteAnime(lastShownList.get(0));
         }
         model.setCurrentTab(TabOption.TabOptions.ALL);
         return new CommandResult(MESSAGE_SUCCESS);
