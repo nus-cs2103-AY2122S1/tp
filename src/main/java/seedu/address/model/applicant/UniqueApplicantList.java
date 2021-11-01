08fac75348c0a2088e756e1973b892f8628e54a8
Harry-Gao-H <59434153+Harry-Gao-H@users.noreply.github.com>
2021-11-01 21:26:06 +0800

    Follow coding standard

--- src/main/java/seedu/address/model/applicant/UniqueApplicantList.java
+++ src/main/java/seedu/address/model/applicant/UniqueApplicantList.java
@@ -6,7 +6,6 @@ import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.function.Predicate;
 
 import javafx.collections.FXCollections;
