6eb0699f31337fa1975fc208aa703edf23a693b5
marcuschj <haojie.chua@gmail.com>
2021-11-01 13:06:18 +0800

    Limit length of input fields `phone`, `address` and `name`

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -10,13 +10,14 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Name implements Comparable<Name> {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names should only contain alphanumeric characters and spaces, and it should not be blank";
+            "Names should only contain alphanumeric characters and spaces, and it should not be blank\n"
+                + "Names cannot be longer than 100 characters";
 
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]{0,100}";
 
     public final String fullName;
 
