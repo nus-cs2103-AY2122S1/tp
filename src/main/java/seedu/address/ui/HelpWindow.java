34c0fd1597bcebedc60a55193a7f5be894124841
Jai Lulla <jaylulla9@gmail.com>
2021-11-01 16:18:20 +0800

    Update Access Modifiers

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -25,8 +25,8 @@ import seedu.address.logic.commands.CommandDetails;
  */
 public class HelpWindow extends UiPart<Stage> {
 
-    public static final String USERGUIDE_URL = "https://ay2122s1-cs2103t-t10-1.github.io/tp/UserGuide.html";
-    public static final String HELP_MESSAGE = "For more Information, please refer to the user guide:\n"
+    private static final String USERGUIDE_URL = "https://ay2122s1-cs2103t-t10-1.github.io/tp/UserGuide.html";
+    private static final String HELP_MESSAGE = "For more Information, please refer to the user guide:\n"
             + USERGUIDE_URL;
 
     private static final String ADD_NEW_CONTACT_FEATURE_NAME = "Add a New Contact";
