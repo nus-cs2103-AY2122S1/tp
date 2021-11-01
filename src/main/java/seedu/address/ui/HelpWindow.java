7b77d1b6b3425c7878b33d1c4978b3c8625ff72f
Jai Lulla <jaylulla9@gmail.com>
2021-11-01 15:55:41 +0800

    GUI Changes
    Made some changes to the GUI,
    to accommodate more number of
    commands (rows).

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -8,12 +8,14 @@ import java.util.logging.Logger;
 
 import javafx.beans.binding.Bindings;
 import javafx.collections.ObservableList;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.ScrollEvent;
 import javafx.stage.Stage;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.CommandDetails;
@@ -89,6 +91,7 @@ public class HelpWindow extends UiPart<Stage> {
         super(FXML, root);
         helpMessage.setText(HELP_MESSAGE);
         setUpCommandDetails();
+        setUpHelpTableView();
     }
 
     /**
@@ -119,12 +122,21 @@ public class HelpWindow extends UiPart<Stage> {
      */
     public void show() {
         logger.fine("Showing help page about the application.");
-        helpTable.setFixedCellSize(25);
-        helpTable.prefHeightProperty().bind(helpTable.fixedCellSizeProperty().multiply(Bindings.size(helpTable.getItems()).add(1.01)));
-        helpTable.minHeightProperty().bind(helpTable.prefHeightProperty());
-        helpTable.maxHeightProperty().bind(helpTable.prefHeightProperty());
         getRoot().show();
         getRoot().centerOnScreen();
+    }
+
+    /**
+     * Sets up the table view. Disables scrolling and
+     * adjusts the height according to the number of rows.
+     */
+    public void setUpHelpTableView() {
+        helpTable.addEventFilter(ScrollEvent.ANY, Event::consume);
+        helpTable.setFixedCellSize(25);
+        helpTable.prefHeightProperty().bind(helpTable.fixedCellSizeProperty()
+                .multiply(Bindings.size(helpTable.getItems()).add(1.01)));
+        helpTable.minHeightProperty().bind(helpTable.prefHeightProperty());
+        helpTable.maxHeightProperty().bind(helpTable.prefHeightProperty());
     }
 
     /**
