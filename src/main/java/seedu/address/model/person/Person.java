8c3b33e1beeeb35003e24803d454b2bc35425625
eugenecsa <eugenechanec@gmail.com>
2021-11-01 20:09:10 +0800

    Bug Fix
    Bugs fixed:
    - Same person now defined as: Same Name and phone number, instead of
    just the same name.

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -116,7 +116,7 @@ public class Person implements Comparable<Person> {
     }
 
     /**
-     * Returns true if both persons have the same name.
+     * Returns true if both persons have the same name and phone number.
      * This defines a weaker notion of equality between two persons.
      */
     public boolean isSamePerson(Person otherPerson) {
@@ -125,7 +125,8 @@ public class Person implements Comparable<Person> {
         }
 
         return otherPerson != null
-                && otherPerson.getName().equals(getName());
+                && otherPerson.getName().equals(getName())
+                && otherPerson.getPhone().equals(getPhone());
     }
 
     /**
