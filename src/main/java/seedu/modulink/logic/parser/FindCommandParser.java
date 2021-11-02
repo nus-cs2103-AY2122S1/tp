b8961be6cb36f41abe65edff3c82a341b8e689fb
charltonator <charltonlucian@gmail.com>
2021-11-02 16:37:35 +0800

    undid previous commit

--- src/main/java/seedu/modulink/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/modulink/logic/parser/FindCommandParser.java
@@ -20,7 +20,7 @@ public class FindCommandParser implements Parser<FindCommand> {
      */
     public FindCommand parse(String args) throws ParseException {
         String trimmedArgs = args.trim();
-        if (trimmedArgs.isEmpty() || trimmedArgs.matches("^.*[^a-zA-Z0-9].*$")) {
+        if (trimmedArgs.isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
