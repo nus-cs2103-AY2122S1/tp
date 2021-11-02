1f55d92aa29cd8b4640bbf0f520e6e0147bb02a4
iopwslw <iopwslw@gmail.com>
2021-11-02 15:21:42 +0800

    Change regex check for remark and message

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,14 +9,13 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Remarks should only contain ASCII characters, and it should not be blank.\n"
-                    + "The limit for remark is 52 characters.";
+            "Remarks should only contain ASCII characters, and it should be at most 52 characters long.";
 
     /**
      * The first character of the remark must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid remark.
      */
-    public static final String VALIDATION_REGEX = "[\\p{ASCII}]+[^\\s]*";
+    public static final String VALIDATION_REGEX = "[\\p{ASCII}]{1,52}";
 
     public final String value;
 
