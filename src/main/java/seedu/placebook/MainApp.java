7de58c8e0b971558587a0925905e2b798dbdcad3
nwhloo99 <77220827+nwhloo99@users.noreply.github.com>
2021-11-02 21:10:34 +0800

    BugFix MainApp

--- src/main/java/seedu/placebook/MainApp.java
+++ src/main/java/seedu/placebook/MainApp.java
@@ -104,6 +104,7 @@ public class MainApp extends Application {
                 logger.info("Data file not found. Will be starting with a sample AddressBook");
             }
             initialData = addressBookOptional.orElseGet(SampleDataUtil::getSampleAddressBook);
+            initialSchedule = SampleDataUtil.getSampleSchedule();
         } catch (DataConversionException e) {
             logger.warning("Data file not in the correct format. Will be starting with an empty AddressBook");
             initialData = new AddressBook();
