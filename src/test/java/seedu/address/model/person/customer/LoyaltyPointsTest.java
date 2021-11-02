71c35d44dd1ef703c5a259e4be1618616cab617d
clementkfj <clement.kfj@gmail.com>
2021-11-02 19:49:09 +0800

    Fix Loyalty Points

--- src/test/java/seedu/address/model/person/customer/LoyaltyPointsTest.java
+++ src/test/java/seedu/address/model/person/customer/LoyaltyPointsTest.java
@@ -23,21 +23,18 @@ public class LoyaltyPointsTest {
 
     @Test
     public void isValidLoyaltyPoints() {
-        // null LoyaltyPoints number
-        assertThrows(NullPointerException.class, () -> LoyaltyPoints.isValidLoyaltyPoints(null));
-
         // invalid LoyaltyPoints numbers
+        assertFalse(LoyaltyPoints.isValidLoyaltyPoints(null)); // null
         assertFalse(LoyaltyPoints.isValidLoyaltyPoints("")); // empty string
         assertFalse(LoyaltyPoints.isValidLoyaltyPoints(" ")); // spaces only
-        assertFalse(LoyaltyPoints.isValidLoyaltyPoints("911")); // less than 3 numbers
         assertFalse(LoyaltyPoints.isValidLoyaltyPoints("LoyaltyPoints")); // non-numeric
         assertFalse(LoyaltyPoints.isValidLoyaltyPoints("9011p041")); // alphabets within digits
         assertFalse(LoyaltyPoints.isValidLoyaltyPoints("9312 1534")); // spaces within digits
 
         // valid LoyaltyPoints numbers
-        assertTrue(LoyaltyPoints.isValidLoyaltyPoints("9110")); // exactly 3 numbers
+        assertTrue(LoyaltyPoints.isValidLoyaltyPoints("9110")); // numerical
         assertTrue(LoyaltyPoints.isValidLoyaltyPoints("10000"));
-        assertTrue(LoyaltyPoints.isValidLoyaltyPoints("124293842033123")); // long phone numbers
+        assertTrue(LoyaltyPoints.isValidLoyaltyPoints("0")); // loyalty points is 0
     }
 
     @Test
