0cc71ecb704a35801908ad8a019cee42aace60ba
CheongYeeMing <e0526001@u.nus.edu>
2021-11-03 13:28:44 +0800

    Add new variables to `CommandTestUtil`.

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -58,13 +58,21 @@ public class CommandTestUtil {
     public static final String TAG_DESC_FRIEND = " " + PREFIX_TAG + VALID_TAG_FRIEND;
     public static final String TAG_DESC_HUSBAND = " " + PREFIX_TAG + VALID_TAG_HUSBAND;
 
-    public static final String INVALID_NAME_DESC = " " + PREFIX_NAME + "James&"; // '&' not allowed in names
-    public static final String INVALID_TELEGRAM_DESC = " " + PREFIX_TELEGRAM + "cannot!"; // '!' not allowed in telegram
-    public static final String INVALID_GITHUB_DESC = " " + PREFIX_GITHUB + "a--b"; // '--' not allowed in github
-    public static final String INVALID_PHONE_DESC = " " + PREFIX_PHONE + "911a"; // 'a' not allowed in phones
-    public static final String INVALID_EMAIL_DESC = " " + PREFIX_EMAIL + "bob!yahoo"; // missing '@' symbol
-    public static final String INVALID_ADDRESS_DESC = " " + PREFIX_ADDRESS; // empty string not allowed for addresses
-    public static final String INVALID_TAG_DESC = " " + PREFIX_TAG + "hubby*"; // '*' not allowed in tags
+    public static final String INVALID_NAME = "James&"; // '&' not allowed in names
+    public static final String INVALID_TELEGRAM = "cannot!"; // '!' not allowed in telegram
+    public static final String INVALID_GITHUB = "a--b"; // '--' not allowed in github
+    public static final String INVALID_PHONE = "911a"; // 'a' not allowed in phones
+    public static final String INVALID_EMAIL = "bob!yahoo"; // missing '@' symbol
+    public static final String INVALID_ADDRESS = ""; // empty string not allowed for addresses
+    public static final String INVALID_TAG = "hubby*"; // '*' not allowed in tags
+
+    public static final String INVALID_NAME_DESC = " " + PREFIX_NAME + INVALID_NAME;
+    public static final String INVALID_TELEGRAM_DESC = " " + PREFIX_TELEGRAM + INVALID_TELEGRAM;
+    public static final String INVALID_GITHUB_DESC = " " + PREFIX_GITHUB + INVALID_GITHUB;
+    public static final String INVALID_PHONE_DESC = " " + PREFIX_PHONE + INVALID_PHONE;
+    public static final String INVALID_EMAIL_DESC = " " + PREFIX_EMAIL + INVALID_EMAIL;
+    public static final String INVALID_ADDRESS_DESC = " " + PREFIX_ADDRESS + INVALID_ADDRESS;
+    public static final String INVALID_TAG_DESC = " " + PREFIX_TAG + INVALID_TAG;
 
     public static final String PREAMBLE_WHITESPACE = "\t  \r  \n";
     public static final String PREAMBLE_NON_EMPTY = "NonEmptyPreamble";
