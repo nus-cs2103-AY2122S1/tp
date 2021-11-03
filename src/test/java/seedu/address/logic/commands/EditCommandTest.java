10de9da57f796c0a0223bd823543c95f509945a9
XXJJXJ <yexinjian1506@gmail.com>
2021-11-03 23:51:29 +0800

    Add testcase for EditCommandTest back

--- src/test/java/seedu/address/logic/commands/EditCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditCommandTest.java
@@ -34,6 +34,20 @@ public class EditCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
+    public void execute_allFieldsSpecifiedUnfilteredList_success() {
+        Participant editedParticipant = new ParticipantBuilder().build();
+        EditParticipantDescriptor descriptor = new EditParticipantDescriptorBuilder(editedParticipant).build();
+        EditCommand editCommand = new EditCommand(INDEX_FIRST_PARTICIPANT, descriptor);
+
+        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PARTICIPANT_SUCCESS, editedParticipant);
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setParticipant(model.getFilteredParticipantList().get(0), editedParticipant);
+
+        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
     public void execute_someFieldsSpecifiedUnfilteredList_success() {
         Index indexLastParticipant = Index.fromOneBased(model.getFilteredParticipantList().size());
         Participant lastParticipant = model.getFilteredParticipantList().get(indexLastParticipant.getZeroBased());
@@ -61,6 +75,26 @@ public class EditCommandTest {
         String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PARTICIPANT_SUCCESS, editedParticipant);
 
         Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+
+        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_filteredList_success() {
+        showParticipantAtIndex(model, INDEX_FIRST_PARTICIPANT);
+
+        Participant participantInFilteredList = model.getFilteredParticipantList()
+                .get(INDEX_FIRST_PARTICIPANT.getZeroBased());
+        Participant editedParticipant = new ParticipantBuilder(participantInFilteredList)
+                .withName(VALID_NAME_BOB).build();
+        EditCommand editCommand = new EditCommand(INDEX_FIRST_PARTICIPANT,
+                new EditParticipantDescriptorBuilder().withName(VALID_NAME_BOB).build());
+
+        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PARTICIPANT_SUCCESS, editedParticipant);
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        showParticipantAtIndex(expectedModel, INDEX_FIRST_PARTICIPANT);
+        expectedModel.setParticipant(model.getFilteredParticipantList().get(0), editedParticipant);
 
         assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
     }
