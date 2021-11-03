738de81ed12222dd79bd355d70f77be6a0d665a8
sin yee <e0563953@u.nus.edu>
2021-11-03 20:14:08 +0800

    Fix checkstyle errors

--- src/test/java/seedu/address/model/person/PersonTest.java
+++ src/test/java/seedu/address/model/person/PersonTest.java
@@ -14,7 +14,6 @@ import static seedu.address.testutil.TypicalPersons.BOB;
 import java.time.DayOfWeek;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 
