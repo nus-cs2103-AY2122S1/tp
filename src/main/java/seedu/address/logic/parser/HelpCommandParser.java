60503bf8afca5e26d6fab5a1729b937da2f4749e
GraceWang2322 <>
2021-11-03 23:02:10 +0800

    Fix bugs

--- src/main/java/seedu/address/logic/parser/HelpCommandParser.java
+++ src/main/java/seedu/address/logic/parser/HelpCommandParser.java
@@ -34,48 +34,36 @@ public class HelpCommandParser implements Parser<HelpCommand> {
         switch (trimmedArgs) {
         case AddCommand.COMMAND_WORD:
             return new HelpCommand(AddCommand.MESSAGE_USAGE);
-
         case EditCommand.COMMAND_WORD:
             return new HelpCommand(EditCommand.MESSAGE_USAGE);
-
         case RemoveCommand.COMMAND_WORD:
             return new HelpCommand(RemoveCommand.MESSAGE_USAGE);
-
         case DeleteCommand.COMMAND_WORD:
             return new HelpCommand(DeleteCommand.MESSAGE_USAGE);
-
         case ClearCommand.COMMAND_WORD:
             return new HelpCommand(ClearCommand.MESSAGE_USAGE);
-
         case FindCommand.COMMAND_WORD:
             return new HelpCommand(FindCommand.MESSAGE_USAGE);
-
         case ListInventoryCommand.COMMAND_WORD:
             String messageUsage = ListInventoryCommand.MESSAGE_USAGE + "\n"
-                    + ListTransactionCommand.MESSAGE_USAGE;
+                  + ListTransactionCommand.MESSAGE_USAGE;
             return new HelpCommand(messageUsage);
-
         case SortCommand.COMMAND_WORD:
             return new HelpCommand(SortCommand.MESSAGE_USAGE);
-
         case ExitCommand.COMMAND_WORD:
             return new HelpCommand(ExitCommand.MESSAGE_USAGE);
-
         case StartOrderCommand.COMMAND_WORD:
             return new HelpCommand(StartOrderCommand.MESSAGE_USAGE);
-
         case AddToOrderCommand.COMMAND_WORD:
             return new HelpCommand(AddToOrderCommand.MESSAGE_USAGE);
-
         case RemoveFromOrderCommand.COMMAND_WORD:
             return new HelpCommand(RemoveFromOrderCommand.MESSAGE_USAGE);
-
         case EndAndTransactOrderCommand.COMMAND_WORD:
             return new HelpCommand(EndAndTransactOrderCommand.MESSAGE_USAGE);
-        }
-        if (trimmedArgs == "") {
+        case "":
             return new HelpCommand("");
+        default:
+            return new HelpCommand("invalid");
         }
-        return new HelpCommand("invalid");
     }
 }
