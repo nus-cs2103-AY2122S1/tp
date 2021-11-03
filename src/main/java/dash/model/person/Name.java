245118de9aa6842508eb7227b1cb91114c073035
DESKTOP-L6MVVEE\Sanguine <e0543860@u.nus.edu>
2021-11-03 22:54:20 +0800

    Update PPP Add character limit to names Update UG to clarify that assignees dont listen to changes to names

--- src/main/java/dash/model/person/Name.java
+++ src/main/java/dash/model/person/Name.java
@@ -11,13 +11,13 @@ import dash.commons.util.AppUtil;
 public class Name {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names should only contain alphanumeric characters and spaces, and it should not be blank";
+            "Names should only contain alphanumeric characters and spaces, be limited to 20 characters, and it should not be blank";
 
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]{1,19}$+";
 
     public final String fullName;
 
