baf7fad0761c0bc36a3e41722601ab5e3d9efd40
ngchisern <ncsern2000@gmail.com>
2021-11-03 11:40:59 +0800

    Refactor main parser class

--- src/main/java/seedu/address/logic/parser/SalesNoteParser.java
+++ src/main/java/seedu/address/logic/parser/SalesNoteParser.java
@@ -37,7 +37,7 @@ import seedu.address.logic.parser.exceptions.ParseException;
 /**
  * Parses user input.
  */
-public class AddressBookParser {
+public class SalesNoteParser {
 
     /**
      * Used for initial separation of command word and args.
