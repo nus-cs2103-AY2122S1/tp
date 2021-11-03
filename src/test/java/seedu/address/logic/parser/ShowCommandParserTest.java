311562420c2bcd0a5d57808273b0dd653e592f50
CheongYeeMing <e0526001@u.nus.edu>
2021-11-03 14:56:16 +0800

    Remove test case with error in `ShowCommandParserTest`.

--- src/test/java/seedu/address/logic/parser/ShowCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ShowCommandParserTest.java
@@ -16,13 +16,6 @@ public class ShowCommandParserTest {
     private ShowCommandParser parser = new ShowCommandParser();
 
     @Test
-    public void parse_invalidArg_throwsParseException() {
-        String invalidName = "Ch4rlo++#";
-        assertParseFailure(parser, invalidName, String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                ShowCommand.MESSAGE_USAGE));
-    }
-
-    @Test
     public void parse_emptyArg_throwsParseException() {
         assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, ShowCommand.MESSAGE_USAGE));
     }
