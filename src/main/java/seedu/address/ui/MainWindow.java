5e253f920e597d3ffe9d8a503b0c3dcdc5370341
eeliana <pjne2301@gmail.com>
2021-11-04 17:49:29 +0800

    Select student for operations

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -315,6 +315,7 @@ public class MainWindow extends UiPart<Stage> {
 
     private void handlePersonGridPanel(Person student) {
         requireNonNull(student);
+        centerPanel.getPersonListView().getSelectionModel().select(student);
         centerPanel.displayPersonGridPanel(student, logic.getLessonList(student));
     }
 
