bb560a840b5f0b63b3ce15507e9ddc0e0a239576
tanruiquan <ruiquann@gmail.com>
2021-11-04 13:49:29 +0800

    Add more unit tests for LessonCode

--- src/test/java/seedu/address/model/lessoncode/LessonCodeTest.java
+++ src/test/java/seedu/address/model/lessoncode/LessonCodeTest.java
@@ -1,5 +1,7 @@
 package seedu.address.model.lessoncode;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -12,15 +14,24 @@ public class LessonCodeTest {
     }
 
     @Test
-    public void constructor_invalidTagName_throwsIllegalArgumentException() {
+    public void constructor_invalidLessonCode_throwsIllegalArgumentException() {
         String invalidTagName = "";
         assertThrows(IllegalArgumentException.class, () -> new LessonCode(invalidTagName));
     }
 
     @Test
-    public void isValidTagName() {
+    public void isValidLessonCode() {
         // null tag name
         assertThrows(NullPointerException.class, () -> LessonCode.isValidLessonCode(null));
+
+        // invalid lesson code
+        assertFalse(LessonCode.isValidLessonCode(" ")); // spaces only
+        assertFalse(LessonCode.isValidLessonCode("(T12)")); // non-alphanumeric characters
+        assertFalse(LessonCode.isValidLessonCode("T 12")); // with space
+
+        // valid lesson code
+        assertTrue(LessonCode.isValidLessonCode("T12")); // alphanumeric characters
+        assertTrue(LessonCode.isValidLessonCode("t12")); // with lower case
     }
 
 }
