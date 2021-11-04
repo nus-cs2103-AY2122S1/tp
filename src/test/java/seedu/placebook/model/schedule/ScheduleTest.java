3dff8febe9dc6a672876fa87b27336e191b31554
nwhloo99 <77220827+nwhloo99@users.noreply.github.com>
2021-11-05 01:28:59 +0800

    Nwhl Schedule testing (#214)
    * Update ScheduleTest.java
    
    * Fix checkstyle

--- src/test/java/seedu/placebook/model/schedule/ScheduleTest.java
+++ src/test/java/seedu/placebook/model/schedule/ScheduleTest.java
@@ -1,12 +1,17 @@
 package seedu.placebook.model.schedule;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.placebook.testutil.Assert.assertThrows;
 import static seedu.placebook.testutil.TypicalAppointment.ALICE_APPOINTMENT;
+import static seedu.placebook.testutil.TypicalAppointment.ALICE_CARL_APPOINTMENT;
+import static seedu.placebook.testutil.TypicalAppointment.CARL_APPOINTMENT;
 
 import org.junit.jupiter.api.Test;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import seedu.placebook.model.schedule.exceptions.AppointmentNotFoundException;
 import seedu.placebook.model.schedule.exceptions.DuplicateAppointmentException;
 
@@ -52,5 +57,68 @@ public class ScheduleTest {
     public void asUnmodifiableObservableList_modifyList_throwsUnsupportedOperationException() {
         assertThrows(UnsupportedOperationException.class, ()
             -> schedule.asUnmodifiableObservableList().remove(0));
+    }
+
+    @Test
+    public void setAppointment_validInput_success() {
+        schedule.addAppointment(ALICE_APPOINTMENT);
+        schedule.addAppointment(CARL_APPOINTMENT);
+        schedule.setAppointment(ALICE_APPOINTMENT, ALICE_CARL_APPOINTMENT);
+        assertFalse(schedule.contains(ALICE_APPOINTMENT));
+        assertTrue(schedule.contains(ALICE_CARL_APPOINTMENT));
+        assertTrue(schedule.contains(CARL_APPOINTMENT));
+    }
+
+    @Test
+    public void setAppointment_targetAppointmentNotInClientList_exceptionThrown() {
+        schedule.addAppointment(ALICE_APPOINTMENT);
+        assertThrows(AppointmentNotFoundException.class, ()
+            -> schedule.setAppointment(CARL_APPOINTMENT, CARL_APPOINTMENT));
+    }
+
+    @Test
+    public void setAppointment_invalidAppointmentToEdit_exceptionThrown() {
+        schedule.addAppointment(ALICE_APPOINTMENT);
+        schedule.addAppointment(CARL_APPOINTMENT);
+        assertThrows(NullPointerException.class, ()
+            -> schedule.setAppointment(null, ALICE_CARL_APPOINTMENT));
+    }
+
+    @Test
+    public void setAppointment_invalidEditedAppointment_exceptionThrown() {
+        schedule.addAppointment(ALICE_APPOINTMENT);
+        schedule.addAppointment(CARL_APPOINTMENT);
+        assertThrows(NullPointerException.class, ()
+            -> schedule.setAppointment(ALICE_CARL_APPOINTMENT, null));
+    }
+
+    @Test
+    public void sortAppointmentByDescription() {
+        schedule.addAppointment(ALICE_APPOINTMENT);
+        schedule.addAppointment(CARL_APPOINTMENT);
+
+        // Order in expected is by description
+        ObservableList<Appointment> expected = FXCollections.observableArrayList();
+        expected.add(CARL_APPOINTMENT);
+        expected.add(ALICE_APPOINTMENT);
+
+        schedule.sortAppointmentByDescription();
+        assertEquals(expected, schedule.getSchedule());
+    }
+
+    @Test
+    public void sortAppointmentByTime() {
+        schedule.addAppointment(ALICE_APPOINTMENT);
+        schedule.addAppointment(CARL_APPOINTMENT);
+
+        // Order in expected is by time
+        ObservableList<Appointment> expected = FXCollections.observableArrayList();
+        expected.add(ALICE_APPOINTMENT);
+        expected.add(CARL_APPOINTMENT);
+
+        // List is by default sorted by time, sort by description first
+        schedule.sortAppointmentByDescription();
+        schedule.sortAppointmentByTimePeriod();
+        assertEquals(expected, schedule.getSchedule());
     }
 }
