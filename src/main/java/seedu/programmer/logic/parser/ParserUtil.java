abf0693e07f4543461a979027aa3de6161d5e332
Samay Sagar <manu.sagarg1@gmail.com>
2021-11-04 15:17:25 +0800

    - Checkstyle

--- src/main/java/seedu/programmer/logic/parser/ParserUtil.java
+++ src/main/java/seedu/programmer/logic/parser/ParserUtil.java
@@ -109,7 +109,7 @@ public class ParserUtil {
         requireNonNull(labNum);
         try {
             int value = Integer.parseInt(labNum);
-            if (value <= 0 && value > 14) {
+            if (value <= 0 || value > 14) {
                 throw new ParseException(Lab.MESSAGE_LAB_NUMBER_CONSTRAINT);
             }
             return value;
@@ -156,7 +156,7 @@ public class ParserUtil {
             Integer res = Integer.parseInt(trimmedResult);
             if (total == null) {
                 return 0;
-            } else if (res <= 0 && res > 100) {
+            } else if (res <= 0 || res > 100) {
                 throw new ParseException(Lab.MESSAGE_LAB_TOTAL_SCORE_CONSTRAINT);
             }
             return Integer.parseInt(total.trim());
