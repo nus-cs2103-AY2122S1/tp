4ba204d6481b8b038e883772db9bcec122742941
Mengzhe Wang <mengzhe_wang@u.nus.edu>
2021-11-04 14:56:27 +0800

    Add tests for ModuleName halfway

--- src/test/java/seedu/address/model/module/ModuleNameTest.java
+++ src/test/java/seedu/address/model/module/ModuleNameTest.java
@@ -1,13 +1,49 @@
 package seedu.address.model.module;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalModules.*;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ModuleNameTest {
+    private final ModuleName moduleName1 = new ModuleName(MODULE_NAME_0);
+    private final ModuleName moduleName2 = new ModuleName(MODULE_NAME_1);
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new ModuleName(null));
+    }
+
+    @Test
+    public void constructor_invalidDeadline_throwsIllegalArgumentException() {
+        String invalidName = " ";
+        assertThrows(IllegalArgumentException.class, () -> new ModuleName(invalidName));
+    }
 
     @Test
     void isValidName() {
+        // name follows the NUS module code format -> returns true
+        assertTrue(ModuleName.isValidName(MODULE_NAME_0));
+
+        // blank -> returns false
+        assertFalse(ModuleName.isValidName(" "));
+
+        // starts with a space -> false
+        assertFalse(ModuleName.isValidName(" CS2103"));
+
+        // contains other symbols -> returns false
+        assertFalse(ModuleName.isValidName("CS2103#"));
+
+        // does not start with 2-4 letters -> returns false
+        assertFalse(ModuleName.isValidName("A2103"));
+
+        // does not 4 digits -> returns false
+        assertFalse(ModuleName.isValidName("CS210"));
+
+        // does not start with 0-4 letters -> returns false
+        assertFalse(ModuleName.isValidName("CS2103AAAAA"));
     }
 
     @Test
