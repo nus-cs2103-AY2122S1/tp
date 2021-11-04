2fadc8a9165a610a577c2244de21a37e22b1f85b
LMAOboxhack <linfangyuan99@gmail.com>
2021-11-04 17:25:29 +0800

    Update tests for Applicant and Position

--- src/test/java/seedu/address/model/applicant/ApplicantTest.java
+++ src/test/java/seedu/address/model/applicant/ApplicantTest.java
@@ -1,6 +1,8 @@
 package seedu.address.model.applicant;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
@@ -28,13 +30,13 @@ public class ApplicantTest {
                 .withAddress(VALID_ADDRESS_BOB).build();
         assertTrue(ALICE.isSameApplicant(editedAlice));
 
+        // name differs in case, all other attributes same -> returns true
+        Applicant editedBob = new ApplicantBuilder(BOB).withName(VALID_NAME_BOB.toLowerCase()).build();
+        assertTrue(BOB.isSameApplicant(editedBob));
+
         // different name, all other attributes same -> returns false
         editedAlice = new ApplicantBuilder(ALICE).withName(VALID_NAME_BOB).build();
         assertFalse(ALICE.isSameApplicant(editedAlice));
-
-        // name differs in case, all other attributes same -> returns false
-        Applicant editedBob = new ApplicantBuilder(BOB).withName(VALID_NAME_BOB.toLowerCase()).build();
-        assertFalse(BOB.isSameApplicant(editedBob));
 
         // name has trailing spaces, all other attributes same -> returns false
         String nameWithTrailingSpaces = VALID_NAME_BOB + " ";
@@ -46,35 +48,35 @@ public class ApplicantTest {
     public void equals() {
         // same values -> returns true
         Applicant aliceCopy = new ApplicantBuilder(ALICE).build();
-        assertTrue(ALICE.equals(aliceCopy));
+        assertEquals(ALICE, aliceCopy);
 
         // same object -> returns true
-        assertTrue(ALICE.equals(ALICE));
+        assertEquals(ALICE, ALICE);
 
         // null -> returns false
-        assertFalse(ALICE.equals(null));
+        assertNotEquals(null, ALICE);
 
         // different type -> returns false
-        assertFalse(ALICE.equals(5));
+        assertNotEquals(5, ALICE);
 
         // different applicant -> returns false
-        assertFalse(ALICE.equals(BOB));
+        assertNotEquals(ALICE, BOB);
 
         // different name -> returns false
         Applicant editedAlice = new ApplicantBuilder(ALICE).withName(VALID_NAME_BOB).build();
-        assertFalse(ALICE.equals(editedAlice));
+        assertNotEquals(ALICE, editedAlice);
 
         // different phone -> returns false
         editedAlice = new ApplicantBuilder(ALICE).withPhone(VALID_PHONE_BOB).build();
-        assertFalse(ALICE.equals(editedAlice));
+        assertNotEquals(ALICE, editedAlice);
 
         // different email -> returns false
         editedAlice = new ApplicantBuilder(ALICE).withEmail(VALID_EMAIL_BOB).build();
-        assertFalse(ALICE.equals(editedAlice));
+        assertNotEquals(ALICE, editedAlice);
 
         // different address -> returns false
         editedAlice = new ApplicantBuilder(ALICE).withAddress(VALID_ADDRESS_BOB).build();
-        assertFalse(ALICE.equals(editedAlice));
+        assertNotEquals(ALICE, editedAlice);
 
     }
 
