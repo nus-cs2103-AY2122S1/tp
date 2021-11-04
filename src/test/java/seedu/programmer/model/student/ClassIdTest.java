97489b84a5f0b0e8a28492b6cfd0f5b79e2806e4
Allard <AllardQuek@users.noreply.github.com>
2021-11-05 03:52:34 +0800

    Update tests and minor coding standard issues

--- src/test/java/seedu/programmer/model/student/ClassIdTest.java
+++ src/test/java/seedu/programmer/model/student/ClassIdTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.programmer.testutil.TypicalClasses.CLASS_1;
+import static seedu.programmer.testutil.TypicalClasses.CLASS_10;
 
 public class ClassIdTest {
 
     @Test
     public void getClassNum_validClass_returnsCorrectly() {
         assertEquals(CLASS_1.getClassNum(), 1);
+        assertEquals(CLASS_10.getClassNum(), 10);
     }
 }
--- src/test/java/seedu/programmer/testutil/TypicalClasses.java
+++ src/test/java/seedu/programmer/testutil/TypicalClasses.java
@@ -6,6 +6,7 @@ public class TypicalClasses {
     public static final ClassId CLASS_1 = new ClassId("B01");
     public static final ClassId CLASS_2 = new ClassId("B02");
     public static final ClassId CLASS_3 = new ClassId("B03");
+    public static final ClassId CLASS_10 = new ClassId("B10");
 
     private TypicalClasses() {
     } // prevents instantiation
