3e2b0e053d9cd3dbd0a75d7f3bd722d3a0459975
tanruiquan <ruiquann@gmail.com>
2021-11-04 13:33:23 +0800

    Add unit tests for ModuleCode

--- src/main/java/seedu/address/model/person/ModuleCode.java
+++ src/main/java/seedu/address/model/person/ModuleCode.java
@@ -74,8 +74,4 @@ public class ModuleCode {
     public int hashCode() {
         return value.hashCode();
     }
-
-    public int compareTo(ModuleCode c) {
-        return this.value.toLowerCase().compareTo(c.value.toLowerCase());
-    }
 }
