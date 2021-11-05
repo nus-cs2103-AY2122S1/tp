6627395dd3e76e2b6c91d3d93d772a8b81670fc7
Leeroy Liu <leeroyliu999@gmail.com>
2021-11-05 20:02:07 +0800

    Fix checkstyle

--- src/main/java/seedu/address/ui/TaskListPanel.java
+++ src/main/java/seedu/address/ui/TaskListPanel.java
@@ -1,6 +1,5 @@
 package seedu.address.ui;
 
-import java.util.Optional;
 import java.util.logging.Logger;
 
 import javafx.collections.ObservableList;
@@ -10,7 +9,6 @@ import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
-import seedu.address.model.module.member.Member;
 import seedu.address.model.module.task.Task;
 
 /**
