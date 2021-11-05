28ceb06f9b76269840dc900820f34236fff83ccd
Zhouyirui <72332263+Zhouyirui@users.noreply.github.com>
2021-11-05 15:11:11 +0800

    Improve test coverage for ClassCode

--- src/test/java/seedu/address/model/student/ClassCodeTest.java
+++ src/test/java/seedu/address/model/student/ClassCodeTest.java
@@ -20,13 +20,33 @@ public class ClassCodeTest {
     }
 
     @Test
+    public void classCodeComparable_validClassCode_success() {
+        ClassCode G06 = new ClassCode("G06");
+        ClassCode G92 = new ClassCode("G92");
+        ClassCode G06_copy = new ClassCode("G06");
+        ClassCode G99 = new ClassCode("G99");
+
+        // compare same ClassCode
+        assertTrue(G06.compareTo(G06_copy) == 0);
+        assertTrue(G06.compareTo(G06) == 0);
+
+        // compare different ClassCode
+        assertTrue(G06.compareTo(G92) < 0);
+        assertTrue(G99.compareTo(G92) > 0);
+    }
+
+    @Test
     public void isValidClassCode() {
         // null classCode
         assertThrows(NullPointerException.class, () -> ClassCode.isValidClassCode(null));
 
-        // invalid ClassCode
+        // blank ClassCode
         assertFalse(ClassCode.isValidClassCode("")); // empty string
+        assertFalse(ClassCode.isValidClassCode(" ")); // spaces only
+
+        // invalid ClassCode
         assertFalse(ClassCode.isValidClassCode("Today")); // must start with G, followed by 2 digits
+        assertFalse(ClassCode.isValidClassCode("12")); // only numbers
         assertFalse(ClassCode.isValidClassCode("T99")); // first character is G/g
         assertFalse(ClassCode.isValidClassCode("g001")); // must have 3 digits
 
