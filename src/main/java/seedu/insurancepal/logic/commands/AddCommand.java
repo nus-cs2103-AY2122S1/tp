bf86c21340c23a00a0869052c2b649f9e754a2dd
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-11-05 22:59:41 +0800

    Add space to add command message

--- src/main/java/seedu/insurancepal/logic/commands/AddCommand.java
+++ src/main/java/seedu/insurancepal/logic/commands/AddCommand.java
@@ -36,7 +36,7 @@ public class AddCommand extends Command {
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
             + PREFIX_TAG + "friends "
             + PREFIX_TAG + "owesMoney "
-            + PREFIX_INSURANCE + "Life Prudential"
+            + PREFIX_INSURANCE + "Life Prudential "
             + PREFIX_NOTE + "Has a medical condition";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
