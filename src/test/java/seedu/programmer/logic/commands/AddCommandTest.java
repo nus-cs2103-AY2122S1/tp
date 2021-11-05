841967a17165d165e8100ab6fd7ff207a56b1ecf
Allard <AllardQuek@users.noreply.github.com>
2021-11-05 10:00:56 +0800

    Update test

--- src/test/java/seedu/programmer/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/programmer/logic/commands/AddCommandTest.java
@@ -161,6 +161,11 @@ public class AddCommandTest {
         }
 
         @Override
+        public boolean hasLab(Lab lab) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public void deleteStudent(Student target) {
             throw new AssertionError("This method should not be called.");
         }
