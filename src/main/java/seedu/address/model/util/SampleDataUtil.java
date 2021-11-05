74616fc2a2dcb16d5d2e6e15b3329d1d9223cbae
GabrielWLM <77065071+GabrielWLM@users.noreply.github.com>
2021-11-05 13:43:05 +0800

    Fix Viewg Bug

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -15,8 +15,6 @@ import seedu.address.model.student.Phone;
 import seedu.address.model.student.Student;
 import seedu.address.model.student.StudentMark;
 import seedu.address.model.tag.Tag;
-import seedu.address.model.tutorialgroup.GroupNumber;
-import seedu.address.model.tutorialgroup.GroupType;
 import seedu.address.model.tutorialgroup.TutorialGroup;
 
 /**
@@ -26,37 +24,28 @@ public class SampleDataUtil {
 
     public static final ClassCode CLASS_CODE = new ClassCode("G00");
 
-    public static final TutorialGroup TUTORIAL_GROUP_OP1 = new TutorialGroup(new GroupNumber("1"),
-            CLASS_CODE, new GroupType("OP1"));
-    public static final TutorialGroup TUTORIAL_GROUP_OP2 = new TutorialGroup(new GroupNumber("1"),
-            CLASS_CODE, new GroupType("OP2"));
+    public static final Set<TutorialGroup> EMPTY_TUTORIAL_GROUP = getTutorialGroupSet();
 
     public static Student[] getSampleStudents() {
         return new Student[] {
             new Student(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                 new Address("Blk 30 Geylang Street 29, #06-40"), CLASS_CODE,
-                getTagSet("friends"), getMarkList("LOW", "EXCELLENT"),
-                    getTutorialGroupSet(TUTORIAL_GROUP_OP1)),
+                getTagSet("friends"), getMarkList("LOW", "EXCELLENT"), EMPTY_TUTORIAL_GROUP),
             new Student(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), CLASS_CODE,
-                getTagSet("colleagues", "friends"), getMarkList("LOW", "HIGH"),
-                    getTutorialGroupSet(TUTORIAL_GROUP_OP2)),
+                getTagSet("colleagues", "friends"), getMarkList("LOW", "HIGH"), EMPTY_TUTORIAL_GROUP),
             new Student(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                 new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), CLASS_CODE,
-                getTagSet("neighbours"), getMarkList("HIGH", "AVG"),
-                    getTutorialGroupSet(TUTORIAL_GROUP_OP1, TUTORIAL_GROUP_OP2)),
+                getTagSet("neighbours"), getMarkList("HIGH", "AVG"), EMPTY_TUTORIAL_GROUP),
             new Student(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), CLASS_CODE,
-                getTagSet("family"), getMarkList("LOW", "EXCELLENT"),
-                    getTutorialGroupSet(TUTORIAL_GROUP_OP1, TUTORIAL_GROUP_OP2)),
+                getTagSet("family"), getMarkList("LOW", "EXCELLENT"), EMPTY_TUTORIAL_GROUP),
             new Student(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                 new Address("Blk 47 Tampines Street 20, #17-35"), CLASS_CODE,
-                getTagSet("classmates"), getMarkList("HIGH", "POOR"),
-                    getTutorialGroupSet(TUTORIAL_GROUP_OP2)),
+                getTagSet("classmates"), getMarkList("HIGH", "POOR"), EMPTY_TUTORIAL_GROUP),
             new Student(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
                 new Address("Blk 45 Aljunied Street 85, #11-31"), CLASS_CODE,
-                getTagSet("colleagues"), getMarkList("LOW", "POOR"),
-                    getTutorialGroupSet())
+                getTagSet("colleagues"), getMarkList("LOW", "POOR"), EMPTY_TUTORIAL_GROUP)
         };
     }
 
