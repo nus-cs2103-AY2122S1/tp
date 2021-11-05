709fe901391d8e6c423ba105871af96937d8cf64
NoraYUuu <norafish79@gmail.com>
2021-11-05 12:07:57 +0800

    TimetableTest in Headless Mode

--- src/test/java/seedu/address/model/tuition/TimetableTest.java
+++ src/test/java/seedu/address/model/tuition/TimetableTest.java
@@ -28,8 +28,14 @@ class TimetableTest {
 
     @BeforeAll
     public static void setUp_javaFX_runtime() throws InterruptedException, TimeoutException {
+        if (Boolean.getBoolean("headless")) {
+            System.setProperty("testfx.robot", "glass");
+            System.setProperty("testfx.headless", "true");
+            System.setProperty("prism.order", "sw");
+            System.setProperty("prism.text", "t2k");
+            System.setProperty("java.awt.headless", "true");
+        }
         FxToolkit.registerPrimaryStage();
-        FxToolkit.hideStage();
         infoPage = new TimetableInfoPage(tuitionClasses, null);
         timetable = new Timetable(tuitionClasses, null, infoPage);
     }
