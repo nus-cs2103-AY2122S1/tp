30241df5ea0c0f1e949c391e2840c0dd51c61eb1
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-11-06 17:03:01 +0800

    Increase tag maximum length

--- src/main/java/seedu/insurancepal/model/tag/Tag.java
+++ src/main/java/seedu/insurancepal/model/tag/Tag.java
@@ -11,7 +11,7 @@ public class Tag {
 
     public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric";
     public static final String VALIDATION_REGEX = "\\p{Alnum}+";
-    public static final int MAX_LENGTH = 15;
+    public static final int MAX_LENGTH = 20;
 
     public final String tagName;
 
