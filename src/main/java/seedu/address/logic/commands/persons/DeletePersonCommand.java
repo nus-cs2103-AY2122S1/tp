c09e10ac02d5ca59b8c15d880448eeaa61fdbeec
Keerthi Vihashini <kvihashini@gmail.com>
2021-11-06 12:59:22 +0800

    Add line that was removed

--- src/main/java/seedu/address/logic/commands/persons/DeletePersonCommand.java
+++ src/main/java/seedu/address/logic/commands/persons/DeletePersonCommand.java
@@ -43,6 +43,7 @@ public class DeletePersonCommand extends Command {
         }
         Person personToDelete = lastShownList.get(targetIndex.getZeroBased());
         model.deletePerson(personToDelete);
+        model.updateLessonWithAttendeesList();
         model.setViewingType(ViewingType.SCHEDULE);
 
         return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete));
