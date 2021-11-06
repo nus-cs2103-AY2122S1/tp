687f408dc4fa547cd4ffba702af7033168545371
trash-bin99 <77224811+trash-bin99@users.noreply.github.com>
2021-11-06 17:05:20 +0800

    Update colors for visibility

--- src/main/java/seedu/fast/commons/util/Colors.java
+++ src/main/java/seedu/fast/commons/util/Colors.java
@@ -9,10 +9,10 @@ public class Colors {
     public static final String BLUE = "-fx-background-color: #3e7b91;";
     public static final String GREY = "-fx-background-color: #808080;";
     public static final String ORANGE = "-fx-background-color: #ffa500;";
-    public static final String GREEN = "-fx-background-color: #07D62C;";
+    public static final String GREEN = "-fx-background-color: #419b47;";
     public static final String PURPLE = "-fx-background-color: #800080;";
-    public static final String TEAL = "-fx-background-color: #28EFFF;";
-    public static final String LIME = "-fx-background-color: #a8ff33;";
+    public static final String GREY_PURPLE = "-fx-background-color: #755f8a;";
+    public static final String DARK_GREEN = "-fx-background-color: #0a6211;";
     public static final String PINK = "-fx-background-color: #f3a5ff;";
     public static final String CRIMSON = "-fx-background-color: #df2121;";
     public static final String MAGENTA = "-fx-background-color: #c33ade;";
