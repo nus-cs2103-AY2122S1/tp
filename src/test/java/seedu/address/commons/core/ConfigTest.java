0f92b154d56d8de5e897de0560b6299be2a49ac8
CheongYeeMing <e0526001@u.nus.edu>
2021-11-06 15:56:38 +0800

    Increase test coverage for 'ConfigTest`.

--- src/test/java/seedu/address/commons/core/ConfigTest.java
+++ src/test/java/seedu/address/commons/core/ConfigTest.java
@@ -1,10 +1,15 @@
 package seedu.address.commons.core;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ConfigTest {
 
@@ -17,10 +22,139 @@ public class ConfigTest {
     }
 
     @Test
+    public void getLogLevel_defaultObject() {
+        Config defaultConfig = new Config();
+        assertNotNull(defaultConfig);
+        String expectedLogLevel = "INFO";
+        assertEquals(expectedLogLevel, defaultConfig.getLogLevel().toString());
+    }
+
+    @Test
+    public void getLogLevel_twoDefaultObject() {
+        Config defaultConfig = new Config();
+        Config defaultConfig2 = new Config();
+        assertNotNull(defaultConfig);
+        assertNotNull(defaultConfig2);
+        assertEquals(defaultConfig.getLogLevel(), defaultConfig.getLogLevel());
+    }
+
+    @Test
+    public void getLogLevel_twoDifferentObject() {
+        Config defaultConfig = new Config();
+        Config defaultConfig2 = new Config();
+        assertNotNull(defaultConfig);
+        assertNotNull(defaultConfig2);
+        Path newPath = Paths.get("userprofile.json");
+        defaultConfig.setUserPrefsFilePath(newPath);
+        assertEquals(defaultConfig.getLogLevel(), defaultConfig.getLogLevel());
+    }
+
+    @Test
+    public void setLogLevel_twoDefaultObject() {
+        Config defaultConfig = new Config();
+        Config defaultConfig2 = new Config();
+        assertNotNull(defaultConfig);
+        assertNotNull(defaultConfig2);
+        defaultConfig.setLogLevel(defaultConfig2.getLogLevel());
+        String expectedLogLevel = "INFO";
+        System.out.println(expectedLogLevel + defaultConfig.getLogLevel().toString());
+    }
+
+    @Test
+    public void getUserPrefsFilePath_defaultObject() {
+        Config defaultConfig = new Config();
+        assertNotNull(defaultConfig);
+        String expectedUserPrefsFilePath = "preferences.json";
+        assertEquals(expectedUserPrefsFilePath, defaultConfig.getUserPrefsFilePath().toString());
+    }
+
+    @Test
+    public void setUserPrefsFilePath_twoDefaultObject() {
+        Config defaultConfig = new Config();
+        Config defaultConfig2 = new Config();
+        assertNotNull(defaultConfig);
+        assertNotNull(defaultConfig2);
+        defaultConfig.setUserPrefsFilePath(defaultConfig2.getUserPrefsFilePath());
+        String expectedUserPrefsFilePath = "preferences.json";
+        assertEquals(expectedUserPrefsFilePath, defaultConfig.getUserPrefsFilePath().toString());
+    }
+
+    @Test
+    public void setUserPrefsFilePath_twoDifferentObject() {
+        Config defaultConfig = new Config();
+        Config defaultConfig2 = new Config();
+        assertNotNull(defaultConfig);
+        assertNotNull(defaultConfig2);
+        Path newPath = Paths.get("userprofile.json");
+        defaultConfig.setUserPrefsFilePath(newPath);
+        String expectedUserPrefsFilePath = "preferences.json";
+        assertNotEquals(expectedUserPrefsFilePath, defaultConfig.getUserPrefsFilePath().toString());
+    }
+
+    @Test
+    public void hashCode_defaultObject() {
+        Config defaultConfig = new Config();
+        assertNotNull(defaultConfig);
+        int expectedHashCode = 696981215;
+        assertEquals(expectedHashCode, defaultConfig.hashCode());
+    }
+
+    @Test
+    public void hashCode_twoDefaultObject() {
+        Config defaultConfig = new Config();
+        Config defaultConfig2 = new Config();
+        assertNotNull(defaultConfig);
+        assertNotNull(defaultConfig2);
+        assertEquals(defaultConfig.hashCode(), defaultConfig2.hashCode());
+    }
+
+    @Test
+    public void hashCode_twoDifferentObject() {
+        Config defaultConfig = new Config();
+        Config defaultConfig2 = new Config();
+        assertNotNull(defaultConfig);
+        assertNotNull(defaultConfig2);
+        Path newPath = Paths.get("userprofile.json");
+        defaultConfig.setUserPrefsFilePath(newPath);
+        assertNotEquals(defaultConfig.hashCode(), defaultConfig2.hashCode());
+    }
+
+    @Test
+    public void toString_twoDefaultObject() {
+        Config defaultConfig = new Config();
+        Config defaultConfig2 = new Config();
+        assertNotNull(defaultConfig);
+        assertNotNull(defaultConfig2);
+        assertEquals(defaultConfig.toString(), defaultConfig2.toString());
+    }
+
+    @Test
+    public void toString_twoDifferentObject() {
+        Config defaultConfig = new Config();
+        Config defaultConfig2 = new Config();
+        assertNotNull(defaultConfig);
+        assertNotNull(defaultConfig2);
+        Path newPath = Paths.get("userprofile.json");
+        defaultConfig.setUserPrefsFilePath(newPath);
+        assertNotEquals(defaultConfig.toString(), defaultConfig2.toString());
+    }
+
+    @Test
     public void equalsMethod() {
         Config defaultConfig = new Config();
         assertNotNull(defaultConfig);
         assertTrue(defaultConfig.equals(defaultConfig));
+    }
+
+    @Test
+    public void equalsMethod_twoDifferentObject() {
+        Config defaultConfig = new Config();
+        Config defaultConfig2 = new Config();
+        assertNotNull(defaultConfig);
+        assertNotNull(defaultConfig2);
+        Path newPath = Paths.get("userprofile.json");
+        defaultConfig.setUserPrefsFilePath(newPath);
+        assertFalse(defaultConfig.equals(defaultConfig2));
     }
 
 
