4ed6a2a37f60ae3be5760b0a59028943093e2ac7
fullfatwasabi <e0543805@u.nus.edu>
2021-11-06 11:13:13 +0800

    Fixed max size issue

--- src/main/java/seedu/address/ui/DayCard.java
+++ src/main/java/seedu/address/ui/DayCard.java
@@ -10,6 +10,7 @@ import java.util.logging.Logger;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import seedu.address.commons.core.LogsCenter;
@@ -52,8 +53,11 @@ public class DayCard extends UiPart<Region> {
         String text = day.getDisplayName(TextStyle.SHORT, Locale.getDefault()) + ", "
                 + date.format(formatter);
         dayDateLabel.setText(text);
-        slotPane.getChildren().addAll(new SlotCard(day, Slot.MORNING, stafflist, currentPeriod).getRoot(),
-                new SlotCard(day, Slot.AFTERNOON, stafflist, currentPeriod).getRoot());
+        SlotCard morningSlotCard = new SlotCard(day, Slot.MORNING, stafflist, currentPeriod);
+        SlotCard afternoonSlotCard = new SlotCard(day, Slot.AFTERNOON, stafflist, currentPeriod);
+        slotPane.getChildren().addAll(morningSlotCard.getRoot(), afternoonSlotCard.getRoot());
+        VBox.setVgrow(morningSlotCard.getRoot(), Priority.ALWAYS);
+        VBox.setVgrow(afternoonSlotCard.getRoot(), Priority.ALWAYS);
     }
 
     @Override
