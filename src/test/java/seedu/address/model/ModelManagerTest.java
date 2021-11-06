646213f4074d3208e7ee6ce3c416b5b8a9dc716d
bryanwee023 <bryanwee023@gmail.com>
2021-11-07 04:49:45 +0800

    Fix compilation bugs

--- src/test/java/seedu/address/model/ModelManagerTest.java
+++ src/test/java/seedu/address/model/ModelManagerTest.java
@@ -82,7 +82,7 @@ public class ModelManagerTest {
 
     @Test
     public void setBookKeeping_validBookKeeping_setBookKeeping() {
-        BookKeeping bookKeeping = new BookKeeping(3.0, 2.0, 1.0);
+        BookKeeping bookKeeping = new BookKeeping(3.0, 2.0);
         modelManager.setBookKeeping(bookKeeping);
         assertEquals(bookKeeping, modelManager.getBookKeeping());
     }
