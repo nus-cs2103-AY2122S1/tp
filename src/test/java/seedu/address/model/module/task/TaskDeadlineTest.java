f300297a90c0306b71222244792ab2aa16c1697b
GWphua <phuaguanwei99@gmail.com>
2021-11-07 00:29:27 +0800

    Add Task Deadline tests.

--- src/test/java/seedu/address/model/module/task/TaskDeadlineTest.java
+++ src/test/java/seedu/address/model/module/task/TaskDeadlineTest.java
@@ -2,6 +2,7 @@ package seedu.address.model.module.task;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.model.module.task.TaskDeadline.isValidTaskDeadline;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -19,19 +20,29 @@ class TaskDeadlineTest {
     }
 
     @Test
-    public void isValidTaskDeadline() {
+    public void isValidTaskDeadline_false() {
         // null time
-        assertThrows(NullPointerException.class, () -> TaskDeadline.isValidTaskDeadline(null));
+        assertThrows(NullPointerException.class, () -> isValidTaskDeadline(null));
 
         // invalid time
-        assertFalse(TaskDeadline.isValidTaskDeadline("")); // empty string
-        assertFalse(TaskDeadline.isValidTaskDeadline("  ")); // space only
-        assertFalse(TaskDeadline.isValidTaskDeadline("1973/10/01 8:00")); // error format
-        assertFalse(TaskDeadline.isValidTaskDeadline("09/08/2021")); // missing time
-        assertFalse(TaskDeadline.isValidTaskDeadline("50/50/2021 23:59")); // invalid date
-        assertFalse(TaskDeadline.isValidTaskDeadline("10/10/2021 50:50")); // invalid time
+        assertFalse(isValidTaskDeadline("")); // empty string
+        assertFalse(isValidTaskDeadline("  ")); // space only
+        assertFalse(isValidTaskDeadline("1973/10/01 8:00")); // error format
+        assertFalse(isValidTaskDeadline("09/08/2021")); // missing time
+        assertFalse(isValidTaskDeadline("50/50/2021 23:59")); // invalid date
+        assertFalse(isValidTaskDeadline("31/12/3000 24:00")); // invalid date
+        assertFalse(isValidTaskDeadline("31/12/1969 00:00")); // invalid date
+        assertFalse(isValidTaskDeadline("29/02/-1999 12:00")); // invalid date
+        assertFalse(isValidTaskDeadline("10/10/2021 50:50")); // invalid time
+    }
 
-        // valid time
-        assertTrue(TaskDeadline.isValidTaskDeadline("10/10/2021 12:00"));
+    @Test
+    public void isValidTaskDeadline_true() {
+        assertTrue(isValidTaskDeadline("10/10/2021 12:00"));
+        assertTrue(isValidTaskDeadline("31/11/1999 12:00"));
+        assertTrue(isValidTaskDeadline("29/02/1999 12:00"));
+        assertTrue(isValidTaskDeadline("17/09/1999 24:00"));
+        assertTrue(isValidTaskDeadline("01/01/1970 00:00"));
+        assertTrue(isValidTaskDeadline("31/12/3000 23:59"));
     }
 }
