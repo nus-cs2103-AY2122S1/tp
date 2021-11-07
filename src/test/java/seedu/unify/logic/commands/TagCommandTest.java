47c25965457590b6bf52bde8e449bd8670c03096
Sourabh Jaiswal <sourabhrj31@gmail.com>
2021-11-07 13:31:06 +0800

    add equals tests

---
+++ src/test/java/seedu/unify/logic/commands/TagCommandTest.java
@@ -0,0 +1,34 @@
+package seedu.unify.logic.commands;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.unify.testutil.TypicalIndexes.INDEX_FIRST_TASK;
+import static seedu.unify.testutil.TypicalIndexes.INDEX_SECOND_TASK;
+
+public class TagCommandTest {
+    @Test
+    public void equals() {
+        TagCommand.TagTaskDescriptor tagTaskDescriptor = new TagCommand.TagTaskDescriptor();
+        TagCommand firstTag = new TagCommand(INDEX_FIRST_TASK, tagTaskDescriptor);
+        TagCommand secondTag = new TagCommand(INDEX_SECOND_TASK, tagTaskDescriptor);
+
+
+        // same object -> returns true
+        assertTrue(firstTag.equals(firstTag));
+
+        // same values -> returns true
+        TagCommand firstCopy = new TagCommand(INDEX_FIRST_TASK, tagTaskDescriptor);
+        assertTrue(firstTag.equals(firstCopy));
+
+        // different types -> returns false
+        assertFalse(firstTag.equals(INDEX_FIRST_TASK));
+
+        // null -> returns false
+        assertFalse(firstTag.equals(null));
+
+        // different tasks -> returns false
+        assertFalse(firstTag.equals(secondTag));
+    }
+}
