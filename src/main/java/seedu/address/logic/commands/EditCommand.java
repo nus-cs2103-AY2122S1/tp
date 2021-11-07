fae30bd834980347fd5ab25a0a098008dd263544
meixuanjin <jin.meixuan02@gmail.com>
2021-11-08 01:01:09 +0800

    Update UI

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -43,12 +43,12 @@ public class EditCommand extends Command {
 
     public static final String COMMAND_WORD = "edit";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the person identified "
-            + "by the index number used in the displayed person list. "
+            + "by the index number used in the displayed person list. \n"
             + "Existing values will be overwritten by the input values.\n"
             + "Parameters: INDEX (must be a positive integer) "
             + "[" + PREFIX_NAME + "NAME] "
             + "[" + PREFIX_EMAIL + "EMAIL] "
-            + "[" + PREFIX_GITHUB_ID + "GITHUB_ID]"
+            + "[" + PREFIX_GITHUB_ID + "GITHUB_ID]\n"
             + "[" + PREFIX_NUS_NETWORK_ID + "NUS_NETWORK_ID]"
             + "[" + PREFIX_TYPE + "TYPE]"
             + "[" + PREFIX_STUDENT_ID + "STUDENT_ID]"
