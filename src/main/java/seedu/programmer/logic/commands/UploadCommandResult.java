a0bb5e14097d252a90ee546cd987d06231195bd7
Allard <AllardQuek@users.noreply.github.com>
2021-11-07 10:01:38 +0800

    Add documentation and remove unnecessary overriding

--- src/main/java/seedu/programmer/logic/commands/UploadCommandResult.java
+++ src/main/java/seedu/programmer/logic/commands/UploadCommandResult.java
@@ -11,18 +11,4 @@ public class UploadCommandResult extends CommandResult {
     public UploadCommandResult(String feedbackToUser) {
         super(feedbackToUser);
     }
-
-    @Override
-    public boolean equals(Object other) {
-        if (other == this) {
-            return true;
-        }
-
-        if (!(other instanceof UploadCommandResult)) {
-            return false;
-        }
-
-        UploadCommandResult otherCommandResult = (UploadCommandResult) other;
-        return super.getFeedbackToUser().equals(otherCommandResult.getFeedbackToUser());
-    }
 }
