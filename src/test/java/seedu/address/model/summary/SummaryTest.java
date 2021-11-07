87c1923a91b9369215463a118e99ac3407107f07
ryantianj <ryantianjun@gmail.com>
2021-11-07 14:43:47 +0800

    Fix checkstyle

--- src/test/java/seedu/address/model/summary/SummaryTest.java
+++ src/test/java/seedu/address/model/summary/SummaryTest.java
@@ -12,9 +12,6 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.contact.CategoryCode;
-import seedu.address.model.contact.Contact;
-import seedu.address.model.contact.Rating;
 import seedu.address.testutil.ContactBuilder;
 
 
