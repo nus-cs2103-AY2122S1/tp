c502069fca0ab77d1af71f8b99b1e37a312e7750
Chesterwongz <psyforce99@gmail.com>
2021-11-08 01:28:09 +0800

    giving back to koon hwee

--- src/main/java/seedu/address/model/lesson/Money.java
+++ src/main/java/seedu/address/model/lesson/Money.java
@@ -71,6 +71,7 @@ public class Money {
         return test.matches(VALIDATION_REGEX);
     }
 
+    //@@author angkoonhwee
     /**
      * Get the value in Money object such that it can be used for calculations in {@code FeesCalculator}.
      *
@@ -80,6 +81,7 @@ public class Money {
         return valueInBigDec;
     }
 
+    //@@author Chesterwongz
     @Override
     public String toString() {
         return value;
