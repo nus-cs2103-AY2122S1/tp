ea81e7c9bea6c763325da907d4b0bd0608fe0156
Sourabh Jaiswal <sourabhrj31@gmail.com>
2021-11-07 13:16:23 +0800

    add success and failure execute tests for done and undone

--- src/test/java/seedu/unify/logic/commands/UndoneCommandTest.java
+++ src/test/java/seedu/unify/logic/commands/UndoneCommandTest.java
@@ -2,12 +2,51 @@ package seedu.unify.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.unify.testutil.TypicalIndexes.INDEX_FIRST_TASK;
-import static seedu.unify.testutil.TypicalIndexes.INDEX_SECOND_TASK;
+import static seedu.unify.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.unify.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.unify.testutil.TypicalIndexes.*;
+import static seedu.unify.testutil.TypicalTasks.getTypicalUniFy;
 
 import org.junit.jupiter.api.Test;
+import seedu.unify.commons.core.Messages;
+import seedu.unify.commons.core.index.Index;
+import seedu.unify.model.Model;
+import seedu.unify.model.ModelManager;
+import seedu.unify.model.UserPrefs;
+import seedu.unify.model.task.State;
+import seedu.unify.model.task.Task;
 
 public class UndoneCommandTest {
+
+    private Model model = new ModelManager(getTypicalUniFy(), new UserPrefs());
+
+    @Test
+    public void execute_validIndexUndoneCommand_success() {
+        Task taskToMark = model.getFilteredTaskList().get(INDEX_DONE_TASK.getZeroBased());
+        UndoneCommand undoneCommand = new UndoneCommand(INDEX_DONE_TASK);
+
+        String expectedMessage = String.format(UndoneCommand.MESSAGE_UNDONE_TASK_SUCCESS,taskToMark);
+
+        ModelManager expectedModel = new ModelManager(model.getUniFy(), new UserPrefs());
+        expectedModel.setTask(taskToMark,
+                new Task(
+                        taskToMark.getName(),
+                        taskToMark.getTime(),
+                        taskToMark.getDate(),
+                        taskToMark.getTags(),
+                        new State(State.ObjectState.TODO),
+                        taskToMark.getPriority()));
+
+        assertCommandSuccess(undoneCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidIndexUndoneCommand_throwsCommandException() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredTaskList().size() + 1);
+        UndoneCommand undoneCommand = new UndoneCommand(outOfBoundIndex);
+
+        assertCommandFailure(undoneCommand, model, Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+    }
 
     @Test
     public void equals() {
--- src/test/java/seedu/unify/testutil/TypicalIndexes.java
+++ src/test/java/seedu/unify/testutil/TypicalIndexes.java
@@ -13,6 +13,7 @@ public class TypicalIndexes {
     public static final Index INDEX_FIRST_TASK = Index.fromOneBased(1);
     public static final Index INDEX_SECOND_TASK = Index.fromOneBased(2);
     public static final Index INDEX_THIRD_TASK = Index.fromOneBased(3);
+    public static final Index INDEX_DONE_TASK = Index.fromOneBased(7);
     public static final List<Index> INDEX_LIST_FIRST_TASK = new ArrayList<>(Arrays.asList(INDEX_FIRST_TASK));
     public static final List<Index> INDEX_LIST_SECOND_TASK = new ArrayList<>(Arrays.asList(Index.fromOneBased(2)));
 }
