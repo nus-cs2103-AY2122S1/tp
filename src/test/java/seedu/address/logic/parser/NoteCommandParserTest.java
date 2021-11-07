82cf376829069e46203fdb1b4c65c533b6223388
Yttruire <teozhihao99@gmail.com>
2021-11-07 13:33:09 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/parser/NoteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/NoteCommandParserTest.java
@@ -1,9 +1,5 @@
 package seedu.address.logic.parser;
 
-import seedu.address.logic.commands.NoteCommand;
-
-import org.junit.jupiter.api.Test;
-
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTE;
@@ -11,21 +7,29 @@ import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailur
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalStudents.ALICE;
 
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.NoteCommand;
+
 public class NoteCommandParserTest {
 
     private NoteCommandParser parser = new NoteCommandParser();
 
     @Test
     public void parse_validArgs_returnsNoteCommand() {
-        assertParseSuccess(parser, " " + PREFIX_NAME + ALICE.getName().toString() + " " +
-                PREFIX_NOTE + ALICE.getNote().toString(), new NoteCommand(ALICE.getName(), ALICE.getNote()));
+        assertParseSuccess(parser, " " + PREFIX_NAME + ALICE.getName().toString() + " "
+                + PREFIX_NOTE + ALICE.getNote().toString(), new NoteCommand(ALICE.getName(), ALICE.getNote()));
     }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
-        assertParseFailure(parser, " Alice", String.format(MESSAGE_INVALID_COMMAND_FORMAT, NoteCommand.MESSAGE_USAGE));
-        assertParseFailure(parser, " Alice Note", String.format(MESSAGE_INVALID_COMMAND_FORMAT, NoteCommand.MESSAGE_USAGE));
-        assertParseFailure(parser, " n/Alice", String.format(MESSAGE_INVALID_COMMAND_FORMAT, NoteCommand.MESSAGE_USAGE));
-        assertParseFailure(parser, " no/A sample note", String.format(MESSAGE_INVALID_COMMAND_FORMAT, NoteCommand.MESSAGE_USAGE));
+        assertParseFailure(parser,
+                " Alice", String.format(MESSAGE_INVALID_COMMAND_FORMAT, NoteCommand.MESSAGE_USAGE));
+        assertParseFailure(parser,
+                " Alice Note", String.format(MESSAGE_INVALID_COMMAND_FORMAT, NoteCommand.MESSAGE_USAGE));
+        assertParseFailure(parser,
+                " n/Alice", String.format(MESSAGE_INVALID_COMMAND_FORMAT, NoteCommand.MESSAGE_USAGE));
+        assertParseFailure(parser,
+                " no/A sample note", String.format(MESSAGE_INVALID_COMMAND_FORMAT, NoteCommand.MESSAGE_USAGE));
     }
 }
