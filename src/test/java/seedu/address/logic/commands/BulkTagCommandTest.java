2701c2ccb07cabf6b19eab7b7ee94270d5d54781
Siddharth Srivastava <siddharth.srivastava2508@gmail.com>
2021-11-08 01:21:08 +0800

    Fixed Checkstyle Issue

--- src/test/java/seedu/address/logic/commands/BulkTagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/BulkTagCommandTest.java
@@ -62,7 +62,7 @@ public class BulkTagCommandTest {
 
         Model expectedModel = new ModelManager(new AddressBook(), new UserPrefs());
         expectedModel.setAddressBook(new AddressBook());
-        
+
         model.addPerson(new PersonBuilder().withName(VALID_NAME_AMY)
                 .withPhone(VALID_PHONE_AMY)
                 .withEmail(VALID_EMAIL_AMY)
