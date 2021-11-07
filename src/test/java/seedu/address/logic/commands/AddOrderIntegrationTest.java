d7112eaa668d64f7e907912e209de71a267aa210
lwlshawn <37958530+lwlshawn@users.noreply.github.com>
2021-11-07 22:19:00 +0800

    Fix checkstyle and amended testcases
    * Had to greatly amend a test case due to the new method we
    have for assigning ID
    * Under AddOrderIntegrationTest there was a testcase related to
    sorting. Removed it since it didn't look appropriate for this file.

--- src/test/java/seedu/address/logic/commands/AddOrderIntegrationTest.java
+++ src/test/java/seedu/address/logic/commands/AddOrderIntegrationTest.java
@@ -1,16 +1,20 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.SortOrdersCommandTest.DATE_DESC;
 import static seedu.address.testutil.TypicalOrders.getTypicalOrderBook;
+import static seedu.address.testutil.TypicalPersons.JOHNSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 import static seedu.address.testutil.TypicalTasks.getTypicalTaskBook;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.OrderBook;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.order.Order;
 import seedu.address.testutil.OrderBuilder;
@@ -25,33 +29,20 @@ public class AddOrderIntegrationTest {
 
     @BeforeEach
     public void setUp() {
-        model = new ModelManager(getTypicalAddressBook(), getTypicalTaskBook(),
-                getTypicalOrderBook(), new UserPrefs());
+        model = new ModelManager(new AddressBook(), getTypicalTaskBook(),
+                new OrderBook(), new UserPrefs());
+        model.addPerson(JOHNSON);
     }
 
     @Test
-    public void execute_newOrder_success() {
+    public void execute_newOrder_success() throws Exception {
         Order validOrder = new OrderBuilder().build();
         expectedModel = new ModelManager(model.getAddressBook(),
-                model.getTaskBook(), model.getOrderBook(), new UserPrefs());
+                model.getTaskBook(), new OrderBook(), new UserPrefs());
 
         expectedModel.addOrder(validOrder);
-
-        assertCommandSuccess(new AddOrderCommand(validOrder), model,
-                String.format(AddOrderCommand.MESSAGE_SUCCESS, validOrder), expectedModel);
-    }
-
-    @Test
-    public void execute_sortedOrder_defaultOrdering() {
-        Order validOrder = new OrderBuilder().build();
-        Model expectedModel = new ModelManager(getTypicalAddressBook(), getTypicalTaskBook(),
-                getTypicalOrderBook(), new UserPrefs());
-
-        expectedModel.addOrder(validOrder);
-
-        model.sortOrderList(DATE_DESC);
-
-        String expectedMessage = String.format(AddOrderCommand.MESSAGE_SUCCESS, validOrder);
-        assertCommandSuccess(new AddOrderCommand(validOrder), model, expectedMessage, expectedModel);
+        Command addCommand = new AddOrderCommand(validOrder);
+        CommandResult commandResult = addCommand.execute(model);
+        assertEquals(model.getOrderBook(), expectedModel.getOrderBook());
     }
 }
