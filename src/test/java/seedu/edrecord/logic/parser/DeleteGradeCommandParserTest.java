56c998f425ca7cdb3b23ae4d10149341261cb218
Marcus Choo <marcuschoo@outlook.com>
2021-11-07 21:10:15 +0800

    Add tests for `DeleteGradeCommandParser`

---
+++ src/test/java/seedu/edrecord/logic/parser/DeleteGradeCommandParserTest.java
@@ -0,0 +1,35 @@
+package seedu.edrecord.logic.parser;
+
+import static seedu.edrecord.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.edrecord.logic.parser.CliSyntax.PREFIX_ID;
+import static seedu.edrecord.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.edrecord.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.jupiter.api.Test;
+import seedu.edrecord.commons.core.index.Index;
+import seedu.edrecord.logic.commands.DeleteGradeCommand;
+
+public class DeleteGradeCommandParserTest {
+    private final Index INDEX_ONE = Index.fromOneBased(1);
+    private final Index INDEX_TWO = Index.fromOneBased(2);
+    private final Index INDEX_THREE = Index.fromOneBased(3);
+
+    private final DeleteGradeCommandParser parser = new DeleteGradeCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsDeleteGradeCommand() {
+        assertParseSuccess(parser, "1 " + PREFIX_ID + "3", new DeleteGradeCommand(INDEX_ONE, INDEX_THREE));
+    }
+
+    @Test
+    public void parse_invalidIndex_throwsParseException() {
+        assertParseFailure(parser, "a " + PREFIX_ID + "3",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteGradeCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidId_throwsParseException() {
+        assertParseFailure(parser, "3 " + PREFIX_ID + "a",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteGradeCommand.MESSAGE_USAGE));
+    }
+}
