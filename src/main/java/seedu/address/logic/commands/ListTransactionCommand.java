7d36b40bb678f36f57a1023a3f331e2ca66e0c44
GraceWang2322 <>
2021-11-07 11:42:59 +0800

    Fix tests

--- src/main/java/seedu/address/logic/commands/ListTransactionCommand.java
+++ src/main/java/seedu/address/logic/commands/ListTransactionCommand.java
@@ -45,9 +45,9 @@ public class ListTransactionCommand extends ListCommand {
         // If no id specified, display all past transactions
         if (transactionId.isEmpty()) {
 
-            String cost = String.format("$%.2f", model.getBookKeeping().getCost());
-            String revenue = String.format("$%.2f", model.getBookKeeping().getRevenue());
-            String profit = String.format("$%.2f", model.getBookKeeping().getProfit());
+            String cost = String.format("%.2f", model.getBookKeeping().getCost());
+            String revenue = String.format("%.2f", model.getBookKeeping().getRevenue());
+            String profit = String.format("%.2f", model.getBookKeeping().getProfit());
 
             String addMessage = "Total costs: " + cost + ", Total revenue: "
                     + revenue + ", Total profit: " + profit;
