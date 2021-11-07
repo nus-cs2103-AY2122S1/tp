6df4ca947f6b29bf7fa832f158cd94c552038eb6
jyrw <77237380+jyrw@users.noreply.github.com>
2021-11-07 17:18:18 +0800

    Fix issue with tests

--- src/main/java/dash/model/UserInputList.java
+++ src/main/java/dash/model/UserInputList.java
@@ -51,7 +51,10 @@ public class UserInputList implements Iterable<String> {
     public void add(String toAdd) {
         requireNonNull(toAdd);
         internalList.add(0, toAdd);
-        internalList.subList(LIST_MAX_LENGTH, internalList.size()).clear();
+        assert !(internalList.size() > LIST_MAX_LENGTH);
+        if (internalList.size() == LIST_MAX_LENGTH) {
+            internalList.remove(LIST_MAX_LENGTH);
+        }
     }
 
     /**
