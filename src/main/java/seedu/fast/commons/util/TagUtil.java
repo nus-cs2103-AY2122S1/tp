710e9fc4691111ff57c3585f07e308566853e239
LauTengHon <e0543465@u.nus.edu>
2021-11-07 12:45:59 +0800

    Remove unused variable

--- src/main/java/seedu/fast/commons/util/TagUtil.java
+++ src/main/java/seedu/fast/commons/util/TagUtil.java
@@ -4,7 +4,6 @@ package seedu.fast.commons.util;
  * Container class for commonly used tags to be colored.
  */
 public class TagUtil {
-    public static final String FRIENDS = "FRIENDS";
     public static final String HIGH_PRIORITY = "HIGHPRIORITY";
     public static final String MEDIUM_PRIORITY = "MEDIUMPRIORITY";
     public static final String LOW_PRIORITY = "LOWPRIORITY";
