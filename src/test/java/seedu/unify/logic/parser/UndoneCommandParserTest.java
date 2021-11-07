0304a0a640856bb9324f9d15ec6ba7266b77d1d4
Sourabh Jaiswal <sourabhrj31@gmail.com>
2021-11-07 22:20:40 +0800

    test undone parser

---
+++ src/test/java/seedu/unify/logic/parser/UndoneCommandParserTest.java
@@ -0,0 +1,27 @@
+package seedu.unify.logic.parser;
+
+import static seedu.unify.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.unify.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.unify.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.unify.testutil.TypicalIndexes.INDEX_FIRST_TASK;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.unify.logic.commands.DeleteCommand;
+import seedu.unify.logic.commands.UndoneCommand;
+
+public class UndoneCommandParserTest {
+
+    private UndoneCommandParser parser = new UndoneCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsUndoneCommand() {
+        assertParseSuccess(parser, "1", new UndoneCommand(INDEX_FIRST_TASK));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, UndoneCommand.MESSAGE_USAGE));
+    }
+
+}
