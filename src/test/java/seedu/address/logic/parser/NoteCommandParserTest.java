5d1f244d89baceccc9fa189d3122f08817571a66
Yttruire <teozhihao99@gmail.com>
2021-11-07 14:13:53 +0800

    Update NoteCommandParserTest.java

--- src/test/java/seedu/address/logic/parser/NoteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/NoteCommandParserTest.java
@@ -13,12 +13,31 @@ import seedu.address.logic.commands.NoteCommand;
 
 public class NoteCommandParserTest {
 
+    private static final String MESSAGE_INVALID_FORMAT =
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, NoteCommand.MESSAGE_USAGE);
+
     private NoteCommandParser parser = new NoteCommandParser();
 
     @Test
     public void parse_validArgs_returnsNoteCommand() {
         assertParseSuccess(parser, " " + PREFIX_NAME + ALICE.getName().toString() + " "
                 + PREFIX_NOTE + ALICE.getNote().toString(), new NoteCommand(ALICE.getName(), ALICE.getNote()));
+    }
+
+    @Test
+    public void parse_invalidPreamble_failure() {
+        // invalid prefix being parsed as preamble
+        assertParseFailure(parser, " i/invalid", MESSAGE_INVALID_FORMAT);
+
+        // invalid arguments being parsed as preamble
+        assertParseFailure(parser, "invalid arguments", MESSAGE_INVALID_FORMAT);
+
+        // invalid preamble before correct arguments
+        assertParseFailure(parser, " -5" + PREFIX_NAME + ALICE.getName().toString() + " "
+                + PREFIX_NOTE + ALICE.getNote().toString(), MESSAGE_INVALID_FORMAT);
+
+        // invalid preamble before incomplete arguments
+        assertParseFailure(parser, " 0" + PREFIX_NAME + ALICE.getName().toString(), MESSAGE_INVALID_FORMAT);
     }
 
     @Test
