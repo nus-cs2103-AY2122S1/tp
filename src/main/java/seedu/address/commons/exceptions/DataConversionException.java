1ae860a44a9a6862ce4172cf16adb1e62f19ff2e
CheongYeeMing <e0526001@u.nus.edu>
2021-11-08 20:06:57 +0800

    Add Javadocs to exceptions.

--- src/main/java/seedu/address/commons/exceptions/DataConversionException.java
+++ src/main/java/seedu/address/commons/exceptions/DataConversionException.java
@@ -4,6 +4,11 @@ package seedu.address.commons.exceptions;
  * Represents an error during conversion of data from one format to another
  */
 public class DataConversionException extends Exception {
+    /**
+     * This exception is thrown when there is data conversion error.
+     *
+     * @param cause of the exception.
+     */
     public DataConversionException(Exception cause) {
         super(cause);
     }
