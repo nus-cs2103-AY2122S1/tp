6ad6bfd8eb80c65bff63f0699a7d06efeae2c50e
elroygohjy <E0543508@u.nus.edu>
2021-11-08 21:37:20 +0800

    Fix: Fix Manual Testing & Fix: Clearing of Notor when Note Window(s) is opened

--- src/main/java/seedu/notor/logic/commands/ClearCommand.java
+++ src/main/java/seedu/notor/logic/commands/ClearCommand.java
@@ -5,9 +5,11 @@ import static java.util.Objects.requireNonNull;
 import java.util.Arrays;
 import java.util.List;
 
+import seedu.notor.logic.executors.exceptions.ExecuteException;
 import seedu.notor.model.Model;
 import seedu.notor.model.Notor;
 import seedu.notor.ui.WarningWindow;
+import seedu.notor.ui.note.NoteWindow;
 
 /**
  * Clears Notor of all entries
@@ -19,9 +21,13 @@ public class ClearCommand implements Command {
     public static final String MESSAGE_SUCCESS = "Notor has been cleared!";
     public static final String CONFIRMATION_MESSAGE = "Do you want to proceed with clearing Notor?";
     public static final String MESSAGE_CANCEL = "Clearing of Notor has been cancelled.";
+    public static final String MESSAGE_FAILURE = "Unable to clear Notor as there is note window(s) opened.";
 
     @Override
-    public CommandResult execute(Model model) {
+    public CommandResult execute(Model model) throws ExecuteException {
+        if (!NoteWindow.OPENED_NOTE_WINDOWS.isEmpty()) {
+            throw new ExecuteException(MESSAGE_FAILURE);
+        }
         requireNonNull(model);
         WarningWindow warningWindow = new WarningWindow(CONFIRMATION_MESSAGE);
         warningWindow.show();
