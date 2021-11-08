d191d09ecfc414d9e5aefeff7b6c073c61e0970d
Fergus Mok <fergusmok1@gmail.com>
2021-11-08 19:43:40 +0800

    Fix mkmod error command

--- src/main/java/seedu/edrecord/logic/parser/MakeModuleCommandParser.java
+++ src/main/java/seedu/edrecord/logic/parser/MakeModuleCommandParser.java
@@ -21,7 +21,7 @@ public class MakeModuleCommandParser implements Parser<MakeModuleCommand> {
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args);
 
         if (argMultimap.getPreamble().isEmpty()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, MakeGroupCommand.MESSAGE_USAGE));
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, MakeModuleCommand.MESSAGE_USAGE));
         }
 
         Module mod;
