dd50a57753208950af71ff195d25085721d11ecb
Sourabh Jaiswal <sourabhrj31@gmail.com>
2021-11-08 19:36:03 +0800

    fix checkstyle issues

--- src/test/java/seedu/unify/logic/CommandHistoryTest.java
+++ src/test/java/seedu/unify/logic/CommandHistoryTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import seedu.unify.logic.commands.exceptions.CommandException;
 
 public class CommandHistoryTest {
