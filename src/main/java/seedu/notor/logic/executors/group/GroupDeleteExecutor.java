6215f9abe43adaca46bc639f8cb7827168ee9a68
elroygohjy <E0543508@u.nus.edu>
2021-11-08 16:22:18 +0800

    Fix: Fix Deleting Group and Person is possible when NoteWindow is opened

--- src/main/java/seedu/notor/logic/executors/group/GroupDeleteExecutor.java
+++ src/main/java/seedu/notor/logic/executors/group/GroupDeleteExecutor.java
@@ -8,10 +8,13 @@ import seedu.notor.model.group.Group;
 import seedu.notor.model.group.SubGroup;
 import seedu.notor.model.group.SuperGroup;
 import seedu.notor.ui.WarningWindow;
+import seedu.notor.ui.note.NoteWindow;
 
 public class GroupDeleteExecutor extends GroupExecutor {
     public static final String MESSAGE_DELETE_GROUP_SUCCESS = "Deleted Group: %1$s";
     public static final String MESSAGE_DELETE_GROUP_CANCEL = "Deleting of Group: %1$s has been cancelled.";
+    public static final String MESSAGE_DELETE_GROUP_FAILURE =
+            "Unable to delete Group: %1$s as note window for the Group is currently opened.";
     public static final String CONFIRMATION_MESSAGE = "Do you want to proceed with deleting of Group: %1$s?";
 
 
@@ -22,6 +25,9 @@ public class GroupDeleteExecutor extends GroupExecutor {
     @Override
     public CommandResult execute() throws ExecuteException {
         Group deletedGroup = super.getGroup();
+        if (NoteWindow.contains(deletedGroup)) {
+            throw new ExecuteException(String.format(MESSAGE_DELETE_GROUP_FAILURE, deletedGroup));
+        }
         WarningWindow warningWindow = new WarningWindow(String.format(CONFIRMATION_MESSAGE, deletedGroup));
         warningWindow.show();
         if (warningWindow.canContinue()) {
