2cdea3dfc044e36c9be0f0d44ba4825dec273296
Win Lynn <ngaiwinlynn0323@gmail.com>
2021-11-08 21:49:04 +0800

    Debug MarkTaskDoneCommand

--- src/main/java/seedu/address/logic/parser/MarkTaskDoneCommandParser.java
+++ src/main/java/seedu/address/logic/parser/MarkTaskDoneCommandParser.java
@@ -2,14 +2,21 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_WEEK;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.MarkStudentAttCommand;
+import seedu.address.logic.commands.MarkStudentPartCommand;
 import seedu.address.logic.commands.MarkTaskDoneCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.student.Attendance;
 
+/**
+ * Parses input arguments and creates a new MarkTaskDoneCommand object.
+ */
 public class MarkTaskDoneCommandParser implements Parser<MarkTaskDoneCommand> {
 
     /**
@@ -19,18 +26,19 @@ public class MarkTaskDoneCommandParser implements Parser<MarkTaskDoneCommand> {
      */
     public MarkTaskDoneCommand parse(String args) throws ParseException {
         requireNonNull(args);
-        List<String> lst = List.of(args.trim().split(" "));
-        List<Index> taskIndexes = new ArrayList<>();
+        String trimmedArgs = args.trim();
+
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, MarkTaskDoneCommand.MESSAGE_USAGE));
+        }
 
         try {
-            for (String str : lst) {
-                Index index = ParserUtil.parseIndex(str);
-                taskIndexes.add(index);
-            }
-            return new MarkTaskDoneCommand(taskIndexes);
-        } catch (ParseException pe) {
+            List<Index> tasksToMark = ParserUtil.parseMultipleIndexWithoutDuplicates(trimmedArgs);
+            return new MarkTaskDoneCommand(tasksToMark);
+        } catch (NumberFormatException e) {
             throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, MarkTaskDoneCommand.MESSAGE_USAGE), pe);
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, MarkTaskDoneCommand.MESSAGE_USAGE));
         }
     }
 }
